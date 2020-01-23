b29e85f105d243cdc222a351a143bf4d77592704
MineGame159 <petulko08@gmail.com>
2020-01-23 18:08:33 +0100

    Improved? NoFall

--- src/main/java/minegame159/meteorclient/modules/movement/NoFall.java
+++ src/main/java/minegame159/meteorclient/modules/movement/NoFall.java
@@ -1,34 +1,20 @@
 package minegame159.meteorclient.modules.movement;
 
 import minegame159.jes.SubscribeEvent;
-import minegame159.meteorclient.events.packets.SendPacketEvent;
+import minegame159.meteorclient.events.TickEvent;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.Module;
 import net.minecraft.server.network.packet.PlayerMoveC2SPacket;
 
-import java.lang.reflect.Field;
-
 public class NoFall extends Module {
-    private Field onGround;
-
     public NoFall() {
         super(Category.Movement, "no-fall", "Protects you from fall damage.");
-
-        try {
-            onGround = PlayerMoveC2SPacket.class.getDeclaredField("onGround");
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        }
     }
 
     @SubscribeEvent
-    private void onSendPacket(SendPacketEvent e) {
-        if (e.packet instanceof PlayerMoveC2SPacket) {
-            try {
-                onGround.set(e.packet, true);
-            } catch (IllegalAccessException ex) {
-                ex.printStackTrace();
-            }
+    private void onTick(TickEvent e) {
+        if (mc.player.fallDistance > 2f && !mc.player.isFallFlying()) {
+            mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket(true));
         }
     }
 }
