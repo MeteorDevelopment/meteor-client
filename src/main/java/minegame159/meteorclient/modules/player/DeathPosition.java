9d844600f0b70af2dfca5b639e135c198a3b635e
MineGame159 <petulko08@gmail.com>
2020-01-29 15:40:44 +0100

    Changed DeathPosition to use TookDamageEvent instead of TickEvent

--- src/main/java/minegame159/meteorclient/modules/player/DeathPosition.java
+++ src/main/java/minegame159/meteorclient/modules/player/DeathPosition.java
@@ -1,7 +1,7 @@
 package minegame159.meteorclient.modules.player;
 
 import minegame159.jes.SubscribeEvent;
-import minegame159.meteorclient.events.TickEvent;
+import minegame159.meteorclient.events.TookDamageEvent;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.Module;
 import minegame159.meteorclient.utils.Utils;
@@ -10,7 +10,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DeathPosition extends Module {
-    private boolean isDead;
     private SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     public DeathPosition() {
@@ -18,13 +17,9 @@ public class DeathPosition extends Module {
     }
 
     @SubscribeEvent
-    private void onTick(TickEvent e) {
-        if (mc.player.getHealth() <= 0 && !isDead) {
+    private void onTookDamage(TookDamageEvent e) {
+        if (e.entity.getUuid().equals(mc.player.getUuid()) && e.entity.getHealth() <= 0) {
             Utils.sendMessage("#yellowDied at #blue%.1f#yellow, #blue%.1f#yellow, #blue%.1f#yellow on #blue%s.", mc.player.getX(), mc.player.getY(), mc.player.getZ(), dateFormat.format(new Date()));
-            isDead = true;
-        }
-        else if (mc.player.getHealth() > 0 && isDead) {
-            isDead = false;
         }
     }
 }
