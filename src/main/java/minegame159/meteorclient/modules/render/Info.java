79e793855c885c487d1e1483cf108ac35a189036
MineGame159 <petulko08@gmail.com>
2020-03-08 20:47:34 +0100

    Fixed Info entity list

--- src/main/java/minegame159/meteorclient/modules/render/Info.java
+++ src/main/java/minegame159/meteorclient/modules/render/Info.java
@@ -5,6 +5,7 @@ import me.zero.alpine.listener.Listener;
 import minegame159.meteorclient.events.EntityAddedEvent;
 import minegame159.meteorclient.events.EntityRemovedEvent;
 import minegame159.meteorclient.events.Render2DEvent;
+import minegame159.meteorclient.events.TickEvent;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.Module;
 import minegame159.meteorclient.settings.Setting;
@@ -19,6 +20,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Info extends Module {
     private Setting<Boolean> fps = addSetting(new BoolSettingBuilder()
@@ -32,7 +34,7 @@ public class Info extends Module {
             .name("entities")
             .description("Display number of entities.")
             .defaultValue(true)
-            .consumer((aBoolean, aBoolean2) -> recalculateEntityCounts())
+            .consumer((aBoolean, aBoolean2) -> updateEntities = true)
             .build()
     );
 
@@ -40,7 +42,7 @@ public class Info extends Module {
             .name("entity-custom-names")
             .description("Use custom names.")
             .defaultValue(true)
-            .consumer((aBoolean, aBoolean2) -> recalculateEntityCounts())
+            .consumer((aBoolean, aBoolean2) -> updateEntities = true)
             .build()
     );
 
@@ -48,20 +50,24 @@ public class Info extends Module {
             .name("separate-sheeps-by-color")
             .description("Separates sheeps by color in entity list.")
             .defaultValue(false)
-            .consumer((aBoolean, aBoolean2) -> recalculateEntityCounts())
+            .consumer((aBoolean, aBoolean2) -> updateEntities = true)
             .build()
     );
 
     private HashMap<String, EntityInfo> entityCounts = new HashMap<>();
     private int maxLetterCount = 0;
+    private boolean updateEntities;
+    private int updateEntitiesTimer = 2;
 
     public Info() {
         super(Category.Render, "info", "Displays various info.");
     }
 
     private String getEntityName(Entity entity) {
+        if (entity instanceof PlayerEntity) return "Player";
+        if (entity instanceof ItemEntity) return "Item";
         String name = entityCustomNames.value() ? entity.getDisplayName().asString() : entity.getType().getName().asString();
-        if (separateSheepsByColor.value() && entity instanceof SheepEntity) return StringUtils.capitalize(((SheepEntity) entity).getColor().getName()) + " - " + name;
+        if (separateSheepsByColor.value() && entity instanceof SheepEntity) return StringUtils.capitalize(((SheepEntity) entity).getColor().getName().replace('_', ' ')) + " - " + name;
         return name;
     }
 
@@ -69,16 +75,8 @@ public class Info extends Module {
         return entityCounts.computeIfAbsent(getEntityName(entity), EntityInfo::new);
     }
 
-    private void recalculateEntityCounts() {
-        entityCounts.clear();
-
-        for (Entity entity : mc.world.getEntities()) {
-            if (entity instanceof PlayerEntity || entity instanceof ItemEntity) continue;
-
-            getEntityInfo(entity).increment();
-        }
-
-        calculateMaxLetterCount();
+    private boolean isValidEntity(Entity entity) {
+        return entity != mc.player;
     }
 
     private void calculateMaxLetterCount() {
@@ -91,27 +89,19 @@ public class Info extends Module {
 
     @Override
     public void onActivate() {
-        recalculateEntityCounts();
+        updateEntities = true;
     }
 
     @EventHandler
     private Listener<EntityAddedEvent> onEntityAdded = new Listener<>(event -> {
-        if (event.entity instanceof PlayerEntity || event.entity instanceof ItemEntity) return;
-
-        getEntityInfo(event.entity).increment();
-
-        calculateMaxLetterCount();
+        if (!isValidEntity(event.entity)) return;
+        updateEntities = true;
     });
 
     @EventHandler
     private Listener<EntityRemovedEvent> onEntityRemoved = new Listener<>(event -> {
-        if (event.entity instanceof PlayerEntity || event.entity instanceof ItemEntity) return;
-
-        EntityInfo entityInfo = getEntityInfo(event.entity);
-        entityInfo.decrement();
-        if (entityInfo.count <= 0) entityCounts.remove(getEntityName(event.entity));
-
-        calculateMaxLetterCount();
+        if (!isValidEntity(event.entity)) return;
+        updateEntities = true;
     });
 
     private void drawInfo(String text1, String text2, int y) {
@@ -125,6 +115,27 @@ public class Info extends Module {
     }
 
     @EventHandler
+    private Listener<TickEvent> onTick = new Listener<>(event -> {
+        updateEntitiesTimer--;
+
+        if (updateEntities && updateEntitiesTimer <= 0 && entities.value()) {
+            for (EntityInfo entityInfo : entityCounts.values()) {
+                entityInfo.reset();
+            }
+
+            for (Entity entity : mc.world.getEntities()) {
+                if (!isValidEntity(entity)) continue;
+
+                getEntityInfo(entity).increment(entity);
+            }
+
+            updateEntities = false;
+            updateEntitiesTimer = 2;
+            calculateMaxLetterCount();
+        }
+    });
+
+    @EventHandler
     private Listener<Render2DEvent> onRender2D = new Listener<>(event -> {
         if (mc.options.debugEnabled) return;
         int y = 2;
@@ -135,8 +146,10 @@ public class Info extends Module {
         }
 
         if (entities.value()) {
-            for (EntityInfo a : entityCounts.values()) {
-                drawEntityCount(a, y);
+            for (EntityInfo renderInfo : entityCounts.values()) {
+                if (!renderInfo.render) continue;
+
+                drawEntityCount(renderInfo, y);
                 y += Utils.getTextHeight() + 2;
             }
         }
@@ -146,19 +159,36 @@ public class Info extends Module {
         public String name;
         public int count;
         public String countStr;
+        public boolean render;
 
         public EntityInfo(String name) {
             this.name = name;
         }
 
-        public void increment() {
-            count++;
+        public void increment(Entity entity) {
+            if (entity instanceof ItemEntity) count += ((ItemEntity) entity).getStack().getCount();
+            else count++;
             countStr = Integer.toString(count);
+            render = true;
         }
 
-        public void decrement() {
-            count--;
-            countStr = Integer.toString(count);
+        public void reset() {
+            int preCount = count;
+            count = 0;
+            render = false;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            EntityInfo that = (EntityInfo) o;
+            return Objects.equals(name, that.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
         }
     }
 }
