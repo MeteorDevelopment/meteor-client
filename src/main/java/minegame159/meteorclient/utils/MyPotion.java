35aa0307dccdbca63a596c8e5424b14548ba305a
MineGame159 <petulko08@gmail.com>
2020-03-20 12:21:10 +0100

    Fixed MyPotion recipes, GLOWSTONE -> GLOWSTONE_DUST

--- src/main/java/minegame159/meteorclient/utils/MyPotion.java
+++ src/main/java/minegame159/meteorclient/utils/MyPotion.java
@@ -10,33 +10,33 @@ import net.minecraft.potion.Potions;
 public enum MyPotion {
     Swiftness(Potions.SWIFTNESS, Items.NETHER_WART, Items.SUGAR),
     SwiftnessLong(Potions.LONG_SWIFTNESS, Items.NETHER_WART, Items.SUGAR, Items.REDSTONE),
-    SwiftnessStrong(Potions.STRONG_SWIFTNESS, Items.NETHER_WART, Items.SUGAR, Items.GLOWSTONE),
+    SwiftnessStrong(Potions.STRONG_SWIFTNESS, Items.NETHER_WART, Items.SUGAR, Items.GLOWSTONE_DUST),
 
     Slowness(Potions.SLOWNESS, Items.NETHER_WART, Items.SUGAR, Items.FERMENTED_SPIDER_EYE),
     SlownessLong(Potions.LONG_SLOWNESS, Items.NETHER_WART, Items.SUGAR, Items.FERMENTED_SPIDER_EYE, Items.REDSTONE),
-    SlownessStrong(Potions.STRONG_SLOWNESS, Items.NETHER_WART, Items.SUGAR, Items.FERMENTED_SPIDER_EYE, Items.GLOWSTONE),
+    SlownessStrong(Potions.STRONG_SLOWNESS, Items.NETHER_WART, Items.SUGAR, Items.FERMENTED_SPIDER_EYE, Items.GLOWSTONE_DUST),
 
     JumpBoost(Potions.LEAPING, Items.NETHER_WART, Items.RABBIT_FOOT),
     JumpBoostLong(Potions.LONG_LEAPING, Items.NETHER_WART, Items.RABBIT_FOOT, Items.REDSTONE),
-    JumpBoostStrong(Potions.STRONG_LEAPING, Items.NETHER_WART, Items.RABBIT_FOOT, Items.GLOWSTONE),
+    JumpBoostStrong(Potions.STRONG_LEAPING, Items.NETHER_WART, Items.RABBIT_FOOT, Items.GLOWSTONE_DUST),
 
     Strength(Potions.STRENGTH, Items.NETHER_WART, Items.BLAZE_POWDER),
     StrengthLong(Potions.LONG_STRENGTH, Items.NETHER_WART, Items.BLAZE_POWDER, Items.REDSTONE),
-    StrengthStrong(Potions.STRONG_STRENGTH, Items.NETHER_WART, Items.BLAZE_POWDER, Items.GLOWSTONE),
+    StrengthStrong(Potions.STRONG_STRENGTH, Items.NETHER_WART, Items.BLAZE_POWDER, Items.GLOWSTONE_DUST),
 
     Healing(Potions.HEALING, Items.NETHER_WART, Items.GLISTERING_MELON_SLICE),
-    HealingStrong(Potions.STRONG_HEALING, Items.NETHER_WART, Items.GLISTERING_MELON_SLICE, Items.GLOWSTONE),
+    HealingStrong(Potions.STRONG_HEALING, Items.NETHER_WART, Items.GLISTERING_MELON_SLICE, Items.GLOWSTONE_DUST),
 
     Harming(Potions.HARMING, Items.NETHER_WART, Items.GLISTERING_MELON_SLICE, Items.FERMENTED_SPIDER_EYE),
-    HarmingStrong(Potions.STRONG_HARMING, Items.NETHER_WART, Items.GLISTERING_MELON_SLICE, Items.FERMENTED_SPIDER_EYE, Items.GLOWSTONE),
+    HarmingStrong(Potions.STRONG_HARMING, Items.NETHER_WART, Items.GLISTERING_MELON_SLICE, Items.FERMENTED_SPIDER_EYE, Items.GLOWSTONE_DUST),
 
     Poison(Potions.POISON, Items.NETHER_WART, Items.SPIDER_EYE),
     PoisonLong(Potions.LONG_POISON, Items.NETHER_WART, Items.SPIDER_EYE, Items.REDSTONE),
-    PoisonStrong(Potions.STRONG_POISON, Items.NETHER_WART, Items.SPIDER_EYE, Items.GLOWSTONE),
+    PoisonStrong(Potions.STRONG_POISON, Items.NETHER_WART, Items.SPIDER_EYE, Items.GLOWSTONE_DUST),
 
     Regeneration(Potions.REGENERATION, Items.NETHER_WART, Items.GHAST_TEAR),
     RegenerationLong(Potions.LONG_REGENERATION, Items.NETHER_WART, Items.GHAST_TEAR, Items.REDSTONE),
-    RegenerationStrong(Potions.STRONG_REGENERATION, Items.NETHER_WART, Items.GHAST_TEAR, Items.GLOWSTONE),
+    RegenerationStrong(Potions.STRONG_REGENERATION, Items.NETHER_WART, Items.GHAST_TEAR, Items.GLOWSTONE_DUST),
 
     FireResistance(Potions.FIRE_RESISTANCE, Items.NETHER_WART, Items.MAGMA_CREAM),
     FireResistanceLong(Potions.LONG_FIRE_RESISTANCE, Items.NETHER_WART, Items.MAGMA_CREAM, Items.REDSTONE),
@@ -52,7 +52,7 @@ public enum MyPotion {
 
     TurtleMaster(Potions.TURTLE_MASTER, Items.NETHER_WART, Items.TURTLE_HELMET),
     TurtleMasterLong(Potions.LONG_TURTLE_MASTER, Items.NETHER_WART, Items.TURTLE_HELMET, Items.REDSTONE),
-    TurtleMasterStrong(Potions.STRONG_TURTLE_MASTER, Items.NETHER_WART, Items.TURTLE_HELMET, Items.GLOWSTONE),
+    TurtleMasterStrong(Potions.STRONG_TURTLE_MASTER, Items.NETHER_WART, Items.TURTLE_HELMET, Items.GLOWSTONE_DUST),
 
     SlowFalling(Potions.SLOW_FALLING, Items.NETHER_WART, Items.PHANTOM_MEMBRANE),
     SlowFallingLong(Potions.LONG_SLOW_FALLING, Items.NETHER_WART, Items.PHANTOM_MEMBRANE, Items.REDSTONE),
