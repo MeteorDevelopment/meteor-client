64bfa191e8c867be716881a01d8e2ae5f5e41e23
MineGame159 <petulko08@gmail.com>
2020-03-21 11:15:38 +0100

    Fixed AutoTool

---
+++ src/main/java/minegame159/meteorclient/mixininterface/IMiningToolItem.java
@@ -0,0 +1,7 @@
+package minegame159.meteorclient.mixininterface;
+
+import net.minecraft.block.Block;
+
+public interface IMiningToolItem {
+    public boolean isEffectiveOn(Block block);
+}
--- src/main/java/minegame159/meteorclient/modules/player/AutoTool.java
+++ src/main/java/minegame159/meteorclient/modules/player/AutoTool.java
@@ -3,15 +3,19 @@ package minegame159.meteorclient.modules.player;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import minegame159.meteorclient.events.StartBreakingBlockEvent;
+import minegame159.meteorclient.mixininterface.IMiningToolItem;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.Module;
 import minegame159.meteorclient.settings.BoolSetting;
 import minegame159.meteorclient.settings.EnumSetting;
 import minegame159.meteorclient.settings.Setting;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.ChestBlock;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 
 public class AutoTool extends Module {
     public enum Prefer {
@@ -34,6 +38,13 @@ public class AutoTool extends Module {
             .build()
     );
 
+    private Setting<Boolean> enderChestOnlyWithSilkTouch = addSetting(new BoolSetting.Builder()
+            .name("ender-chest-only-with-silk-touch")
+            .description("Mine ender chest only wiht silk touch.")
+            .defaultValue(true)
+            .build()
+    );
+
     public AutoTool() {
         super(Category.Player, "auto-tool", "Automatically switches to the most effective tool when breaking blocks.");
     }
@@ -47,8 +58,10 @@ public class AutoTool extends Module {
 
         for (int i = 0; i < 9; i++) {
             ItemStack itemStack = mc.player.inventory.getInvStack(i);
-            if (!itemStack.getItem().isEffectiveOn(blockState)) continue;
+            if (!isEffectiveOn(itemStack.getItem(), blockState.getBlock())) continue;
             int score = 0;
+
+            if (enderChestOnlyWithSilkTouch.get() && blockState.getBlock() == Blocks.ENDER_CHEST && EnchantmentHelper.getLevel(Enchantments.SILK_TOUCH, itemStack) == 0) continue;
 
             score += Math.round(itemStack.getMiningSpeed(blockState));
             score += EnchantmentHelper.getLevel(Enchantments.UNBREAKING, itemStack);
@@ -67,4 +80,12 @@ public class AutoTool extends Module {
             mc.player.inventory.selectedSlot = bestSlot;
         }
     });
+
+    private boolean isEffectiveOn(Item item, Block block) {
+        if (item instanceof SwordItem && block == Blocks.COBWEB) return true;
+        if (item instanceof AxeItem && (block instanceof ChestBlock || block == Blocks.BARREL)) return true;
+        if (item instanceof PickaxeItem && block == Blocks.ENDER_CHEST) return true;
+        if (item instanceof MiningToolItem) return ((IMiningToolItem) item).isEffectiveOn(block);
+        return false;
+    }
 }
