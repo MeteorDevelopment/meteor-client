30681b6df2dbad3493bcf22a08b0ef700bd63484
MineGame159 <minegame459@gmal.com>
2020-03-30 20:19:50 +0200

    StashFinder now supports csv and json export

--- src/main/java/minegame159/meteorclient/modules/misc/StashFinder.java
+++ src/main/java/minegame159/meteorclient/modules/misc/StashFinder.java
@@ -1,5 +1,7 @@
 package minegame159.meteorclient.modules.misc;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.mojang.blaze3d.platform.GlStateManager;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
@@ -27,7 +29,9 @@ import java.util.List;
 import java.util.Objects;
 
 public class StashFinder extends ToggleModule {
-    private static final File FILE = new File(MeteorClient.FOLDER, "stashes.csv");
+    private static final File FILE_CSV = new File(MeteorClient.FOLDER, "stashes.csv");
+    private static final File FILE_JSON = new File(MeteorClient.FOLDER, "stashes.json");
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     private Setting<Integer> minimumStorageCount = addSetting(new IntSetting.Builder()
             .name("minimum-storage-cont")
@@ -40,7 +44,7 @@ public class StashFinder extends ToggleModule {
     public List<Chunk> chunks = new ArrayList<>();
 
     public StashFinder() {
-        super(Category.Misc, "stash-finder", "Searches loaded chunks for storage blocks. Saves to <your minecraft folder>/meteor-client/stashes.csv");
+        super(Category.Misc, "stash-finder", "Searches loaded chunks for storage blocks. Saves to <your minecraft folder>/meteor-client/stashes");
     }
 
     @EventHandler
@@ -93,7 +97,8 @@ public class StashFinder extends ToggleModule {
         // Reset
         WHorizontalList topBar = list.add(new WHorizontalList(4));
         WButton reset = topBar.add(new WButton("Reset"));
-        WButton saveToFile = topBar.add(new WButton("Save to file"));
+        WButton saveToFileCsv = topBar.add(new WButton("Save to file csv"));
+        WButton saveToFileJson = topBar.add(new WButton("Save to file json"));
 
         WGrid grid = list.add(new WGrid(8, 4, 4));
 
@@ -103,7 +108,8 @@ public class StashFinder extends ToggleModule {
             list.layout();
         };
 
-        saveToFile.action = this::save;
+        saveToFileCsv.action = this::saveCsv;
+        saveToFileJson.action = this::saveJson;
 
         // Chunks
         fillGrid(grid);
@@ -134,14 +140,24 @@ public class StashFinder extends ToggleModule {
         }
     }
 
-    private void save() {
+    private void saveCsv() {
         try {
-            FILE.getParentFile().mkdirs();
-            Writer writer = new FileWriter(FILE);
+            FILE_CSV.getParentFile().mkdirs();
+            Writer writer = new FileWriter(FILE_CSV);
 
             writer.write("X,Z,Chests,Shulkers,EnderChests,Furnaces,DispensersDroppers,Hopper");
             for (Chunk chunk : chunks) chunk.write(writer);
 
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveJson() {
+        try {
+            Writer writer = new FileWriter(FILE_JSON);
+            GSON.toJson(chunks, writer);
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
