5a27ea7a361747fcf340a5f962fcb9f7573b2680
MineGame159 <minegame459@gmal.com>
2020-04-07 11:34:59 +0200

    Added animation to checkboxes

--- src/main/java/minegame159/meteorclient/gui/widgets/WCheckbox.java
+++ src/main/java/minegame159/meteorclient/gui/widgets/WCheckbox.java
@@ -3,6 +3,7 @@ package minegame159.meteorclient.gui.widgets;
 import minegame159.meteorclient.modules.setting.GUI;
 import minegame159.meteorclient.utils.Color;
 import minegame159.meteorclient.utils.RenderUtils;
+import minegame159.meteorclient.utils.Utils;
 import minegame159.meteorclient.utils.Vector2;
 
 import java.util.function.Consumer;
@@ -12,11 +13,21 @@ public class WCheckbox extends WWidget {
 
     public double size = 6;
     private Consumer<WCheckbox> action;
+    private double animationProgress;
+    private double animationMultiplier;
 
     public WCheckbox(boolean checked) {
         boundingBox.setMargin(3);
 
         this.checked = checked;
+
+        if (checked) {
+            animationProgress = 1;
+            animationMultiplier = 1;
+        } else {
+            animationProgress = 0;
+            animationMultiplier = -1;
+        }
     }
 
     @Override
@@ -24,6 +35,7 @@ public class WCheckbox extends WWidget {
         if (mouseOver) {
             checked = !checked;
             if (action != null) action.accept(this);
+            animationMultiplier = checked ? 1 : -1;
             return true;
         }
 
@@ -45,7 +57,17 @@ public class WCheckbox extends WWidget {
         }
         renderBackground(background, outline);
 
-        if (checked) RenderUtils.quad(boundingBox.getInnerX(), boundingBox.getInnerY(), boundingBox.innerWidth, boundingBox.innerHeight, GUI.checkbox);
+        animationProgress += delta * 0.6 * animationMultiplier;
+        animationProgress = Utils.clamp(animationProgress, 0, 1);
+
+        if (animationProgress > 0) {
+            double w = boundingBox.innerWidth / 2;
+            double h = boundingBox.innerHeight / 2;
+            double cX = boundingBox.getInnerX() + w;
+            double cY = boundingBox.getInnerY() + h;
+
+            RenderUtils.quad(cX - w * animationProgress, cY - h * animationProgress, boundingBox.innerWidth * animationProgress, boundingBox.innerHeight * animationProgress, GUI.checkbox);
+        }
     }
 
     public void setAction(Consumer<WCheckbox> action) {
