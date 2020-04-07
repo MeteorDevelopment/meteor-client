44fa7d3e2f6686e380e974abd8d195fc3e1c77b6
MineGame159 <minegame459@gmal.com>
2020-04-07 10:10:52 +0200

    Added height setting to Step

--- src/main/java/minegame159/meteorclient/modules/movement/Step.java
+++ src/main/java/minegame159/meteorclient/modules/movement/Step.java
@@ -5,14 +5,24 @@ import me.zero.alpine.listener.Listener;
 import minegame159.meteorclient.events.TickEvent;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.ToggleModule;
+import minegame159.meteorclient.settings.DoubleSetting;
+import minegame159.meteorclient.settings.Setting;
 
 public class Step extends ToggleModule {
+    private Setting<Double> height = addSetting(new DoubleSetting.Builder()
+            .name("height")
+            .description("Step height.")
+            .defaultValue(1)
+            .min(0)
+            .build()
+    );
+
     public Step() {
         super(Category.Movement, "step", "Allows you to step up full blocks.");
     }
 
     @EventHandler
     private Listener<TickEvent> onTick = new Listener<>(event -> {
-        mc.player.stepHeight = 1;
+        mc.player.stepHeight = height.get().floatValue();
     });
 }
