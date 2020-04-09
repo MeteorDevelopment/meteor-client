df292000e707385cb1695628b6f056167eea4bd3
MineGame159 <minegame459@gmal.com>
2020-04-09 21:33:53 +0200

    DeathPosition is now "saved" and displays in its gui

--- src/main/java/minegame159/meteorclient/modules/player/DeathPosition.java
+++ src/main/java/minegame159/meteorclient/modules/player/DeathPosition.java
@@ -3,6 +3,8 @@ package minegame159.meteorclient.modules.player;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import minegame159.meteorclient.events.TookDamageEvent;
+import minegame159.meteorclient.gui.widgets.WLabel;
+import minegame159.meteorclient.gui.widgets.WWidget;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.ToggleModule;
 import minegame159.meteorclient.utils.Utils;
@@ -13,6 +15,8 @@ import java.util.Date;
 public class DeathPosition extends ToggleModule {
     private SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
+    private WLabel label = new WLabel("No latest death");
+
     public DeathPosition() {
         super(Category.Player, "death-position", "Sends to your chat where you died.");
     }
@@ -20,7 +24,15 @@ public class DeathPosition extends ToggleModule {
     @EventHandler
     private Listener<TookDamageEvent> onTookDamage = new Listener<>(event -> {
         if (event.entity.getUuid().equals(mc.player.getUuid()) && event.entity.getHealth() <= 0) {
+            label.text = String.format("Latest death: %.1f, %.1f, %.1f", mc.player.x, mc.player.y, mc.player.z);
+            label.layout();
+
             Utils.sendMessage("#yellowDied at #blue%.1f#yellow, #blue%.1f#yellow, #blue%.1f#yellow on #blue%s.", mc.player.x, mc.player.y, mc.player.z, dateFormat.format(new Date()));
         }
     });
+
+    @Override
+    public WWidget getWidget() {
+        return label;
+    }
 }
