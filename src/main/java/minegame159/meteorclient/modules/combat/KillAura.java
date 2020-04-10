fef084205bf1ad54914928c6c42d1cfe4aeded1a
MineGame159 <minegame459@gmal.com>
2020-04-10 11:41:55 +0200

    Added smartDelay, hitSpeed and rotate to KillAura

--- src/main/java/minegame159/meteorclient/modules/combat/KillAura.java
+++ src/main/java/minegame159/meteorclient/modules/combat/KillAura.java
@@ -5,17 +5,19 @@ import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import minegame159.meteorclient.accountsfriends.FriendManager;
 import minegame159.meteorclient.events.TickEvent;
+import minegame159.meteorclient.mixininterface.IVec3d;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.ToggleModule;
-import minegame159.meteorclient.settings.BoolSetting;
-import minegame159.meteorclient.settings.DoubleSetting;
-import minegame159.meteorclient.settings.EnumSetting;
-import minegame159.meteorclient.settings.Setting;
+import minegame159.meteorclient.settings.*;
 import minegame159.meteorclient.utils.EntityUtils;
+import net.minecraft.command.arguments.EntityAnchorArgumentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Hand;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RayTraceContext;
 
 public class KillAura extends ToggleModule {
     public enum Priority {
@@ -25,37 +27,10 @@ public class KillAura extends ToggleModule {
         HighestHealth
     }
 
-    public Setting<Boolean> players = addSetting(new BoolSetting.Builder()
-            .name("players")
-            .description("Attack players.")
-            .defaultValue(true)
-            .build()
-    );
-
-    public Setting<Boolean> friends = addSetting(new BoolSetting.Builder()
-            .name("friends")
-            .description("Attack friends, useful only if attack players is on.")
-            .defaultValue(false)
-            .build()
-    );
-
-    public Setting<Boolean> animals = addSetting(new BoolSetting.Builder()
-            .name("animals")
-            .description("Attack animals.")
-            .defaultValue(true)
-            .build()
-    );
-
-    public Setting<Boolean> mobs = addSetting(new BoolSetting.Builder()
-            .name("mobs")
-            .description("Attack mobs.")
-            .defaultValue(true)
-            .build()
-    );
-
     public Setting<Double> range = addSetting(new DoubleSetting.Builder()
             .name("range")
             .description("Attack range.")
+            .group("General")
             .defaultValue(5.5)
             .min(0.0)
             .build()
@@ -64,6 +39,7 @@ public class KillAura extends ToggleModule {
     public Setting<Boolean> ignoreWalls = addSetting(new BoolSetting.Builder()
             .name("ignore-walls")
             .description("Attack through walls.")
+            .group("General")
             .defaultValue(true)
             .build()
     );
@@ -71,12 +47,86 @@ public class KillAura extends ToggleModule {
     public Setting<Priority> priority = addSetting(new EnumSetting.Builder<Priority>()
             .name("priority")
             .description("What entities to target.")
+            .group("General")
             .defaultValue(Priority.LowestHealth)
             .build()
     );
 
+    private Setting<Boolean> rotate = addSetting(new BoolSetting.Builder()
+            .name("rotate")
+            .description("Rotates you towards the target.")
+            .group("General")
+            .defaultValue(false)
+            .build()
+    );
+
+    public Setting<Boolean> players = addSetting(new BoolSetting.Builder()
+            .name("players")
+            .description("Attack players.")
+            .group("To Attack")
+            .defaultValue(true)
+            .build()
+    );
+
+    public Setting<Boolean> friends = addSetting(new BoolSetting.Builder()
+            .name("friends")
+            .description("Attack friends, useful only if attack players is on.")
+            .group("To Attack")
+            .defaultValue(false)
+            .build()
+    );
+
+    public Setting<Boolean> animals = addSetting(new BoolSetting.Builder()
+            .name("animals")
+            .description("Attack animals.")
+            .group("To Attack")
+            .defaultValue(true)
+            .build()
+    );
+
+    public Setting<Boolean> mobs = addSetting(new BoolSetting.Builder()
+            .name("mobs")
+            .description("Attack mobs.")
+            .group("To Attack")
+            .defaultValue(true)
+            .build()
+    );
+
+    private Setting<Integer> hitDelay;
+    private Setting<Boolean> smartDelay = addSetting(new BoolSetting.Builder()
+            .name("smart-delay")
+            .description("Smart delay.")
+            .group("Delay")
+            .onChanged(aBoolean -> {
+                hitDelay.setVisible(!aBoolean);
+                hitDelayTimer = 0;
+            })
+            .defaultValue(true)
+            .build()
+    );
+
+    private int hitDelayTimer;
+
+    private Vec3d vec3d1 = new Vec3d(0, 0, 0);
+    private Vec3d vec3d2 = new Vec3d(0, 0, 0);
+
     public KillAura() {
         super(Category.Combat, "kill-aura", "Automatically attacks entities.");
+
+        hitDelay = addSetting(new IntSetting.Builder()
+                .name("hit-delay")
+                .description("Hit delay in ticks. 20 ticks = 1 second.")
+                .group("Delay")
+                .defaultValue(0)
+                .min(0)
+                .visible(false)
+                .build()
+        );
+    }
+
+    @Override
+    public void onActivate() {
+        hitDelayTimer = 0;
     }
 
     private boolean isInRange(Entity entity) {
@@ -94,7 +144,16 @@ public class KillAura extends ToggleModule {
     }
 
     private boolean canSeeEntity(Entity entity) {
-        return ignoreWalls.get() || mc.player.canSee(entity);
+        if (ignoreWalls.get()) return true;
+
+        ((IVec3d) vec3d1).set(mc.player.x, mc.player.y + mc.player.getStandingEyeHeight(), mc.player.z);
+        ((IVec3d) vec3d2).set(entity.x, entity.y, entity.z);
+        boolean canSeeFeet =  mc.world.rayTrace(new RayTraceContext(vec3d1, vec3d2, RayTraceContext.ShapeType.COLLIDER, RayTraceContext.FluidHandling.NONE, mc.player)).getType() == HitResult.Type.MISS;
+
+        ((IVec3d) vec3d2).set(entity.x, entity.y + entity.getStandingEyeHeight(), entity.z);
+        boolean canSeeEyes =  mc.world.rayTrace(new RayTraceContext(vec3d1, vec3d2, RayTraceContext.ShapeType.COLLIDER, RayTraceContext.FluidHandling.NONE, mc.player)).getType() == HitResult.Type.MISS;
+
+        return canSeeFeet || canSeeEyes;
     }
 
     private int invertSort(int sort) {
@@ -114,7 +173,19 @@ public class KillAura extends ToggleModule {
 
     @EventHandler
     private Listener<TickEvent> onTick = new Listener<>(event -> {
-        if (mc.player.getHealth() <= 0 || mc.player.getAttackCooldownProgress(0.5f) < 1) return;
+        if (mc.player.getHealth() <= 0) return;
+
+        if (smartDelay.get()) {
+            // Smart delay
+            if (mc.player.getAttackCooldownProgress(0.5f) < 1) return;
+        } else {
+            // Manual delay
+            if (hitDelayTimer < hitDelay.get()) {
+                hitDelayTimer++;
+                return;
+            }
+            else hitDelayTimer = 0;
+        }
 
         Streams.stream(mc.world.getEntities())
                 .filter(this::isInRange)
@@ -124,6 +195,10 @@ public class KillAura extends ToggleModule {
                 .filter(entity -> entity.getHealth() > 0)
                 .min(this::sort)
                 .ifPresent(entity -> {
+                    if (rotate.get()) {
+                        ((IVec3d) vec3d1).set(entity.x, entity.y + entity.getHeight() / 2, entity.z);
+                        mc.player.lookAt(EntityAnchorArgumentType.EntityAnchor.EYES, vec3d1);
+                    }
                     mc.interactionManager.attackEntity(mc.player, entity);
                     mc.player.swingHand(Hand.MAIN_HAND);
                 });
