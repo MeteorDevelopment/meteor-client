b7283c33f8832de6857d1e1bae4e7c060fca9e86
MineGame159 <minegame459@gmal.com>
2020-04-10 17:29:31 +0200

    Added swords setting to AutoMend

--- src/main/java/minegame159/meteorclient/modules/player/AutoMend.java
+++ src/main/java/minegame159/meteorclient/modules/player/AutoMend.java
@@ -5,14 +5,24 @@ import me.zero.alpine.listener.Listener;
 import minegame159.meteorclient.events.TickEvent;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.ToggleModule;
+import minegame159.meteorclient.settings.BoolSetting;
+import minegame159.meteorclient.settings.Setting;
 import minegame159.meteorclient.utils.InvUtils;
 import net.minecraft.client.gui.screen.ingame.ContainerScreen;
 import net.minecraft.container.SlotActionType;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
 
 public class AutoMend extends ToggleModule {
+    private Setting<Boolean> swords = addSetting(new BoolSetting.Builder()
+            .name("swords")
+            .description("Move swords.")
+            .defaultValue(true)
+            .build()
+    );
+
     public AutoMend() {
         super(Category.Player, "auto-mend", "Automatically replaces items in offhand with mending when fully repaired.");
     }
@@ -21,6 +31,7 @@ public class AutoMend extends ToggleModule {
         for (int i = 0; i < mc.player.inventory.main.size(); i++) {
             ItemStack itemStack = mc.player.inventory.getInvStack(i);
             if (EnchantmentHelper.getLevel(Enchantments.MENDING, itemStack) == 0 || !itemStack.isDamaged()) continue;
+            if (!swords.get() && itemStack.getItem() instanceof SwordItem) continue;
 
             InvUtils.clickSlot(InvUtils.invIndexToSlotId(i), 0, SlotActionType.PICKUP);
             InvUtils.clickSlot(InvUtils.OFFHAND_SLOT, 0, SlotActionType.PICKUP);
