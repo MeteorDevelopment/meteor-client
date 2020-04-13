b226815ff21ad6b412192436f7a385ae0ed4d344
MineGame159 <minegame459@gmal.com>
2020-04-13 19:43:33 +0200

    FullBright is now the same as having night vision

--- src/main/java/minegame159/meteorclient/modules/render/FullBright.java
+++ src/main/java/minegame159/meteorclient/modules/render/FullBright.java
@@ -2,21 +2,9 @@ package minegame159.meteorclient.modules.render;
 
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.ToggleModule;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.options.Option;
 
 public class FullBright extends ToggleModule {
     public FullBright() {
         super(Category.Render, "full-bright", "No more darkness.");
-    }
-
-    @Override
-    public void onActivate() {
-        MinecraftClient.getInstance().options.gamma = 16;
-    }
-
-    @Override
-    public void onDeactivate() {
-        mc.options.gamma = Option.GAMMA.get(mc.options);
     }
 }
