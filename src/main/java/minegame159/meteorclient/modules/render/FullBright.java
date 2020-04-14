f01a2dfd1d193d1e4dd08ec1ebd97468fba58378
MineGame159 <minegame459@gmal.com>
2020-04-14 11:47:21 +0200

    Fixed FullBright

--- src/main/java/minegame159/meteorclient/modules/render/FullBright.java
+++ src/main/java/minegame159/meteorclient/modules/render/FullBright.java
@@ -1,10 +1,31 @@
 package minegame159.meteorclient.modules.render;
 
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
+import minegame159.meteorclient.events.TickEvent;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.ToggleModule;
 
 public class FullBright extends ToggleModule {
+    private double preGamma;
+
     public FullBright() {
         super(Category.Render, "full-bright", "No more darkness.");
     }
+
+    @Override
+    public void onActivate() {
+        preGamma = mc.options.gamma;
+        mc.options.gamma = 16;
+    }
+
+    @Override
+    public void onDeactivate() {
+        mc.options.gamma = preGamma;
+    }
+
+    @EventHandler
+    private Listener<TickEvent> onTick = new Listener<>(event -> {
+        mc.options.gamma = 16;
+    });
 }
