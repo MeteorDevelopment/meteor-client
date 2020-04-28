fe607f8d31a044fc6f69d75371ada76b087077db
MineGame159 <minegame459@gmal.com>
2020-04-28 15:45:10 +0200

    Added small animation to checkboxes

--- src/main/java/minegame159/meteorclient/gui/widgets/WCheckbox.java
+++ src/main/java/minegame159/meteorclient/gui/widgets/WCheckbox.java
@@ -3,6 +3,7 @@ package minegame159.meteorclient.gui.widgets;
 import minegame159.meteorclient.gui.GuiConfig;
 import minegame159.meteorclient.gui.GuiRenderer;
 import minegame159.meteorclient.gui.listeners.CheckboxClickListener;
+import minegame159.meteorclient.utils.Utils;
 
 public class WCheckbox extends WWidget {
     public CheckboxClickListener action;
@@ -10,8 +11,11 @@ public class WCheckbox extends WWidget {
     public boolean checked;
     private boolean pressed;
 
+    private double animationProgress;
+
     public WCheckbox(boolean checked) {
         this.checked = checked;
+        this.animationProgress = checked ? 1 : 0;
     }
 
     @Override
@@ -46,8 +50,11 @@ public class WCheckbox extends WWidget {
     protected void onRender(GuiRenderer renderer, double mouseX, double mouseY, double delta) {
         renderer.renderBackground(this, mouseOver, pressed);
 
-        if (checked) {
-            renderer.renderQuad(x + 3, y + 3, 8, 8, pressed ? GuiConfig.INSTANCE.checkboxPressed : GuiConfig.INSTANCE.checkbox);
+        animationProgress += delta * (checked ? 1 : -1);
+        animationProgress = Utils.clamp(animationProgress, 0, 1);
+
+        if (animationProgress > 0) {
+            renderer.renderQuad(x + 3 + 4 * (1 - animationProgress), y + 3 + 4 * (1 - animationProgress), 8 * animationProgress, 8 * animationProgress, pressed ? GuiConfig.INSTANCE.checkboxPressed : GuiConfig.INSTANCE.checkbox);
         }
     }
 }
