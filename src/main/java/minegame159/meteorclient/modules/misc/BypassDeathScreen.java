067407c613e31a4c55d80c572328415becede006
squidoodly <levimax08@gmail.com>
2020-05-27 15:08:40 +0100

    Have fun, because I sure didn't.

---
+++ src/main/java/minegame159/meteorclient/modules/misc/BypassDeathScreen.java
@@ -0,0 +1,15 @@
+package minegame159.meteorclient.modules.misc;
+
+//Created by squidoodly 27/05/2020
+
+import minegame159.meteorclient.modules.Category;
+import minegame159.meteorclient.modules.ToggleModule;
+
+public class BypassDeathScreen extends ToggleModule {
+
+    public boolean shouldBypass = false;
+
+    public BypassDeathScreen(){
+        super(Category.Misc, "bypass-death-screen", "Let's you spy on people");
+    }
+}
