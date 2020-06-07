1b20482b7acde068ba70ecff25c97f0ad3d44e02
MineGame159 <petulko08@gmail.com>
2020-06-07 15:58:04 +0200

    Fixed Search crash

--- src/main/java/minegame159/meteorclient/utils/MeteorTaskExecutor.java
+++ src/main/java/minegame159/meteorclient/utils/MeteorTaskExecutor.java
@@ -1,5 +1,7 @@
 package minegame159.meteorclient.utils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -8,29 +10,43 @@ public class MeteorTaskExecutor {
     private static ExecutorService executor;
     private static int startedCount = 0;
 
-    public static void start() {
-        if (startedCount == 0) {
-            executor = Executors.newSingleThreadExecutor();
-        }
+    private static final List<Runnable> tasks = new ArrayList<>();
 
-        startedCount++;
+    public static void start() {
+        synchronized (tasks) {
+            if (startedCount == 0) {
+                executor = Executors.newSingleThreadExecutor();
+
+                for (Runnable task : tasks) executor.execute(task);
+                tasks.clear();
+            }
+
+            startedCount++;
+        }
     }
 
     public static void stop() {
-        startedCount--;
+        synchronized (tasks) {
+            if (startedCount > 0) {
+                startedCount--;
 
-        if (startedCount == 0) {
-            try {
-                executor.shutdown();
-                executor.awaitTermination(5, TimeUnit.SECONDS);
-                executor = null;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                if (startedCount == 0) {
+                    try {
+                        executor.shutdown();
+                        executor.awaitTermination(5, TimeUnit.SECONDS);
+                        executor = null;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
     }
 
     public static void execute(Runnable task) {
-        executor.execute(task);
+        synchronized (tasks) {
+            if (executor == null) tasks.add(task);
+            else executor.execute(task);
+        }
     }
 }
