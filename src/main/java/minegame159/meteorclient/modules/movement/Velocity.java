c4455a67f68a1ebe486bb29856c247b4112cd1b9
MineGame159 <petulko08@gmail.com>
2020-06-15 20:36:18 +0200

    Replaced NoPush with Velocity, fixed crash by removing mixin from BypassDeathScreen

---
+++ src/main/java/minegame159/meteorclient/modules/movement/Velocity.java
@@ -0,0 +1,45 @@
+package minegame159.meteorclient.modules.movement;
+
+import minegame159.meteorclient.modules.Category;
+import minegame159.meteorclient.modules.ToggleModule;
+import minegame159.meteorclient.settings.DoubleSetting;
+import minegame159.meteorclient.settings.Setting;
+import minegame159.meteorclient.settings.SettingGroup;
+
+public class Velocity extends ToggleModule {
+    private final SettingGroup sg = settings.getDefaultGroup();
+
+    private final Setting<Double> horizontal = sg.add(new DoubleSetting.Builder()
+            .name("horizontal-multiplier")
+            .description("How much velocity to apply horizontally.")
+            .defaultValue(0)
+            .min(0)
+            .max(1)
+            .sliderMin(0)
+            .sliderMax(1)
+            .build()
+    );
+
+    private final Setting<Double> vertical = sg.add(new DoubleSetting.Builder()
+            .name("vertical-multiplier")
+            .description("How much velocity to apply certically.")
+            .defaultValue(0)
+            .min(0)
+            .max(1)
+            .sliderMin(0)
+            .sliderMax(1)
+            .build()
+    );
+
+    public Velocity() {
+        super(Category.Movement, "velocity", "Prevents u from getting pushed by mobs, taking damage, etc.");
+    }
+
+    public double getHorizontal() {
+        return isActive() ? horizontal.get() : 1;
+    }
+
+    public double getVertical() {
+        return isActive() ? vertical.get() : 1;
+    }
+}
