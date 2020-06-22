31d87ed43677776dc0fe91c0d752c902a71453e0
MineGame159 <petulko08@gmail.com>
2020-06-22 13:36:05 +0200

    Added noArmor to NoRender

---
+++ src/main/java/minegame159/meteorclient/mixin/ArmorFeatureRendererMixin.java
@@ -0,0 +1,21 @@
+package minegame159.meteorclient.mixin;
+
+import minegame159.meteorclient.modules.ModuleManager;
+import minegame159.meteorclient.modules.render.NoRender;
+import net.minecraft.client.render.entity.feature.ArmorFeatureRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ArmorFeatureRenderer.class)
+public class ArmorFeatureRendererMixin<T extends LivingEntity, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>> {
+    @Inject(method = "renderArmor", at = @At("HEAD"), cancellable = true)
+    private void onRenderArmor(T livingEntity, float f, float g, float h, float i, float j, float k, float l, EquipmentSlot equipmentSlot, CallbackInfo info) {
+        if (livingEntity instanceof PlayerEntity && ModuleManager.INSTANCE.get(NoRender.class).noArmor()) info.cancel();
+    }
+}
