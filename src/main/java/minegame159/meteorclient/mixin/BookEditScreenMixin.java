5d0a936197008a7649b8eb4ade391dfabfcd93d7
MineGame159 <petulko08@gmail.com>
2020-06-23 15:26:06 +0200

    Added copy and paste button for copying entire books

---
+++ src/main/java/minegame159/meteorclient/mixin/BookEditScreenMixin.java
@@ -0,0 +1,96 @@
+package minegame159.meteorclient.mixin;
+
+import it.unimi.dsi.fastutil.io.FastByteArrayOutputStream;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.BookEditScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.NbtIo;
+import net.minecraft.nbt.StringTag;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.List;
+
+@Mixin(BookEditScreen.class)
+public abstract class BookEditScreenMixin extends Screen {
+    @Shadow @Final private ItemStack itemStack;
+
+    @Shadow @Final private List<String> pages;
+
+    @Shadow private int currentPage;
+
+    @Shadow private int cursorIndex;
+
+    @Shadow private boolean dirty;
+
+    @Shadow private int highlightTo;
+
+    @Shadow protected abstract void updateButtons();
+
+    public BookEditScreenMixin(Text title) {
+        super(title);
+    }
+
+    @Inject(method = "init", at = @At("TAIL"))
+    private void onInit(CallbackInfo info) {
+        addButton(new ButtonWidget(4, 4, 70, 16, "Copy", button -> {
+            ListTag listTag = new ListTag();
+            pages.stream().map(StringTag::new).forEach(listTag::add);
+
+            CompoundTag tag = new CompoundTag();
+            tag.put("pages", listTag);
+            tag.putInt("currentPage", currentPage);
+
+            FastByteArrayOutputStream bytes = new FastByteArrayOutputStream();
+            DataOutputStream out = new DataOutputStream(bytes);
+            try {
+                NbtIo.write(tag, out);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            GLFW.glfwSetClipboardString(MinecraftClient.getInstance().window.getHandle(), Base64.getEncoder().encodeToString(bytes.array));
+        }));
+        addButton(new ButtonWidget(4, 4 + 16 + 4, 70, 16, "Paste", button -> {
+            byte[] bytes = Base64.getDecoder().decode(GLFW.glfwGetClipboardString(MinecraftClient.getInstance().window.getHandle()));
+            DataInputStream in = new DataInputStream(new ByteArrayInputStream(bytes));
+
+            try {
+                CompoundTag tag = NbtIo.read(in);
+
+                ListTag listTag = tag.getList("pages", 8).copy();
+
+                pages.clear();
+                for(int i = 0; i < listTag.size(); ++i) {
+                    pages.add(listTag.getString(i));
+                }
+
+                if (pages.isEmpty()) {
+                    pages.add("");
+                }
+
+                currentPage = tag.getInt("currentPage");
+
+                dirty = true;
+                updateButtons();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
+    }
+}
---
+++ src/main/java/minegame159/meteorclient/mixin/BookScreenMixin.java
@@ -0,0 +1,55 @@
+package minegame159.meteorclient.mixin;
+
+import it.unimi.dsi.fastutil.io.FastByteArrayOutputStream;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.BookScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.NbtIo;
+import net.minecraft.nbt.StringTag;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+@Mixin(BookScreen.class)
+public class BookScreenMixin extends Screen {
+    @Shadow private BookScreen.Contents contents;
+
+    @Shadow private int pageIndex;
+
+    public BookScreenMixin(Text title) {
+        super(title);
+    }
+
+    @Inject(method = "init", at = @At("TAIL"))
+    private void onInit(CallbackInfo info) {
+        addButton(new ButtonWidget(4, 4, 70, 16, "Copy", button -> {
+            ListTag listTag = new ListTag();
+            for (int i = 0; i < contents.getLineCount(); i++) listTag.add(new StringTag(contents.getLine(i).asString()));
+
+            CompoundTag tag = new CompoundTag();
+            tag.put("pages", listTag);
+            tag.putInt("currentPage", pageIndex);
+
+            FastByteArrayOutputStream bytes = new FastByteArrayOutputStream();
+            DataOutputStream out = new DataOutputStream(bytes);
+            try {
+                NbtIo.write(tag, out);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            GLFW.glfwSetClipboardString(MinecraftClient.getInstance().window.getHandle(), Base64.getEncoder().encodeToString(bytes.array));
+        }));
+    }
+}
