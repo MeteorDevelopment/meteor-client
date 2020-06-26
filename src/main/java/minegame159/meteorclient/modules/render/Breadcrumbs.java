e812a181892f7d4724ab154bc0c7b123f4244889
MineGame159 <petulko08@gmail.com>
2020-06-26 16:52:40 +0200

    Added Breadcrumbs

---
+++ src/main/java/minegame159/meteorclient/modules/render/Breadcrumbs.java
@@ -0,0 +1,111 @@
+package minegame159.meteorclient.modules.render;
+
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
+import minegame159.meteorclient.events.RenderEvent;
+import minegame159.meteorclient.events.TickEvent;
+import minegame159.meteorclient.modules.Category;
+import minegame159.meteorclient.modules.ToggleModule;
+import minegame159.meteorclient.rendering.ShapeBuilder;
+import minegame159.meteorclient.settings.*;
+import minegame159.meteorclient.utils.Color;
+import minegame159.meteorclient.utils.Pool;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Breadcrumbs extends ToggleModule {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Color> color = sgGeneral.add(new ColorSetting.Builder()
+            .name("color")
+            .description("Color of the line.")
+            .defaultValue(new Color(225, 25, 25))
+            .build()
+    );
+
+    private final Setting<Integer> maxSections = sgGeneral.add(new IntSetting.Builder()
+            .name("max-sections")
+            .description("Maximum number of sections.")
+            .defaultValue(1000)
+            .min(0)
+            .build()
+    );
+
+    private final Setting<Double> sectionLength = sgGeneral.add(new DoubleSetting.Builder()
+            .name("section-length")
+            .description("Section length, 1 is equal to 1 block.")
+            .defaultValue(0.5)
+            .min(0)
+            .sliderMin(0)
+            .sliderMax(1)
+            .build()
+    );
+
+    private final Pool<Section> sectionPool = new Pool<>(Section::new);
+    private final Queue<Section> sections = new ArrayDeque<>();
+
+    private Section section;
+
+    public Breadcrumbs() {
+        super(Category.Render, "breadcrumbs", "Displays a line where you walked.");
+    }
+
+    @Override
+    public void onActivate() {
+        section = sectionPool.get();
+        section.set1();
+    }
+
+    @Override
+    public void onDeactivate() {
+        for (Section section : sections) sectionPool.free(section);
+        sections.clear();
+    }
+
+    @EventHandler
+    private final Listener<TickEvent> onTick = new Listener<>(event -> {
+        if (isFarEnough(section.x1, section.y1, section.z1)) {
+            section.set2();
+
+            System.out.println(sections.size());
+            if (sections.size() >= maxSections.get()) {
+                sectionPool.free(sections.poll());
+            }
+
+            sections.add(section);
+            section = sectionPool.get();
+            section.set1();
+        }
+    });
+
+    @EventHandler
+    private final Listener<RenderEvent> onRender = new Listener<>(event -> {
+        for (Section section : sections) section.render();
+    });
+
+    private boolean isFarEnough(double x, double y, double z) {
+        return Math.abs(mc.player.x - x) >= sectionLength.get() || Math.abs(mc.player.y - y) >= sectionLength.get() || Math.abs(mc.player.z - z) >= sectionLength.get();
+    }
+
+    private class Section {
+        public float x1, y1, z1;
+        public float x2, y2, z2;
+
+        public void set1() {
+            x1 = (float) mc.player.x;
+            y1 = (float) mc.player.y;
+            z1 = (float) mc.player.z;
+        }
+
+        public void set2() {
+            x2 = (float) mc.player.x;
+            y2 = (float) mc.player.y;
+            z2 = (float) mc.player.z;
+        }
+
+        public void render() {
+            ShapeBuilder.line(x1, y1, z1, x2, y2, z2, color.get());
+        }
+    }
+}
