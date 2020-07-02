a8bb8c25a5f20c94f0dcc00adb01acfe0672fd66
MineGame159 <petulko08@gmail.com>
2020-07-02 16:06:36 +0200

    Added UwUAura

--- src/main/java/minegame159/meteorclient/modules/ModuleManager.java
+++ src/main/java/minegame159/meteorclient/modules/ModuleManager.java
@@ -308,5 +308,6 @@ public class ModuleManager extends Savable<ModuleManager> implements Listenable 
         addModule(new EChestPreview());
         addModule(new Timer());
         addModule(new Suffix());
+        addModule(new UwUAura());
     }
 }
