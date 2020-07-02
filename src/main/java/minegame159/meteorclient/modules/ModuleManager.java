757be177ad451d920492f42e6bab44aaa0bbe8d0
MineGame159 <petulko08@gmail.com>
2020-07-02 12:42:03 +0200

    Fixes

--- src/main/java/minegame159/meteorclient/modules/ModuleManager.java
+++ src/main/java/minegame159/meteorclient/modules/ModuleManager.java
@@ -307,5 +307,6 @@ public class ModuleManager extends Savable<ModuleManager> implements Listenable 
         addModule(new EntityLogger());
         addModule(new EChestPreview());
         addModule(new Timer());
+        addModule(new Suffix());
     }
 }
