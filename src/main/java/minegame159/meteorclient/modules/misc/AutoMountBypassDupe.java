0f9073cc79f7d6d57434a71392c5813794725c9e
MineGame159 <petulko08@gmail.com>
2020-07-15 10:42:49 +0200

    Fixed errors caused by the last pull request :eyes:

--- src/main/java/minegame159/meteorclient/modules/misc/AutoMountBypassDupe.java
+++ src/main/java/minegame159/meteorclient/modules/misc/AutoMountBypassDupe.java
@@ -66,18 +66,6 @@ public class AutoMountBypassDupe extends ToggleModule {
     private boolean sneak = false;
     private int timer;
 
-
-    private final List<Integer> slotsToMove = new ArrayList<>();
-    private final List<Integer> slotsToThrow = new ArrayList<>();
-
-    private final SettingGroup sgGeneral = settings.getDefaultGroup();
-
-    private final Setting<Boolean> shulkersOnly = sgGeneral.add(new BoolSetting.Builder()
-            .name("shulker-only")
-            .description("Only moves shulker boxes into the inventory")
-            .defaultValue(false)
-            .build());
-
     public AutoMountBypassDupe() {
         super(Category.Misc, "auto-mount-bypass-dupe", "Does the mount bypass dupe for you. Disable with esc.");
     }
