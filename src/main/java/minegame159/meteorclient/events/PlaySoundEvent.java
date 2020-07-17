36ed2b23b701056d9a72e9f222c33fcf18efc834
MineGame159 <petulko08@gmail.com>
2020-07-17 14:19:57 +0200

    Added SoundBlocker

---
+++ src/main/java/minegame159/meteorclient/events/PlaySoundEvent.java
@@ -0,0 +1,7 @@
+package minegame159.meteorclient.events;
+
+import net.minecraft.client.sound.SoundInstance;
+
+public class PlaySoundEvent extends Cancellable {
+    public SoundInstance sound;
+}
---
+++ src/main/java/minegame159/meteorclient/mixin/SoundSystemMixin.java
@@ -0,0 +1,22 @@
+package minegame159.meteorclient.mixin;
+
+import minegame159.meteorclient.MeteorClient;
+import minegame159.meteorclient.events.EventStore;
+import minegame159.meteorclient.events.PlaySoundEvent;
+import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.client.sound.SoundSystem;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SoundSystem.class)
+public class SoundSystemMixin {
+    @Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("HEAD"), cancellable = true)
+    private void onPlay(SoundInstance soundInstance, CallbackInfo info) {
+        PlaySoundEvent event = EventStore.playSoundEvent(soundInstance);
+        MeteorClient.EVENT_BUS.post(event);
+
+        if (event.isCancelled()) info.cancel();
+    }
+}
