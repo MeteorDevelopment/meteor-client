692d9baa2aa9288da61314b3fb60834303ca1cae
MineGame159 <petulko08@gmail.com>
2020-07-18 11:15:44 +0200

    Limited Reach's slider to 6. Still can be set to higher value.

--- src/main/java/minegame159/meteorclient/modules/player/Reach.java
+++ src/main/java/minegame159/meteorclient/modules/player/Reach.java
@@ -14,6 +14,7 @@ public class Reach extends ToggleModule {
             .description("Reach.")
             .defaultValue(5)
             .min(0)
+            .sliderMax(6)
             .build()
     );
 
