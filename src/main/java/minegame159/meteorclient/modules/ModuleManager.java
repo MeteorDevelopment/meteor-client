8dec874e796fc8147b9d6c8e2b18abc1ae8493a1
MineGame159 <petulko08@gmail.com>
2020-07-19 18:34:21 +0200

    Fixed AutoMountBypassDupe

--- src/main/java/minegame159/meteorclient/modules/ModuleManager.java
+++ src/main/java/minegame159/meteorclient/modules/ModuleManager.java
@@ -327,5 +327,6 @@ public class ModuleManager extends Savable<ModuleManager> implements Listenable 
         addModule(new AutoMountBypassDupe());
         addModule(new Nuker());
         addModule(new SoundBlocker());
+        addModule(new AntiChunkBan());
     }
 }
