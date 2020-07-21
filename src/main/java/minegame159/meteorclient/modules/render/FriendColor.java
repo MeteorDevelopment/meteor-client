1009ec6a1d3707fdc1a91d6af6d973a0e11497a6
squidoodly <levimax08@gmail.com>
2020-07-21 17:32:23 +0100

    Added FriendColour in chat.

---
+++ src/main/java/minegame159/meteorclient/modules/render/FriendColor.java
@@ -0,0 +1,10 @@
+package minegame159.meteorclient.modules.render;
+
+//Created by squidoodly 21/07/2020
+
+import minegame159.meteorclient.modules.Category;
+import minegame159.meteorclient.modules.ToggleModule;
+
+public class FriendColor extends ToggleModule {
+    public FriendColor(){super(Category.Render, "friend-color", "Shows friends as different colors in chat.");}
+}
