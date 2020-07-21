59e45c85fb8e6a5be1dd99d5dcd7a8c029e07779
MineGame159 <petulko08@gmail.com>
2020-07-21 16:04:54 +0200

    Removed PlayerNotifier cuz theres EntityLogger

--- src/main/java/minegame159/meteorclient/modules/ModuleManager.java
+++ src/main/java/minegame159/meteorclient/modules/ModuleManager.java
@@ -327,6 +327,5 @@ public class ModuleManager extends Savable<ModuleManager> implements Listenable 
         addModule(new Nuker());
         addModule(new SoundBlocker());
         addModule(new AntiChunkBan());
-        addModule(new PlayerNotifier());
     }
 }
