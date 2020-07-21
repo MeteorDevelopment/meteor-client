286cb0b2165f8bfe0622f4e8474dab46213b5b16
MineGame159 <petulko08@gmail.com>
2020-07-21 11:34:14 +0200

    Fixed EntityControl only working for horses

--- src/main/java/minegame159/meteorclient/modules/movement/EntityControl.java
+++ src/main/java/minegame159/meteorclient/modules/movement/EntityControl.java
@@ -8,7 +8,7 @@ import minegame159.meteorclient.events.TickEvent;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.ToggleModule;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.passive.HorseEntity;
+import net.minecraft.entity.passive.HorseBaseEntity;
 
 public class EntityControl extends ToggleModule {
     public EntityControl(){super(Category.Movement, "entity-control", "Lets you control horses without a saddle.");}
@@ -16,24 +16,24 @@ public class EntityControl extends ToggleModule {
     @Override
     public void onDeactivate() {
         mc.world.getEntities().forEach(entity -> {
-            if(entity instanceof HorseEntity){
-                ((HorseEntity) entity).setSaddled(false);
+            if(entity instanceof HorseBaseEntity){
+                ((HorseBaseEntity) entity).setSaddled(false);
             }
         });
     }
 
     @EventHandler
-    private Listener<TickEvent> onTick = new Listener<>(event -> {
+    private final Listener<TickEvent> onTick = new Listener<>(event -> {
         mc.world.getEntities().forEach(entity -> {
-            if(entity instanceof HorseEntity){
-                ((HorseEntity) entity).setSaddled(true);
+            if(entity instanceof HorseBaseEntity){
+                ((HorseBaseEntity) entity).setSaddled(true);
             }
         });
         if(!mc.player.hasVehicle()) return;
         Entity entity = mc.player.getVehicle();
-        if(entity instanceof HorseEntity){
-            ((HorseEntity) entity).setAiDisabled(true);
-            ((HorseEntity) entity).setTame(true);
+        if(entity instanceof HorseBaseEntity){
+            ((HorseBaseEntity) entity).setAiDisabled(true);
+            ((HorseBaseEntity) entity).setTame(true);
         }
     });
 }
