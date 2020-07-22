3731d0a02349cf23fb48ef951c663d487e7df78c
squidoodly <levimax08@gmail.com>
2020-07-22 23:58:34 +0100

    Added Hold mode to AutoRightClick

--- src/main/java/minegame159/meteorclient/modules/player/AutoRightClick.java
+++ src/main/java/minegame159/meteorclient/modules/player/AutoRightClick.java
@@ -3,16 +3,25 @@ package minegame159.meteorclient.modules.player;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import minegame159.meteorclient.events.TickEvent;
+import minegame159.meteorclient.mixininterface.IKeyBinding;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.ToggleModule;
-import minegame159.meteorclient.settings.BoolSetting;
-import minegame159.meteorclient.settings.IntSetting;
-import minegame159.meteorclient.settings.Setting;
-import minegame159.meteorclient.settings.SettingGroup;
+import minegame159.meteorclient.settings.*;
 import minegame159.meteorclient.utils.Utils;
 
 public class AutoRightClick extends ToggleModule {
+    public enum Mode{
+        Hold,
+        Press
+    }
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Mode> mode = sgGeneral.add(new EnumSetting.Builder<Mode>()
+            .name("mode")
+            .description("How it right clicks.")
+            .defaultValue(Mode.Press)
+            .build()
+    );
     
     private final Setting<Integer> delay = sgGeneral.add(new IntSetting.Builder()
             .name("delay")
@@ -42,8 +51,12 @@ public class AutoRightClick extends ToggleModule {
     }
 
     @EventHandler
-    private Listener<TickEvent> onTick = new Listener<>(event -> {
+    private final Listener<TickEvent> onTick = new Listener<>(event -> {
         if (mc.player.getHealth() <= 0) return;
+        if (mode.get() == Mode.Hold && !mc.options.keyUse.isPressed()) {
+            ((IKeyBinding)mc.options.keyUse).setPressed(true);
+            return;
+        } else if (mode.get() == Mode.Hold) return;
 
         timer++;
 
