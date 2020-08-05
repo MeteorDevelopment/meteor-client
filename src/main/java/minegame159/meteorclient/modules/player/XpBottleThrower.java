170b02cef4854c293922474a8595cd254ecc8ec4
squidoodly <levimax08@gmail.com>
2020-08-05 11:16:16 +0100

    Added look down setting to xp bottle thrower

--- src/main/java/minegame159/meteorclient/modules/player/XpBottleThrower.java
+++ src/main/java/minegame159/meteorclient/modules/player/XpBottleThrower.java
@@ -5,16 +5,30 @@ import me.zero.alpine.listener.Listener;
 import minegame159.meteorclient.events.TickEvent;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.ToggleModule;
+import minegame159.meteorclient.settings.BoolSetting;
+import minegame159.meteorclient.settings.Setting;
+import minegame159.meteorclient.settings.SettingGroup;
 import net.minecraft.item.Items;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.util.Hand;
 
 public class XpBottleThrower extends ToggleModule {
+
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Boolean> lookDown = sgGeneral.add(new BoolSetting.Builder()
+            .name("look-down")
+            .description("Makes you look down when throwing bottles")
+            .defaultValue(true)
+            .build()
+    );
+
     public XpBottleThrower() {
         super(Category.Player, "xp-bottle-thrower", "Automatically throws xp bottles in your hotbar.");
     }
 
     @EventHandler
-    private Listener<TickEvent> onTick = new Listener<>(event -> {
+    private final Listener<TickEvent> onTick = new Listener<>(event -> {
         int slot = -1;
 
         for (int i = 0; i < 9; i++) {
@@ -25,6 +39,9 @@ public class XpBottleThrower extends ToggleModule {
         }
 
         if (slot != -1) {
+            if (lookDown.get()) {
+                mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.LookOnly(mc.player.yaw, 90, mc.player.onGround));
+            }
             int preSelectedSlot = mc.player.inventory.selectedSlot;
             mc.player.inventory.selectedSlot = slot;
             mc.interactionManager.interactItem(mc.player, mc.world, Hand.MAIN_HAND);
