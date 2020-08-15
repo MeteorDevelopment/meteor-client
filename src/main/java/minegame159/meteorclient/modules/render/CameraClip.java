7b07087cc40e9a4b6d6834c7ca578d4adc3a22dc
MineGame159 <petulko08@gmail.com>
2020-08-15 13:17:08 +0200

    Renamed NoClip to CameraClip

---
+++ src/main/java/minegame159/meteorclient/modules/render/CameraClip.java
@@ -0,0 +1,10 @@
+package minegame159.meteorclient.modules.render;
+
+import minegame159.meteorclient.modules.Category;
+import minegame159.meteorclient.modules.ToggleModule;
+
+public class CameraClip extends ToggleModule {
+    public CameraClip() {
+        super(Category.Render, "camera-clip", "Allows your 3rd person camera to move through blocks..");
+    }
+}
