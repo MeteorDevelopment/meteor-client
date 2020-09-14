ca785570b23dde716dfa6f99f803268ceb5b440f
MineGame159 <petulko08@gmail.com>
2020-09-14 10:40:37 +0200

    Improved ChestSwap (now works with netherite)

--- src/main/java/minegame159/meteorclient/modules/player/ChestSwap.java
+++ src/main/java/minegame159/meteorclient/modules/player/ChestSwap.java
@@ -4,17 +4,33 @@ import minegame159.meteorclient.Config;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.ToggleModule;
 import minegame159.meteorclient.settings.BoolSetting;
+import minegame159.meteorclient.settings.EnumSetting;
 import minegame159.meteorclient.settings.Setting;
 import minegame159.meteorclient.settings.SettingGroup;
 import minegame159.meteorclient.utils.Chat;
 import minegame159.meteorclient.utils.InvUtils;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.screen.slot.SlotActionType;
 
 public class ChestSwap extends ToggleModule {
+    public enum Chestplate {
+        Diamond,
+        Netherite,
+        PreferDiamond,
+        PrederNetherite
+    }
+
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Chestplate> chestplate = sgGeneral.add(new EnumSetting.Builder<Chestplate>()
+            .name("chestplate")
+            .description("Which chestplate to switch to.")
+            .defaultValue(Chestplate.PrederNetherite)
+            .build()
+    );
 
     private final Setting<Boolean> stayOn = sgGeneral.add(new BoolSetting.Builder()
             .name("stay-on")
@@ -40,17 +56,66 @@ public class ChestSwap extends ToggleModule {
 
     public void swap() {
         Item currentItem = mc.player.getEquippedStack(EquipmentSlot.CHEST).getItem();
-        Item desiredItem = null;
-        if (currentItem == Items.DIAMOND_CHESTPLATE) desiredItem = Items.ELYTRA;
-        else if (currentItem == Items.ELYTRA) desiredItem = Items.DIAMOND_CHESTPLATE;
+
+        if (currentItem == Items.ELYTRA) {
+            equipChestplate();
+        } else if (currentItem instanceof ArmorItem && ((ArmorItem) currentItem).getSlotType() == EquipmentSlot.CHEST) {
+            equipElytra();
+        } else {
+            if (!equipChestplate()) equipElytra();
+        }
+    }
+
+    private boolean equipChestplate() {
+        int bestSlot = -1;
+        boolean breakLoop = false;
 
         for (int i = 0; i < mc.player.inventory.main.size(); i++) {
             Item item = mc.player.inventory.main.get(i).getItem();
 
-            if (desiredItem == null && (item == Items.DIAMOND_CHESTPLATE || item == Items.ELYTRA)) {
-                equip(i);
-                break;
-            } else if (item == desiredItem) {
+            switch (chestplate.get()) {
+                case Diamond:
+                    if (item == Items.DIAMOND_CHESTPLATE) {
+                        bestSlot = i;
+                        breakLoop = true;
+                    }
+                    break;
+                case Netherite:
+                    if (item == Items.NETHERITE_CHESTPLATE) {
+                        bestSlot = i;
+                        breakLoop = true;
+                    }
+                    break;
+                case PreferDiamond:
+                    if (item == Items.DIAMOND_CHESTPLATE) {
+                        bestSlot = i;
+                        breakLoop = true;
+                    } else if (item == Items.NETHERITE_CHESTPLATE) {
+                        bestSlot = i;
+                    }
+                    break;
+                case PrederNetherite:
+                    if (item == Items.DIAMOND_CHESTPLATE) {
+                        bestSlot = i;
+                    } else if (item == Items.NETHERITE_CHESTPLATE) {
+                        bestSlot = i;
+                        breakLoop = true;
+                    }
+                    break;
+            }
+
+            if (breakLoop) break;
+        }
+
+        if (bestSlot != -1) equip(bestSlot);
+        return bestSlot != -1;
+    }
+
+    private void equipElytra() {
+        for (int i = 0; i < mc.player.inventory.main.size(); i++) {
+            Item item = mc.player.inventory.main.get(i).getItem();
+
+            if (item == Items.ELYTRA) {
                 equip(i);
                 break;
             }
