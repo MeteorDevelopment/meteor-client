55efb8d5c971c92ae72b3001436b62c29721c27e
squidoodly <35968667+squidoodly@users.noreply.github.com>
2020-09-20 17:34:20 +0100

    Fixed AutoArmor prioritisation.

--- src/main/java/minegame159/meteorclient/modules/combat/AutoArmor.java
+++ src/main/java/minegame159/meteorclient/modules/combat/AutoArmor.java
@@ -26,7 +26,6 @@ import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.Items;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.dimension.DimensionType;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -171,6 +170,8 @@ public class AutoArmor extends ToggleModule {
 
     private int delayLeft = delay.get();
     private boolean didSkip = false;
+    private int currentBest, currentProt, currentBlast, currentFire, currentProj, currentArmour = 0;
+    private float currentToughness = 0;
 
     @EventHandler
     private final Listener<TickEvent> onTick = new Listener<>(event -> {
@@ -192,18 +193,23 @@ public class AutoArmor extends ToggleModule {
             delayLeft = switchCooldown.get();
             didSkip = false;
         }
-        int currentItemScore = -1;
         ItemStack itemStack;
         for (int a = 0; a < 4; a++) {
-            currentItemScore = 0;
             itemStack = mc.player.inventory.getArmorStack(a);
+            currentBest = 0;
+            currentProt = 0;
+            currentBlast = 0;
+            currentFire = 0;
+            currentProj = 0;
+            currentArmour = 0;
+            currentToughness = 0;
             if ((ignoreElytra.get() || ModuleManager.INSTANCE.get(ChestSwap.class).isActive()) && itemStack.getItem() == Items.ELYTRA) continue;
             if (EnchantmentHelper.hasBindingCurse(itemStack)) continue;
             if (itemStack.getItem() instanceof ArmorItem) {
                 if (a == 1 && bProtLegs.get()) {
                     mode.set(Prot.Blast_Protection);
                 }
-                currentItemScore = getItemScore(itemStack);
+                getCurrentScore(itemStack);
             }
             int bestSlot = -1;
             int bestScore = 0;
@@ -218,7 +224,7 @@ public class AutoArmor extends ToggleModule {
                     }
                 }
             }
-            if (bestScore > currentItemScore && bestSlot > -1) {
+            if (bestSlot > -1) {
                 InvUtils.clickSlot(InvUtils.invIndexToSlotId(bestSlot), 0, SlotActionType.PICKUP);
                 InvUtils.clickSlot(8 - a, 0, SlotActionType.PICKUP);
                 InvUtils.clickSlot(InvUtils.invIndexToSlotId(bestSlot), 0, SlotActionType.PICKUP);
@@ -236,15 +242,26 @@ public class AutoArmor extends ToggleModule {
         for (Enchantment ench = bad.next(); bad.hasNext(); ench = bad.next()) {
             if (EnchantmentHelper.getLevel(ench, itemStack) > 0) return 1;
         }
-        score += 8 * EnchantmentHelper.getLevel(mode.get().enchantment, itemStack);
-        score += 2 * EnchantmentHelper.getLevel(Enchantments.PROTECTION, itemStack);
-        score += 2 * EnchantmentHelper.getLevel(Enchantments.BLAST_PROTECTION, itemStack);
-        score += 2 * EnchantmentHelper.getLevel(Enchantments.FIRE_PROTECTION, itemStack);
-        score += 2 * EnchantmentHelper.getLevel(Enchantments.PROJECTILE_PROTECTION, itemStack);
-        score += 2 * ((ArmorItem) itemStack.getItem()).getProtection();
+        score += 4 * (EnchantmentHelper.getLevel(mode.get().enchantment, itemStack) - currentBest);
+        score += 2 * (EnchantmentHelper.getLevel(Enchantments.PROTECTION, itemStack) - currentProt);
+        score += 2 * (EnchantmentHelper.getLevel(Enchantments.BLAST_PROTECTION, itemStack) - currentBlast);
+        score += 2 * (EnchantmentHelper.getLevel(Enchantments.FIRE_PROTECTION, itemStack) - currentFire);
+        score += 2 * (EnchantmentHelper.getLevel(Enchantments.PROJECTILE_PROTECTION, itemStack) - currentProj);
+        score += 2 * (((ArmorItem) itemStack.getItem()).getProtection() - currentArmour);
+        score += 2 * (((ArmorItem) itemStack.getItem()).method_26353() - currentToughness);
         score += EnchantmentHelper.getLevel(Enchantments.UNBREAKING, itemStack);
         if (preferMending.get() && EnchantmentHelper.getLevel(Enchantments.MENDING, itemStack) > 0) score += weight.get();
         return score;
+    }
+
+    private void getCurrentScore(ItemStack itemStack) {
+        currentBest = EnchantmentHelper.getLevel(mode.get().enchantment, itemStack);
+        currentProt = EnchantmentHelper.getLevel(Enchantments.PROTECTION, itemStack);
+        currentBlast = EnchantmentHelper.getLevel(Enchantments.BLAST_PROTECTION, itemStack);
+        currentFire = EnchantmentHelper.getLevel(Enchantments.FIRE_PROTECTION, itemStack);
+        currentProj = EnchantmentHelper.getLevel(Enchantments.PROJECTILE_PROTECTION, itemStack);
+        currentArmour = ((ArmorItem) itemStack.getItem()).getProtection();
+        currentToughness = ((ArmorItem) itemStack.getItem()).method_26353();
     }
 
     private boolean explosionNear() {
@@ -253,7 +270,7 @@ public class AutoArmor extends ToggleModule {
                 return true;
             }
         }
-        if (!mc.world.getRegistryKey().getValue().getPath().equals("overworld")) {
+        if (!mc.world.getDimension().isBedWorking()) {
             for (BlockEntity blockEntity : mc.world.blockEntities) {
                 BlockPos pos = blockEntity.getPos();
                 if (blockEntity instanceof BedBlockEntity && DamageCalcUtils.bedDamage(mc.player, new Vec3d(pos.getX(), pos.getY(), pos.getZ())) > boomDamage.get()) {
