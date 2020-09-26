534078848ff21b93ffdf2b3b7c15827ba26322f4
MineGame159 <petulko08@gmail.com>
2020-09-26 12:19:50 +0200

    Added BowSpam

---
+++ src/main/java/minegame159/meteorclient/modules/combat/BowSpam.java
@@ -0,0 +1,63 @@
+package minegame159.meteorclient.modules.combat;
+
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
+import minegame159.meteorclient.events.TickEvent;
+import minegame159.meteorclient.mixininterface.IKeyBinding;
+import minegame159.meteorclient.modules.Category;
+import minegame159.meteorclient.modules.ToggleModule;
+import minegame159.meteorclient.settings.IntSetting;
+import minegame159.meteorclient.settings.Setting;
+import minegame159.meteorclient.settings.SettingGroup;
+import net.minecraft.item.Items;
+
+public class BowSpam extends ToggleModule {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Integer> charge = sgGeneral.add(new IntSetting.Builder()
+            .name("charge")
+            .description("How much to charge the bow in ticks. (0-20)")
+            .defaultValue(5)
+            .min(5)
+            .max(20)
+            .sliderMin(5)
+            .sliderMax(20)
+            .build()
+    );
+
+    private boolean wasBow = false;
+
+    public BowSpam() {
+        super(Category.Combat, "bow-spam", "Spams arrows.");
+    }
+
+    @Override
+    public void onActivate() {
+        wasBow = false;
+    }
+
+    @Override
+    public void onDeactivate() {
+        setPressed(false);
+    }
+
+    @EventHandler
+    private final Listener<TickEvent> onTick = new Listener<>(event -> {
+        boolean isBow = mc.player.getMainHandStack().getItem() == Items.BOW;
+        if (!isBow && wasBow) setPressed(false);
+
+        wasBow = isBow;
+        if (!isBow) return;
+
+        if (mc.player.getItemUseTime() >= charge.get()) {
+            mc.player.stopUsingItem();
+            mc.interactionManager.stopUsingItem(mc.player);
+        } else {
+            setPressed(true);
+        }
+    });
+
+    private void setPressed(boolean pressed) {
+        ((IKeyBinding) mc.options.keyUse).setPressed(pressed);
+    }
+}
