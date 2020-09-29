658d08cc64c8e5705c7ef89a58e0a21ba0b9507a
MineGame159 <petulko08@gmail.com>
2020-09-29 17:50:43 +0200

    ???????????????

--- .circleci/discordwebhook/main.js
+++ .circleci/discordwebhook/main.js
@@ -5,33 +5,37 @@ const version = process.argv[3]
 const build = parseInt(process.argv[4]) + 1
 const compareUrl = process.argv[5]
 
-console.log(compareUrl)
+axios
+  .get(compareUrl)
+  .then(res => {
+    let success = true
+    let description = ""
 
-let success = true
-let description = ""
+    console.log(res)
 
-description += "**Branch:** " + branch
-description += "\n**Status:** " + (success ? "success" : "failure")
+    description += "**Branch:** " + branch
+    description += "\n**Status:** " + (success ? "success" : "failure")
 
-description += "\n\n**Changes:**"
-description += "\n- and what here"
+    description += "\n\n**Changes:**"
+    description += "\n- and what here"
 
-if (success) {
-  description += "\n\n**Download:** [meteor-client-" + version + "](https://" + build + "-256699023-gh.circle-artifacts.com/0/build/libs/meteor-client-" + version + ".jar)"
-}
-
-axios.post("https://discordapp.com/api/webhooks/760506437348229151/PDbacrTK-dHeYtRVb4YPj-bzb_bj4Bs_Q6Bga8iA4SLXFeKS6prj13uqQs0St5FLKWHF", {
-  username: "Dev Builds",
-  avatar_url: "https://meteorclient.com/icon.png",
-  embeds: [
-    {
-      title: "meteor client v" + version + " build #" + build,
-      description: description,
-      url: "https://meteorclient.com",
-      color: success ? 3066993 : 15158332,
-      thumbnail: {
-        url: "https://meteorclient.com/icon.png"
-      }
+    if (success) {
+      description += "\n\n**Download:** [meteor-client-" + version + "](https://" + build + "-256699023-gh.circle-artifacts.com/0/build/libs/meteor-client-" + version + ".jar)"
     }
-  ]
-})
+
+    axios.post("https://discordapp.com/api/webhooks/760506437348229151/PDbacrTK-dHeYtRVb4YPj-bzb_bj4Bs_Q6Bga8iA4SLXFeKS6prj13uqQs0St5FLKWHF", {
+      username: "Dev Builds",
+      avatar_url: "https://meteorclient.com/icon.png",
+      embeds: [
+        {
+          title: "meteor client v" + version + " build #" + build,
+          description: description,
+          url: "https://meteorclient.com",
+          color: success ? 3066993 : 15158332,
+          thumbnail: {
+            url: "https://meteorclient.com/icon.png"
+          }
+        }
+      ]
+    })
+  })
