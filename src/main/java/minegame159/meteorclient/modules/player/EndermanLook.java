6f96f85877ec78d6fc22c7b464651b6ce1220885
MineGame159 <petulko08@gmail.com>
2020-10-02 16:05:20 +0200

    Fixed formatting of one class

--- src/main/java/minegame159/meteorclient/modules/player/EndermanLook.java
+++ src/main/java/minegame159/meteorclient/modules/player/EndermanLook.java
@@ -14,42 +14,41 @@ import net.minecraft.util.math.Vec3d;
 
 public class EndermanLook extends ToggleModule {
 
-	public EndermanLook() {
-		super(Category.Player, "ender-man-look", "Prevents endermen from getting angry at you");
-	}
-	
-	@EventHandler
-  private final Listener<TickEvent> onTick = new Listener<>(event -> {
-    if (mc.player.abilities.creativeMode || !shouldLook())
-      return;
+    public EndermanLook() {
+        super(Category.Player, "enderman-look", "Prevents endermen from getting angry at you");
+    }
 
-    PlayerMoveC2SPacket.LookOnly packet = new PlayerMoveC2SPacket.LookOnly(
-    		mc.player.yaw,
-    		90.0f,
-    		mc.player.isOnGround());
-    
-    mc.player.networkHandler.sendPacket(packet);
-  });
-	
-	private boolean shouldLook() {
-    return Streams.stream(mc.world.getEntities())
-			.filter(entity -> entity instanceof EndermanEntity)
-			.filter(Entity::isAlive)
-			.filter(this::angleCheck)
-			.count() > 0;
-	}
-	
-	private boolean angleCheck(Entity entity) {
-		Vec3d vec3d = mc.player.getRotationVec(1.0F).normalize();
-		Vec3d vec3d2 = new Vec3d(
-				entity.getX() - mc.player.getX(),
-				entity.getEyeY() - mc.player.getEyeY(),
-				entity.getZ() - mc.player.getZ());
-		
-		double d = vec3d2.length();
-		vec3d2 = vec3d2.normalize();
-		double e = vec3d.dotProduct(vec3d2);
-		
-    return e > 1.0D - 0.025D / d ? mc.player.canSee(entity) : false;
-  }
+    @EventHandler
+    private final Listener<TickEvent> onTick = new Listener<>(event -> {
+        if (mc.player.abilities.creativeMode || !shouldLook())
+            return;
+
+        PlayerMoveC2SPacket.LookOnly packet = new PlayerMoveC2SPacket.LookOnly(
+                mc.player.yaw,
+                90.0f,
+                mc.player.isOnGround());
+
+        mc.player.networkHandler.sendPacket(packet);
+    });
+
+    private boolean shouldLook() {
+        return Streams.stream(mc.world.getEntities())
+                .filter(entity -> entity instanceof EndermanEntity)
+                .filter(Entity::isAlive)
+                .anyMatch(this::angleCheck);
+    }
+
+    private boolean angleCheck(Entity entity) {
+        Vec3d vec3d = mc.player.getRotationVec(1.0F).normalize();
+        Vec3d vec3d2 = new Vec3d(
+                entity.getX() - mc.player.getX(),
+                entity.getEyeY() - mc.player.getEyeY(),
+                entity.getZ() - mc.player.getZ());
+
+        double d = vec3d2.length();
+        vec3d2 = vec3d2.normalize();
+        double e = vec3d.dotProduct(vec3d2);
+
+        return e > 1.0D - 0.025D / d && mc.player.canSee(entity);
+    }
 }
