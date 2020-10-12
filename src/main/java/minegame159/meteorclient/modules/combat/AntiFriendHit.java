6d2551fbecd5197f79a528c33e9161143bffba7e
MineGame159 <petulko08@gmail.com>
2020-10-12 14:24:24 +0200

    Improved code for AntiFriendHit

--- src/main/java/minegame159/meteorclient/modules/combat/AntiFriendHit.java
+++ src/main/java/minegame159/meteorclient/modules/combat/AntiFriendHit.java
@@ -2,9 +2,22 @@ package minegame159.meteorclient.modules.combat;
 
 //Created by squidoodly 16/07/2020 (Yay! Empty class!!!)
 
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
+import minegame159.meteorclient.events.AttackEntityEvent;
+import minegame159.meteorclient.friends.FriendManager;
 import minegame159.meteorclient.modules.Category;
+import minegame159.meteorclient.modules.ModuleManager;
 import minegame159.meteorclient.modules.ToggleModule;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class AntiFriendHit extends ToggleModule {
-    public AntiFriendHit(){super(Category.Combat, "anti-friend-hit", "Cancels attacks that hit friends");}
+    public AntiFriendHit() {
+        super(Category.Combat, "anti-friend-hit", "Cancels attacks that hit friends");
+    }
+
+    @EventHandler
+    private final Listener<AttackEntityEvent> onAttackEntity = new Listener<>(event -> {
+        if (event.entity instanceof PlayerEntity &&  ModuleManager.INSTANCE.get(AntiFriendHit.class).isActive() && !FriendManager.INSTANCE.attack((PlayerEntity) event.entity)) event.cancel();
+    });
 }
