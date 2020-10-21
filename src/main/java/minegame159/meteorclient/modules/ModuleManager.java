66ff58b223896e094867a85e186add342149fb25
seasnail8169 <mcwalterarlo@gmail.com>
2020-10-22 00:19:11 +0100

    Added OffHandCrash

--- src/main/java/minegame159/meteorclient/modules/ModuleManager.java
+++ src/main/java/minegame159/meteorclient/modules/ModuleManager.java
@@ -348,5 +348,6 @@ public class ModuleManager extends Savable<ModuleManager> implements Listenable 
         addModule(new Announcer());
         addModule(new BetterChat());
         addModule(new FancyChat());
+        addModule(new OffHandCrash());
     }
 }
