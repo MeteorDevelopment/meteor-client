382b0a039dbddaa6698819be382b81854d708b21
seasnail8169 <mcwalterarlo@gmail.com>
2020-10-27 18:58:38 +0000

    Added NoRender gui background

---
+++ src/main/java/minegame159/meteorclient/mixin/ScreenMixin.java
@@ -0,0 +1,17 @@
+package minegame159.meteorclient.mixin;
+
+import minegame159.meteorclient.modules.ModuleManager;
+import minegame159.meteorclient.modules.render.NoRender;
+import net.minecraft.client.gui.screen.Screen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Screen.class)
+public class ScreenMixin {
+    @Inject(method = "renderBackground(Lnet/minecraft/client/util/math/MatrixStack;)V", at = @At("HEAD"), cancellable = true)
+    private void onRenderBackground(CallbackInfo info) {
+        if (ModuleManager.INSTANCE.get(NoRender.class).noGuiBackground()) info.cancel();
+    }
+}
