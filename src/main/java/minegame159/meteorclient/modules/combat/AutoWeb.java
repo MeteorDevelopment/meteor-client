5ffbc244baf8958b638fae64f106d3048ea613cf
seasnail8169 <mcwalterarlo@gmail.com>
2020-11-01 23:37:44 +0000

    Added SelfWeb and AutoWeb

---
+++ src/main/java/minegame159/meteorclient/modules/combat/AutoWeb.java
@@ -0,0 +1,87 @@
+package minegame159.meteorclient.modules.combat;
+
+import me.zero.alpine.listener.EventHandler;
+import me.zero.alpine.listener.Listener;
+import minegame159.meteorclient.events.PostTickEvent;
+import minegame159.meteorclient.friends.FriendManager;
+import minegame159.meteorclient.modules.Category;
+import minegame159.meteorclient.modules.ToggleModule;
+import minegame159.meteorclient.settings.BoolSetting;
+import minegame159.meteorclient.settings.DoubleSetting;
+import minegame159.meteorclient.settings.Setting;
+import minegame159.meteorclient.settings.SettingGroup;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+
+public class AutoWeb extends ToggleModule {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Double> range = sgGeneral.add(new DoubleSetting.Builder()
+            .name("range")
+            .description("How far away it will place webs.")
+            .defaultValue(4)
+            .min(0)
+            .build()
+    );
+
+    private final Setting<Boolean> doubles = sgGeneral.add(new BoolSetting.Builder()
+            .name("doubles")
+            .description("Places in the  upper hitbox as well.")
+            .defaultValue(false)
+            .build()
+    );
+
+    public AutoWeb() {
+        super(Category.Combat, "auto-web", "Automatically places webs at your enemies feet.");
+    }
+
+    private PlayerEntity target = null;
+
+    @EventHandler
+    private final Listener<PostTickEvent> onTick = new Listener<>(event -> {
+        int webSlot = -1;
+        for (int i = 0; i < 9; i++) {
+            Item item = mc.player.inventory.getStack(i).getItem();
+
+            if (item == Items.COBWEB) {
+                webSlot = i;
+                break;
+            }
+        }
+        if (webSlot == -1) return;
+
+        if (target != null) {
+            if (mc.player.distanceTo(target) > range.get() || !target.isAlive()) target = null;
+        }
+
+        for (PlayerEntity player : mc.world.getPlayers()) {
+            if (player == mc.player || !FriendManager.INSTANCE.attack(player) || !player.isAlive() || mc.player.distanceTo(player) > range.get()) continue;
+            if (target == null) {
+                target = player;
+            } else if (mc.player.distanceTo(target) > mc.player.distanceTo(player)) {
+                target = player;
+            }
+        }
+
+        if (target != null) {
+            int prevSlot = mc.player.inventory.selectedSlot;
+            mc.player.inventory.selectedSlot = webSlot;
+            BlockPos targetPos = target.getBlockPos();
+            if (mc.world.getBlockState(targetPos.add(0, 0, 0)).isAir()) {
+                mc.interactionManager.interactBlock(mc.player, mc.world, Hand.MAIN_HAND, new BlockHitResult(target.getPos(), Direction.DOWN, target.getBlockPos(), true));
+                mc.player.swingHand(Hand.MAIN_HAND);
+            }
+            if (doubles.get() && mc.world.getBlockState(targetPos.add(0, 1, 0)).isAir()) {
+                mc.interactionManager.interactBlock(mc.player, mc.world, Hand.MAIN_HAND, new BlockHitResult(target.getPos(), Direction.DOWN, target.getBlockPos().add(0, 1,0), true));
+                mc.player.swingHand(Hand.MAIN_HAND);
+            }
+            mc.player.swingHand(Hand.MAIN_HAND);
+            mc.player.inventory.selectedSlot = prevSlot;
+        }
+    });
+}
