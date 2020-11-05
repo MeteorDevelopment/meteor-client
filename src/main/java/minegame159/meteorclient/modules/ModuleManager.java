7f2dbad202df0c0fc86585614c46d44892f946cb
bowlz1 <42359039+bowlz1@users.noreply.github.com>
2020-11-05 19:30:46 +0100

    Add files via upload

--- src/main/java/minegame159/meteorclient/modules/ModuleManager.java
+++ src/main/java/minegame159/meteorclient/modules/ModuleManager.java
@@ -221,6 +221,7 @@ public class ModuleManager extends Savable<ModuleManager> implements Listenable 
 
     private void initCombat() {
         addModule(new Auto32K());
+        addModule(new AutoAirPlace());
         addModule(new AntiFriendHit());
         addModule(new Criticals());
         addModule(new AutoTotem());
