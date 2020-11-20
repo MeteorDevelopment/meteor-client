0724ab9cbe0430a03cd29b9f81053b00733fb930
ttrssreal <jamesmachurchill@gmail.com>
2020-11-20 19:22:02 +1300

    added AntiCactus module & added CactusBlockMixin

---
+++ src/main/java/minegame159/meteorclient/modules/player/AntiCactus.java
@@ -0,0 +1,10 @@
+package minegame159.meteorclient.modules.player;
+
+import minegame159.meteorclient.modules.Category;
+import minegame159.meteorclient.modules.ToggleModule;
+
+public class AntiCactus extends ToggleModule {
+    public AntiCactus() {
+        super(Category.Player, "anti-cactus", "Stops cactus damage.");
+    }
+}
