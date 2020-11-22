811f7303c267935b321f3fe8b49f518c56e8faab
MineGame159 <petulko08@gmail.com>
2020-11-22 13:24:02 +0100

    Added a few slash texts that can appear

---
+++ src/main/java/minegame159/meteorclient/mixin/SplashTextResourceSupplierMixin.java
@@ -0,0 +1,21 @@
+package minegame159.meteorclient.mixin;
+
+import net.minecraft.client.resource.SplashTextResourceSupplier;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.profiler.Profiler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(SplashTextResourceSupplier.class)
+public class SplashTextResourceSupplierMixin {
+    @Inject(method = "apply", at = @At("HEAD"))
+    private void onApply(List<String> list, ResourceManager resourceManager, Profiler profiler, CallbackInfo info) {
+        list.add("Meteor on Crack!");
+        list.add("Star Meteor Client on GitHub!");
+        list.add("All hail Meteor!");
+    }
+}
