e94847cb25d2534cfb8d1f47fd87390fa9d6a0fa
MineGame159 <petulko08@gmail.com>
2020-11-22 13:24:31 +0100

    Removed debug messages

--- src/main/java/minegame159/meteorclient/utils/OnlinePlayers.java
+++ src/main/java/minegame159/meteorclient/utils/OnlinePlayers.java
@@ -7,14 +7,12 @@ public class OnlinePlayers {
         long time = System.currentTimeMillis();
 
         if (time - lastPingTime > 5 * 60 * 1000) {
-            System.out.println("Sending online ping");
             MeteorExecutor.execute(() -> HttpUtils.get("http://meteorclient.com:8082/api/online/ping"));
             lastPingTime = time;
         }
     }
 
     public static void leave() {
-        System.out.println("Sending online leave");
         MeteorExecutor.execute(() -> HttpUtils.get("http://meteorclient.com:8082/api/online/leave"));
     }
 }
