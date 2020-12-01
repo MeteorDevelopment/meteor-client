f32c4de28ed12143c508829bcf6f78c5938680ce
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-12-01 01:56:02 +0100

    Fix TODO: Friend argument

--- src/main/java/minegame159/meteorclient/commands/commands/Friend.java
+++ src/main/java/minegame159/meteorclient/commands/commands/Friend.java
@@ -5,14 +5,26 @@
 
 package minegame159.meteorclient.commands.commands;
 
-import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import minegame159.meteorclient.commands.Command;
 import minegame159.meteorclient.commands.CommandSource;
 import minegame159.meteorclient.friends.FriendManager;
 import minegame159.meteorclient.utils.Chat;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
+import static minegame159.meteorclient.utils.Utils.mc;
+import static net.minecraft.command.CommandSource.suggestMatching;
 
 public class Friend extends Command {
     public Friend() {
@@ -22,24 +34,24 @@ public class Friend extends Command {
     @Override
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         builder.then(literal("add")
-                .then(argument("friend", StringArgumentType.greedyString())
+                .then(argument("friend", FriendArgumentType.friend())
                         .executes(context -> {
-                            // TODO: Friend argument
-                            String friend = context.getArgument("friend", String.class);
-                            if (FriendManager.INSTANCE.add(new minegame159.meteorclient.friends.Friend(friend))) {
-                                Chat.info("Added (highlight)%s (default)to friends.", friend);
+                            minegame159.meteorclient.friends.Friend friend =
+                                    context.getArgument("friend", minegame159.meteorclient.friends.Friend.class);
+                            if (FriendManager.INSTANCE.add(friend)) {
+                                Chat.info("Added (highlight)%s (default)to friends.", friend.name);
                             } else {
                                 Chat.error("That person is already your friend.");
                             }
 
                             return SINGLE_SUCCESS;
                         }))
-                .then(literal("remove").then(argument("friend", StringArgumentType.greedyString())
+                .then(literal("remove").then(argument("friend", FriendArgumentType.friend())
                         .executes(context -> {
-                            // TODO: Friend argument
-                            String friend = context.getArgument("friend", String.class);
-                            if (FriendManager.INSTANCE.remove(new minegame159.meteorclient.friends.Friend(friend))) {
-                                Chat.info("Removed (highlight)%s (default)from friends.", friend);
+                            minegame159.meteorclient.friends.Friend friend =
+                                    context.getArgument("friend", minegame159.meteorclient.friends.Friend.class);
+                            if (FriendManager.INSTANCE.remove(friend)) {
+                                Chat.info("Removed (highlight)%s (default)from friends.", friend.name);
                             } else {
                                 Chat.error("That person is not your friend.");
                             }
@@ -55,6 +67,30 @@ public class Friend extends Command {
 
                     return SINGLE_SUCCESS;
                 })));
+    }
+
+    private static class FriendArgumentType implements ArgumentType<minegame159.meteorclient.friends.Friend> {
+
+        public static FriendArgumentType friend() {
+            return new FriendArgumentType();
+        }
+
+        @Override
+        public minegame159.meteorclient.friends.Friend parse(StringReader reader) throws CommandSyntaxException {
+            return new minegame159.meteorclient.friends.Friend(reader.readString());
+        }
+
+        @Override
+        public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
+            return suggestMatching(mc.getNetworkHandler().getPlayerList().stream()
+                    .map(entry -> entry.getProfile().getName()).collect(Collectors.toList()), builder);
+        }
+
+        @Override
+        public Collection<String> getExamples() {
+            // :)
+            return Arrays.asList("086", "seasnail8169", "squidoodly");
+        }
     }
 
 }
