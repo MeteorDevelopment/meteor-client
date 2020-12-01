0210e0ef746db0caaeb8837fc7861e221db0b83c
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2020-12-01 01:43:40 +0100

    Port all commands to brigadier

--- src/main/java/minegame159/meteorclient/commands/Command.java
+++ src/main/java/minegame159/meteorclient/commands/Command.java
@@ -6,6 +6,9 @@
 package minegame159.meteorclient.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import net.minecraft.client.MinecraftClient;
 
 public abstract class Command {
@@ -20,5 +23,20 @@ public abstract class Command {
         MC = MinecraftClient.getInstance();
     }
 
-    abstract void registerTo(CommandDispatcher<CommandSource> dispatcher);
+    // Helper methods to painlessly infer the CommandSource generic type argument
+    protected static <T> RequiredArgumentBuilder<CommandSource, T> argument(final String name, final ArgumentType<T> type) {
+        return RequiredArgumentBuilder.argument(name, type);
+    }
+
+    protected static <T> LiteralArgumentBuilder<CommandSource> literal(final String name) {
+        return LiteralArgumentBuilder.literal(name);
+    }
+
+    public final void registerTo(CommandDispatcher<CommandSource> dispatcher) {
+        LiteralArgumentBuilder<CommandSource> builder = LiteralArgumentBuilder.literal(this.name);
+        this.build(builder);
+        dispatcher.register(builder);
+    }
+
+    public abstract void build(LiteralArgumentBuilder<CommandSource> builder);
 }
