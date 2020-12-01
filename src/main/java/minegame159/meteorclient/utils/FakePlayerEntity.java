aea7dca899a869cf5a264d832df937a439420654
seasnail8169 <seasnail8169@gmail.com>
2020-12-01 18:53:32 +0000

    Initial fakeplayer (visual bugs).

---
+++ src/main/java/minegame159/meteorclient/utils/FakePlayerEntity.java
@@ -0,0 +1,58 @@
+package minegame159.meteorclient.utils;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.OtherClientPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.data.DataTracker;
+
+public class FakePlayerEntity extends OtherClientPlayerEntity {
+    private static final MinecraftClient mc = MinecraftClient.getInstance();
+    private final ClientPlayerEntity player = mc.player;
+    private final ClientWorld world = mc.world;
+
+    public FakePlayerEntity() {
+        super(mc.world, mc.player.getGameProfile());
+        copyPositionAndRotation(player);
+
+        copyInventory();
+        copyPlayerModel(player, this);
+        copyRotation();
+        resetCapeMovement();
+
+        spawn();
+    }
+
+    private void copyInventory() {
+        inventory.clone(player.inventory);
+    }
+
+    private void copyPlayerModel(Entity from, Entity to) {
+        DataTracker fromTracker = from.getDataTracker();
+        DataTracker toTracker = to.getDataTracker();
+        Byte playerModel = fromTracker.get(PlayerEntity.PLAYER_MODEL_PARTS);
+        toTracker.set(PlayerEntity.PLAYER_MODEL_PARTS, playerModel);
+    }
+
+    private void copyRotation() {
+        headYaw = player.headYaw;
+        bodyYaw = player.bodyYaw;
+    }
+
+    private void resetCapeMovement() {
+        capeX = getX();
+        capeY = getY();
+        capeZ = getZ();
+    }
+
+    private void spawn() {
+        world.addEntity(getEntityId(), this);
+    }
+
+    public void despawn() {
+        removed = true;
+    }
+}
