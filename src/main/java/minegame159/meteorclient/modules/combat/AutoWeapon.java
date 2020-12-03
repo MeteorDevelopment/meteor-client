cbfb9e1a5b5190c7297405fc6003895d74084015
Columner <58351403+Columner@users.noreply.github.com>
2020-12-03 18:17:19 +1100

    Update AutoWeapon.java

--- src/main/java/minegame159/meteorclient/modules/combat/AutoWeapon.java
+++ src/main/java/minegame159/meteorclient/modules/combat/AutoWeapon.java
@@ -25,28 +25,28 @@ public class AutoWeapon extends ToggleModule {
     }
 
     public AutoWeapon(){
-        super(Category.Combat, "auto-weapon", "Finds the best weapon in your hotbar.");
+        super(Category.Combat, "auto-weapon", "Searches for the best weapon in your hotbar.");
     }
 
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
     private final Setting<Weapon> weapon = sgGeneral.add(new EnumSetting.Builder<Weapon>()
             .name("Weapon")
-            .description("Which weapon to use for AutoWeapon")
+            .description("Which weapon to use for Auto Weapon.")
             .defaultValue(Weapon.Sword)
             .build()
     );
 
     private final Setting<Integer> threshold = sgGeneral.add(new IntSetting.Builder()
             .name("threshold")
-            .description("If the non-prefered weapon does this much damage more then it will chose that over the prefered")
+            .description("If the non-preferred weapon does this much damage more then it will choose that weapon over the preffered weapon.")
             .defaultValue(4)
             .build()
     );
 
     private final Setting<Boolean> antiBreak = sgGeneral.add(new BoolSetting.Builder()
             .name("anti-break")
-            .description("Stops you from breaking your weapon.")
+            .description("Prevents you from breaking your weapon.")
             .defaultValue(false)
             .build()
     );
