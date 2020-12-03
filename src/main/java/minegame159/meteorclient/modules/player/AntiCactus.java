675b45b075a494e2b793142404fc7622a81d8cd1
Columner <58351403+Columner@users.noreply.github.com>
2020-12-03 18:50:30 +1100

    Update AntiCactus.java

--- src/main/java/minegame159/meteorclient/modules/player/AntiCactus.java
+++ src/main/java/minegame159/meteorclient/modules/player/AntiCactus.java
@@ -10,6 +10,6 @@ import minegame159.meteorclient.modules.ToggleModule;
 
 public class AntiCactus extends ToggleModule {
     public AntiCactus() {
-        super(Category.Player, "anti-cactus", "Stops cactus damage.");
+        super(Category.Player, "anti-cactus", "Blocks cactus damage.");
     }
 }
