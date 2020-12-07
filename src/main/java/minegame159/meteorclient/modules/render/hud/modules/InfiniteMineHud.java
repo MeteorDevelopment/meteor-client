10625d557b356cc2511458aa48192a0388ba6251
MineGame159 <petulko08@gmail.com>
2020-12-07 17:14:18 +0100

    Added better block selecter gui to InfinityMiner

--- src/main/java/minegame159/meteorclient/modules/render/hud/modules/InfiniteMineHud.java
+++ src/main/java/minegame159/meteorclient/modules/render/hud/modules/InfiniteMineHud.java
@@ -3,6 +3,7 @@ package minegame159.meteorclient.modules.render.hud.modules;
 import minegame159.meteorclient.modules.ModuleManager;
 import minegame159.meteorclient.modules.player.InfinityMiner;
 import minegame159.meteorclient.modules.render.hud.HUD;
+import net.minecraft.block.Block;
 
 import java.util.Arrays;
 
@@ -15,22 +16,19 @@ public class InfiniteMineHud extends DoubleTextHudModule {
     protected String getRight() {
         InfinityMiner infinityMiner = ModuleManager.INSTANCE.get(InfinityMiner.class);
         if (!infinityMiner.isActive()) return "Disabled";
-        else if (InfinityMiner.Mode.HOME == infinityMiner.getMode()) {
-            int[] coords = infinityMiner.getHomeCoords();
-            return "Heading Home: " + coords[0] + " " + coords[1] + " " + coords[2];
-        } else if (InfinityMiner.Mode.TARGET == infinityMiner.getMode()) {
-            return "Mining: " + formatTargetBlock(infinityMiner.getCurrentTarget());
-        } else if (InfinityMiner.Mode.REPAIR == infinityMiner.getMode()) {
-            return "Repair-Mining: " + formatTargetBlock(infinityMiner.getCurrentTarget());
-        } else {
-            return "Resting";
-        }
-    }
 
-    private String formatTargetBlock(String target) {
-        StringBuilder formattedTarget = new StringBuilder();
-        for (String token : target.split("_"))
-            formattedTarget.append(token.substring(0, 1).toUpperCase()).append(token.substring(1)).append(" ");
-        return formattedTarget.toString();
+        switch (infinityMiner.getMode()) {
+            case HOME:
+                int[] coords = infinityMiner.getHomeCoords();
+                return "Heading Home: " + coords[0] + " " + coords[1] + " " + coords[2];
+            case TARGET:
+                return "Mining: " + infinityMiner.getCurrentTarget().getName().getString();
+            case REPAIR:
+                return "Repair-Mining: " + infinityMiner.getCurrentTarget().getName().getString();
+            case STILL:
+                return "Resting";
+            default:
+                return "";
+        }
     }
 }
