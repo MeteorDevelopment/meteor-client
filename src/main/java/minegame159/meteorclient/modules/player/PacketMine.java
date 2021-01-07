e07571a8a27361862c319bae3d668c4d0066bf84
seasnail8169 <seasnail8169@gmail.com>
2021-01-07 01:31:12 +0000

    Added rendering to PacketMine

--- src/main/java/minegame159/meteorclient/modules/player/PacketMine.java
+++ src/main/java/minegame159/meteorclient/modules/player/PacketMine.java
@@ -8,14 +8,16 @@ package minegame159.meteorclient.modules.player;
 import me.zero.alpine.listener.EventHandler;
 import me.zero.alpine.listener.Listener;
 import minegame159.meteorclient.events.entity.player.StartBreakingBlockEvent;
+import minegame159.meteorclient.events.render.RenderEvent;
 import minegame159.meteorclient.events.world.PostTickEvent;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.Module;
-import minegame159.meteorclient.settings.BoolSetting;
-import minegame159.meteorclient.settings.Setting;
-import minegame159.meteorclient.settings.SettingGroup;
+import minegame159.meteorclient.rendering.Renderer;
+import minegame159.meteorclient.rendering.ShapeMode;
+import minegame159.meteorclient.settings.*;
 import minegame159.meteorclient.utils.Utils;
 import minegame159.meteorclient.utils.misc.Pool;
+import minegame159.meteorclient.utils.render.color.SettingColor;
 import net.minecraft.block.Blocks;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.util.math.BlockPos;
@@ -26,11 +28,40 @@ import java.util.List;
 
 public class PacketMine extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final SettingGroup sgRender = settings.createGroup("Render");
 
     private final Setting<Boolean> oneByOne = sgGeneral.add(new BoolSetting.Builder()
             .name("one-by-one")
             .description("Mines blocks one by one.")
             .defaultValue(true)
+            .build()
+    );
+
+    private final Setting<Boolean> render = sgRender.add(new BoolSetting.Builder()
+            .name("render")
+            .description("Whether to render blocks being mined.")
+            .defaultValue(true)
+            .build()
+    );
+
+    private final Setting<ShapeMode> shapeMode = sgRender.add(new EnumSetting.Builder<ShapeMode>()
+            .name("shape-mode")
+            .description("How the shapes are rendered.")
+            .defaultValue(ShapeMode.Both)
+            .build()
+    );
+
+    private final Setting<SettingColor> sideColor = sgRender.add(new ColorSetting.Builder()
+            .name("side-color")
+            .description("The color of the sides of the blocks being rendered.")
+            .defaultValue(new SettingColor(204, 0, 0, 10))
+            .build()
+    );
+
+    private final Setting<SettingColor> lineColor = sgRender.add(new ColorSetting.Builder()
+            .name("line-color")
+            .description("The color of the lines of the blocks being rendered.")
+            .defaultValue(new SettingColor(204, 0, 0, 255))
             .build()
     );
 
@@ -52,35 +83,39 @@ public class PacketMine extends Module {
         if (mc.world.getBlockState(event.blockPos).getHardness(mc.world, event.blockPos) < 0) return;
 
         Block block = blockPool.get();
-        block.blockPos = event.blockPos;
-        block.direction = event.direction;
-        blocks.add(block);
+        if (!blocks.contains(block)) {
+            block.blockPos = event.blockPos;
+            block.direction = event.direction;
+            blocks.add(block);
+        }
 
         event.cancel();
     });
 
     @EventHandler
     private final Listener<PostTickEvent> onTick = new Listener<>(event -> {
-        if (oneByOne.get()) {
-            if (blocks.size() > 0 && blocks.get(blocks.size() - 1).sendPackets()) {
-                blocks.remove(blocks.size() - 1);
-            }
-        } else {
-            blocks.removeIf(Block::sendPackets);
-        }
+        if (oneByOne.get() && !blocks.isEmpty() && !blocks.get(blocks.size() - 1).mining()) blocks.remove(blocks.size() - 1);
+        else blocks.removeIf(block -> !block.mining());
+    });
+
+    @EventHandler
+    private final Listener<RenderEvent> onRender = new Listener<>(event -> {
+        if (!render.get()) return;
+        for (Block block : blocks) Renderer.boxWithLines(Renderer.NORMAL, Renderer.LINES, block.blockPos, sideColor.get(), lineColor.get(), shapeMode.get(), 0);
     });
 
     private class Block {
         public BlockPos blockPos;
         public Direction direction;
 
-        public boolean sendPackets() {
-            if (mc.world.getBlockState(blockPos).getBlock() == Blocks.AIR) return true;
-            if (Utils.distance(mc.player.getX() - 0.5, mc.player.getY() + mc.player.getEyeHeight(mc.player.getPose()), mc.player.getZ() - 0.5, blockPos.getX() + direction.getOffsetX(), blockPos.getY() + direction.getOffsetY(), blockPos.getZ() + direction.getOffsetZ()) > mc.interactionManager.getReachDistance()) return true;
+        public boolean mining() {
+            if (mc.world.getBlockState(blockPos).getBlock() == Blocks.AIR
+                    || Utils.distance(mc.player.getX() - 0.5, mc.player.getY() + mc.player.getEyeHeight(mc.player.getPose()), mc.player.getZ() - 0.5, blockPos.getX() + direction.getOffsetX(), blockPos.getY() + direction.getOffsetY(), blockPos.getZ() + direction.getOffsetZ()) > mc.interactionManager.getReachDistance())
+                return false;
 
             mc.getNetworkHandler().sendPacket(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.START_DESTROY_BLOCK, blockPos, direction));
             mc.getNetworkHandler().sendPacket(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.STOP_DESTROY_BLOCK, blockPos, direction));
-            return false;
+            return true;
         }
     }
 }
