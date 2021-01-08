35559999782494390043cecb049c36b0e92be46a
squidoodly <35968667+squidoodly@users.noreply.github.com>
2021-01-08 18:04:06 +0000

    Try to stop OffhandExtra getting stuck on the wrong item.

--- src/main/java/minegame159/meteorclient/modules/combat/OffhandExtra.java
+++ src/main/java/minegame159/meteorclient/modules/combat/OffhandExtra.java
@@ -126,6 +126,7 @@ public class OffhandExtra extends Module {
     @EventHandler
     private final Listener<PostTickEvent> onTick = new Listener<>(event -> {
         assert mc.player != null;
+        currentMode = mode.get();
 
         if (mc.currentScreen != null && ((!(mc.currentScreen instanceof InventoryScreen) && !(mc.currentScreen instanceof WidgetScreen)) || !asimov.get())) return;
         if (!mc.player.isUsingItem()) isClicking = false;
@@ -165,7 +166,6 @@ public class OffhandExtra extends Module {
             }
 
         }
-        if (!(mc.player.getMainHandStack().getItem() instanceof SwordItem) && !(mc.player.getMainHandStack().getItem() instanceof EnchantedGoldenAppleItem)) currentMode = mode.get();
     });
 
     @EventHandler
@@ -193,6 +193,7 @@ public class OffhandExtra extends Module {
                 doMove(result);
                 sentMessage = false;
             }
+            currentMode = mode.get();
         }
     });
 
