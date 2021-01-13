9f1f9667726492ec5c5bdd27bafffcbb450e7fa2
MineGame159 <petulko08@gmail.com>
2021-01-13 13:57:33 +0100

    Hitboxes now supports F3+B

--- src/main/java/minegame159/meteorclient/mixin/EntityRenderDispatcherMixin.java
+++ src/main/java/minegame159/meteorclient/mixin/EntityRenderDispatcherMixin.java
@@ -5,9 +5,12 @@
 
 package minegame159.meteorclient.mixin;
 
+import minegame159.meteorclient.mixininterface.IBox;
 import minegame159.meteorclient.modules.ModuleManager;
+import minegame159.meteorclient.modules.combat.Hitboxes;
 import minegame159.meteorclient.modules.render.Chams;
 import minegame159.meteorclient.modules.render.NoRender;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
@@ -16,11 +19,13 @@ import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.FallingBlockEntity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.util.math.Box;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(EntityRenderDispatcher.class)
 public class EntityRenderDispatcherMixin {
@@ -59,5 +64,11 @@ public class EntityRenderDispatcherMixin {
 //            GL11.glPolygonOffset(1.0f, -1000000.0f);
             GL11.glDepthRange(0.0, 1.0);
         }
+    }
+
+    @Inject(method = "drawBox", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;drawBox(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;Lnet/minecraft/util/math/Box;FFFF)V"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void onDrawBox(MatrixStack matrix, VertexConsumer vertices, Entity entity, float red, float green, float blue, CallbackInfo info, Box box) {
+        double v = ModuleManager.INSTANCE.get(Hitboxes.class).getEntityValue(entity);
+        if (v != 0) ((IBox) box).expand(v);
     }
 }
