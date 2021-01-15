c60846ea746939dc1880ca178d66318e4ff28d25
Clashbestie <57112209+Clashbestie@users.noreply.github.com>
2021-01-15 10:08:28 +0100

    Autoreconnect Fix

--- src/main/java/minegame159/meteorclient/mixin/DisconnectedScreenMixin.java
+++ src/main/java/minegame159/meteorclient/mixin/DisconnectedScreenMixin.java
@@ -4,35 +4,51 @@
  */
 
 package minegame159.meteorclient.mixin;
-
-import minegame159.meteorclient.mixininterface.IDisconnectedScreen;
-import net.minecraft.client.gui.screen.DisconnectedScreen;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.Text;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import minegame159.meteorclient.mixininterface.IAbstractButtonWidget;
+import minegame159.meteorclient.modules.misc.AutoReconnect;
+import minegame159.meteorclient.utils.Utils;
+import net.minecraft.client.gui.screen.ConnectScreen;
+import net.minecraft.client.gui.screen.DisconnectedScreen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.LiteralText;
 
 @Mixin(DisconnectedScreen.class)
 public class DisconnectedScreenMixin implements IDisconnectedScreen {
-    @Shadow @Final private Screen parent;
 
-    @Shadow @Final private Text reason;
+    @Shadow 
+    private int reasonHeight;
+    
+    private ButtonWidget reconnectBtn;
+	private boolean timerActive = true;
+	private double time = AutoReconnect.INSTANCE.time.get() * 20;
+    
+    @Inject(method = "init", at = @At("HEAD"))
+	private void onRenderBackground(CallbackInfo info) {
+		reconnectBtn = super.addButton(new ButtonWidget(width / 2 - 100, height / 2 + reasonHeight / 2 + 9 + 30, 200,
+				20, new LiteralText("Reconnecting in " + time / 20f), button -> timerActive = !timerActive));
+		timerActive = AutoReconnect.INSTANCE.isActive();
+	}
 
-    @Shadow private int reasonHeight;
+	@Override
+	public void tick() {
+		if (timerActive) {
+			time--;
+			if (time <= 0) {
+				Utils.mc.openScreen(new ConnectScreen(new MultiplayerScreen(new TitleScreen()), Utils.mc,
+						AutoReconnect.INSTANCE.lastServerInfo));
 
-    @Override
-    public Screen getParent() {
-        return parent;
-    }
-
-    @Override
-    public Text getReason() {
-        return reason;
-    }
-
-    @Override
-    public int getReasonHeight() {
-        return reasonHeight;
-    }
+			} else {
+				((IAbstractButtonWidget) reconnectBtn)
+						.setText(new LiteralText(String.format("Reconnecting in %.1f", time / 20f)));
+			}
+		}
+	}
 }
