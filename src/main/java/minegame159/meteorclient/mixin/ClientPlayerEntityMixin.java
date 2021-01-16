a2a6ed3f73a633494e9024c4baf5c642fe94d96b
dakone22 <35692622+dakone22@users.noreply.github.com>
2021-01-16 15:38:10 +0300

    Made SendMessageEvent cancelable

--- src/main/java/minegame159/meteorclient/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/minegame159/meteorclient/mixin/ClientPlayerEntityMixin.java
@@ -45,9 +45,11 @@ public abstract class ClientPlayerEntityMixin {
             SendMessageEvent event = SendMessageEvent.get(msg);
             MeteorClient.EVENT_BUS.post(event);
 
-            ignoreChatMessage = true;
-            sendChatMessage(event.msg);
-            ignoreChatMessage = false;
+            if (!event.isCancelled()) {
+                ignoreChatMessage = true;
+                sendChatMessage(event.msg);
+                ignoreChatMessage = false;
+            }
 
             info.cancel();
             return;
