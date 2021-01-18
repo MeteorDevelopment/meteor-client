f75a7c11c0a4ce088e4f138b6055b5e521da2af4
supakeks <thiaguitoszwarc@gmail.com>
2021-01-18 16:08:11 -0500

    Refactored AirPlace to CrosshairPlace.

--- src/main/java/minegame159/meteorclient/modules/ModuleManager.java
+++ src/main/java/minegame159/meteorclient/modules/ModuleManager.java
@@ -342,7 +342,7 @@ public class ModuleManager extends Savable<ModuleManager> implements Listenable 
         addModule(new NoInteract());
         addModule(new NoRotate());
         addModule(new Trail());
-        addModule(new AirPlace());
+        addModule(new CrosshairPlace());
     }
 
     private void initMovement() {
