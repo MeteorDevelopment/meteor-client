33e290343f6b2930786c38173cefadd661e83b06
seasnail <seasnail8169@gmail.com>
2021-01-20 22:13:46 +0000

    Cleaned up gilded's mess (not sexual i promise)

--- src/main/java/minegame159/meteorclient/modules/render/hud/modules/RotationHud.java
+++ src/main/java/minegame159/meteorclient/modules/render/hud/modules/RotationHud.java
@@ -1,19 +1,48 @@
 /*
  * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
- * Copyright (c) 2020 Meteor Development.
+ * Copyright (c) 2021 Meteor Development.
  */
 
 package minegame159.meteorclient.modules.render.hud.modules;
 
 import minegame159.meteorclient.modules.render.hud.HUD;
-import net.minecraft.client.MinecraftClient;
 import org.apache.commons.lang3.StringUtils;
+
+// Credits to snail for helping, Finished in 3 am ez. This was in total 20 hours of work. subscribe to pewdipie. Meteor on top ez
+//Gilded moment ez
 
 public class RotationHud extends DoubleTextHudModule {
     public RotationHud(HUD hud) {
         super(hud, "rotation", "Displays your rotation.", "invalid ");
     }
- private enum Direction {
+
+    @Override
+    protected String getRight() {
+        Direction playerDirection;
+        float yaw = mc.gameRenderer.getCamera().getYaw() % 360;
+        float pitch = mc.gameRenderer.getCamera().getPitch() % 360;
+
+        if ((yaw >= 0 && yaw < 45) || (yaw <= 0 && yaw > -45)) playerDirection = Direction.South;
+        else if ((yaw >= 45 && yaw < 90) || (yaw <= -45 && yaw > -90)) playerDirection = Direction.SouthWest;
+        else if ((yaw >= 90 && yaw < 135) || (yaw <= -90 && yaw > -135)) playerDirection = Direction.West;
+        else if ((yaw >= 135 && yaw < 180) || (yaw <= -135 && yaw > -180)) playerDirection = Direction.NorthWest;
+        else if ((yaw >= 180 && yaw < 225) || (yaw <= -180 && yaw > -225)) playerDirection = Direction.North;
+        else if ((yaw >= 225 && yaw < 270) || (yaw <= -225 && yaw > -270)) playerDirection = Direction.NorthEast;
+        else if ((yaw >= 270 && yaw < 315) || (yaw <= -270 && yaw > -315)) playerDirection = Direction.East;
+        else if ((yaw >= 315 && yaw < 360) || (yaw <= -315 && yaw > -360)) playerDirection = Direction.SouthEast;
+        else playerDirection = Direction.Null;
+
+        if (yaw < 0) yaw += 360;
+        if (yaw > 180) yaw -= 360;
+
+        if (pitch < 0) pitch += 360;
+        if (pitch > 180) pitch -= 360;
+
+        setLeft(String.format("%s %s ", StringUtils.capitalize(playerDirection.name()), playerDirection.getAxis()));
+        return String.format("(%.1f, %.1f)", yaw, pitch);
+    }
+
+    private enum Direction {
         South("Z+"),
         SouthEast("X+ Z+"),
         West("X-"),
@@ -22,57 +51,16 @@ public class RotationHud extends DoubleTextHudModule {
         NorthEast("X+ Z-"),
         East("X+"),
         SouthWest("Z+ X-"),
-        Omniscient("All"),
-
         Null("NaN");
 
-        String alternate;
+        String axis;
 
-        Direction(String alternate) {
-            this.alternate = alternate;
+        Direction(String axis) {
+            this.axis = axis;
         }
 
-        public String getAlternate() {
-            return alternate;
+        public String getAxis() {
+            return axis;
         }
-    }
-// Credits to snail for helping, Finished in 3 am ez. This was in total 20 hours of work. subscribe to pewdipie. Meteor on top ez
-    @Override
-    protected String getRight() {
-        MinecraftClient mc = MinecraftClient.getInstance();
-
-        float determiner = mc.gameRenderer.getCamera().getYaw() % 360;
-
-
-        Direction playerDirection = Direction.Null;
-
-        if (determiner >= 0 && determiner < 45) playerDirection = Direction.South;
-        if (determiner >= 45 && determiner < 90) playerDirection = Direction.SouthWest;
-        if (determiner >= 90 && determiner < 135) playerDirection = Direction.West;
-        if (determiner >= 135 && determiner < 180) playerDirection = Direction.NorthWest;
-        if (determiner >= 180 && determiner < 225) playerDirection = Direction.North;
-        if (determiner >= 225 && determiner < 270) playerDirection = Direction.NorthEast;
-        if (determiner >= 270 && determiner < 315) playerDirection = Direction.East;
-        if (determiner >= 315 && determiner < 360) playerDirection = Direction.SouthEast;
-
-         if (determiner <= 0 && determiner > -45) playerDirection = Direction.South;
-         if (determiner <= -45 && determiner > -90) playerDirection = Direction.SouthEast;
-         if (determiner <= -90 && determiner > -135) playerDirection = Direction.East;
-         if (determiner <= -135 && determiner > -180) playerDirection = Direction.NorthEast;
-         if (determiner <= -180 && determiner > -225) playerDirection = Direction.North;
-         if (determiner <= -225 && determiner > -270) playerDirection = Direction.NorthWest;
-         if (determiner <= -270 && determiner > -315) playerDirection = Direction.West;
-         if (determiner <= -315 && determiner > -360) playerDirection = Direction.SouthWest;
-
-        float yaw = mc.gameRenderer.getCamera().getYaw() % 360;
-        if (yaw < 0) yaw += 360;
-        if (yaw > 180) yaw -= 360;
-
-        float pitch = mc.gameRenderer.getCamera().getPitch() % 360;
-        if (pitch < 0) pitch += 360;
-        if (pitch > 180) pitch -= 360;
-
-        setLeft(String.format("%s %s ", StringUtils.capitalize(playerDirection.name()), playerDirection.getAlternate()));
-        return String.format("(%.1f, %.1f)", yaw, pitch);
     }
 }
