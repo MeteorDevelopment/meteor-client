570189d5019af6c900c3d92ae459dd6b51fd499b
dakone22 <35692622+dakone22@users.noreply.github.com>
2021-01-22 22:10:22 +0300

    Replaced \t (tabs) to 4 spaces in source code

--- src/main/java/minegame159/meteorclient/mixin/DisconnectedScreenMixin.java
+++ src/main/java/minegame159/meteorclient/mixin/DisconnectedScreenMixin.java
@@ -5,12 +5,6 @@
 
 package minegame159.meteorclient.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import minegame159.meteorclient.mixininterface.IAbstractButtonWidget;
 import minegame159.meteorclient.modules.ModuleManager;
 import minegame159.meteorclient.modules.misc.AutoReconnect;
@@ -21,36 +15,41 @@ import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.LiteralText;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(DisconnectedScreen.class)
 public class DisconnectedScreenMixin extends ScreenMixin {
 
-	@Shadow
-	private int reasonHeight;
+    @Shadow
+    private int reasonHeight;
 
-	private ButtonWidget reconnectBtn;
-	private boolean timerActive = true;
-	private double time = ((AutoReconnect) ModuleManager.INSTANCE.get(AutoReconnect.class)).time.get() * 20;
+    private ButtonWidget reconnectBtn;
+    private boolean timerActive = true;
+    private double time = ((AutoReconnect) ModuleManager.INSTANCE.get(AutoReconnect.class)).time.get() * 20;
 
-	@Inject(method = "init", at = @At("HEAD"))
-	private void onRenderBackground(CallbackInfo info) {
-		reconnectBtn = super.addButton(new ButtonWidget(width / 2 - 100, height / 2 + reasonHeight / 2 + 9 + 30, 200,
-				20, new LiteralText("Reconnecting in " + time / 20f), button -> timerActive = !timerActive));
-		timerActive = ModuleManager.INSTANCE.isActive(AutoReconnect.class);
-	}
+    @Inject(method = "init", at = @At("HEAD"))
+    private void onRenderBackground(CallbackInfo info) {
+        reconnectBtn = super.addButton(new ButtonWidget(width / 2 - 100, height / 2 + reasonHeight / 2 + 9 + 30, 200,
+                20, new LiteralText("Reconnecting in " + time / 20f), button -> timerActive = !timerActive));
+        timerActive = ModuleManager.INSTANCE.isActive(AutoReconnect.class);
+    }
 
-	@Override
-	public void tick() {
-		if (timerActive) {
-			time--;
-			if (time <= 0) {
-				Utils.mc.openScreen(new ConnectScreen(new MultiplayerScreen(new TitleScreen()), Utils.mc,
-						((AutoReconnect) ModuleManager.INSTANCE.get(AutoReconnect.class)).lastServerInfo));
+    @Override
+    public void tick() {
+        if (timerActive) {
+            time--;
+            if (time <= 0) {
+                Utils.mc.openScreen(new ConnectScreen(new MultiplayerScreen(new TitleScreen()), Utils.mc,
+                        ((AutoReconnect) ModuleManager.INSTANCE.get(AutoReconnect.class)).lastServerInfo));
 
-			} else {
-				((IAbstractButtonWidget) reconnectBtn)
-						.setText(new LiteralText(String.format("Reconnecting in %.1f", time / 20f)));
-			}
-		}
-	}
+            } else {
+                ((IAbstractButtonWidget) reconnectBtn)
+                        .setText(new LiteralText(String.format("Reconnecting in %.1f", time / 20f)));
+            }
+        }
+    }
 }
--- src/main/java/minegame159/meteorclient/mixin/ScreenMixin.java
+++ src/main/java/minegame159/meteorclient/mixin/ScreenMixin.java
@@ -5,40 +5,39 @@
 
 package minegame159.meteorclient.mixin;
 
+import minegame159.meteorclient.modules.ModuleManager;
+import minegame159.meteorclient.modules.render.NoRender;
+import minegame159.meteorclient.utils.Utils;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.AbstractButtonWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import minegame159.meteorclient.modules.ModuleManager;
-import minegame159.meteorclient.modules.render.NoRender;
-import minegame159.meteorclient.utils.Utils;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.AbstractButtonWidget;
-
 @Mixin(Screen.class)
 public class ScreenMixin {
 
-	@Shadow
-	public int width;
+    @Shadow
+    public int width;
 
-	@Shadow
-	public int height;
+    @Shadow
+    public int height;
 
-	@Inject(method = "renderBackground(Lnet/minecraft/client/util/math/MatrixStack;)V", at = @At("HEAD"), cancellable = true)
-	private void onRenderBackground(CallbackInfo info) {
-		if (Utils.canUpdate() && ModuleManager.INSTANCE.get(NoRender.class).noGuiBackground())
-			info.cancel();
-	}
+    @Inject(method = "renderBackground(Lnet/minecraft/client/util/math/MatrixStack;)V", at = @At("HEAD"), cancellable = true)
+    private void onRenderBackground(CallbackInfo info) {
+        if (Utils.canUpdate() && ModuleManager.INSTANCE.get(NoRender.class).noGuiBackground())
+            info.cancel();
+    }
 
-	@Shadow
-	public <T extends AbstractButtonWidget> T addButton(final T button) {
-		return null;
-	}
+    @Shadow
+    public <T extends AbstractButtonWidget> T addButton(final T button) {
+        return null;
+    }
 
-	@Shadow
-	public void tick() {
+    @Shadow
+    public void tick() {
 
-	}
+    }
 }
