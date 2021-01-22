47ea228cd217b55873a600a96a1fe7c8bb91ebe7
MineGame159 <petulko08@gmail.com>
2021-01-22 17:31:44 +0100

    Fixed capes and online players ping

--- src/main/java/minegame159/meteorclient/mixin/TitleScreenMixin.java
+++ src/main/java/minegame159/meteorclient/mixin/TitleScreenMixin.java
@@ -80,7 +80,7 @@ public class TitleScreenMixin extends Screen {
             Utils.firstTimeTitleScreen = false;
             MeteorClient.LOG.info("Checking latest version of Meteor Client");
 
-            MeteorExecutor.execute(() -> HttpUtils.getLines("http://meteorclient.com/api/version", s -> {
+            MeteorExecutor.execute(() -> HttpUtils.getLines("http://meteorclient.com:8082/api/version", s -> {
                 Version latestVer = new Version(s);
                 if (latestVer.isHigherThan(Config.INSTANCE.version)) MinecraftClient.getInstance().openScreen(new NewUpdateScreen(latestVer));
             }));
--- src/main/java/minegame159/meteorclient/utils/network/OnlinePlayers.java
+++ src/main/java/minegame159/meteorclient/utils/network/OnlinePlayers.java
@@ -12,12 +12,12 @@ public class OnlinePlayers {
         long time = System.currentTimeMillis();
 
         if (time - lastPingTime > 5 * 60 * 1000) {
-            MeteorExecutor.execute(() -> HttpUtils.get("http://meteorclient.com/api/online/ping"));
+            MeteorExecutor.execute(() -> HttpUtils.get("http://meteorclient.com:8082/api/online/ping"));
             lastPingTime = time;
         }
     }
 
     public static void leave() {
-        MeteorExecutor.execute(() -> HttpUtils.get("http://meteorclient.com/api/online/leave"));
+        MeteorExecutor.execute(() -> HttpUtils.get("http://meteorclient.com:8082/api/online/leave"));
     }
 }
