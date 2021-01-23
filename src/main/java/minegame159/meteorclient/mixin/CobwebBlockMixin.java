492a88d5e711afba019f436e1d623655a1a2d775
notseanyay lmfao ezzzzz <69701622+notseanyay@users.noreply.github.com>
2021-01-22 23:35:11 -0600

    finish the last copyright changes

--- src/main/java/minegame159/meteorclient/mixin/CobwebBlockMixin.java
+++ src/main/java/minegame159/meteorclient/mixin/CobwebBlockMixin.java
@@ -1,8 +1,7 @@
 /*
- * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
- * Copyright (c) 2020 Meteor Development.
- */
-
+* This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
+* Copyright (c) 2021 Meteor Development.
+*/
 package minegame159.meteorclient.mixin;
 
 import minegame159.meteorclient.modules.ModuleManager;
