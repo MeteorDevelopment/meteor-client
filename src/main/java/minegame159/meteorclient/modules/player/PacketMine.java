3162a244d5608c6c122cb10abd642380259b2354
MineGame159 <petulko08@gmail.com>
2021-01-30 17:11:06 +0100

    Improved PacketMine and added delay setting so now it works on server with anti cheat

--- src/main/java/minegame159/meteorclient/modules/player/PacketMine.java
+++ src/main/java/minegame159/meteorclient/modules/player/PacketMine.java
@@ -19,7 +19,9 @@ import minegame159.meteorclient.utils.misc.Pool;
 import minegame159.meteorclient.utils.player.Rotations;
 import minegame159.meteorclient.utils.render.color.SettingColor;
 import net.minecraft.block.Block;
+import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 
@@ -31,6 +33,15 @@ public class PacketMine extends Module {
     private final SettingGroup sgRender = settings.createGroup("Render");
 
     // General
+
+    private final Setting<Integer> delay = sgGeneral.add(new IntSetting.Builder()
+            .name("delay")
+            .description("Delay between mining blocks in ticks.")
+            .defaultValue(1)
+            .min(0)
+            .sliderMax(10)
+            .build()
+    );
 
     private final Setting<Boolean> rotate = sgGeneral.add(new BoolSetting.Builder()
             .name("rotate")
@@ -105,7 +116,7 @@ public class PacketMine extends Module {
     }
 
     @EventHandler
-    private void onTick(TickEvent.Post event) {
+    private void onTick(TickEvent.Pre event) {
         blocks.removeIf(MyBlock::shouldRemove);
 
         if (!blocks.isEmpty()) blocks.get(0).mine();
@@ -122,17 +133,21 @@ public class PacketMine extends Module {
         public BlockPos blockPos;
         public Direction direction;
         public Block originalBlock;
+        public int timer;
         public boolean mining;
 
         public void set(StartBreakingBlockEvent event) {
             this.blockPos = event.blockPos;
             this.direction = event.direction;
             this.originalBlock = mc.world.getBlockState(blockPos).getBlock();
+            this.timer = delay.get();
             this.mining = false;
         }
 
         public boolean shouldRemove() {
-            return mc.world.getBlockState(blockPos).getBlock() != originalBlock || Utils.distance(mc.player.getX() - 0.5, mc.player.getY() + mc.player.getEyeHeight(mc.player.getPose()), mc.player.getZ() - 0.5, blockPos.getX() + direction.getOffsetX(), blockPos.getY() + direction.getOffsetY(), blockPos.getZ() + direction.getOffsetZ()) > mc.interactionManager.getReachDistance();
+            boolean remove = mc.world.getBlockState(blockPos).getBlock() != originalBlock || Utils.distance(mc.player.getX() - 0.5, mc.player.getY() + mc.player.getEyeHeight(mc.player.getPose()), mc.player.getZ() - 0.5, blockPos.getX() + direction.getOffsetX(), blockPos.getY() + direction.getOffsetY(), blockPos.getZ() + direction.getOffsetZ()) > mc.interactionManager.getReachDistance();
+            if (remove) mc.getNetworkHandler().sendPacket(new HandSwingC2SPacket(Hand.MAIN_HAND));
+            return remove;
         }
 
         public void mine() {
@@ -141,11 +156,16 @@ public class PacketMine extends Module {
         }
 
         private void sendMinePackets() {
-            if (!mining) {
-                mc.getNetworkHandler().sendPacket(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.START_DESTROY_BLOCK, blockPos, direction));
-                mc.getNetworkHandler().sendPacket(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.STOP_DESTROY_BLOCK, blockPos, direction));
+            if (timer <= 0) {
+                if (!mining) {
+                    mc.getNetworkHandler().sendPacket(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.START_DESTROY_BLOCK, blockPos, direction));
+                    mc.getNetworkHandler().sendPacket(new PlayerActionC2SPacket(PlayerActionC2SPacket.Action.STOP_DESTROY_BLOCK, blockPos, direction));
 
-                mining = true;
+                    mining = true;
+                }
+            }
+            else {
+                timer--;
             }
         }
 
