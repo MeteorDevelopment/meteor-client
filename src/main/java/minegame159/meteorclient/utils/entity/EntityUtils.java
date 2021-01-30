82ae1d88c7ac76683490583460658703ea1648a3
seasnail8169 <seasnail8169@gmail.com>
2021-01-30 16:46:27 +0000

    New entity targeting utils

--- src/main/java/minegame159/meteorclient/utils/entity/EntityUtils.java
+++ src/main/java/minegame159/meteorclient/utils/entity/EntityUtils.java
@@ -12,44 +12,16 @@ import minegame159.meteorclient.utils.render.color.Color;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.decoration.EndCrystalEntity;
-import net.minecraft.entity.mob.AmbientEntity;
-import net.minecraft.entity.mob.Monster;
-import net.minecraft.entity.mob.WaterCreatureEntity;
-import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.entity.passive.GolemEntity;
-import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.vehicle.AbstractMinecartEntity;
-import net.minecraft.entity.vehicle.BoatEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
 
 public class EntityUtils {
     public static MinecraftClient mc;
-
-    public static boolean isPlayer(Entity entity) {
-        return entity instanceof PlayerEntity;
-    }
-
-    public static boolean isAnimal(Entity entity) {
-        return entity instanceof AnimalEntity || entity instanceof AmbientEntity || entity instanceof WaterCreatureEntity || entity instanceof GolemEntity || entity instanceof VillagerEntity;
-    }
-
-    public static boolean isMob(Entity entity) {
-        return entity instanceof Monster;
-    }
-
-    public static boolean isItem(Entity entity) {
-        return entity instanceof ItemEntity;
-    }
-
-    public static boolean isCrystal(Entity entity) {
-        return entity instanceof EndCrystalEntity;
-    }
-
-    public static boolean isVehicle(Entity entity) {
-        return entity instanceof BoatEntity || entity instanceof AbstractMinecartEntity;
-    }
+    public static List<Entity> entities = new ArrayList<>();
 
     public static boolean isAttackable(EntityType<?> type) {
         return type != EntityType.AREA_EFFECT_CLOUD && type != EntityType.ARROW && type != EntityType.FALLING_BLOCK && type != EntityType.FIREWORK_ROCKET && type != EntityType.ITEM && type != EntityType.LLAMA_SPIT && type != EntityType.SPECTRAL_ARROW && type != EntityType.ENDER_PEARL && type != EntityType.EXPERIENCE_BOTTLE && type != EntityType.POTION && type != EntityType.TRIDENT && type != EntityType.LIGHTNING_BOLT && type != EntityType.FISHING_BOBBER && type != EntityType.EXPERIENCE_ORB && type != EntityType.EGG;
@@ -73,5 +45,52 @@ public class EntityUtils {
         }
 
         return Utils.WHITE;
+    }
+
+    public static Entity get(Predicate<Entity> isGood, SortPriority sortPriority) {
+        for (Entity entity : mc.world.getEntities()) {
+            if (isGood.test(entity)) entities.add(entity);
+        }
+
+        for (Entity entity : FakePlayerUtils.getPlayers().keySet()) {
+            if (isGood.test(entity)) entities.add(entity);
+        }
+
+        entities.sort((e1, e2) -> sort(e1, e2, sortPriority));
+
+        if (!entities.isEmpty()) {
+            Entity res = entities.get(0);
+
+            entities.clear();
+            return res;
+        }
+
+        return null;
+    }
+
+    private static int sort(Entity e1, Entity e2, SortPriority priority) {
+        switch (priority) {
+            case LowestDistance:  return Double.compare(e1.distanceTo(mc.player), e2.distanceTo(mc.player));
+            case HighestDistance: return invertSort(Double.compare(e1.distanceTo(mc.player), e2.distanceTo(mc.player)));
+            case LowestHealth:    return sortHealth(e1, e2);
+            case HighestHealth:   return invertSort(sortHealth(e1, e2));
+            default:              return 0;
+        }
+    }
+
+    private static int sortHealth(Entity e1, Entity e2) {
+        boolean e1l = e1 instanceof LivingEntity;
+        boolean e2l = e2 instanceof LivingEntity;
+
+        if (!e1l && !e2l) return 0;
+        else if (e1l && !e2l) return 1;
+        else if (!e1l) return -1;
+
+        return Float.compare(((LivingEntity) e1).getHealth(), ((LivingEntity) e2).getHealth());
+    }
+
+    private static int invertSort(int sort) {
+        if (sort == 0) return 0;
+        return sort > 0 ? -1 : 1;
     }
 }
