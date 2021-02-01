72b8d3c1597dab2e5b4ae5d14d9003fcf8b07e1e
MineGame159 <petulko08@gmail.com>
2021-02-01 12:00:56 +0100

    Fixed rotation hud

--- src/main/java/minegame159/meteorclient/modules/render/hud/modules/RotationHud.java
+++ src/main/java/minegame159/meteorclient/modules/render/hud/modules/RotationHud.java
@@ -6,61 +6,59 @@
 package minegame159.meteorclient.modules.render.hud.modules;
 
 import minegame159.meteorclient.modules.render.hud.HUD;
-import org.apache.commons.lang3.StringUtils;
 
 // Credits to snail for helping, Finished in 3 am ez. This was in total 20 hours of work. subscribe to pewdipie. Meteor on top ez
-//Gilded moment ez
+// Gilded moment ez
+// Gilded did monky code so I fixed :coool: - MineGame159
 
 public class RotationHud extends DoubleTextHudModule {
     public RotationHud(HUD hud) {
-        super(hud, "rotation", "Displays your rotation.", "invalid ");
+        super(hud, "rotation", "Displays your rotation.", "");
     }
 
     @Override
     protected String getRight() {
-        Direction playerDirection;
+        Direction dir;
         float yaw = mc.gameRenderer.getCamera().getYaw() % 360;
         float pitch = mc.gameRenderer.getCamera().getPitch() % 360;
 
-        if ((yaw >= 0 && yaw < 45) || (yaw <= 0 && yaw > -45)) playerDirection = Direction.South;
-        else if ((yaw >= 45 && yaw < 90) || (yaw <= -45 && yaw > -90)) playerDirection = Direction.SouthWest;
-        else if ((yaw >= 90 && yaw < 135) || (yaw <= -90 && yaw > -135)) playerDirection = Direction.West;
-        else if ((yaw >= 135 && yaw < 180) || (yaw <= -135 && yaw > -180)) playerDirection = Direction.NorthWest;
-        else if ((yaw >= 180 && yaw < 225) || (yaw <= -180 && yaw > -225)) playerDirection = Direction.North;
-        else if ((yaw >= 225 && yaw < 270) || (yaw <= -225 && yaw > -270)) playerDirection = Direction.NorthEast;
-        else if ((yaw >= 270 && yaw < 315) || (yaw <= -270 && yaw > -315)) playerDirection = Direction.East;
-        else if ((yaw >= 315 && yaw < 360) || (yaw <= -315 && yaw > -360)) playerDirection = Direction.SouthEast;
-        else playerDirection = Direction.Null;
-
         if (yaw < 0) yaw += 360;
-        if (yaw > 180) yaw -= 360;
-
         if (pitch < 0) pitch += 360;
+
+        if (yaw >= 337.5 || yaw < 22.5) dir = Direction.South;
+        else if (yaw >= 22.5 && yaw < 67.5) dir = Direction.SouthWest;
+        else if (yaw >= 67.5 && yaw < 112.5) dir = Direction.West;
+        else if (yaw >= 112.5 && yaw < 157.5) dir = Direction.NorthWest;
+        else if (yaw >= 157.5 && yaw < 202.5) dir = Direction.North;
+        else if (yaw >= 202.5 && yaw < 247.5) dir = Direction.NorthEast;
+        else if (yaw >= 247.5 && yaw < 292.5) dir = Direction.East;
+        else if (yaw >= 292.5 && yaw < 337.5) dir = Direction.SouthEast;
+        else dir = Direction.NaN;
+
+        if (yaw > 180) yaw -= 360;
         if (pitch > 180) pitch -= 360;
 
-        setLeft(String.format("%s %s ", StringUtils.capitalize(playerDirection.name()), playerDirection.getAxis()));
+        setLeft(String.format("%s %s ", dir.name, dir.axis));
         return String.format("(%.1f, %.1f)", yaw, pitch);
     }
 
     private enum Direction {
-        South("Z+"),
-        SouthEast("X+ Z+"),
-        West("X-"),
-        NorthWest("X- Z-"),
-        North("Z-"),
-        NorthEast("X+ Z-"),
-        East("X+"),
-        SouthWest("Z+ X-"),
-        Null("NaN");
+        South("South", "Z+"),
+        SouthEast("South East", "Z+ X+"),
+        West("West", "X-"),
+        NorthWest("North West", "Z- X-"),
+        North("North", "Z-"),
+        NorthEast("North East", "Z- X+"),
+        East("East", "X+"),
+        SouthWest("South West", "Z+ X-"),
+        NaN("NaN", "NaN");
 
-        String axis;
+        public String name;
+        public String axis;
 
-        Direction(String axis) {
+        Direction(String name, String axis) {
             this.axis = axis;
-        }
-
-        public String getAxis() {
-            return axis;
+            this.name = name;
         }
     }
 }
