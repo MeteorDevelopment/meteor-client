c0aa3d4283abcbdddb5f613145edb5b83a169537
seasnail8169 <seasnail8169@gmail.com>
2021-02-04 16:02:49 +0000

    AnchorAura improvements

--- src/main/java/minegame159/meteorclient/modules/combat/AnchorAura.java
+++ src/main/java/minegame159/meteorclient/modules/combat/AnchorAura.java
@@ -7,41 +7,32 @@ package minegame159.meteorclient.modules.combat;
 
 //Created by squidoodly 03/08/2020
 // Official Squidoodly Watermark!
+//snale
 
-import com.google.common.collect.Streams;
 import meteordevelopment.orbit.EventHandler;
 import minegame159.meteorclient.events.render.RenderEvent;
 import minegame159.meteorclient.events.world.TickEvent;
-import minegame159.meteorclient.friends.Friends;
 import minegame159.meteorclient.modules.Category;
 import minegame159.meteorclient.modules.Module;
 import minegame159.meteorclient.rendering.Renderer;
 import minegame159.meteorclient.rendering.ShapeMode;
 import minegame159.meteorclient.settings.*;
+import minegame159.meteorclient.utils.Utils;
+import minegame159.meteorclient.utils.entity.EntityUtils;
+import minegame159.meteorclient.utils.entity.SortPriority;
 import minegame159.meteorclient.utils.player.*;
 import minegame159.meteorclient.utils.render.color.SettingColor;
 import minegame159.meteorclient.utils.world.BlockUtils;
 import net.minecraft.block.Blocks;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
-import net.minecraft.item.PotionItem;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 
-import java.util.Comparator;
-import java.util.Optional;
-
 public class AnchorAura extends Module {
-
-    public enum Mode {
-        Safe,
-        Suicide
-    }
-
     public enum PlaceMode {
         Above,
         AboveAndBelow,
@@ -65,23 +56,23 @@ public class AnchorAura extends Module {
 
     private final Setting<Integer> placeDelay = sgPlace.add(new IntSetting.Builder()
             .name("place-delay")
-            .description("The amount of delay in ticks for placement.")
+            .description("The tick delay between placing anchors.")
             .defaultValue(2)
             .min(0)
             .max(20)
             .build()
     );
 
-    private final Setting<Mode> placeMode = sgPlace.add(new EnumSetting.Builder<Mode>()
+    private final Setting<Safety> placeMode = sgPlace.add(new EnumSetting.Builder<Safety>()
             .name("place-mode")
-            .description("The way anchors are placed.")
-            .defaultValue(Mode.Safe)
+            .description("The way anchors are allowed to be placed near you.")
+            .defaultValue(Safety.Safe)
             .build()
     );
 
     private final Setting<Double> placeRange = sgPlace.add(new DoubleSetting.Builder()
             .name("place-range")
-            .description("The radius in which the anchors are placed in.")
+            .description("The radius in which anchors are placed in.")
             .defaultValue(3)
             .min(0)
             .sliderMax(5)
@@ -106,23 +97,23 @@ public class AnchorAura extends Module {
 
     private final Setting<Integer> breakDelay = sgBreak.add(new IntSetting.Builder()
             .name("break-delay")
-            .description("The amount of delay in ticks for breaking Anchors.")
+            .description("The tick delay between breaking anchors.")
             .defaultValue(10)
             .min(0)
             .max(10)
             .build()
     );
 
-    private final Setting<Mode> breakMode = sgBreak.add(new EnumSetting.Builder<Mode>()
+    private final Setting<Safety> breakMode = sgBreak.add(new EnumSetting.Builder<Safety>()
             .name("break-mode")
-            .description("The way Anchors are broken.")
-            .defaultValue(Mode.Safe)
+            .description("The way anchors are allowed to be placed near you.")
+            .defaultValue(Safety.Safe)
             .build()
     );
 
     private final Setting<Double> breakRange = sgBreak.add(new DoubleSetting.Builder()
             .name("break-range")
-            .description("The radius in which the Anchors are broken in.")
+            .description("The radius in which anchors are broken in.")
             .defaultValue(3)
             .min(0)
             .sliderMax(5)
@@ -133,21 +124,21 @@ public class AnchorAura extends Module {
 
     private final Setting<Boolean> pauseOnEat = sgPause.add(new BoolSetting.Builder()
             .name("pause-on-eat")
-            .description("Pauses Anchor Aura while eating.")
+            .description("Pauses while eating.")
             .defaultValue(false)
             .build()
     );
 
     private final Setting<Boolean> pauseOnDrink = sgPause.add(new BoolSetting.Builder()
             .name("pause-on-drink")
-            .description("Pauses Anchor Aura while drinking a potion.")
+            .description("Pauses while drinking potions.")
             .defaultValue(false)
             .build()
     );
 
     private final Setting<Boolean> pauseOnMine = sgPause.add(new BoolSetting.Builder()
             .name("pause-on-mine")
-            .description("Pauses Anchor Aura while mining blocks.")
+            .description("Pauses while mining blocks.")
             .defaultValue(false)
             .build()
     );
@@ -167,6 +158,13 @@ public class AnchorAura extends Module {
             .defaultValue(4)
             .min(0)
             .sliderMax(5)
+            .build()
+    );
+
+    private final Setting<SortPriority> priority = sgMisc.add(new EnumSetting.Builder<SortPriority>()
+            .name("priority")
+            .description("How to select the player to target.")
+            .defaultValue(SortPriority.LowestHealth)
             .build()
     );
 
@@ -245,6 +243,12 @@ public class AnchorAura extends Module {
     public void onActivate() {
         placeDelayLeft = 0;
         breakDelayLeft = 0;
+        target = null;
+    }
+
+    @Override
+    public void onDeactivate() {
+        target = null;
     }
 
     @EventHandler
@@ -255,47 +259,31 @@ public class AnchorAura extends Module {
             return;
         }
 
-        if (!checkItems()) {
-            target = null;
-            return;
-        }
+        if (PlayerUtils.shouldPause(pauseOnMine.get(), pauseOnEat.get(), pauseOnDrink.get())) return;
+        if (EntityUtils.getTotalHealth(mc.player) <= minHealth.get()) return;
 
-        if ((mc.player.isUsingItem() && (mc.player.getMainHandStack().getItem().isFood() || mc.player.getOffHandStack().getItem().isFood()) && pauseOnEat.get())
-                || (mc.interactionManager.isBreakingBlock() && pauseOnMine.get())
-                || (mc.player.isUsingItem() && (mc.player.getMainHandStack().getItem() instanceof PotionItem || mc.player.getOffHandStack().getItem() instanceof PotionItem) && pauseOnDrink.get())) {
-            return;
-        }
-
-        if (getTotalHealth(mc.player) <= minHealth.get() && placeMode.get() != Mode.Suicide && breakMode.get() != Mode.Suicide) return;
-
-        if (target == null || mc.player.distanceTo(target) > targetRange.get() || !target.isAlive()) target = findTarget();
+        if (EntityUtils.isInvalid(target, targetRange.get())) target = EntityUtils.getPlayerTarget(targetRange.get(), priority.get());
         if (target == null) return;
 
         int anchorSlot = InvUtils.findItemInHotbar(Items.RESPAWN_ANCHOR);
         int glowSlot = InvUtils.findItemInHotbar(Items.GLOWSTONE);
 
-
+        if (anchorSlot == -1 || glowSlot == -1) return;
 
         if (breakDelayLeft >= breakDelay.get()) {
-
-            BlockPos breakPos = findAnchor(target);
-
+            BlockPos breakPos = findBreakPos(target.getBlockPos());
             if (breakPos != null) {
                 mc.player.setSneaking(false);
                 mc.options.keySneak.setPressed(false);
                 if (rotationMode.get() == RotationMode.Both || rotationMode.get() == RotationMode.Break) {
-                    Rotations.rotate(Rotations.getYaw(breakPos), Rotations.getPitch(breakPos), 50, () -> {
-                        breakAnchor(breakPos, glowSlot, anchorSlot);
-                    });
-                } else
-                    breakAnchor(breakPos, glowSlot, anchorSlot);
+                    Rotations.rotate(Rotations.getYaw(breakPos), Rotations.getPitch(breakPos), 50, () -> breakAnchor(breakPos, glowSlot, anchorSlot));
+                } else breakAnchor(breakPos, glowSlot, anchorSlot);
                 breakDelayLeft = 0;
             }
         }
 
         if (placeDelayLeft >= placeDelay.get() && place.get()) {
-
-            BlockPos placePos = findPlacePos(target);
+            BlockPos placePos = findPlacePos(target.getBlockPos());
 
             if (placePos != null) {
                 mc.player.setSneaking(false);
@@ -312,70 +300,66 @@ public class AnchorAura extends Module {
     @EventHandler
     private void onRender(RenderEvent event) {
         if (target != null) {
-            if (renderPlace.get() && findPlacePos(target) != null) Renderer.boxWithLines(Renderer.NORMAL, Renderer.LINES, findPlacePos(target).getX(), findPlacePos(target).getY(), findPlacePos(target).getZ(), 1, sideColor.get(), lineColor.get(), shapeMode.get(), 0);
-            if (renderBreak.get() && findAnchor(target) != null) Renderer.boxWithLines(Renderer.NORMAL, Renderer.LINES, findAnchor(target).getX(), findAnchor(target).getY(), findAnchor(target).getZ(), 1, breakSideColor.get(), breakLineColor.get(), shapeMode.get(), 0);
+            if (renderPlace.get()) {
+                BlockPos placePos = findPlacePos(target.getBlockPos());
+                if (placePos != null) {
+                    Renderer.boxWithLines(Renderer.NORMAL, Renderer.LINES, placePos.getX(), placePos.getY(), placePos.getZ(), 1, sideColor.get(), lineColor.get(), shapeMode.get(), 0);
+                }
+            }
+            if (renderBreak.get()) {
+                BlockPos breakPos = findBreakPos(target.getBlockPos());
+                if (breakPos != null) {
+                    Renderer.boxWithLines(Renderer.NORMAL, Renderer.LINES, breakPos.getX(), breakPos.getY(), breakPos.getZ(), 1, breakSideColor.get(), breakLineColor.get(), shapeMode.get(), 0);
+                }
+            }
         }
     }
 
-    private boolean checkItems() {
-        return InvUtils.findItemInHotbar(Items.RESPAWN_ANCHOR) != -1 && InvUtils.findItemInHotbar(Items.GLOWSTONE) != -1;
-    }
-
-    private BlockPos findPlacePos(PlayerEntity target) {
-        BlockPos targetPlacePos = target.getBlockPos();
-        BlockPos finalPlacePos = null;
-
+    private BlockPos findPlacePos(BlockPos targetPlacePos) {
         switch (placePositions.get()){
             case All:
-                if (isValidPlace(targetPlacePos.down())) finalPlacePos = targetPlacePos.down();
-                else if (isValidPlace(targetPlacePos.up(2))) finalPlacePos = targetPlacePos.up(2);
-                else if (isValidPlace(targetPlacePos.add(1, 0, 0))) finalPlacePos = targetPlacePos.add(1, 0, 0);
-                else if (isValidPlace(targetPlacePos.add(-1, 0, 0))) finalPlacePos = targetPlacePos.add(-1, 0, 0);
-                else if (isValidPlace(targetPlacePos.add(0, 0, 1))) finalPlacePos = targetPlacePos.add(0, 0, 1);
-                else if (isValidPlace(targetPlacePos.add(0, 0, -1))) finalPlacePos = targetPlacePos.add(0, 0, -1);
-                else if (isValidPlace(targetPlacePos.add(1, 1, 0))) finalPlacePos = targetPlacePos.add(1, 1, 0);
-                else if (isValidPlace(targetPlacePos.add(-1, -1, 0))) finalPlacePos = targetPlacePos.add(-1, -1, 0);
-                else if (isValidPlace(targetPlacePos.add(0, 1, 1))) finalPlacePos = targetPlacePos.add(0, 1, 1);
-                else if (isValidPlace(targetPlacePos.add(0, 0, -1))) finalPlacePos = targetPlacePos.add(0, 0, -1);
+                if (isValidPlace(targetPlacePos.down())) return targetPlacePos.down();
+                else if (isValidPlace(targetPlacePos.up(2))) return targetPlacePos.up(2);
+                else if (isValidPlace(targetPlacePos.add(1, 0, 0))) return targetPlacePos.add(1, 0, 0);
+                else if (isValidPlace(targetPlacePos.add(-1, 0, 0))) return targetPlacePos.add(-1, 0, 0);
+                else if (isValidPlace(targetPlacePos.add(0, 0, 1))) return targetPlacePos.add(0, 0, 1);
+                else if (isValidPlace(targetPlacePos.add(0, 0, -1))) return targetPlacePos.add(0, 0, -1);
+                else if (isValidPlace(targetPlacePos.add(1, 1, 0))) return targetPlacePos.add(1, 1, 0);
+                else if (isValidPlace(targetPlacePos.add(-1, -1, 0))) return targetPlacePos.add(-1, -1, 0);
+                else if (isValidPlace(targetPlacePos.add(0, 1, 1))) return targetPlacePos.add(0, 1, 1);
+                else if (isValidPlace(targetPlacePos.add(0, 0, -1))) return targetPlacePos.add(0, 0, -1);
                 break;
             case Above:
-                if (isValidPlace(targetPlacePos.up(2))) finalPlacePos = targetPlacePos.up(2);
+                if (isValidPlace(targetPlacePos.up(2))) return targetPlacePos.up(2);
                 break;
             case AboveAndBelow:
-                if (isValidPlace(targetPlacePos.down())) finalPlacePos = targetPlacePos.down();
-                else if (isValidPlace(targetPlacePos.up(2))) finalPlacePos = targetPlacePos.up(2);
+                if (isValidPlace(targetPlacePos.down())) return targetPlacePos.down();
+                else if (isValidPlace(targetPlacePos.up(2))) return targetPlacePos.up(2);
                 break;
         }
-
-        return finalPlacePos;
+        return null ;
     }
 
-    private BlockPos findAnchor(PlayerEntity target) {
-        BlockPos targetPos = target.getBlockPos();
-        BlockPos finalPlacePos = null;
-
-        if (isValidBreak(targetPos.down())) finalPlacePos = targetPos.down();
-        else if (isValidBreak(targetPos.up(2))) finalPlacePos = targetPos.up(2);
-        else if (isValidBreak(targetPos.add(1, 0, 0))) finalPlacePos = targetPos.add(1, 0, 0);
-        else if (isValidBreak(targetPos.add(-1, 0, 0))) finalPlacePos = targetPos.add(-1, 0, 0);
-        else if (isValidBreak(targetPos.add(0, 0, 1))) finalPlacePos = targetPos.add(0, 0, 1);
-        else if (isValidBreak(targetPos.add(0, 0, -1))) finalPlacePos = targetPos.add(0, 0, -1);
-        else if (isValidBreak(targetPos.add(1, 1, 0))) finalPlacePos = targetPos.add(1, 1, 0);
-        else if (isValidBreak(targetPos.add(-1, -1, 0))) finalPlacePos = targetPos.add(-1, -1, 0);
-        else if (isValidBreak(targetPos.add(0, 1, 1))) finalPlacePos = targetPos.add(0, 1, 1);
-        else if (isValidBreak(targetPos.add(0, 0, -1))) finalPlacePos = targetPos.add(0, 0, -1);
-
-        return finalPlacePos;
+    private BlockPos findBreakPos(BlockPos targetPos) {
+        if (isValidBreak(targetPos.down())) return targetPos.down();
+        else if (isValidBreak(targetPos.up(2))) return targetPos.up(2);
+        else if (isValidBreak(targetPos.add(1, 0, 0))) return targetPos.add(1, 0, 0);
+        else if (isValidBreak(targetPos.add(-1, 0, 0))) return targetPos.add(-1, 0, 0);
+        else if (isValidBreak(targetPos.add(0, 0, 1))) return targetPos.add(0, 0, 1);
+        else if (isValidBreak(targetPos.add(0, 0, -1))) return targetPos.add(0, 0, -1);
+        else if (isValidBreak(targetPos.add(1, 1, 0))) return targetPos.add(1, 1, 0);
+        else if (isValidBreak(targetPos.add(-1, -1, 0))) return targetPos.add(-1, -1, 0);
+        else if (isValidBreak(targetPos.add(0, 1, 1))) return targetPos.add(0, 1, 1);
+        else if (isValidBreak(targetPos.add(0, 0, -1))) return targetPos.add(0, 0, -1);
+        return null;
     }
 
     private boolean getDamagePlace(BlockPos pos){
-        assert mc.player != null;
-        return placeMode.get() == Mode.Suicide || DamageCalcUtils.bedDamage(mc.player, new Vec3d(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5)) <= maxDamage.get();
+        return placeMode.get() == Safety.Suicide || DamageCalcUtils.bedDamage(mc.player, Utils.vec3d(pos.add(0.5, 0.5, 0.5))) <= maxDamage.get();
     }
 
     private boolean getDamageBreak(BlockPos pos){
-        assert mc.player != null;
-        return breakMode.get() == Mode.Suicide || DamageCalcUtils.anchorDamage(mc.player, new Vec3d(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5)) <= maxDamage.get();
+        return breakMode.get() == Safety.Suicide || DamageCalcUtils.anchorDamage(mc.player, Utils.vec3d(pos.add(0.5, 0.5, 0.5))) <= maxDamage.get();
     }
 
     private boolean isValidPlace(BlockPos pos) {
@@ -384,18 +368,6 @@ public class AnchorAura extends Module {
 
     private boolean isValidBreak(BlockPos pos) {
         return mc.world.getBlockState(pos).getBlock() == Blocks.RESPAWN_ANCHOR && Math.sqrt(mc.player.getBlockPos().getSquaredDistance(pos)) <= breakRange.get() && getDamageBreak(pos);
-    }
-
-    private PlayerEntity findTarget(){
-        Optional<PlayerEntity> livingEntity = Streams.stream(mc.world.getEntities())
-                .filter(entity -> entity instanceof PlayerEntity)
-                .filter(Entity::isAlive)
-                .filter(entity -> entity != mc.player)
-                .filter(entity -> Friends.get().attack((PlayerEntity) entity))
-                .filter(entity -> entity.distanceTo(mc.player) <= targetRange.get() * 2)
-                .min(Comparator.comparingDouble(o -> o.distanceTo(mc.player)))
-                .map(entity -> (PlayerEntity) entity);
-        return livingEntity.orElse(null);
     }
 
     private void breakAnchor(BlockPos pos, int glowSlot, int nonGlowSlot) {
@@ -411,14 +383,9 @@ public class AnchorAura extends Module {
         }
     }
 
-    private float getTotalHealth(PlayerEntity target) {
-        return target.getHealth() + target.getAbsorptionAmount();
-    }
-
     @Override
     public String getInfoString() {
-        if (target != null && target instanceof PlayerEntity) return target.getEntityName();
-        if (target != null) return target.getType().getName().getString();
+        if (target != null) return target.getEntityName();
         return null;
     }
 }
