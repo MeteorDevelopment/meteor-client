d7a40b6e07d7c439870e0c2fcd23ed800203c37f
MineGame159 <petulko08@gmail.com>
2021-02-04 12:21:36 +0100

    Tracers and ESP now dont show fake players outside of render distance (roughly)

--- src/main/java/minegame159/meteorclient/utils/entity/FakePlayerUtils.java
+++ src/main/java/minegame159/meteorclient/utils/entity/FakePlayerUtils.java
@@ -2,11 +2,15 @@ package minegame159.meteorclient.utils.entity;
 
 import minegame159.meteorclient.modules.Modules;
 import minegame159.meteorclient.modules.player.FakePlayer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class FakePlayerUtils {
+    private static final MinecraftClient mc = MinecraftClient.getInstance();
+
     private static final Map<FakePlayerEntity, Integer> players = new HashMap<>();
     public static int ID;
 
@@ -44,14 +48,18 @@ public class FakePlayerUtils {
     }
 
     public static int getID(FakePlayerEntity entity) {
-        int id = 0;
+        return players.getOrDefault(entity, 0);
+    }
 
-        if (!players.isEmpty()) {
-            for (Map.Entry<FakePlayerEntity, Integer> player : players.entrySet()) {
-                if (player.getKey() == entity) id = player.getValue();
-            }
+    public static boolean isFakePlayerOutOfRenderDistance(Entity entity) {
+        if (entity instanceof FakePlayerEntity) {
+            double x = Math.abs(mc.gameRenderer.getCamera().getPos().x - entity.getX());
+            double z = Math.abs(mc.gameRenderer.getCamera().getPos().z - entity.getZ());
+            double d = (mc.options.viewDistance + 1) * 16;
+
+            return x > d || z > d;
         }
 
-        return id;
+        return false;
     }
 }
