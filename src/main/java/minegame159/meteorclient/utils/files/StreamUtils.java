ade9086bafa6152e1345760bd81e1d2656c39e09
MineGame159 <petulko08@gmail.com>
2021-02-06 17:14:22 +0100

    Fixed waypoints not saving

---
+++ src/main/java/minegame159/meteorclient/utils/files/StreamUtils.java
@@ -0,0 +1,30 @@
+package minegame159.meteorclient.utils.files;
+
+import java.io.*;
+
+public class StreamUtils {
+    public static void copy(File from, File to) {
+        try {
+            InputStream in = new FileInputStream(from);
+            OutputStream out = new FileOutputStream(to);
+
+            copy(in, out);
+
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void copy(InputStream in, OutputStream out) {
+        byte[] bytes = new byte[512];
+        int read;
+
+        try {
+            while ((read = in.read(bytes)) != -1) out.write(bytes, 0, read);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
