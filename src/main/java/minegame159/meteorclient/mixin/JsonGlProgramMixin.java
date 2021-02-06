43c9d0bbe157ffcba3f824ac837997d25614f9c4
MineGame159 <petulko08@gmail.com>
2021-02-06 13:59:44 +0100

    Probably fixed compatibility with some mods

--- src/main/java/minegame159/meteorclient/mixin/JsonGlProgramMixin.java
+++ src/main/java/minegame159/meteorclient/mixin/JsonGlProgramMixin.java
@@ -10,25 +10,25 @@ import net.minecraft.client.gl.JsonGlProgram;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(JsonGlProgram.class)
 public class JsonGlProgramMixin {
-    @Redirect(method = "<init>", at = @At(value = "NEW", target = "net/minecraft/util/Identifier"))
-    private static Identifier onInit(String string) {
-        if (Outlines.loadingOutlineShader && string.equals("shaders/program/my_entity_outline.json")) {
-            return new Identifier("meteor-client", string);
+    @ModifyVariable(method = "<init>", at = @At("STORE"), name = "identifier")
+    private Identifier onInitNewIdentifierModifyVariable(Identifier identifier) {
+        if (Outlines.loadingOutlineShader && identifier.getPath().equals("shaders/program/my_entity_outline.json")) {
+            return new Identifier("meteor-client", identifier.getPath());
         }
 
-        return new Identifier(string);
+        return identifier;
     }
 
-    @Redirect(method = "getShader", at = @At(value = "NEW", target = "net/minecraft/util/Identifier"))
-    private static Identifier onGetShader(String string) {
-        if (Outlines.loadingOutlineShader && string.equals("shaders/program/my_entity_sobel.fsh")) {
-            return new Identifier("meteor-client", string);
+    @ModifyVariable(method = "getShader", at = @At("STORE"), name = "identifier")
+    private static Identifier onGetShaderNewIdentifierModifyVariable(Identifier identifier) {
+        if (Outlines.loadingOutlineShader && identifier.getPath().equals("shaders/program/my_entity_sobel.fsh")) {
+            return new Identifier("meteor-client", identifier.getPath());
         }
 
-        return new Identifier(string);
+        return identifier;
     }
 }
