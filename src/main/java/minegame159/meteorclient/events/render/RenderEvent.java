b7309f7c0db8c0bf6a9c2537d0b8ed4150f2ea9c
seasnail8169 <seasnail8169@gmail.com>
2021-02-07 17:42:05 +0000

    Prep for new nametags?

--- src/main/java/minegame159/meteorclient/events/render/RenderEvent.java
+++ src/main/java/minegame159/meteorclient/events/render/RenderEvent.java
@@ -5,14 +5,17 @@
 
 package minegame159.meteorclient.events.render;
 
+import net.minecraft.client.util.math.MatrixStack;
+
 public class RenderEvent {
     private static final RenderEvent INSTANCE = new RenderEvent();
 
     public float tickDelta;
     public double offsetX, offsetY, offsetZ;
+    public MatrixStack matrixStack;
 
-
-    public static RenderEvent get(float tickDelta, double offsetX, double offsetY, double offsetZ) {
+    public static RenderEvent get(MatrixStack martixStack, float tickDelta, double offsetX, double offsetY, double offsetZ) {
+        INSTANCE.matrixStack = martixStack;
         INSTANCE.tickDelta = tickDelta;
         INSTANCE.offsetX = offsetX;
         INSTANCE.offsetY = offsetY;
