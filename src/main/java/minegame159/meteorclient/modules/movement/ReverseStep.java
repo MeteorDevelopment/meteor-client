3d0322ca8852a8a59bf831504d32a3f81bd8b989
MineGame159 <petulko08@gmail.com>
2021-02-08 14:21:50 +0100

    Fixed ReverseStep while walking on beds

--- src/main/java/minegame159/meteorclient/modules/movement/ReverseStep.java
+++ src/main/java/minegame159/meteorclient/modules/movement/ReverseStep.java
@@ -13,6 +13,8 @@ import minegame159.meteorclient.modules.Module;
 import minegame159.meteorclient.settings.DoubleSetting;
 import minegame159.meteorclient.settings.Setting;
 import minegame159.meteorclient.settings.SettingGroup;
+import net.minecraft.block.BedBlock;
+import net.minecraft.util.math.BlockPos;
 
 public class ReverseStep extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
@@ -43,6 +45,33 @@ public class ReverseStep extends Module {
     private void onTick(TickEvent.Post event) {
         if (!mc.player.isOnGround() || mc.player.isHoldingOntoLadder() || mc.player.isSubmergedInWater() || mc.player.isInLava() ||mc.options.keyJump.isPressed() || mc.player.noClip || mc.player.forwardSpeed == 0 && mc.player.sidewaysSpeed == 0) return;
 
-        if (!mc.world.isSpaceEmpty(mc.player.getBoundingBox().offset(0.0, (float) -(fallDistance.get() + 0.01), 0.0))) ((IVec3d) mc.player.getVelocity()).setY(-fallSpeed.get());
+        if (!isOnBed() && !mc.world.isSpaceEmpty(mc.player.getBoundingBox().offset(0.0, (float) -(fallDistance.get() + 0.01), 0.0))) ((IVec3d) mc.player.getVelocity()).setY(-fallSpeed.get());
+    }
+
+    private boolean isOnBed() {
+        BlockPos.Mutable blockPos = mc.player.getBlockPos().mutableCopy();
+
+        if (check(blockPos, 0, 0)) return true;
+
+        double xa = mc.player.getX() - blockPos.getX();
+        double za = mc.player.getZ() - blockPos.getZ();
+
+        if (xa >= 0 && xa <= 0.3 && check(blockPos, -1, 0)) return true;
+        if (xa >= 0.7 && check(blockPos, 1, 0)) return true;
+        if (za >= 0 && za <= 0.3 && check(blockPos, 0, -1)) return true;
+        if (za >= 0.7 && check(blockPos, 0, 1)) return true;
+
+        if (xa >= 0 && xa <= 0.3 && za >= 0 && za <= 0.3 && check(blockPos, -1, -1)) return true;
+        if (xa >= 0 && xa <= 0.3 && za >= 0.7 && check(blockPos, -1, 1)) return true;
+        if (xa >= 0.7 && za >= 0 && za <= 0.3 && check(blockPos, 1, -1)) return true;
+        return xa >= 0.7 && za >= 0.7 && check(blockPos, 1, 1);
+    }
+
+    private boolean check(BlockPos.Mutable blockPos, int x, int z) {
+        blockPos.move(x, 0, z);
+        boolean is = mc.world.getBlockState(blockPos).getBlock() instanceof BedBlock;
+        blockPos.move(-x, 0, -z);
+
+        return is;
     }
 }
