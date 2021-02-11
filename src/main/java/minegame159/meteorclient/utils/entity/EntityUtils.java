2ea51c6e9b2702eefad29991ee8f1ed50b6eabfa
seasnail8169 <seasnail8169@gmail.com>
2021-02-11 00:05:49 +0000

    Cleanup of the last PR

--- src/main/java/minegame159/meteorclient/utils/entity/EntityUtils.java
+++ src/main/java/minegame159/meteorclient/utils/entity/EntityUtils.java
@@ -23,7 +23,7 @@ import java.util.function.Predicate;
 
 public class EntityUtils {
     public static MinecraftClient mc;
-    private static List<Entity> entities = new ArrayList<>();
+    private static final List<Entity> entities = new ArrayList<>();
 
     public static boolean isAttackable(EntityType<?> type) {
         return type != EntityType.AREA_EFFECT_CLOUD && type != EntityType.ARROW && type != EntityType.FALLING_BLOCK && type != EntityType.FIREWORK_ROCKET && type != EntityType.ITEM && type != EntityType.LLAMA_SPIT && type != EntityType.SPECTRAL_ARROW && type != EntityType.ENDER_PEARL && type != EntityType.EXPERIENCE_BOTTLE && type != EntityType.POTION && type != EntityType.TRIDENT && type != EntityType.LIGHTNING_BOLT && type != EntityType.FISHING_BOBBER && type != EntityType.EXPERIENCE_ORB && type != EntityType.EGG;
