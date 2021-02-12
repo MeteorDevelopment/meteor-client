f63cf87991eb5cefdc8de42da2c234b01ffd4a57
seasnail8169 <seasnail8169@gmail.com>
2021-02-12 15:28:54 +0000

    Fixed copyright

--- src/main/java/minegame159/meteorclient/gui/renderer/Region.java
+++ src/main/java/minegame159/meteorclient/gui/renderer/Region.java
@@ -1,8 +1,6 @@
 /*
- *
- *  * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
- *  * Copyright (c) 2021 Meteor Development.
- *
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
+ * Copyright (c) 2021 Meteor Development.
  */
 
 package minegame159.meteorclient.gui.renderer;
