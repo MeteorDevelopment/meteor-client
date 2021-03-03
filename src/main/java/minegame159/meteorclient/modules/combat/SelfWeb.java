0c61bae4f92fdde68180ee01ff5aa20a948021f8
supakeks <thiaguitoszwarc@gmail.com>
2021-03-03 13:49:19 -0500

    Added smart mode to SelfWeb

--- src/main/java/minegame159/meteorclient/modules/combat/SelfWeb.java
+++ src/main/java/minegame159/meteorclient/modules/combat/SelfWeb.java
@@ -7,19 +7,35 @@ package minegame159.meteorclient.modules.combat;
 
 import meteordevelopment.orbit.EventHandler;
 import minegame159.meteorclient.events.world.TickEvent;
+import minegame159.meteorclient.friends.Friends;
 import minegame159.meteorclient.modules.Categories;
 import minegame159.meteorclient.modules.Module;
-import minegame159.meteorclient.settings.BoolSetting;
-import minegame159.meteorclient.settings.Setting;
-import minegame159.meteorclient.settings.SettingGroup;
+import minegame159.meteorclient.settings.*;
+import minegame159.meteorclient.utils.entity.EntityUtils;
+import minegame159.meteorclient.utils.entity.SortPriority;
+import minegame159.meteorclient.utils.player.ChatUtils;
+import minegame159.meteorclient.utils.player.InvUtils;
 import minegame159.meteorclient.utils.world.BlockUtils;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 
 public class SelfWeb extends Module {
+
+    public enum Mode {
+        Normal,
+        Smart
+    }
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Mode> mode = sgGeneral.add(new EnumSetting.Builder<Mode>()
+            .name("mode")
+            .description("The mode to use for selfweb.")
+            .defaultValue(Mode.Normal)
+            .build()
+    );
 
     private final Setting<Boolean> doubles = sgGeneral.add(new BoolSetting.Builder()
             .name("double-place")
@@ -32,6 +48,15 @@ public class SelfWeb extends Module {
             .name("auto-toggle")
             .description("Toggles off after placing the webs.")
             .defaultValue(true)
+            .build()
+    );
+
+    private final Setting<Integer> range = sgGeneral.add(new IntSetting.Builder()
+            .name("range")
+            .description("How far away the player has to be from you to place webs. Requires Mode to Smart.")
+            .defaultValue(3)
+            .min(1)
+            .sliderMax(7)
             .build()
     );
 
@@ -48,32 +73,27 @@ public class SelfWeb extends Module {
 
     @EventHandler
     private void onTick(TickEvent.Pre event) {
-        int slot = findSlot();
-        if (slot == -1) return;
+        switch (mode.get()) {
+            case Normal:
+                placeWeb();
+                break;
+            case Smart:
+                if (EntityUtils.getPlayerTarget(range.get(), SortPriority.LowestDistance, false) != null) placeWeb();
+                break;
+        }
+    }
+
+    private void placeWeb() {
+        int slot = InvUtils.findItemInHotbar(Items.COBWEB);
 
         BlockPos blockPos = mc.player.getBlockPos();
         BlockUtils.place(blockPos, Hand.MAIN_HAND, slot, rotate.get(), 0, false);
 
         if (doubles.get()) {
-            slot = findSlot();
-            if (slot == -1) return;
-
             blockPos = mc.player.getBlockPos().add(0, 1, 0);
             BlockUtils.place(blockPos, Hand.MAIN_HAND, slot, rotate.get(), 0, false);
         }
 
         if (turnOff.get()) toggle();
-    }
-
-    private int findSlot() {
-        for (int i = 0; i < 9; i++) {
-            Item item = mc.player.inventory.getStack(i).getItem();
-
-            if (item == Items.COBWEB) {
-                return i;
-            }
-        }
-
-        return -1;
     }
 }
