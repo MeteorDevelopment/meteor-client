b825a8cc9ed7d8bd133ef45d47097cb5425eab4b
MineGame159 <petulko08@gmail.com>
2021-03-04 17:59:17 +0100

    Rewritten MiddleClickExtra, added rocket and chorus modes

---
+++ src/main/java/minegame159/meteorclient/events/entity/player/StoppedUsingItemEvent.java
@@ -0,0 +1,19 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
+ * Copyright (c) 2021 Meteor Development.
+ */
+
+package minegame159.meteorclient.events.entity.player;
+
+import net.minecraft.item.ItemStack;
+
+public class StoppedUsingItemEvent {
+    private static final StoppedUsingItemEvent INSTANCE = new StoppedUsingItemEvent();
+
+    public ItemStack itemStack;
+
+    public static StoppedUsingItemEvent get(ItemStack itemStack) {
+        INSTANCE.itemStack = itemStack;
+        return INSTANCE;
+    }
+}
