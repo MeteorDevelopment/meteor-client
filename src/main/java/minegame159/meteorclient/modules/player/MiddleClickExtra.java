ee72c8cfbf92f9847e2edbbdf6ad5bc0ef937735
MineGame159 <petulko08@gmail.com>
2021-03-04 17:59:17 +0100

    Rewritten MiddleClickExtra, added rocket and chorus modes

--- src/main/java/minegame159/meteorclient/modules/player/MiddleClickExtra.java
+++ src/main/java/minegame159/meteorclient/modules/player/MiddleClickExtra.java
@@ -8,6 +8,8 @@ package minegame159.meteorclient.modules.player;
 //Created by squidoodly 06/07/2020
 
 import meteordevelopment.orbit.EventHandler;
+import minegame159.meteorclient.events.entity.player.FinishUsingItem;
+import minegame159.meteorclient.events.entity.player.StoppedUsingItemEvent;
 import minegame159.meteorclient.events.meteor.MiddleMouseButtonEvent;
 import minegame159.meteorclient.events.world.TickEvent;
 import minegame159.meteorclient.modules.Categories;
@@ -18,21 +20,36 @@ import minegame159.meteorclient.settings.Setting;
 import minegame159.meteorclient.settings.SettingGroup;
 import minegame159.meteorclient.utils.player.ChatUtils;
 import minegame159.meteorclient.utils.player.InvUtils;
+import net.minecraft.item.BowItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 
 public class MiddleClickExtra extends Module {
+    private enum Type {
+        Immediate,
+        LongerSingleClick,
+        Longer
+    }
+
     public enum Mode {
-        Pearl(Items.ENDER_PEARL),
-        Bow(Items.ARROW),
-        Gap(Items.GOLDEN_APPLE),
-        EGap(Items.ENCHANTED_GOLDEN_APPLE),
-        Rod(Items.FISHING_ROD);
+        Pearl(Items.ENDER_PEARL, Type.Immediate),
+        Rocket(Items.FIREWORK_ROCKET, Type.Immediate),
+
+        Rod(Items.FISHING_ROD, Type.LongerSingleClick),
+
+        Bow(Items.BOW, Type.Longer),
+        Gap(Items.GOLDEN_APPLE, Type.Longer),
+        EGap(Items.ENCHANTED_GOLDEN_APPLE, Type.Longer),
+        Chorus(Items.CHORUS_FRUIT, Type.Longer);
 
         private final Item item;
+        private final Type type;
 
-        Mode(Item item){this.item = item;}
+        Mode(Item item, Type type) {
+            this.item = item;
+            this.type = type;
+        }
     }
 
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
@@ -51,91 +68,76 @@ public class MiddleClickExtra extends Module {
             .build()
     );
 
-    // Item specification errors.
-
-    private boolean wasUsing = false;
+    private boolean isUsing;
     private int preSlot;
-    private int preCount;
 
     public MiddleClickExtra(){
         super(Categories.Player, "middle-click-extra", "Lets you use items when you middle click. Works the same as Middle Click Friend.");
     }
 
+    @Override
+    public void onDeactivate() {
+        stopIfUsing();
+    }
+
     @EventHandler
-    private void onMiddleMouse(MiddleMouseButtonEvent event) {
-        InvUtils.FindItemResult result;
-        switch(mode.get()){
-            case Pearl: {
-                result = InvUtils.findItemWithCount(Items.ENDER_PEARL);
-                if (result.slot <= 8 && result.slot != -1) {
-                    preSlot = mc.player.inventory.selectedSlot;
-                    mc.player.inventory.selectedSlot = result.slot;
-                    mc.interactionManager.interactItem(mc.player, mc.world, Hand.MAIN_HAND);
-                    mc.player.inventory.selectedSlot = preSlot;
-                } else if (notify.get()) {
-                    ChatUtils.moduleWarning(this, "Unable to find specified item.");
-                }
+    private void onMiddleMouseButton(MiddleMouseButtonEvent event) {
+        InvUtils.FindItemResult result = InvUtils.findItemWithCount(mode.get().item);
+
+        if (result.slot == -1 || result.slot > 8) {
+            if (notify.get()) ChatUtils.moduleWarning(this, "Unable to find specified item.");
+            return;
+        }
+
+        preSlot = mc.player.inventory.selectedSlot;
+        mc.player.inventory.selectedSlot = result.slot;
+
+        switch (mode.get().type) {
+            case Immediate:
+                mc.interactionManager.interactItem(mc.player, mc.world, Hand.MAIN_HAND);
+                mc.player.inventory.selectedSlot = preSlot;
+
                 break;
-            }case Gap: {
-                result = InvUtils.findItemWithCount(Items.GOLDEN_APPLE);
-                if (result.slot <= 8 && result.slot != -1) {
-                    preSlot = mc.player.inventory.selectedSlot;
-                    mc.player.inventory.selectedSlot = result.slot;
-                    preCount = result.count;
-                    mc.options.keyUse.setPressed(true);
-                    wasUsing = true;
-                } else if(notify.get()) {
-                    ChatUtils.moduleWarning(this, "Unable to find specified item.");
-                }
+            case LongerSingleClick:
+                mc.interactionManager.interactItem(mc.player, mc.world, Hand.MAIN_HAND);
+
                 break;
-            }case EGap:{
-                result = InvUtils.findItemWithCount(Items.ENCHANTED_GOLDEN_APPLE);
-                if (result.slot <= 8 && result.slot != -1) {
-                    preSlot = mc.player.inventory.selectedSlot;
-                    mc.player.inventory.selectedSlot = result.slot;
-                    preCount = result.count;
-                    mc.options.keyUse.setPressed(true);
-                    wasUsing = true;
-                } else if(notify.get()) {
-                    ChatUtils.moduleWarning(this, "Unable to find selected item.");
-                }
+            case Longer:
+                mc.options.keyUse.setPressed(true);
+                isUsing = true;
+
                 break;
-            }case Bow:{
-                result = InvUtils.findItemWithCount(Items.BOW);
-                if (result.slot <= 8 && result.slot != -1) {
-                    preSlot = mc.player.inventory.selectedSlot;
-                    mc.player.inventory.selectedSlot = result.slot;
-                    result = InvUtils.findItemWithCount(Items.ARROW);
-                    preCount = result.count;
-                    wasUsing = true;
-                } else if(notify.get()) {
-                    ChatUtils.moduleWarning(this, "Unable to find specified item.");
-                }
-                break;
-            }case Rod: {
-                result = InvUtils.findItemWithCount(Items.FISHING_ROD);
-                if (result.slot <= 8 && result.slot != -1) {
-                    preSlot = mc.player.inventory.selectedSlot;
-                    mc.player.inventory.selectedSlot = result.slot;
-                    mc.interactionManager.interactItem(mc.player, mc.world, Hand.MAIN_HAND);
-                } else if (notify.get()) {
-                    ChatUtils.moduleWarning(this, "Unable to find specified item.");
-                }
-                break;
-            }
         }
     }
 
     @EventHandler
-    private void onTick(TickEvent.Post event) {
-        if (!wasUsing) return;
+    private void onTick(TickEvent.Pre event) {
+        if (isUsing) {
+            boolean pressed = true;
 
-        if (preCount > InvUtils.findItemWithCount(mode.get().item).count || (mc.player.getMainHandStack().getItem() != mode.get().item && (mode.get() == Mode.Bow && mc.player.getMainHandStack().getItem() != Items.BOW))){
+            if (mc.player.getMainHandStack().getItem() instanceof BowItem) {
+                pressed = BowItem.getPullProgress(mc.player.getItemUseTime()) < 1;
+            }
+
+            mc.options.keyUse.setPressed(pressed);
+        }
+    }
+
+    @EventHandler
+    private void onFinishUsingItem(FinishUsingItem event) {
+        stopIfUsing();
+    }
+
+    @EventHandler
+    private void onStoppedUsingItem(StoppedUsingItemEvent event) {
+        stopIfUsing();
+    }
+
+    private void stopIfUsing() {
+        if (isUsing) {
             mc.options.keyUse.setPressed(false);
             mc.player.inventory.selectedSlot = preSlot;
-            wasUsing = false;
-        } else {
-            mc.options.keyUse.setPressed(true);
+            isUsing = false;
         }
     }
 }
