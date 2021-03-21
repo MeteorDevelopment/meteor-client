f59a370b0f23e4f9a1cb794fdf4f232ece00b9fa
Cloudburst <serek.lego@gmail.com>
2021-03-21 12:58:06 +0100

    Made death position actually work on multiplayer

--- src/main/java/minegame159/meteorclient/modules/player/DeathPosition.java
+++ src/main/java/minegame159/meteorclient/modules/player/DeathPosition.java
@@ -10,6 +10,7 @@ import net.minecraft.util.math.Vec3d;
 import baritone.api.pathing.goals.GoalXZ;
 import meteordevelopment.orbit.EventHandler;
 import minegame159.meteorclient.events.entity.TookDamageEvent;
+import minegame159.meteorclient.events.packets.PacketEvent;
 import minegame159.meteorclient.gui.widgets.WButton;
 import minegame159.meteorclient.gui.widgets.WLabel;
 import minegame159.meteorclient.gui.widgets.WTable;
@@ -23,6 +24,8 @@ import minegame159.meteorclient.utils.Utils;
 import minegame159.meteorclient.utils.player.ChatUtils;
 import minegame159.meteorclient.waypoints.Waypoint;
 import minegame159.meteorclient.waypoints.Waypoints;
+
+import net.minecraft.network.packet.s2c.play.HealthUpdateS2CPacket;
 import net.minecraft.text.BaseText;
 import net.minecraft.text.LiteralText;
 
@@ -49,61 +52,28 @@ public class DeathPosition extends Module {
     private final Map<String, Double> deathPos = new HashMap<>();
     private Waypoint waypoint;
 
-    private double damagedplayerX;
-    private double damagedplayerY;
-    private double damagedplayerZ;
+    private Vec3d dmgPos;
 
     public DeathPosition() {
         super(Categories.Player, "death-position", "Sends you the coordinates to your latest death.");
     }
 
     @EventHandler
+    private void onPacketReceive(PacketEvent.Receive event) {
+        if (event.packet instanceof HealthUpdateS2CPacket) {
+            HealthUpdateS2CPacket packet = (HealthUpdateS2CPacket) event.packet;
+            if (packet.getHealth() <= 0) {
+                onDeath();
+            }
+        }
+    }
+
+    @EventHandler
     private void onTookDamage(TookDamageEvent event) {
         if (mc.player == null) return;
-        
-        if (event.entity.getUuid() != null && event.entity.getUuid().equals(mc.player.getUuid()) && event.entity.getHealth() >= 0) {
-            damagedplayerX = mc.player.getX();
-            damagedplayerY = mc.player.getY();
-            damagedplayerZ = mc.player.getZ(); }
 
         if (event.entity.getUuid() != null && event.entity.getUuid().equals(mc.player.getUuid()) && event.entity.getHealth() <= 0) {
-            deathPos.put("x", damagedplayerX);
-            deathPos.put("z", damagedplayerZ);
-            label.setText(String.format("Latest death: %.1f, %.1f, %.1f", damagedplayerX, damagedplayerY, damagedplayerZ));
-
-            String time = dateFormat.format(new Date());
-            //ChatUtils.moduleInfo(this, "Died at (highlight)%.0f(default), (highlight)%.0f(default), (highlight)%.0f (default)on (highlight)%s(default).", damagedplayerX, damagedplayerY, damagedplayerZ, time);
-            BaseText msg = new LiteralText("Died at ");
-            Vec3d damagedcords = new Vec3d(damagedplayerX, damagedplayerY, damagedplayerZ);
-            msg.append(ChatUtils.formatCoords(damagedcords));
-            msg.append(".");
-            ChatUtils.moduleInfo(this,msg);
-
-            // Create waypoint
-            if (createWaypoint.get()) {
-                waypoint = new Waypoint();
-                waypoint.name = "Death " + time;
-
-                waypoint.x = (int) damagedplayerX;
-                waypoint.y = (int) damagedplayerY + 2;
-                waypoint.z = (int) damagedplayerZ;
-                waypoint.maxVisibleDistance = Integer.MAX_VALUE;
-                waypoint.actualDimension = Utils.getDimension();
-
-                switch (Utils.getDimension()) {
-                    case Overworld:
-                        waypoint.overworld = true;
-                        break;
-                    case Nether:
-                        waypoint.nether = true;
-                        break;
-                    case End:
-                        waypoint.end = true;
-                        break;
-                }
-
-                Waypoints.get().add(waypoint);
-            }
+            onDeath();
         }
     }
 
@@ -120,12 +90,52 @@ public class DeathPosition extends Module {
         return table;
     }
 
+    private void onDeath() {
+        if (dmgPos==null) dmgPos = mc.player.getPos();
+        deathPos.put("x", dmgPos.x);
+        deathPos.put("z", dmgPos.z);
+        label.setText(String.format("Latest death: %.1f, %.1f, %.1f", dmgPos.x, dmgPos.y, dmgPos.z));
+
+        String time = dateFormat.format(new Date());
+        //ChatUtils.moduleInfo(this, "Died at (highlight)%.0f(default), (highlight)%.0f(default), (highlight)%.0f (default)on (highlight)%s(default).", damagedplayerX, damagedplayerY, damagedplayerZ, time);
+        BaseText msg = new LiteralText("Died at ");
+        msg.append(ChatUtils.formatCoords(dmgPos));
+        msg.append(String.format(" on %s. Git gud.", time));
+        ChatUtils.moduleInfo(this,msg);
+
+        // Create waypoint
+        if (createWaypoint.get()) {
+            waypoint = new Waypoint();
+            waypoint.name = "Death " + time;
+
+            waypoint.x = (int) dmgPos.x;
+            waypoint.y = (int) dmgPos.y + 2;
+            waypoint.z = (int) dmgPos.z;
+            waypoint.maxVisibleDistance = Integer.MAX_VALUE;
+            waypoint.actualDimension = Utils.getDimension();
+
+            switch (Utils.getDimension()) {
+                case Overworld:
+                    waypoint.overworld = true;
+                    break;
+                case Nether:
+                    waypoint.nether = true;
+                    break;
+                case End:
+                    waypoint.end = true;
+                    break;
+            }
+
+            Waypoints.get().add(waypoint);
+        }
+    }
+
     private void path() {
         if (deathPos.isEmpty() && mc.player != null) {
             ChatUtils.moduleWarning(this,"No latest death found.");
         } else {
             if (mc.world != null) {
-                double x = damagedplayerX, z = damagedplayerZ;
+                double x = dmgPos.x, z = dmgPos.z;
                 if (BaritoneAPI.getProvider().getPrimaryBaritone().getPathingBehavior().isPathing())
                     BaritoneAPI.getProvider().getPrimaryBaritone().getPathingBehavior().cancelEverything();
                 BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().setGoalAndPath(new GoalXZ((int) x, (int) z));
