35d5bd188ff9e9145da6a0da6078aeed685a05ba
Walaryne <hazardoustheradioactive@gmail.com>
2021-03-25 18:08:04 -0400

    Added WallHack :troll:

---
+++ src/main/java/minegame159/meteorclient/mixin/RenderLayersMixin.java
@@ -0,0 +1,29 @@
+package minegame159.meteorclient.mixin;
+
+import minegame159.meteorclient.modules.Modules;
+import minegame159.meteorclient.modules.render.WallHack;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.RenderLayers;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(RenderLayers.class)
+public class RenderLayersMixin {
+
+    @Inject(method = "getBlockLayer", at = @At("HEAD"), cancellable = true)
+    private static void onGetBlockLayer(BlockState state, CallbackInfoReturnable<RenderLayer> cir) {
+        if(Modules.get() != null) {
+            WallHack wallHack = Modules.get().get(WallHack.class);
+
+            if(wallHack.isActive()) {
+                if(wallHack.blocks.get().contains(state.getBlock())) {
+                    cir.setReturnValue(RenderLayer.getTranslucent());
+                }
+            }
+        }
+    }
+
+}
