fe02bf95c8096ffbc6098337a2ee955498309f35
AstroOrbis <astroorbis@gmail.com>
2021-03-29 19:39:41 -0400

    Added Fish

---
+++ src/main/java/minegame159/meteorclient/modules/movement/Fish.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
+ * Copyright (c) 2021 Meteor Development.
+ *
+ *
+ * Source used:
+ *
+ * Copyright (c) 2014-2021 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ *
+ */
+
+
+package minegame159.meteorclient.modules.movement;
+
+import meteordevelopment.orbit.EventHandler;
+import minegame159.meteorclient.modules.Categories;
+import minegame159.meteorclient.modules.Module;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.util.math.Vec3d;
+
+
+
+public class Fish extends Module {
+
+    public Fish() {
+        super(Categories.Movement, "fish", "Disables underwater gravity.")
+    }
+
+    ClientPlayerEntity player = MC.player;
+		if(!player.isWet() || player.isSneaking())
+                return;
+
+    Vec3d velocity = player.getVelocity();
+		player.setVelocity(velocity.x, velocity.y + 0.005, velocity.z);
+}
