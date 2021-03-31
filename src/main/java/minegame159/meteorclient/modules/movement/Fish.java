bd2bde13e65892ca8e53eede551836b50a3dd1c6
AstroOrbis <65419194+AstroOrbis@users.noreply.github.com>
2021-03-31 10:52:03 -0400

    Update Fish.java

--- src/main/java/minegame159/meteorclient/modules/movement/Fish.java
+++ src/main/java/minegame159/meteorclient/modules/movement/Fish.java
@@ -1,17 +1,6 @@
 /*
  * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
  * Copyright (c) 2021 Meteor Development.
- *
- *
- * Source used:
- *
- * Copyright (c) 2014-2021 Wurst-Imperium and contributors.
- *
- * This source code is subject to the terms of the GNU General Public
- * License, version 3. If a copy of the GPL was not distributed with this
- * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
- *
- *
  */
 
 
