0607c9fcb373f0bdc51f2f4ab6dde6c56be7d514
MineGame159 <petulko08@gmail.com>
2021-04-05 16:53:30 +0200

    Added infiniteChatBox setting to BetterChat

---
+++ src/main/java/minegame159/meteorclient/mixin/ChatScreenMixin.java
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
+ * Copyright (c) 2021 Meteor Development.
+ */
+
+package minegame159.meteorclient.mixin;
+
+import minegame159.meteorclient.systems.modules.Modules;
+import minegame159.meteorclient.systems.modules.misc.BetterChat;
+import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ChatScreen.class)
+public class ChatScreenMixin {
+    @Shadow protected TextFieldWidget chatField;
+
+    @Inject(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;setMaxLength(I)V", shift = At.Shift.AFTER))
+    private void onInit(CallbackInfo info) {
+        if (Modules.get().get(BetterChat.class).isInfiniteChatBox()) chatField.setMaxLength(Integer.MAX_VALUE);
+    }
+}
