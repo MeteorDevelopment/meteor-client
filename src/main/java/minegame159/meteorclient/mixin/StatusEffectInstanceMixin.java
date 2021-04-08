8838e77e4658d156a2ff704fe17d07227a82dd87
seasnail8169 <seasnail8169@gmail.com>
2021-04-08 23:15:36 +0100

    Added PotionSaver (#376)

---
+++ src/main/java/minegame159/meteorclient/mixin/StatusEffectInstanceMixin.java
@@ -0,0 +1,27 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
+ * Copyright (c) 2021 Meteor Development.
+ */
+
+package minegame159.meteorclient.mixin;
+
+import minegame159.meteorclient.systems.modules.Modules;
+import minegame159.meteorclient.systems.modules.player.PotionSaver;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(StatusEffectInstance.class)
+public class StatusEffectInstanceMixin {
+
+    @Shadow private int duration;
+
+    @Inject(method = "updateDuration", at = @At("HEAD"), cancellable = true)
+    private void tick(CallbackInfoReturnable<Integer> info) {
+        if (Modules.get().get(PotionSaver.class).shouldFreeze()) info.setReturnValue(duration);
+    }
+
+}
