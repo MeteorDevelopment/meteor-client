cf7175db53c864ae820f47afabe97472b2462ae0
seasnail8169 <seasnail8169@gmail.com>
2021-04-11 23:13:04 +0100

    Add a nicer slider for BetterTab tabsize

--- src/main/java/minegame159/meteorclient/systems/modules/misc/BetterTab.java
+++ src/main/java/minegame159/meteorclient/systems/modules/misc/BetterTab.java
@@ -28,6 +28,7 @@ public class BetterTab extends Module {
             .description("Bypasses the 80 player limit on the tablist.")
             .defaultValue(100)
             .min(1)
+            .sliderMax(1000)
             .sliderMin(1)
             .build()
     );
