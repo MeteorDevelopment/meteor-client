9640342c4ae8af8d2b31449d81f7706d3a252583
Cloudburst <serek.lego@gmail.com>
2021-04-12 15:32:48 +0200

    Antbot improvements

--- src/main/java/minegame159/meteorclient/systems/modules/misc/AntiBot.java
+++ src/main/java/minegame159/meteorclient/systems/modules/misc/AntiBot.java
@@ -20,11 +20,47 @@ import net.minecraft.entity.player.PlayerEntity;
 public class AntiBot extends Module {
 
 	private final SettingGroup sgGeneral = settings.getDefaultGroup();
+	private final SettingGroup sgFilters = settings.createGroup("Filters");
 
 	private final Setting<Boolean> removeInvisible = sgGeneral.add(new BoolSetting.Builder()
 			.name("remove-invisible")
 			.description("Removes bot only if they are invisible.")
 			.defaultValue(true)
+			.build()
+	);
+
+	private final Setting<Boolean> gameMode = sgFilters.add(new BoolSetting.Builder()
+			.name("null-gamemode")
+			.description("Removes players without a gamemode")
+			.defaultValue(true)
+			.build()
+	);
+
+	private final Setting<Boolean> api = sgFilters.add(new BoolSetting.Builder()
+			.name("null-entry")
+			.description("Removes players without a player entry")
+			.defaultValue(true)
+			.build()
+	);
+
+	private final Setting<Boolean> profile = sgFilters.add(new BoolSetting.Builder()
+			.name("null-profile")
+			.description("Removes players without a game profile")
+			.defaultValue(true)
+			.build()
+	);
+
+	private final Setting<Boolean> latency = sgFilters.add(new BoolSetting.Builder()
+			.name("ping-check")
+			.description("Removes players using ping check")
+			.defaultValue(false)
+			.build()
+	);
+
+	private final Setting<Boolean> nullException = sgFilters.add(new BoolSetting.Builder()
+			.name("null-exception")
+			.description("Removes players if a NullPointerException occurred")
+			.defaultValue(false)
 			.build()
 	);
 
@@ -49,7 +85,20 @@ public class AntiBot extends Module {
 		if (entity == null) return false;
 		if (!(entity instanceof PlayerEntity)) return false;
 
-		// Gamemode check
-		return EntityUtils.getGameMode(((PlayerEntity)entity)) == null; // Assume bot if invalid gamemode
+		PlayerEntity player = (PlayerEntity)entity;
+		try {
+			if (gameMode.get() && EntityUtils.getGameMode(player) == null) return true;
+			if (api.get() && 
+				mc.getNetworkHandler().getPlayerListEntry(player.getUuid()) == null) return true;
+			if (profile.get() && 
+				mc.getNetworkHandler().getPlayerListEntry(player.getUuid()).getProfile() == null) return true;
+			if (latency.get() &&
+				mc.getNetworkHandler().getPlayerListEntry(player.getUuid()).getLatency() > 1) return true;
+		} catch (NullPointerException e) {
+			if (nullException.get()) return true;
+		}
+		
+
+		return false;
 	}
 }
