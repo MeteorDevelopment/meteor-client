d576c7f63e95edcb7451088fbd75a93ee14fa18f
seasnail8169 <seasnail8169@gmail.com>
2021-04-12 03:39:08 +0100

    Forgot this

--- src/main/java/minegame159/meteorclient/mixin/EntityRenderDispatcherMixin.java
+++ src/main/java/minegame159/meteorclient/mixin/EntityRenderDispatcherMixin.java
@@ -15,11 +15,7 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.FallingBlockEntity;
-import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.decoration.ArmorStandEntity;
-import net.minecraft.entity.vehicle.MinecartEntity;
 import net.minecraft.util.math.Box;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -36,12 +32,9 @@ public class EntityRenderDispatcherMixin {
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
     private <E extends Entity> void onRenderHead(E entity, double x, double y, double z, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo info) {
         NoRender noRender = Modules.get().get(NoRender.class);
-        if ((noRender.noItems() && entity instanceof ItemEntity)
-                || (noRender.noFallingBlocks() && entity instanceof FallingBlockEntity)
-                || (noRender.noArmorStands() && entity instanceof ArmorStandEntity)
-                || ( noRender.noXpOrbs() && entity instanceof ExperienceOrbEntity)
-                || (noRender.noMinecarts() && entity instanceof MinecartEntity)
-        ) info.cancel();
+        if (!noRender.isActive()) return;
+
+        if (noRender.noEntity(entity) || (noRender.noFallingBlocks() && entity instanceof FallingBlockEntity)) info.cancel();
     }
 
     @Inject(method = "drawBox", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;drawBox(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;Lnet/minecraft/util/math/Box;FFFF)V"), locals = LocalCapture.CAPTURE_FAILSOFT)
