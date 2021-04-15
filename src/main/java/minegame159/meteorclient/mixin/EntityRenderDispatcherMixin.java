9f1cf933d1e3813d15c3b267f00db597f150a7f7
seasnail8169 <seasnail@meteorclient.com>
2021-04-15 03:46:07 +0100

    Rewrote and merged Chams and ModelTweaks

--- src/main/java/minegame159/meteorclient/mixin/EntityRenderDispatcherMixin.java
+++ src/main/java/minegame159/meteorclient/mixin/EntityRenderDispatcherMixin.java
@@ -8,14 +8,11 @@ package minegame159.meteorclient.mixin;
 import minegame159.meteorclient.mixininterface.IBox;
 import minegame159.meteorclient.systems.modules.Modules;
 import minegame159.meteorclient.systems.modules.combat.Hitboxes;
-import minegame159.meteorclient.systems.modules.render.NoRender;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.FallingBlockEntity;
 import net.minecraft.util.math.Box;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -28,14 +25,6 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @Mixin(EntityRenderDispatcher.class)
 public class EntityRenderDispatcherMixin {
     @Shadow public Camera camera;
-
-    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
-    private <E extends Entity> void onRenderHead(E entity, double x, double y, double z, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo info) {
-        NoRender noRender = Modules.get().get(NoRender.class);
-        if (!noRender.isActive()) return;
-
-        if (noRender.noEntity(entity) || (noRender.noFallingBlocks() && entity instanceof FallingBlockEntity)) info.cancel();
-    }
 
     @Inject(method = "drawBox", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;drawBox(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;Lnet/minecraft/util/math/Box;FFFF)V"), locals = LocalCapture.CAPTURE_FAILSOFT)
     private void onDrawBox(MatrixStack matrix, VertexConsumer vertices, Entity entity, float red, float green, float blue, CallbackInfo info, Box box) {
---
+++ src/main/java/minegame159/meteorclient/mixin/PlayerEntityRendererMixin.java
@@ -0,0 +1,65 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
+ * Copyright (c) 2021 Meteor Development.
+ */
+
+package minegame159.meteorclient.mixin;
+
+import minegame159.meteorclient.systems.modules.Modules;
+import minegame159.meteorclient.systems.modules.render.Chams;
+import minegame159.meteorclient.systems.modules.render.HandView;
+import minegame159.meteorclient.utils.render.color.Color;
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+
+@Mixin(PlayerEntityRenderer.class)
+public class PlayerEntityRendererMixin {
+
+    @ModifyArgs(method = "renderArm", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ModelPart;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;II)V", ordinal = 0))
+    private void modifyRenderLayer(Args args, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, AbstractClientPlayerEntity player, ModelPart arm, ModelPart sleeve) {
+        Chams chams = Modules.get().get(Chams.class);
+
+        if (chams.isActive() && chams.hand.get()) {
+            Identifier texture = chams.handTexture.get() ? player.getSkinTexture() : Chams.BLANK;
+            args.set(1, vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(texture)));
+        }
+    }
+
+    @Redirect(method = "renderArm", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ModelPart;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;II)V", ordinal = 0))
+    private void redirectRenderMain(ModelPart modelPart, MatrixStack matrices, VertexConsumer vertices, int light, int overlay) {
+        Chams chams = Modules.get().get(Chams.class);
+
+        if (chams.isActive() && chams.hand.get()) {
+            Color color = chams.handColor.get();
+            modelPart.render(matrices, vertices, light, overlay, color.r/255f, color.g/255f, color.b/255f, color.a/255f);
+        } else {
+            modelPart.render(matrices, vertices, light, overlay);
+        }
+    }
+
+    @Redirect(method = "renderArm", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ModelPart;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;II)V", ordinal = 1))
+    private void redirectRenderSleeve(ModelPart modelPart, MatrixStack matrices, VertexConsumer vertices, int light, int overlay) {
+        Chams chams = Modules.get().get(Chams.class);
+
+        if (Modules.get().isActive(HandView.class)) return;
+
+        if (chams.isActive() && chams.hand.get()) {
+            Color color = chams.handColor.get();
+            modelPart.render(matrices, vertices, light, overlay, color.r/255f, color.g/255f, color.b/255f, color.a/255f);
+        } else {
+            modelPart.render(matrices, vertices, light, overlay);
+        }
+    }
+
+}
