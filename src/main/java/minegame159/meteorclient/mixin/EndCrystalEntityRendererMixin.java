4cae3c093d7e941baf3e37b0ae12c38094ee37d9
seasnail8169 <seasnail@meteorclient.com>
2021-04-15 04:07:54 +0100

    Actually this is even better

--- src/main/java/minegame159/meteorclient/mixin/EndCrystalEntityRendererMixin.java
+++ src/main/java/minegame159/meteorclient/mixin/EndCrystalEntityRendererMixin.java
@@ -43,9 +43,8 @@ public abstract class EndCrystalEntityRendererMixin {
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
     private void render(EndCrystalEntity endCrystalEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, CallbackInfo ci) {
         Chams module = Modules.get().get(Chams.class);
-        if (!module.isActive()) return;
 
-        END_CRYSTAL = RenderLayer.getEntityTranslucent((module.crystals.get() && !module.crystalsTexture.get()) ? Chams.BLANK : TEXTURE);
+        END_CRYSTAL = RenderLayer.getEntityTranslucent((module.isActive() && module.crystals.get() && !module.crystalsTexture.get()) ? Chams.BLANK : TEXTURE);
     }
 
     // Scale
