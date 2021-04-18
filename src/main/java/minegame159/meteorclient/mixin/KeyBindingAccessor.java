71decaa818d9dc4adbd5e223b49915a8ffa87bf7
MineGame159 <petulko08@gmail.com>
2021-04-18 12:50:48 +0200

    Fabric API is now not required

---
+++ src/main/java/minegame159/meteorclient/mixin/KeyBindingAccessor.java
@@ -0,0 +1,22 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
+ * Copyright (c) 2021 Meteor Development.
+ */
+
+package minegame159.meteorclient.mixin;
+
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@Mixin(KeyBinding.class)
+public interface KeyBindingAccessor {
+    @Accessor("categoryOrderMap")
+    static Map<String, Integer> getCategoryOrderMap() { return null; }
+
+    @Accessor("boundKey")
+    InputUtil.Key getKey();
+}
