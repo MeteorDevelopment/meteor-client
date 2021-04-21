5fe1d3e4276e63df195d2d2fd6ecb97dfb76e6ec
MineGame159 <petulko08@gmail.com>
2021-04-21 18:54:17 +0200

    Rewrote Trajectories

---
+++ src/main/java/minegame159/meteorclient/mixin/CrossbowItemAccessor.java
@@ -0,0 +1,17 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
+ * Copyright (c) 2021 Meteor Development.
+ */
+
+package minegame159.meteorclient.mixin;
+
+import net.minecraft.item.CrossbowItem;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(CrossbowItem.class)
+public interface CrossbowItemAccessor {
+    @Invoker("getSpeed")
+    static float getSpeed(ItemStack itemStack) { return 0; }
+}
