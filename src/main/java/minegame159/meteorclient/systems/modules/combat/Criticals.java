b4c34a73e33fed7f3a459a7e2b9e72d058faa3f4
seasnail8169 <seasnail@meteorclient.com>
2021-04-28 23:08:57 +0100

    Added NCP criticals bypass

--- src/main/java/minegame159/meteorclient/systems/modules/combat/Criticals.java
+++ src/main/java/minegame159/meteorclient/systems/modules/combat/Criticals.java
@@ -19,17 +19,11 @@ import minegame159.meteorclient.settings.SettingGroup;
 import minegame159.meteorclient.systems.modules.Categories;
 import minegame159.meteorclient.systems.modules.Module;
 import minegame159.meteorclient.systems.modules.Modules;
-import net.minecraft.entity.decoration.EndCrystalEntity;
 import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
 public class Criticals extends Module {
-    public enum Mode {
-        Packet,
-        Jump,
-        MiniJump
-    }
 
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
@@ -43,13 +37,6 @@ public class Criticals extends Module {
     private final Setting<Boolean> ka = sgGeneral.add(new BoolSetting.Builder()
             .name("only-killaura")
             .description("Only performs crits when using killaura.")
-            .defaultValue(false)
-            .build()
-    );
-
-    private final Setting<Boolean> crystals = sgGeneral.add(new BoolSetting.Builder()
-            .name("crystals")
-            .description("Wether to crit crystals or not.")
             .defaultValue(false)
             .build()
     );
@@ -74,14 +61,39 @@ public class Criticals extends Module {
     @EventHandler
     private void onSendPacket(PacketEvent.Send event) {
         if (event.packet instanceof PlayerInteractEntityC2SPacket && ((PlayerInteractEntityC2SPacket) event.packet).getType() == PlayerInteractEntityC2SPacket.InteractionType.ATTACK) {
-            if(((PlayerInteractEntityC2SPacket) event.packet).getEntity(mc.world) != Modules.get().get(KillAura.class).getTarget() && ka.get()) return;
-            if((((PlayerInteractEntityC2SPacket) event.packet).getEntity(mc.world) instanceof EndCrystalEntity) && !crystals.get()) return;
+
             if (skipCrit()) return;
-            if (mode.get() == Mode.Packet) doPacketMode();
-            else doJumpMode(event);
+            if (((PlayerInteractEntityC2SPacket) event.packet).getEntity(mc.world) != Modules.get().get(KillAura.class).getTarget() && ka.get()) return;
+
+            switch (mode.get()) {
+                case Packet:
+                    sendPacket(0.0625);
+                    sendPacket(0);
+                    break;
+                case Bypass:
+                    sendPacket(0.11);
+                    sendPacket(0.1100013579);
+                    sendPacket(0.0000013579);
+                    break;
+                default:
+                    if (!sendPackets) {
+                        sendPackets = true;
+                        sendTimer = mode.get() == Mode.Jump ? 6 : 4;
+                        attackPacket = (PlayerInteractEntityC2SPacket) event.packet;
+
+                        if (mode.get() == Mode.Jump) mc.player.jump();
+                        else ((IVec3d) mc.player.getVelocity()).setY(0.25);
+                        event.cancel();
+                    }
+            }
         } else if (event.packet instanceof HandSwingC2SPacket && mode.get() != Mode.Packet) {
             if (skipCrit()) return;
-            doJumpModeSwing(event);
+
+            if (sendPackets && swingPacket == null) {
+                swingPacket = (HandSwingC2SPacket) event.packet;
+
+                event.cancel();
+            }
         }
     }
 
@@ -103,50 +115,30 @@ public class Criticals extends Module {
         }
     }
 
-    private void doPacketMode() {
+    private void sendPacket(double height) {
         double x = mc.player.getX();
         double y = mc.player.getY();
         double z = mc.player.getZ();
 
-        PlayerMoveC2SPacket p1 = new PlayerMoveC2SPacket.PositionOnly(x, y + 0.0625, z, false);
-        PlayerMoveC2SPacket p2 = new PlayerMoveC2SPacket.PositionOnly(x, y, z, false);
+        PlayerMoveC2SPacket packet = new PlayerMoveC2SPacket.PositionOnly(x, y + height, z, false);
+        ((IPlayerMoveC2SPacket) packet).setTag(1337);
 
-        //Ignore IntelliJ, it's fucking stupid. This is valid.
-        ((IPlayerMoveC2SPacket) p1).setTag(1337);
-        ((IPlayerMoveC2SPacket) p2).setTag(1337);
-
-        mc.player.networkHandler.sendPacket(p1);
-        mc.player.networkHandler.sendPacket(p2);
-    }
-
-    private void doJumpMode(PacketEvent.Send event) {
-        if (!sendPackets) {
-            sendPackets = true;
-            sendTimer = mode.get() == Mode.Jump ? 6 : 4;
-            attackPacket = (PlayerInteractEntityC2SPacket) event.packet;
-
-            if (mode.get() == Mode.Jump) mc.player.jump();
-            else ((IVec3d) mc.player.getVelocity()).setY(0.25);
-            event.cancel();
-        }
-    }
-
-    private void doJumpModeSwing(PacketEvent.Send event) {
-        if (sendPackets && swingPacket == null) {
-            swingPacket = (HandSwingC2SPacket) event.packet;
-
-            event.cancel();
-        }
+        mc.player.networkHandler.sendPacket(packet);
     }
 
     private boolean skipCrit() {
-        boolean a = !mc.player.isSubmergedInWater() && !mc.player.isInLava() && !mc.player.isClimbing();
-        if (!mc.player.isOnGround()) return true;
-        return !a;
+        return !mc.player.isOnGround() || mc.player.isSubmergedInWater() || mc.player.isInLava()  || mc.player.isClimbing();
     }
 
     @Override
     public String getInfoString() {
         return mode.get().name();
+    }
+
+    public enum Mode {
+        Packet,
+        Bypass,
+        Jump,
+        MiniJump
     }
 }
