1249544dc8edb7ef6e046246c2f35db1d77cfbb8
Declipsonator <72658686+Declipsonator@users.noreply.github.com>
2021-04-29 07:56:13 -0400

    Add Weapon Option to Trigger
    Implements #590

--- src/main/java/minegame159/meteorclient/systems/modules/combat/Trigger.java
+++ src/main/java/minegame159/meteorclient/systems/modules/combat/Trigger.java
@@ -8,6 +8,7 @@ package minegame159.meteorclient.systems.modules.combat;
 import meteordevelopment.orbit.EventHandler;
 import minegame159.meteorclient.events.world.TickEvent;
 import minegame159.meteorclient.settings.BoolSetting;
+import minegame159.meteorclient.settings.EnumSetting;
 import minegame159.meteorclient.settings.Setting;
 import minegame159.meteorclient.settings.SettingGroup;
 import minegame159.meteorclient.systems.modules.Categories;
@@ -15,15 +16,31 @@ import minegame159.meteorclient.systems.modules.Module;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.SwordItem;
 import net.minecraft.util.Hand;
 
 public class Trigger extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
-    
+
+    public enum weapons {
+        Sword,
+        Axe,
+        Both,
+        Any
+    }
+
     private final Setting<Boolean> whenHoldingLeftClick = sgGeneral.add(new BoolSetting.Builder()
             .name("when-holding-left-click")
             .description("Attacks only when you are holding left click.")
             .defaultValue(false)
+            .build()
+    );
+
+    private final Setting<weapons> weapon = sgGeneral.add(new EnumSetting.Builder<weapons>()
+            .name("weapon")
+            .description("Only attacks an entity when a specified weapon is in your hand.")
+            .defaultValue(weapons.Any)
             .build()
     );
 
@@ -37,28 +54,46 @@ public class Trigger extends Module {
     private void onTick(TickEvent.Post event) {
         target = null;
 
+        //Make sure that the player is not dead and that the entity is still alive
         if (mc.player.getHealth() <= 0 || mc.player.getAttackCooldownProgress(0.5f) < 1) return;
         if (!(mc.targetedEntity instanceof LivingEntity)) return;
         if (((LivingEntity) mc.targetedEntity).getHealth() <= 0) return;
 
         target = mc.targetedEntity;
 
-        if (whenHoldingLeftClick.get()) {
-            if (mc.options.keyAttack.isPressed()) attack(target);
-        } else {
-            attack(target);
+        //Checks if the player is holding the specified weapon
+        if(
+                weapon.get() == weapons.Axe && mc.player.getMainHandStack().getItem() instanceof AxeItem ||
+                weapon.get() == weapons.Sword && mc.player.getMainHandStack().getItem() instanceof SwordItem ||
+                weapon.get() == weapons.Both && mc.player.getMainHandStack().getItem() instanceof AxeItem ||
+                weapon.get() == weapons.Both && mc.player.getMainHandStack().getItem() instanceof SwordItem ||
+                weapon.get() == weapons.Any
+        ) {
+            //Attack the entity
+            if (whenHoldingLeftClick.get()) {
+                if (mc.options.keyAttack.isPressed()) attack(target);
+            } else {
+                attack(target);
+            }
+
         }
     }
 
     private void attack(Entity entity) {
+        //Attack the entity
         mc.interactionManager.attackEntity(mc.player, entity);
         mc.player.swingHand(Hand.MAIN_HAND);
     }
 
+
+
     @Override
     public String getInfoString() {
+        //Get a target to attack
         if (target != null && target instanceof PlayerEntity) return target.getEntityName();
         if (target != null) return target.getType().getName().getString();
         return null;
     }
+
+
 }
