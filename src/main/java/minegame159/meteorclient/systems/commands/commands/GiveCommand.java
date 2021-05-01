6b7972106ede17345bc4ab4577612b4eef700ca2
seasnail8169 <seasnail@meteorclient.com>
2021-05-01 17:52:46 +0100

    Fixed .give command

--- src/main/java/minegame159/meteorclient/systems/commands/commands/GiveCommand.java
+++ src/main/java/minegame159/meteorclient/systems/commands/commands/GiveCommand.java
@@ -4,19 +4,12 @@ package minegame159.meteorclient.systems.commands.commands;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import minegame159.meteorclient.systems.commands.Command;
 import minegame159.meteorclient.utils.player.SlotUtils;
-import minegame159.meteorclient.utils.player.ChatUtils;
-import minegame159.meteorclient.utils.Utils;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.argument.ItemStackArgumentType;
-import net.minecraft.command.argument.NbtTagArgumentType;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.StringNbtReader;
 import net.minecraft.network.packet.c2s.play.CreativeInventoryActionC2SPacket;
 import net.minecraft.text.LiteralText;
 
@@ -31,28 +24,29 @@ public class GiveCommand extends Command {
 
     @Override
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
-        builder.then(argument("item", ItemStackArgumentType.itemStack()).then(argument("number", IntegerArgumentType.integer()).executes(context -> {
-            if (!mc.player.isCreative()) throw NOT_IN_CREATIVE.create();
-            
-            ItemStack item = new ItemStack(context.getArgument("item", Item.class),context.getArgument("number", int.class));
-            addItem(item);
-            
-            return SINGLE_SUCCESS;
-        })));
         builder.then(argument("item", ItemStackArgumentType.itemStack()).executes(context -> {
             if (!mc.player.isCreative()) throw NOT_IN_CREATIVE.create();
-            
-            ItemStack item = new ItemStack(context.getArgument("item", Item.class),1);
+
+            ItemStack item = ItemStackArgumentType.getItemStackArgument(context, "item").createStack(1, false);
             addItem(item);
             
             return SINGLE_SUCCESS;
-        }));
+        }).then(argument("number", IntegerArgumentType.integer()).executes(context -> {
+            if (!mc.player.isCreative()) throw NOT_IN_CREATIVE.create();
+
+            ItemStack item = ItemStackArgumentType.getItemStackArgument(context, "item").createStack(IntegerArgumentType.getInteger(context, "number"), false);
+            addItem(item);
+
+            return SINGLE_SUCCESS;
+        })));
     }
-    public static void addItem(ItemStack item) {
-		for(int i = 0; i < 36; i++)
-		{
-			if(!mc.player.inventory.getStack(SlotUtils.indexToId(i)).isEmpty()) continue;
+
+    private void addItem(ItemStack item) {
+		for(int i = 0; i < 36; i++) {
+		    ItemStack stack = mc.player.inventory.getStack(SlotUtils.indexToId(i));
+			if (!stack.isEmpty()) continue;
 			mc.player.networkHandler.sendPacket(new CreativeInventoryActionC2SPacket(SlotUtils.indexToId(i), item));
+			return;
 		}
     }
 }
