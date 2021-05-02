000f8037ebb5610eed29d877f0bac0fd526734ce
seasnail8169 <seasnail@meteorclient.com>
2021-05-02 13:52:21 +0100

    Fixed some issues in AutoCity and CityESP

--- src/main/java/minegame159/meteorclient/systems/modules/combat/AutoCity.java
+++ src/main/java/minegame159/meteorclient/systems/modules/combat/AutoCity.java
@@ -5,6 +5,8 @@
 
 package minegame159.meteorclient.systems.modules.combat;
 
+import meteordevelopment.orbit.EventHandler;
+import minegame159.meteorclient.events.world.TickEvent;
 import minegame159.meteorclient.mixininterface.IClientPlayerInteractionManager;
 import minegame159.meteorclient.settings.BoolSetting;
 import minegame159.meteorclient.settings.DoubleSetting;
@@ -12,12 +14,13 @@ import minegame159.meteorclient.settings.Setting;
 import minegame159.meteorclient.settings.SettingGroup;
 import minegame159.meteorclient.systems.modules.Categories;
 import minegame159.meteorclient.systems.modules.Module;
+import minegame159.meteorclient.utils.entity.EntityUtils;
+import minegame159.meteorclient.utils.entity.SortPriority;
 import minegame159.meteorclient.utils.player.ChatUtils;
-import minegame159.meteorclient.utils.player.CityUtils;
 import minegame159.meteorclient.utils.player.InvUtils;
+import minegame159.meteorclient.utils.player.PlayerUtils;
 import minegame159.meteorclient.utils.player.Rotations;
 import minegame159.meteorclient.utils.world.BlockUtils;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
@@ -26,17 +29,16 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.MathHelper;
 
 public class AutoCity extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    private final Setting<Double> range = sgGeneral.add(new DoubleSetting.Builder()
-            .name("range")
-            .description("The maximum range a city-able block will be found.")
-            .defaultValue(5)
+    private final Setting<Double> targetRange = sgGeneral.add(new DoubleSetting.Builder()
+            .name("target-range")
+            .description("The radius in which players get targeted.")
+            .defaultValue(4)
             .min(0)
-            .sliderMax(20)
+            .sliderMax(5)
             .build()
     );
 
@@ -47,9 +49,16 @@ public class AutoCity extends Module {
             .build()
     );
 
-    private final Setting<Boolean> chatInfo = sgGeneral.add(new BoolSetting.Builder()
-            .name("chat-info")
-            .description("Sends a client-side message if you city a player.")
+    private final Setting<Boolean> rotate = sgGeneral.add(new BoolSetting.Builder()
+            .name("rotate")
+            .description("Automatically rotates you towards the city block.")
+            .defaultValue(true)
+            .build()
+    );
+
+    private final Setting<Boolean> selfToggle = sgGeneral.add(new BoolSetting.Builder()
+            .name("self-toggle")
+            .description("Automatically toggles off after activation.")
             .defaultValue(true)
             .build()
     );
@@ -61,89 +70,86 @@ public class AutoCity extends Module {
             .build()
     );
 
-    private final Setting<Boolean> rotate = sgGeneral.add(new BoolSetting.Builder()
-            .name("rotate")
-            .description("Automatically rotates you towards the city block.")
-            .defaultValue(true)
-            .build()
-    );
+    private PlayerEntity target;
+    private BlockPos blockPosTarget;
+    private boolean sentMessage;
 
     public AutoCity() {
         super(Categories.Combat, "auto-city", "Automatically cities a target by mining the nearest obsidian next to them.");
     }
 
-    private PlayerEntity target;
-
-    @Override
-    public void onActivate() {
-        target = CityUtils.getPlayerTarget(range.get());
-        BlockPos mineTarget = CityUtils.getTargetBlock(target);
-
-        if (target == null || mineTarget == null) {
-            if (chatInfo.get()) ChatUtils.moduleError(this, "No target block found... disabling.");
-        } else {
-            if (chatInfo.get()) ChatUtils.moduleInfo(this, "Attempting to city " + target.getGameProfile().getName());
-
-            if (MathHelper.sqrt(mc.player.squaredDistanceTo(mineTarget.getX(), mineTarget.getY(), mineTarget.getZ())) > mc.interactionManager.getReachDistance()) {
-                if (chatInfo.get()) ChatUtils.moduleError(this, "Target block out of reach... disabling.");
-                toggle();
-                return;
-            }
-
-            int slot = InvUtils.findItemInHotbar(Items.NETHERITE_PICKAXE);
-            if (slot == -1) slot = InvUtils.findItemInHotbar(Items.DIAMOND_PICKAXE);
-            if (mc.player.abilities.creativeMode) slot = mc.player.inventory.selectedSlot;
-
-            if (slot == -1) {
-                if (chatInfo.get()) ChatUtils.moduleError(this, "No pick found... disabling.");
-                toggle();
-                return;
-            }
-
-
-            if (support.get()) {
-                int obbySlot = InvUtils.findItemInHotbar(Items.OBSIDIAN);
-                BlockPos blockPos = mineTarget.down(1);
-
-                if (!BlockUtils.canPlace(blockPos)
-                        && mc.world.getBlockState(blockPos).getBlock() != Blocks.OBSIDIAN
-                        && mc.world.getBlockState(blockPos).getBlock() != Blocks.BEDROCK
-                        && chatInfo.get()) {
-                    ChatUtils.moduleWarning(this, "Couldn't place support block, mining anyway.");
-                }
-                else {
-                    if (obbySlot == -1) {
-                        if (chatInfo.get()) ChatUtils.moduleWarning(this, "No obsidian found for support, mining anyway.");
-                    }
-                    else {
-                        BlockUtils.place(blockPos, Hand.MAIN_HAND, obbySlot, rotate.get(), 0, true);
-                    }
-                }
-            }
-
-            mc.player.inventory.selectedSlot = slot;
-
-            if (rotate.get()) Rotations.rotate(Rotations.getYaw(mineTarget), Rotations.getPitch(mineTarget), () -> mine(mineTarget));
-            else mine(mineTarget);
-
-            if (crystal.get()) {
-                if (!BlockUtils.canPlace(mineTarget, true)) return;
-                Hand hand = InvUtils.getHand(Items.END_CRYSTAL);
-
-                if (hand == Hand.MAIN_HAND) {
-                    int crystalSlot = InvUtils.findItemInHotbar(Items.END_CRYSTAL);
-                    int preSlot = mc.player.inventory.selectedSlot;
-                    mc.player.inventory.selectedSlot = crystalSlot;
-                    ((IClientPlayerInteractionManager) mc.interactionManager).syncSelectedSlot2();
-                    mc.player.networkHandler.sendPacket(new PlayerInteractBlockC2SPacket(hand, new BlockHitResult(mc.player.getPos(), Direction.UP, mineTarget, false)));
-                    mc.player.inventory.selectedSlot = preSlot;
-                } else if (hand == Hand.OFF_HAND) {
-                    mc.player.networkHandler.sendPacket(new PlayerInteractBlockC2SPacket(hand, new BlockHitResult(mc.player.getPos(), Direction.UP, mineTarget, false)));
-                }
-            }
+    @EventHandler
+    private void onTick(TickEvent.Pre event) {
+        if (EntityUtils.isBadTarget(target, targetRange.get())) {
+            PlayerEntity search = EntityUtils.getPlayerTarget(targetRange.get(), SortPriority.LowestDistance, false);
+            if (search != target) sentMessage = false;
+            target = search;
         }
 
-        this.toggle();
+        if (EntityUtils.isBadTarget(target, targetRange.get())) {
+            target = null;
+            blockPosTarget = null;
+            if (selfToggle.get()) toggle();
+            return;
+        }
+
+        blockPosTarget = EntityUtils.getCityBlock(target);
+
+        if (blockPosTarget == null) {
+            if (selfToggle.get()) {
+                ChatUtils.moduleError(this, "No target block found... disabling.");
+                toggle();
+            }
+            target = null;
+            return;
+        }
+
+        if (PlayerUtils.distanceTo(blockPosTarget) > mc.interactionManager.getReachDistance() && selfToggle.get()) {
+            ChatUtils.moduleError(this, "Target block out of reach... disabling.");
+            toggle();
+            return;
+        }
+
+        if (!sentMessage) {
+            ChatUtils.moduleInfo(this, "Attempting to city " + target.getGameProfile().getName());
+            sentMessage = true;
+        }
+
+        int slot = InvUtils.findItemInHotbar(itemStack -> itemStack.getItem() == Items.DIAMOND_PICKAXE || itemStack.getItem() == Items.NETHERITE_PICKAXE);
+        if (mc.player.abilities.creativeMode) slot = mc.player.inventory.selectedSlot;
+
+        if (slot == -1) {
+            if (selfToggle.get()) {
+                ChatUtils.moduleError(this, "No pickaxe found... disabling.");
+                toggle();
+            }
+            return;
+        }
+
+        if (support.get()) {
+            BlockUtils.place(blockPosTarget.down(1), Hand.MAIN_HAND, InvUtils.findItemInHotbar(Items.OBSIDIAN), rotate.get(), 0, true);
+        }
+
+        mc.player.inventory.selectedSlot = slot;
+
+        if (rotate.get()) Rotations.rotate(Rotations.getYaw(blockPosTarget), Rotations.getPitch(blockPosTarget), () -> mine(blockPosTarget));
+        else mine(blockPosTarget);
+
+        if (crystal.get()) {
+            if (!BlockUtils.canPlace(blockPosTarget, true)) return;
+            Hand hand = InvUtils.getHand(Items.END_CRYSTAL);
+
+            if (hand == Hand.MAIN_HAND) {
+                int crystalSlot = InvUtils.findItemInHotbar(Items.END_CRYSTAL);
+                int preSlot = mc.player.inventory.selectedSlot;
+                mc.player.inventory.selectedSlot = crystalSlot;
+                ((IClientPlayerInteractionManager) mc.interactionManager).syncSelectedSlot2();
+                mc.player.networkHandler.sendPacket(new PlayerInteractBlockC2SPacket(hand, new BlockHitResult(mc.player.getPos(), Direction.UP, blockPosTarget, false)));
+                mc.player.inventory.selectedSlot = preSlot;
+            } else if (hand == Hand.OFF_HAND) {
+                mc.player.networkHandler.sendPacket(new PlayerInteractBlockC2SPacket(hand, new BlockHitResult(mc.player.getPos(), Direction.UP, blockPosTarget, false)));
+            }
+        }
     }
 
     private void mine(BlockPos blockPos) {
