f3fbb7bd8966b87a3b756107b8e437c8325b1815
seasnail8169 <seasnail@meteorclient.com>
2021-05-02 01:17:08 +0100

    cleaned up AutoWeb

--- src/main/java/minegame159/meteorclient/systems/modules/combat/AutoWeb.java
+++ src/main/java/minegame159/meteorclient/systems/modules/combat/AutoWeb.java
@@ -7,30 +7,32 @@ package minegame159.meteorclient.systems.modules.combat;
 
 import meteordevelopment.orbit.EventHandler;
 import minegame159.meteorclient.events.world.TickEvent;
-import minegame159.meteorclient.settings.BoolSetting;
-import minegame159.meteorclient.settings.DoubleSetting;
-import minegame159.meteorclient.settings.Setting;
-import minegame159.meteorclient.settings.SettingGroup;
-import minegame159.meteorclient.systems.friends.Friends;
+import minegame159.meteorclient.settings.*;
 import minegame159.meteorclient.systems.modules.Categories;
 import minegame159.meteorclient.systems.modules.Module;
-import minegame159.meteorclient.utils.entity.FakePlayerEntity;
-import minegame159.meteorclient.utils.entity.FakePlayerUtils;
+import minegame159.meteorclient.utils.entity.EntityUtils;
+import minegame159.meteorclient.utils.entity.SortPriority;
 import minegame159.meteorclient.utils.player.InvUtils;
 import minegame159.meteorclient.utils.world.BlockUtils;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockPos;
 
 public class AutoWeb extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
     private final Setting<Double> range = sgGeneral.add(new DoubleSetting.Builder()
-            .name("range")
-            .description("The maximum distance to be able to place webs.")
+            .name("target-range")
+            .description("The maximum distance to target players.")
             .defaultValue(4)
             .min(0)
+            .build()
+    );
+
+    private final Setting<SortPriority> priority = sgGeneral.add(new EnumSetting.Builder<SortPriority>()
+            .name("target-priority")
+            .description("How to select the player to target.")
+            .defaultValue(SortPriority.LowestDistance)
             .build()
     );
 
@@ -56,44 +58,12 @@ public class AutoWeb extends Module {
 
     @EventHandler
     private void onTick(TickEvent.Pre event) {
-        int slot = InvUtils.findItemInHotbar(Items.COBWEB);
-        if (slot == -1) return;
-
-        if (target != null) {
-            if (mc.player.distanceTo(target) > range.get() || !target.isAlive()) target = null;
+        if (EntityUtils.isBadTarget(target, range.get())) {
+            target = EntityUtils.getPlayerTarget(range.get(), priority.get(), false);
         }
+        if (EntityUtils.isBadTarget(target, range.get())) return;
 
-        for (PlayerEntity player : mc.world.getPlayers()) {
-            if (player == mc.player || !Friends.get().attack(player) || !player.isAlive() || mc.player.distanceTo(player) > range.get()) continue;
-
-            if (target == null) {
-                target = player;
-            } else if (mc.player.distanceTo(target) > mc.player.distanceTo(player)) {
-                target = player;
-            }
-        }
-
-        if (target == null) {
-            for (FakePlayerEntity fakeTarget : FakePlayerUtils.getPlayers().keySet()) {
-                if (fakeTarget.getHealth() <= 0 || !Friends.get().attack(fakeTarget) || !fakeTarget.isAlive()) continue;
-
-                if (target == null) {
-                    target = fakeTarget;
-                    continue;
-                }
-
-                if (mc.player.distanceTo(fakeTarget) < mc.player.distanceTo(target)) target = fakeTarget;
-            }
-        }
-
-        if (target != null) {
-            BlockPos targetPos = target.getBlockPos();
-            BlockUtils.place(targetPos, Hand.MAIN_HAND, slot, rotate.get(), 0, false);
-
-            if (doubles.get()) {
-                targetPos = targetPos.add(0, 1, 0);
-                BlockUtils.place(targetPos, Hand.MAIN_HAND, InvUtils.findItemInHotbar(Items.COBWEB), rotate.get(), 0, false);
-            }
-        }
+        BlockUtils.place(target.getBlockPos(), Hand.MAIN_HAND, InvUtils.findItemInHotbar(Items.COBWEB), rotate.get(), 0, false);
+        if (doubles.get()) BlockUtils.place(target.getBlockPos().add(0, 1, 0), Hand.MAIN_HAND, InvUtils.findItemInHotbar(Items.COBWEB), rotate.get(), 0, false);
     }
 }
