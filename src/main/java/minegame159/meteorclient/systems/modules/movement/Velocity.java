5cac31d99115f3f2a3c09d17e0733effed843f7f
seasnail8169 <seasnail@meteorclient.com>
2021-05-02 17:42:37 +0100

    General SettingGroup consistency

--- src/main/java/minegame159/meteorclient/systems/modules/movement/Velocity.java
+++ src/main/java/minegame159/meteorclient/systems/modules/movement/Velocity.java
@@ -13,37 +13,37 @@ import minegame159.meteorclient.systems.modules.Categories;
 import minegame159.meteorclient.systems.modules.Module;
 
 public class Velocity extends Module {
-    private final SettingGroup sgDefault = settings.getDefaultGroup();
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    public final Setting<Boolean> entities = sgDefault.add(new BoolSetting.Builder()
+    public final Setting<Boolean> entities = sgGeneral.add(new BoolSetting.Builder()
             .name("entities")
             .description("Modifies the amount of knockback you take from entities and attacks.")
             .defaultValue(true)
             .build()
     );
 
-    public final Setting<Boolean> explosions = sgDefault.add(new BoolSetting.Builder()
+    public final Setting<Boolean> explosions = sgGeneral.add(new BoolSetting.Builder()
             .name("explosions")
             .description("Modifies your knockback from explosions.")
             .defaultValue(true)
             .build()
     );
 
-    public final Setting<Boolean> liquids = sgDefault.add(new BoolSetting.Builder()
+    public final Setting<Boolean> liquids = sgGeneral.add(new BoolSetting.Builder()
             .name("liquids")
             .description("Modifies the amount you are pushed by flowing liquids.")
             .defaultValue(false)
             .build()
     );
 
-    public final Setting<Boolean> noPush = sgDefault.add(new BoolSetting.Builder()
+    public final Setting<Boolean> noPush = sgGeneral.add(new BoolSetting.Builder()
             .name("no-push")
             .description("Attempts to stop getting pushed out of blocks.")
             .defaultValue(true)
             .build()
     );
 
-    private final Setting<Double> horizontal = sgDefault.add(new DoubleSetting.Builder()
+    private final Setting<Double> horizontal = sgGeneral.add(new DoubleSetting.Builder()
             .name("horizontal-multiplier")
             .description("How much velocity you will take horizontally.")
             .defaultValue(0)
@@ -52,7 +52,7 @@ public class Velocity extends Module {
             .build()
     );
 
-    private final Setting<Double> vertical = sgDefault.add(new DoubleSetting.Builder()
+    private final Setting<Double> vertical = sgGeneral.add(new DoubleSetting.Builder()
             .name("vertical-multiplier")
             .description("How much velocity you will take vertically.")
             .defaultValue(0)
