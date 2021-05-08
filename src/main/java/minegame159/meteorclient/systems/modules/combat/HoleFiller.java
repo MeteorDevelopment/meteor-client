da7b5cc4d695d00ab74808bca1add23c84745431
seasnail8169 <seasnail@meteorclient.com>
2021-05-08 17:25:57 +0100

    Rewrote HoleFiller to fill double holes and render all holes its going to fill.

--- src/main/java/minegame159/meteorclient/systems/modules/combat/HoleFiller.java
+++ src/main/java/minegame159/meteorclient/systems/modules/combat/HoleFiller.java
@@ -9,32 +9,41 @@ import meteordevelopment.orbit.EventHandler;
 import meteordevelopment.orbit.EventPriority;
 import minegame159.meteorclient.events.render.RenderEvent;
 import minegame159.meteorclient.events.world.TickEvent;
+import minegame159.meteorclient.mixin.AbstractBlockAccessor;
 import minegame159.meteorclient.rendering.Renderer;
 import minegame159.meteorclient.rendering.ShapeMode;
 import minegame159.meteorclient.settings.*;
 import minegame159.meteorclient.systems.modules.Categories;
 import minegame159.meteorclient.systems.modules.Module;
+import minegame159.meteorclient.utils.misc.Pool;
+import minegame159.meteorclient.utils.player.InvUtils;
 import minegame159.meteorclient.utils.render.color.SettingColor;
 import minegame159.meteorclient.utils.world.BlockIterator;
 import minegame159.meteorclient.utils.world.BlockUtils;
+import minegame159.meteorclient.utils.world.Dir;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class HoleFiller extends Module {
-    public enum PlaceMode {
-        Obsidian,
-        Cobweb,
-        Both,
-        Any
-    }
 
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
     private final SettingGroup sgRender = settings.createGroup("Render");
+
+    private final Setting<List<Block>> blocks = sgGeneral.add(new BlockListSetting.Builder()
+            .name("blocks")
+            .description("Which blocks can be used to fill holes.")
+            .defaultValue(Collections.singletonList(Blocks.OBSIDIAN))
+            .build()
+    );
 
     private final Setting<Integer> horizontalRadius = sgGeneral.add(new IntSetting.Builder()
             .name("horizontal-radius")
@@ -54,19 +63,20 @@ public class HoleFiller extends Module {
             .build()
     );
 
-    private final Setting<Integer> placeDelay = sgGeneral.add(new IntSetting.Builder()
-            .name("place-delay")
-            .description("The delay in ticks in between placement.")
-            .defaultValue(1)
-            .min(0)
-            .sliderMax(10)
+
+    private final Setting<Boolean> doubles = sgGeneral.add(new BoolSetting.Builder()
+            .name("doubles")
+            .description("Fills double holes.")
+            .defaultValue(true)
             .build()
     );
 
-    private final Setting<PlaceMode> mode = sgGeneral.add(new EnumSetting.Builder<PlaceMode>()
-            .name("block")
-            .description("The blocks you use to fill holes with.")
-            .defaultValue(PlaceMode.Obsidian)
+    private final Setting<Integer> placeDelay = sgGeneral.add(new IntSetting.Builder()
+            .name("delay")
+            .description("The ticks delay between placement.")
+            .defaultValue(1)
+            .min(0)
+            .sliderMax(10)
             .build()
     );
 
@@ -81,7 +91,7 @@ public class HoleFiller extends Module {
 
     private final Setting<Boolean> render = sgRender.add(new BoolSetting.Builder()
             .name("render")
-            .description("Renders a block overlay where the obsidian will be placed.")
+            .description("Renders an overlay where blocks will be placed.")
             .defaultValue(true)
             .build()
     );
@@ -95,20 +105,37 @@ public class HoleFiller extends Module {
 
     private final Setting<SettingColor> sideColor = sgRender.add(new ColorSetting.Builder()
             .name("side-color")
-            .description("The color of the sides of the blocks being rendered.")
-            .defaultValue(new SettingColor(204, 0, 0, 45))
+            .description("The side color of the target block rendering.")
+            .defaultValue(new SettingColor(197, 137, 232, 10))
             .build()
     );
 
     private final Setting<SettingColor> lineColor = sgRender.add(new ColorSetting.Builder()
             .name("line-color")
-            .description("The color of the lines of the blocks being rendered.")
-            .defaultValue(new SettingColor(204, 0, 0, 255))
+            .description("The line color of the target block rendering.")
+            .defaultValue(new SettingColor(197, 137, 232))
             .build()
     );
 
-    private final BlockPos.Mutable blockPos = new BlockPos.Mutable();
-    private int tickDelayLeft;
+    private final Setting<SettingColor> nextSideColor = sgRender.add(new ColorSetting.Builder()
+            .name("next-side-color")
+            .description("The side color of the next block to be placed.")
+            .defaultValue(new SettingColor(227, 196, 245, 10))
+            .build()
+    );
+
+    private final Setting<SettingColor> nextLineColor = sgRender.add(new ColorSetting.Builder()
+            .name("next-line-color")
+            .description("The line color of the next block to be placed.")
+            .defaultValue(new SettingColor(227, 196, 245))
+            .build()
+    );
+
+    private final Pool<Hole> holePool = new Pool<>(Hole::new);
+    private final List<Hole> holes = new ArrayList<>();
+    private int timer;
+
+    private final byte NULL = 0;
 
     public HoleFiller() {
         super(Categories.Combat, "hole-filler", "Fills holes with specified blocks.");
@@ -116,72 +143,91 @@ public class HoleFiller extends Module {
 
     @Override
     public void onActivate() {
-        tickDelayLeft = 0;
+        timer = 0;
     }
 
     @EventHandler
     private void onTick(TickEvent.Pre event) {
-        int slot = findSlot();
+        for (Hole hole : holes) holePool.free(hole);
+        holes.clear();
 
-        if (slot != -1 && tickDelayLeft <= 0) {
-            tickDelayLeft = placeDelay.get();
-            BlockIterator.register(horizontalRadius.get(), verticalRadius.get(), (blockPos1, blockState) -> {
-                if (!blockState.getMaterial().isReplaceable()) return;
+        int slot = InvUtils.findItemInHotbar(itemStack -> itemStack.getItem() instanceof BlockItem && blocks.get().contains(Block.getBlockFromItem(itemStack.getItem())));
 
-                blockPos.set(blockPos1);
+        if (slot != -1) {
+            BlockIterator.register(horizontalRadius.get(), verticalRadius.get(), (blockPos, blockState) -> {
+                if (!validHole(blockPos)) return;
 
-                Block bottom = mc.world.getBlockState(add(0, -1, 0)).getBlock();
-                if (bottom != Blocks.BEDROCK && bottom != Blocks.OBSIDIAN) return;
-                Block forward = mc.world.getBlockState(add(0, 1, 1)).getBlock();
-                if (forward != Blocks.BEDROCK && forward != Blocks.OBSIDIAN) return;
-                Block back = mc.world.getBlockState(add(0, 0, -2)).getBlock();
-                if (back != Blocks.BEDROCK && back != Blocks.OBSIDIAN) return;
-                Block right = mc.world.getBlockState(add(1, 0, 1)).getBlock();
-                if (right != Blocks.BEDROCK && right != Blocks.OBSIDIAN) return;
-                Block left = mc.world.getBlockState(add(-2, 0, 0)).getBlock();
-                if (left != Blocks.BEDROCK && left != Blocks.OBSIDIAN) return;
-                add(1, 0, 0);
+                int bedrock = 0, obsidian = 0;
+                Direction air = null;
 
-                if (BlockUtils.place(blockPos, Hand.MAIN_HAND, slot, rotate.get(), 0, true)) {
-                    BlockIterator.disableCurrent();
+                for (Direction direction : Direction.values()) {
+                    if (direction == Direction.UP) continue;
+
+                    BlockState state = mc.world.getBlockState(blockPos.offset(direction));
+
+                    if (state.getBlock() == Blocks.BEDROCK) bedrock++;
+                    else if (state.getBlock() == Blocks.OBSIDIAN) obsidian++;
+                    else if (direction == Direction.DOWN) return;
+                    else if (validHole(blockPos.offset(direction)) && air == null) {
+                        for (Direction dir : Direction.values()) {
+                            if (dir == direction.getOpposite() || dir == Direction.UP) continue;
+
+                            BlockState blockState1 = mc.world.getBlockState(blockPos.offset(direction).offset(dir));
+
+                            if (blockState1.getBlock() == Blocks.BEDROCK) bedrock++;
+                            else if (blockState1.getBlock() == Blocks.OBSIDIAN) obsidian++;
+                            else return;
+                        }
+
+                        air = direction;
+                    }
+                }
+
+                if (obsidian + bedrock == 5 && air == null) holes.add(holePool.get().set(blockPos, NULL));
+                else if (obsidian + bedrock == 8 && doubles.get() && air != null) {
+                    holes.add(holePool.get().set(blockPos, Dir.get(air)));
                 }
             });
         }
-        tickDelayLeft--;
     }
 
-    private int findSlot() {
-        for (int i = 0; i < 9; i++) {
-            ItemStack itemStack = mc.player.inventory.getStack(i);
-            switch (mode.get()) {
-                case Obsidian:
-                    if (itemStack.getItem() == Items.OBSIDIAN || itemStack.getItem() == Items.CRYING_OBSIDIAN) return i;
-                    break;
-                case Cobweb:
-                    if (itemStack.getItem() == Items.COBWEB) return i;
-                    break;
-                case Both:
-                    if (itemStack.getItem() == Items.COBWEB || itemStack.getItem() == Items.OBSIDIAN || itemStack.getItem() == Items.CRYING_OBSIDIAN) return i;
-                    break;
-                case Any:
-                    if (itemStack.getItem() instanceof BlockItem && ((BlockItem) itemStack.getItem()).getBlock().getDefaultState().isFullCube(mc.world, blockPos)) return i;
-                    break;
-            }
+    @EventHandler
+    private void onTickPost(TickEvent.Post event) {
+        if (timer <= 0 && !holes.isEmpty()) {
+            int slot = InvUtils.findItemInHotbar(itemStack -> itemStack.getItem() instanceof BlockItem && blocks.get().contains(Block.getBlockFromItem(itemStack.getItem())));
+            BlockUtils.place(holes.get(0).blockPos, Hand.MAIN_HAND, slot, rotate.get(), 0, true);
+            timer = placeDelay.get();
         }
 
-        return -1;
+        timer--;
+    }
+
+    private boolean validHole(BlockPos pos) {
+        if (mc.player.getBlockPos().equals(pos)) return false;
+        if (((AbstractBlockAccessor) mc.world.getBlockState(pos).getBlock()).isCollidable()) return false;
+        return !((AbstractBlockAccessor) mc.world.getBlockState(pos.up()).getBlock()).isCollidable();
     }
 
     @EventHandler(priority = EventPriority.HIGH)
     private void onRender(RenderEvent event) {
-        if (!render.get() || blockPos == null || blockPos.getY() == -1 || mc.world.getBlockState(blockPos).isAir()) return;
-        Renderer.boxWithLines(Renderer.NORMAL, Renderer.LINES, blockPos, sideColor.get(), lineColor.get(), shapeMode.get(), 0);
+        if (!render.get()) return;
+
+        for (Hole hole : holes) {
+            if (hole == holes.get(0)) Renderer.boxWithLines(Renderer.NORMAL, Renderer.LINES, hole.blockPos, nextSideColor.get(), nextLineColor.get(), shapeMode.get(), hole.exclude);
+            else Renderer.boxWithLines(Renderer.NORMAL, Renderer.LINES, hole.blockPos, sideColor.get(), lineColor.get(), shapeMode.get(), hole.exclude);
+        }
     }
 
-    private BlockPos.Mutable add(int x, int y, int z) {
-        blockPos.setX(blockPos.getX() + x);
-        blockPos.setY(blockPos.getY() + y);
-        blockPos.setZ(blockPos.getZ() + z);
-        return blockPos;
+    private static class Hole {
+        public BlockPos.Mutable blockPos = new BlockPos.Mutable();
+        public byte exclude;
+
+        public Hole set(BlockPos blockPos, byte exclude) {
+            this.blockPos.set(blockPos);
+            this.exclude = exclude;
+
+            return this;
+        }
+
     }
 }
