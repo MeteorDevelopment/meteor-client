1d491de707c02650fc61051cff674a3ac83264a7
Cypphi <altcypphi@gmail.com>
2021-05-23 16:32:15 +0200

    Fixed ugly hud colors lol

--- src/main/java/minegame159/meteorclient/systems/modules/render/hud/HUD.java
+++ src/main/java/minegame159/meteorclient/systems/modules/render/hud/HUD.java
@@ -55,7 +55,7 @@ public class HUD extends Module {
     public final Setting<SettingColor> secondaryColor = sgGeneral.add(new ColorSetting.Builder()
             .name("secondary-color")
             .description("Secondary color of text.")
-            .defaultValue(new SettingColor(188, 26, 141))
+            .defaultValue(new SettingColor(255, 255, 255))
             .build()
     );
 
