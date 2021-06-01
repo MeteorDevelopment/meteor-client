8b25f02890565ba6ef55d66cedf95b5664e2f75f
seasnail8169 <seasnail@meteorclient.com>
2021-06-01 13:38:26 +0100

    Improved the Spectate Command

--- src/main/java/minegame159/meteorclient/systems/commands/commands/SpectateCommand.java
+++ src/main/java/minegame159/meteorclient/systems/commands/commands/SpectateCommand.java
@@ -6,13 +6,19 @@
 package minegame159.meteorclient.systems.commands.commands;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import minegame159.meteorclient.systems.commands.arguments.PlayerArgumentType;
+import meteordevelopment.orbit.EventHandler;
+import minegame159.meteorclient.MeteorClient;
+import minegame159.meteorclient.events.meteor.KeyEvent;
 import minegame159.meteorclient.systems.commands.Command;
+import minegame159.meteorclient.systems.commands.arguments.PlayerArgumentType;
 import net.minecraft.command.CommandSource;
+import net.minecraft.text.LiteralText;
 
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
 
 public class SpectateCommand extends Command {
+
+    private final StaticListener shiftListener = new StaticListener();
 
     public SpectateCommand() {
         super("spectate", "Allows you to spectate nearby players");
@@ -20,15 +26,27 @@ public class SpectateCommand extends Command {
 
     @Override
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
-        builder.executes(ctx -> {
+        builder.then(literal("reset").executes(context -> {
             mc.setCameraEntity(mc.player);
             return SINGLE_SUCCESS;
-        });
+        }));
 
         builder.then(argument("player", PlayerArgumentType.player()).executes(context -> {
             mc.setCameraEntity(PlayerArgumentType.getPlayer(context));
+            mc.player.sendMessage(new LiteralText("Sneak to un-spectate."), true);
+            MeteorClient.EVENT_BUS.subscribe(shiftListener);
             return SINGLE_SUCCESS;
         }));
     }
 
+    private class StaticListener {
+        @EventHandler
+        private void onKey(KeyEvent event) {
+            if (mc.options.keySneak.matchesKey(event.key, 0) || mc.options.keySneak.matchesMouse(event.key)) {
+                mc.setCameraEntity(mc.player);
+                event.cancel();
+                MeteorClient.EVENT_BUS.unsubscribe(this);
+            }
+        }
+    }
 }
