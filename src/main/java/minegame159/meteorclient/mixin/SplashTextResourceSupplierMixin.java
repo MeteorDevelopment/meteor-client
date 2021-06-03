8a7a4b410562dbe342ce074d160efa5eb725e3e1
hashalite <hashalite@gmail.com>
2021-06-03 14:50:08 -0400

    Made Meteor splash texts toggleable

--- src/main/java/minegame159/meteorclient/mixin/SplashTextResourceSupplierMixin.java
+++ src/main/java/minegame159/meteorclient/mixin/SplashTextResourceSupplierMixin.java
@@ -5,6 +5,7 @@
 
 package minegame159.meteorclient.mixin;
 
+import minegame159.meteorclient.systems.config.Config;
 import net.minecraft.client.resource.SplashTextResourceSupplier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -25,7 +26,7 @@ public class SplashTextResourceSupplierMixin {
 
     @Inject(method = "get", at = @At("HEAD"), cancellable = true)
     private void onApply(CallbackInfoReturnable<String> cir) {
-        if (override) {
+        if (override && Config.get() != null && Config.get().titleScreenSplashes) {
             cir.setReturnValue(meteorSplashes.get(random.nextInt(meteorSplashes.size())));
         }
         override = !override;
