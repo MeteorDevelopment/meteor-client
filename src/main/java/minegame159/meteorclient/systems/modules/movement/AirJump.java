1a6dee1e24082d9462e7406520e91405c1d4fa1b
seasnail8169 <seasnail@meteorclient.com>
2021-06-07 15:21:34 +0100

    Rewrote AirJump

--- src/main/java/minegame159/meteorclient/systems/modules/movement/AirJump.java
+++ src/main/java/minegame159/meteorclient/systems/modules/movement/AirJump.java
@@ -20,49 +20,44 @@ import minegame159.meteorclient.utils.misc.input.KeyAction;
 public class AirJump extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    private final Setting<Boolean> maintainY = sgGeneral.add(new BoolSetting.Builder()
+    private final Setting<Boolean> maintainLevel = sgGeneral.add(new BoolSetting.Builder()
             .name("maintain-level")
-            .description("Maintains your current Y level.")
+            .description("Maintains your current Y level when holding the jump key.")
             .defaultValue(false)
             .build()
     );
 
-    private final Setting<Boolean> onHold = sgGeneral.add(new BoolSetting.Builder()
-            .name("on-hold")
-            .description("Whether or not to air jump if you hold down the space bar.")
-            .defaultValue(true)
-            .build()
-    );
-
-    private final Setting<Boolean> onGround = sgGeneral.add(new BoolSetting.Builder()
-            .name("on-ground")
-            .description("Whether to airjump if you are on the ground.")
-            .defaultValue(false)
-            .build()
-    );
+    private int level;
 
     public AirJump() {
         super(Categories.Movement, "air-jump", "Lets you jump in the air.");
     }
 
-    private int level = 0;
+    @Override
+    public void onActivate() {
+        level = mc.player.getBlockPos().getY();
+    }
 
     @EventHandler
     private void onKey(KeyEvent event) {
-        if (Modules.get().isActive(Freecam.class) || mc.currentScreen != null || (!onGround.get() && mc.player.isOnGround())) return;
-        if ((event.action == KeyAction.Press || (event.action == KeyAction.Repeat && onHold.get())) && mc.options.keyJump.matchesKey(event.key, 0)) {
-            mc.player.jump();
+        if (Modules.get().isActive(Freecam.class) || mc.currentScreen != null || mc.player.isOnGround()) return;
+
+        if (event.action != KeyAction.Press) return;
+
+        if (mc.options.keyJump.matchesKey(event.key, 0)) {
             level = mc.player.getBlockPos().getY();
+            mc.player.jump();
         }
-        if ((event.action == KeyAction.Press || (event.action == KeyAction.Repeat && onHold.get())) && mc.options.keySneak.matchesKey(event.key, 0)){
-            level -= 1;
+        else if (mc.options.keySneak.matchesKey(event.key, 0)) {
+            level--;
         }
     }
 
     @EventHandler
-    private void onTick(TickEvent.Post event) {
-        if (Modules.get().isActive(Freecam.class) || (!onGround.get() && mc.player.isOnGround())) return;
-        if (maintainY.get() && mc.player.getBlockPos().getY() == level){
+    private void onTick(TickEvent.Pre event) {
+        if (Modules.get().isActive(Freecam.class) || mc.player.isOnGround()) return;
+
+        if (maintainLevel.get() && mc.player.getBlockPos().getY() == level && mc.options.keyJump.isPressed()) {
             mc.player.jump();
         }
     }
