07745b53f4c549299f6f94aa302a1d6adf66c885
seasnail8169 <seasnail@meteorclient.com>
2021-06-10 17:01:14 +0100

    Updated accessors for mutability

--- src/main/java/minegame159/meteorclient/mixin/IdentifierAccessor.java
+++ src/main/java/minegame159/meteorclient/mixin/IdentifierAccessor.java
@@ -7,10 +7,12 @@ package minegame159.meteorclient.mixin;
 
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(Identifier.class)
 public interface IdentifierAccessor {
+    @Mutable
     @Accessor
     void setPath(String path);
 }
