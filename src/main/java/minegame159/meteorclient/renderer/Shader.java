6ad78d9c0b3cc3b0e24a2b4b26ac8cfdca2a73f9
MineGame159 <petulko08@gmail.com>
2021-06-11 20:07:45 +0200

    Maybe fixed AMB shader ESP crash

--- src/main/java/minegame159/meteorclient/renderer/Shader.java
+++ src/main/java/minegame159/meteorclient/renderer/Shader.java
@@ -4,14 +4,21 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import minegame159.meteorclient.MeteorClient;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Matrix4f;
 import org.apache.commons.io.IOUtils;
 import org.lwjgl.BufferUtils;
+import org.lwjgl.PointerBuffer;
+import org.lwjgl.system.APIUtil;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.MemoryUtil;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.nio.charset.StandardCharsets;
 
+import static minegame159.meteorclient.utils.Utils.mc;
 import static org.lwjgl.opengl.GL32C.*;
 
 public class Shader {
@@ -24,7 +31,7 @@ public class Shader {
 
     public Shader(String vertPath, String fragPath) {
         int vert = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vert, read(vertPath));
+        shaderSource(vert, read(vertPath));
         glCompileShader(vert);
 
         int[] a = new int[1];
@@ -35,7 +42,7 @@ public class Shader {
         }
 
         int frag = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(frag, read(fragPath));
+        shaderSource(frag, read(fragPath));
         glCompileShader(frag);
 
         glGetShaderiv(frag, GL_COMPILE_STATUS, a);
@@ -59,9 +66,27 @@ public class Shader {
         glDeleteShader(frag);
     }
 
+    // Apparently there is an AMD bug and this supposedly fixes it
+    private void shaderSource(int shader, String source) {
+        MemoryStack stack = MemoryStack.stackGet();
+        int stackPointer = stack.getPointer();
+
+        try {
+            ByteBuffer sourceBuffer = MemoryUtil.memUTF8(source, true);
+
+            PointerBuffer pointers = stack.mallocPointer(1);
+            pointers.put(sourceBuffer);
+
+            nglShaderSource(shader, 1, pointers.address0(), 0);
+            APIUtil.apiArrayFree(pointers.address0(), 1);
+        } finally {
+            stack.setPointer(stackPointer);
+        }
+    }
+
     private String read(String path) {
         try {
-            return IOUtils.toString(Shader.class.getResourceAsStream("/assets/meteor-client/shaders/" + path), StandardCharsets.UTF_8);
+            return IOUtils.toString(mc.getResourceManager().getResource(new Identifier("meteor-client", "shaders/" + path)).getInputStream(), StandardCharsets.UTF_8);
         } catch (IOException e) {
             e.printStackTrace();
             return "";
