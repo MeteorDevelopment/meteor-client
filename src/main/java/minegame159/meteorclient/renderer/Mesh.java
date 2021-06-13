5cb688a53c06173d97ad81482df3ce4ca5c8ef0c
MineGame159 <petulko08@gmail.com>
2021-06-13 15:31:44 +0200

    Revert "Improved how OpenGL state is handled" until fabric loader bug is fixed

--- src/main/java/minegame159/meteorclient/renderer/Mesh.java
+++ src/main/java/minegame159/meteorclient/renderer/Mesh.java
@@ -208,13 +208,12 @@ public class Mesh {
 
         if (indicesCount > 0) {
             // Setup opengl state and matrix stack
-            GL.saveState();
-
-            if (depthTest) GL.enableDepth();
-            else GL.disableDepth();
-            GL.enableBlend();
-            GL.disableCull();
-            GL.enableLineSmooth();
+            if (!depthTest) glDisable(GL_DEPTH_TEST);
+            glEnable(GL_BLEND);
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            glDisable(GL_CULL_FACE);
+            glEnable(GL_LINE_SMOOTH);
+            glLineWidth(1);
 
             MatrixStack matrixStack = RenderSystem.getModelViewStack();
 
@@ -244,7 +243,10 @@ public class Mesh {
 
             if (rendering3D) matrixStack.pop();
 
-            GL.restoreState();
+            glDisable(GL_LINE_SMOOTH);
+            glEnable(GL_CULL_FACE);
+            glDisable(GL_BLEND);
+            if (!depthTest) glEnable(GL_DEPTH_TEST);
         }
     }
 
