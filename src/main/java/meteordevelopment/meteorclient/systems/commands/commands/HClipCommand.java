db7a1a3693903d40602cdd13a9b4f5fa05bf9935
Cloudburst <serek.lego@gmail.com>
2021-06-15 21:44:19 +0200

    made .vclip & .hclip work with vehicles

--- src/main/java/meteordevelopment/meteorclient/systems/commands/commands/HClipCommand.java
+++ src/main/java/meteordevelopment/meteorclient/systems/commands/commands/HClipCommand.java
@@ -10,6 +10,7 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import meteordevelopment.meteorclient.systems.commands.Command;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.command.CommandSource;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.math.Vec3d;
 
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
@@ -27,6 +28,10 @@ public class HClipCommand extends Command {
 
             double blocks = context.getArgument("blocks", Double.class);
             Vec3d forward = Vec3d.fromPolar(0, player.getYaw()).normalize();
+            if (player.hasVehicle()) {
+                Entity vehicle = player.getVehicle();
+                vehicle.setPosition(vehicle.getX() + forward.x * blocks, vehicle.getY(), vehicle.getZ() + forward.z * blocks);
+            }
             player.setPosition(player.getX() + forward.x * blocks, player.getY(), player.getZ() + forward.z * blocks);
 
             return SINGLE_SUCCESS;
