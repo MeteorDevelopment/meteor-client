01a7fbd42b8a0ed09a560744e18cbc44651751ef
seasnail8169 <seasnail@meteorclient.com>
2021-06-20 01:35:43 +0100

    Cleaned up HandView

--- src/main/java/meteordevelopment/meteorclient/systems/modules/render/HandView.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/render/HandView.java
@@ -15,119 +15,114 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Quaternion;
 
 public class HandView extends Module {
-    public enum SwingMode {
-        Offhand,
-        Mainhand,
-        None
-    }
-
-    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final SettingGroup sgScale = settings.createGroup("Scale");
+    private final SettingGroup sgPosition = settings.createGroup("Position");
+    private final SettingGroup sgRotation = settings.createGroup("Rotation");
     private final SettingGroup sgSwing = settings.createGroup("Swing");
 
-    private final Setting<Double> rotationX = sgGeneral.add(new DoubleSetting.Builder()
-            .name("rotation-x")
-            .description("The X rotation of your hands.")
-            .defaultValue(0.00)
-            .sliderMin(-1)
-            .sliderMax(1)
-            .build()
+    // Scale
+
+    private final Setting<Double> scaleX = sgScale.add(new DoubleSetting.Builder()
+        .name("x")
+        .description("The X scale of your hands.")
+        .defaultValue(1)
+        .sliderMin(0).sliderMax(5)
+        .build()
     );
 
-    private final Setting<Double> rotationY = sgGeneral.add(new DoubleSetting.Builder()
-            .name("rotation-y")
-            .description("The Y rotation of your hands.")
-            .defaultValue(0.00)
-            .sliderMin(-1)
-            .sliderMax(1)
-            .build()
+    private final Setting<Double> scaleY = sgScale.add(new DoubleSetting.Builder()
+        .name("y")
+        .description("The Y scale of your hands.")
+        .defaultValue(1)
+        .sliderMin(0).sliderMax(5)
+        .build()
     );
 
-    private final Setting<Double> rotationZ = sgGeneral.add(new DoubleSetting.Builder()
-            .name("rotation-z")
-            .description("The Z rotation of your hands.")
-            .defaultValue(0.00)
-            .sliderMin(-1)
-            .sliderMax(1)
-            .build()
+    private final Setting<Double> scaleZ = sgScale.add(new DoubleSetting.Builder()
+        .name("z")
+        .description("The Z scale of your hands.")
+        .defaultValue(1)
+        .sliderMin(0).sliderMax(5)
+        .build()
     );
 
-    private final Setting<Double> scaleX = sgGeneral.add(new DoubleSetting.Builder()
-            .name("scale-x")
-            .description("The X scale of the items rendered in your hands.")
-            .defaultValue(0.75)
-            .sliderMin(0)
-            .sliderMax(1.5)
-            .build()
+    // Position
+
+    private final Setting<Double> posX = sgPosition.add(new DoubleSetting.Builder()
+        .name("x")
+        .description("The X position offset of your hands.")
+        .defaultValue(0)
+        .sliderMin(-3).sliderMax(3)
+        .build()
     );
 
-    private final Setting<Double> scaleY = sgGeneral.add(new DoubleSetting.Builder()
-            .name("scale-y")
-            .description("The Y scale of the items rendered in your hands.")
-            .defaultValue(0.60)
-            .sliderMin(0)
-            .sliderMax(2)
-            .build()
+    private final Setting<Double> posY = sgPosition.add(new DoubleSetting.Builder()
+        .name("y")
+        .description("The Y position offset of your hands.")
+        .defaultValue(0)
+        .sliderMin(-3).sliderMax(3)
+        .build()
     );
 
-    private final Setting<Double> scaleZ = sgGeneral.add(new DoubleSetting.Builder()
-            .name("scale-z")
-            .description("The Z scale of the items rendered in your hands.")
-            .defaultValue(1.00)
-            .sliderMin(0)
-            .sliderMax(5)
-            .build()
+    private final Setting<Double> posZ = sgPosition.add(new DoubleSetting.Builder()
+        .name("z")
+        .description("The Z position offset of your hands.")
+        .defaultValue(0)
+        .sliderMin(-3).sliderMax(3)
+        .build()
     );
 
-    private final Setting<Double> posX = sgGeneral.add(new DoubleSetting.Builder()
-            .name("pos-x")
-            .description("The X offset of your hands.")
-            .defaultValue(0.00)
-            .sliderMin(-3)
-            .sliderMax(3)
-            .build()
+    // Rotation
+
+    private final Setting<Double> rotationX = sgRotation.add(new DoubleSetting.Builder()
+        .name("x")
+        .description("The X orientation of your hands.")
+        .defaultValue(0)
+        .sliderMin(-1).sliderMax(1)
+        .build()
     );
 
-    private final Setting<Double> posY = sgGeneral.add(new DoubleSetting.Builder()
-            .name("pos-y")
-            .description("The Y offset of your hands.")
-            .defaultValue(0.00)
-            .sliderMin(-3)
-            .sliderMax(3)
-            .build()
+    private final Setting<Double> rotationY = sgRotation.add(new DoubleSetting.Builder()
+        .name("y")
+        .description("The Y orientation of your hands.")
+        .defaultValue(0)
+        .sliderMin(-1).sliderMax(1)
+        .build()
     );
 
-    private final Setting<Double> posZ = sgGeneral.add(new DoubleSetting.Builder()
-            .name("pos-z")
-            .description("The Z offset of your hands.")
-            .defaultValue(-0.10)
-            .sliderMin(-3)
-            .sliderMax(3)
-            .build()
+    private final Setting<Double> rotationZ = sgRotation.add(new DoubleSetting.Builder()
+        .name("z")
+        .description("The Z orientation of your hands.")
+        .defaultValue(0)
+        .sliderMin(-1).sliderMax(1)
+        .build()
+    );
+
+    // Swing
+
+    public final Setting<SwingMode> swingMode = sgSwing.add(new EnumSetting.Builder<SwingMode>()
+        .name("mode")
+        .description("Modifies your client & server hand swinging.")
+        .defaultValue(SwingMode.None)
+        .build()
     );
 
     public final Setting<Double> mainSwing = sgSwing.add(new DoubleSetting.Builder()
-            .name("main-swing-progress")
-            .description("The swing progress of your mainhand.")
-            .defaultValue(0)
-            .sliderMin(0)
-            .sliderMax(1)
-            .build()
+        .name("main-progress")
+        .description("The swing progress of your main hand.")
+        .defaultValue(0)
+        .min(0).max(1)
+        .sliderMin(0).sliderMax(1)
+        .build()
     );
 
     public final Setting<Double> offSwing = sgSwing.add(new DoubleSetting.Builder()
-            .name("off-swing-progress")
-            .description("The swing progress of your offhand.")
-            .defaultValue(0)
-            .sliderMin(0)
-            .sliderMax(1)
-            .build()
-    );
-
-    public final Setting<SwingMode> swingMode = sgSwing.add(new EnumSetting.Builder<SwingMode>()
-            .name("swing-mode")
-            .description("Modifies your client & server hand swinging.")
-            .defaultValue(SwingMode.None)
-            .build()
+        .name("offhand-progress")
+        .description("The swing progress of your offhand.")
+        .defaultValue(0)
+        .min(0).max(1)
+        .sliderMin(0).sliderMax(1)
+        .build()
     );
 
     public HandView() {
@@ -140,5 +135,11 @@ public class HandView extends Module {
         matrices.scale(scaleX.get().floatValue(), scaleY.get().floatValue(), scaleZ.get().floatValue());
         matrices.translate(posX.get(), posY.get(), posZ.get());
         matrices.multiply(Quaternion.method_35825(rotationX.get().floatValue(), rotationY.get().floatValue(), rotationZ.get().floatValue()));
+    }
+
+    public enum SwingMode {
+        Offhand,
+        Mainhand,
+        None
     }
 }
