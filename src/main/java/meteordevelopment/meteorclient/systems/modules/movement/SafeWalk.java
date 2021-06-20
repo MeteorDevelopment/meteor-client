a0320914fe467b4866b376361c8a69c56b85eff2
Cloudburst <serek.lego@gmail.com>
2021-06-20 14:58:41 +0200

    improved safewalk

--- src/main/java/meteordevelopment/meteorclient/systems/modules/movement/SafeWalk.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/movement/SafeWalk.java
@@ -6,17 +6,68 @@
 package meteordevelopment.meteorclient.systems.modules.movement;
 
 import meteordevelopment.meteorclient.events.entity.player.ClipAtLedgeEvent;
+import meteordevelopment.meteorclient.events.world.CollisionShapeEvent;
+import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.block.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShapes;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class SafeWalk extends Module {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Boolean> ledge = sgGeneral.add(new BoolSetting.Builder()
+        .name("ledge")
+        .description("Prevents you from walking of blocks, like pressing shift.")
+        .defaultValue(true)
+        .build()
+    );
+
+    private final Setting<List<Block>> blocks = sgGeneral.add(new BlockListSetting.Builder()
+        .name("blocks")
+        .description("Which blocks to prevent on walking")
+        .filter(this::blockFilter)
+        .defaultValue(Arrays.asList())
+        .build()
+    );
+
+    private final Setting<Boolean> magma = sgGeneral.add(new BoolSetting.Builder()
+        .name("magma")
+        .description("Prevents you from walking over magma blocks.")
+        .defaultValue(false)
+        .build()
+    );
+
     public SafeWalk() {
-        super(Categories.Movement, "safe-walk", "Prevents you from walking off blocks. Useful over a void.");
+        super(Categories.Movement, "safe-walk", "Prevents you from walking off blocks or on blocks that you dont want.");
     }
 
     @EventHandler
-    private void onClipAtLedge(ClipAtLedgeEvent event) {
-        event.setClip(true);
+    private void onClipAtLedge(ClipAtLedgeEvent event) { event.setClip(ledge.get()); }
+
+    @EventHandler
+    private void onCollisionShape(CollisionShapeEvent event) {
+        if (mc.world == null || mc.player == null) return;
+        if (event.type != CollisionShapeEvent.CollisionType.BLOCK) return;
+        if (blocks.get().contains(event.state.getBlock())) {
+            event.shape = VoxelShapes.fullCube();
+        }
+        else if (magma.get() && !mc.player.isSneaking()
+            && event.state.isAir()
+            && mc.world.getBlockState(event.pos.down()).getBlock() == Blocks.MAGMA_BLOCK) {
+            event.shape = VoxelShapes.fullCube();
+        }
+    }
+
+    private boolean blockFilter(Block block) {
+        if (block == Blocks.AIR || block == Blocks.CAVE_AIR) return false;
+        if (block instanceof FluidBlock) return false;
+        if (mc.world == null) return true;
+        return (block.getDefaultState().getCollisionShape(mc.world, new BlockPos(0,0,0)) != VoxelShapes.fullCube());
     }
 }
