9295bb277fcd06285b1f560fd8cc5aa54667f980
StormyBytes <stormybytes@gmail.com>
2021-06-22 06:01:51 +0700

    Smol oops

--- src/main/java/meteordevelopment/meteorclient/systems/modules/world/AutoWither.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/world/AutoWither.java
@@ -119,7 +119,7 @@ public class AutoWither extends Module {
 
             // Register
             BlockIterator.register(horizontalRadius.get(), verticalRadius.get(), (blockPos, blockState) -> {
-                Direction dir = Direction.fromRotation(Rotations.getYaw(blockPos));
+                Direction dir = Direction.fromRotation(Rotations.getYaw(blockPos)).getOpposite();
                 if (isValidSpawn(blockPos, dir)) withers.add(witherPool.get().set(blockPos, dir));
             });
         }
