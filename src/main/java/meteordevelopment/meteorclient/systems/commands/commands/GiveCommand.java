c88beaf217f74f424eb3bf3929b771533c3fbff2
Cloudburst <serek.lego@gmail.com>
2021-06-24 09:36:42 +0200

    ficks .give

--- src/main/java/meteordevelopment/meteorclient/systems/commands/commands/GiveCommand.java
+++ src/main/java/meteordevelopment/meteorclient/systems/commands/commands/GiveCommand.java
@@ -22,6 +22,7 @@ import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
 
 public class GiveCommand extends Command {
     private final static SimpleCommandExceptionType NOT_IN_CREATIVE = new SimpleCommandExceptionType(new LiteralText("You must be in creative mode to use this."));
+    private final static SimpleCommandExceptionType NO_SPACE = new SimpleCommandExceptionType(new LiteralText("No space in hotbar."));
 
     public GiveCommand() {
         super("give", "Gives you any item.");
@@ -47,11 +48,8 @@ public class GiveCommand extends Command {
     }
 
     private void addItem(ItemStack item) {
-		for(int i = 0; i < 36; i++) {
-		    ItemStack stack = mc.player.getInventory().getStack(SlotUtils.indexToId(i));
-			if (!stack.isEmpty()) continue;
-			mc.player.networkHandler.sendPacket(new CreativeInventoryActionC2SPacket(SlotUtils.indexToId(i), item));
-			return;
+		if (!mc.player.getInventory().insertStack(item)) {
+            throw NO_SPACE.create();
 		}
     }
 }
