b72469b40b5ef131d933a22585606cdaa5626d84
Cloudburst <serek.lego@gmail.com>
2021-06-24 08:58:24 +0200

    added llamas to EntityControl

---
+++ src/main/java/meteordevelopment/meteorclient/mixin/LlamaEntityMixin.java
@@ -0,0 +1,20 @@
+package meteordevelopment.meteorclient.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.entity.passive.LlamaEntity;
+
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.systems.modules.movement.EntityControl;
+
+
+@Mixin(LlamaEntity.class)
+public class LlamaEntityMixin {
+    @Inject(method = "canBeControlledByRider", at = @At("HEAD"), cancellable = true)
+    public void canBeControlledByRider(CallbackInfoReturnable<Boolean> ci) {
+        if (Modules.get().get(EntityControl.class).isActive()) ci.setReturnValue(true);
+    }
+}
