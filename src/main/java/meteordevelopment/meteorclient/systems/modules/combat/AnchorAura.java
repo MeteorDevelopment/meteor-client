37c49d297afb2d78833ae78fa1738f59e9d6d4d3
supakeks <thiaguitoszwarc@gmail.com>
2021-06-25 20:05:54 -0400

    Made AnchorAura place in liquids

--- src/main/java/meteordevelopment/meteorclient/systems/modules/combat/AnchorAura.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/combat/AnchorAura.java
@@ -21,6 +21,7 @@ import meteordevelopment.meteorclient.utils.world.BlockUtils;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -350,7 +351,7 @@ public class AnchorAura extends Module {
     }
 
     private boolean isValidPlace(BlockPos pos) {
-        return mc.world.getBlockState(pos).isAir() && Math.sqrt(mc.player.getBlockPos().getSquaredDistance(pos)) <= placeRange.get() && getDamagePlace(pos);
+        return (mc.world.getBlockState(pos).isAir() || mc.world.getBlockState(pos).getFluidState().getFluid() instanceof FlowableFluid) && Math.sqrt(mc.player.getBlockPos().getSquaredDistance(pos)) <= placeRange.get() && getDamagePlace(pos);
     }
 
     private boolean isValidBreak(BlockPos pos) {
