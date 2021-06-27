cd31ebbc2d6b39f79b1e84d116777a86c1652644
seasnail8169 <seasnail@meteorclient.com>
2021-06-27 13:11:00 +0100

    Fix BookScreen Copy button

--- src/main/java/meteordevelopment/meteorclient/mixin/BookScreenMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/BookScreenMixin.java
@@ -42,7 +42,7 @@ public class BookScreenMixin extends Screen {
 
     @Inject(method = "init", at = @At("TAIL"))
     private void onInit(CallbackInfo info) {
-        addDrawableChild(new ButtonWidget(4, 4, 70, 16, new LiteralText("Copy"), button -> {
+        addDrawableChild(new ButtonWidget(4, 4, 70, 20, new LiteralText("Copy"), button -> {
             NbtList listTag = new NbtList();
             for (int i = 0; i < contents.getPageCount(); i++) listTag.add(NbtString.of(contents.getPage(i).getString()));
 
