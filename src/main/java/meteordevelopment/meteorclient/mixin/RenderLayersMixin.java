8075af86fdf243383a08894ccd02678bcf83c358
Walaryne <hazardoustheradioactive@gmail.com>
2021-07-06 15:29:10 -0400

    Partial reversion of "Merged Xray and wallhack - Resolves #1145"
    This reverts commit 552ec317

--- src/main/java/meteordevelopment/meteorclient/mixin/RenderLayersMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/RenderLayersMixin.java
@@ -6,7 +6,7 @@
 package meteordevelopment.meteorclient.mixin;
 
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import meteordevelopment.meteorclient.systems.modules.render.Xray;
+import meteordevelopment.meteorclient.systems.modules.render.WallHack;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.RenderLayers;
@@ -17,14 +17,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(RenderLayers.class)
 public class RenderLayersMixin {
+
     @Inject(method = "getBlockLayer", at = @At("HEAD"), cancellable = true)
     private static void onGetBlockLayer(BlockState state, CallbackInfoReturnable<RenderLayer> cir) {
-        if (Modules.get() == null) return;
+        if(Modules.get() != null) {
+            WallHack wallHack = Modules.get().get(WallHack.class);
 
-        Xray xray = Modules.get().get(Xray.class);
-
-        if (xray.isActive() && xray.isBlocked(state.getBlock())) {
-            cir.setReturnValue(RenderLayer.getTranslucent());
+            if(wallHack.isActive()) {
+                if(wallHack.blocks.get().contains(state.getBlock())) {
+                    cir.setReturnValue(RenderLayer.getTranslucent());
+                }
+            }
         }
     }
+
 }
