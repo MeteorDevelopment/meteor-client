4c2afc4ce5cb8688574a7188afc32a9db09a2552
timoreo <timo.oreo34@gmail.com>
2021-07-13 16:32:33 +0200

    Added Baritone in a really bad way

--- src/main/java/meteordevelopment/meteorclient/mixin/baritone/ComeCommandMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/baritone/ComeCommandMixin.java
@@ -5,18 +5,25 @@
 
 package meteordevelopment.meteorclient.mixin.baritone;
 
-import net.minecraft.client.MinecraftClient;
+import baritone.api.pathing.goals.GoalBlock;
+import baritone.command.defaults.ComeCommand;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.systems.modules.render.Freecam;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
-// TODO: Baritone
-@Mixin(MinecraftClient.class)
+import static meteordevelopment.meteorclient.utils.Utils.mc;
+
+@Mixin(ComeCommand.class)
 public class ComeCommandMixin {
-    /*@ModifyArgs(method = "execute", at = @At(value = "INVOKE", target = "Lbaritone/api/process/ICustomGoalProcess;setGoalAndPath(Lbaritone/api/pathing/goals/Goal;)V"), remap = false)
+    @ModifyArgs(method = "execute", at = @At(value = "INVOKE", target = "Lbaritone/api/process/ICustomGoalProcess;setGoalAndPath(Lbaritone/api/pathing/goals/Goal;)V"), remap = false)
     private void getComeCommandTarget(Args args) {
         Freecam freecam = Modules.get().get(Freecam.class);
         if (freecam.isActive()) {
             float tickDelta = mc.getTickDelta();
             args.set(0, new GoalBlock((int) freecam.getX(tickDelta), (int) freecam.getY(tickDelta), (int) freecam.getZ(tickDelta)));
         }
-    }*/
+    }
 }
