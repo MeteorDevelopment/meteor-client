4c91330273f67321367486ecfde55a8a17cb8687
MineGame159 <petulko08@gmail.com>
2021-07-15 15:28:17 +0200

    A

--- src/main/java/meteordevelopment/meteorclient/mixin/PowderSnowBlockMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/PowderSnowBlockMixin.java
@@ -14,10 +14,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static meteordevelopment.meteorclient.utils.Utils.mc;
+
 @Mixin(PowderSnowBlock.class)
 public class PowderSnowBlockMixin {
     @Inject(method = "canWalkOnPowderSnow", at = @At("HEAD"), cancellable = true)
     private static void onCanWalkOnPowderSnow(Entity entity, CallbackInfoReturnable<Boolean> info) {
-        if (Modules.get().get(Jesus.class).canWalkOnPowderSnow()) info.setReturnValue(true);
+        if (entity == mc.player && Modules.get().get(Jesus.class).canWalkOnPowderSnow()) info.setReturnValue(true);
     }
 }
