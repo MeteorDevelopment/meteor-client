18eecdf3f07cb75d43f47a3ee9ad9d6ebfb4bf04
MineGame159 <petulko08@gmail.com>
2021-07-16 12:01:50 +0200

    Added ability to select individual items in FastUse

--- src/main/java/meteordevelopment/meteorclient/systems/modules/player/FastUse.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/player/FastUse.java
@@ -7,16 +7,16 @@ package meteordevelopment.meteorclient.systems.modules.player;
 
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.mixin.MinecraftClientAccessor;
-import meteordevelopment.meteorclient.settings.BoolSetting;
-import meteordevelopment.meteorclient.settings.EnumSetting;
-import meteordevelopment.meteorclient.settings.IntSetting;
-import meteordevelopment.meteorclient.settings.Setting;
-import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FastUse extends Module {
     public enum Mode {
@@ -27,34 +27,36 @@ public class FastUse extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
     private final Setting<Mode> mode = sgGeneral.add(new EnumSetting.Builder<Mode>()
-            .name("mode")
-            .description("Which items to fast use.")
-            .defaultValue(Mode.All)
-            .build()
+        .name("mode")
+        .description("Which items to fast use.")
+        .defaultValue(Mode.All)
+        .build()
     );
 
-    private final Setting<Boolean> exp = sgGeneral.add(new BoolSetting.Builder()
-            .name("xP")
-            .description("Fast-throws XP bottles if the mode is \"Some\".")
-            .defaultValue(false)
-            .build()
+    private final Setting<List<Item>> items = sgGeneral.add(new ItemListSetting.Builder()
+        .name("items")
+        .description("Which items should fast place work on in \"Some\" mode.")
+        .visible(() -> mode.get() == Mode.Some)
+        .defaultValue(new ArrayList<>(0))
+        .build()
     );
 
     private final Setting<Boolean> blocks = sgGeneral.add(new BoolSetting.Builder()
-            .name("blocks")
-            .description("Fast-places blocks if the mode is \"Some\".")
-            .defaultValue(false)
-            .build()
+        .name("blocks")
+        .description("Fast-places blocks if the mode is \"Some\" mode.")
+        .visible(() -> mode.get() == Mode.Some)
+        .defaultValue(false)
+        .build()
     );
 
     private final Setting<Integer> cooldown = sgGeneral.add(new IntSetting.Builder()
-            .name("cooldown")
-            .description("Fast-use cooldown in ticks.")
-            .defaultValue(0)
-            .min(0)
-            .sliderMin(0)
-            .sliderMax(4)
-            .build()
+        .name("cooldown")
+        .description("Fast-use cooldown in ticks.")
+        .defaultValue(0)
+        .min(0)
+        .sliderMin(0)
+        .sliderMax(4)
+        .build()
     );
 
     public FastUse() {
@@ -64,15 +66,15 @@ public class FastUse extends Module {
     @EventHandler
     private void onTick(TickEvent.Post event) {
         int cooldownTicks = Math.min(((MinecraftClientAccessor) mc).getItemUseCooldown(), cooldown.get());
-        switch (mode.get()) {
-            case All:
-                ((MinecraftClientAccessor) mc).setItemUseCooldown(cooldownTicks);
-                break;
-            case Some:
-                if ((exp.get() && (mc.player.getMainHandStack().getItem() == Items.EXPERIENCE_BOTTLE || mc.player.getOffHandStack().getItem() == Items.EXPERIENCE_BOTTLE))
-                        || (blocks.get() && (mc.player.getMainHandStack().getItem() instanceof BlockItem || mc.player.getOffHandStack().getItem() instanceof BlockItem)))
-                    ((MinecraftClientAccessor) mc).setItemUseCooldown(cooldownTicks);
-                break;
-        }
+        if (mode.get() == Mode.All || shouldWorkSome()) ((MinecraftClientAccessor) mc).setItemUseCooldown(cooldownTicks);
+    }
+
+    private boolean shouldWorkSome() {
+        if (shouldWorkSome(mc.player.getMainHandStack())) return true;
+        return shouldWorkSome(mc.player.getOffHandStack());
+    }
+
+    private boolean shouldWorkSome(ItemStack itemStack) {
+        return (blocks.get() && itemStack.getItem() instanceof BlockItem) || items.get().contains(itemStack.getItem());
     }
 }
