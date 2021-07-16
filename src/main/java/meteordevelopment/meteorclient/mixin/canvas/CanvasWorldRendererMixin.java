0b366b46b74429bbc1e7dfeb0ab61049ec20ffbb
MineGame159 <petulko08@gmail.com>
2021-07-16 19:22:18 +0200

    OpenGL stuff

---
+++ src/main/java/meteordevelopment/meteorclient/mixin/canvas/CanvasWorldRendererMixin.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
+ * Copyright (c) 2021 Meteor Development.
+ */
+
+package meteordevelopment.meteorclient.mixin.canvas;
+
+import grondag.canvas.render.world.CanvasWorldRenderer;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.systems.modules.render.BlockSelection;
+import meteordevelopment.meteorclient.systems.modules.render.Fullbright;
+import meteordevelopment.meteorclient.utils.Utils;
+import meteordevelopment.meteorclient.utils.render.Outlines;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Matrix4f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = CanvasWorldRenderer.class, remap = false)
+public class CanvasWorldRendererMixin {
+    @ModifyVariable(method = "renderWorld", at = @At("LOAD"), name = "blockOutlines")
+    private boolean renderWorld_blockOutlines(boolean blockOutlines) {
+        if (Modules.get().isActive(BlockSelection.class)) return false;
+        return blockOutlines;
+    }
+
+    @Inject(method = "renderWorld", at = @At("HEAD"))
+    private void onRenderHead(MatrixStack viewMatrixStack, float tickDelta, long frameStartNanos, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f projectionMatrix, CallbackInfo info) {
+        Utils.minimumLightLevel = Modules.get().get(Fullbright.class).getMinimumLightLevel();
+
+        Outlines.beginRender();
+    }
+
+    @Inject(method = "renderWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/BufferBuilderStorage;getOutlineVertexConsumers()Lnet/minecraft/client/render/OutlineVertexConsumerProvider;", shift = At.Shift.AFTER))
+    private void onRender(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f matrix4f, CallbackInfo info) {
+        Outlines.endRender();
+    }
+}
