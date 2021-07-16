059a66315b08e0b03d3bdd8140a5aee6d90fa958
MineGame159 <petulko08@gmail.com>
2021-07-16 11:51:48 +0200

    Added range setting to AirPlace

--- src/main/java/meteordevelopment/meteorclient/systems/modules/world/AirPlace.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/world/AirPlace.java
@@ -17,9 +17,13 @@ import meteordevelopment.orbit.EventHandler;
 import net.minecraft.item.BlockItem;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 
 public class AirPlace extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final SettingGroup sgRange = settings.createGroup("Range");
+
+    // General
 
     private final Setting<Boolean> render = sgGeneral.add(new BoolSetting.Builder()
         .name("render")
@@ -49,27 +53,50 @@ public class AirPlace extends Module {
         .build()
     );
 
+    // Range
+
+    private final Setting<Boolean> customRange = sgRange.add(new BoolSetting.Builder()
+        .name("custom-range")
+        .description("Use custom range for air place.")
+        .defaultValue(false)
+        .build()
+    );
+
+    private final Setting<Double> range = sgRange.add(new DoubleSetting.Builder()
+        .name("range")
+        .description("Custom range to place at.")
+        .visible(customRange::get)
+        .defaultValue(5)
+        .min(0)
+        .sliderMax(6)
+        .build()
+    );
+
+    private HitResult hitResult;
+
     public AirPlace() {
         super(Categories.Player, "air-place", "Places a block where your crosshair is pointing at.");
     }
 
     @EventHandler
     private void onTick(TickEvent.Post event) {
-        if (!(mc.crosshairTarget instanceof BlockHitResult) || !(mc.player.getMainHandStack().getItem() instanceof BlockItem))
-            return;
+        double r = customRange.get() ? range.get() : mc.interactionManager.getReachDistance();
+        hitResult = mc.getCameraEntity().raycast(r, 0, false);
+
+        if (!(hitResult instanceof BlockHitResult) || !(mc.player.getMainHandStack().getItem() instanceof BlockItem)) return;
 
         if (mc.options.keyUse.isPressed()) {
-            BlockUtils.place(((BlockHitResult) mc.crosshairTarget).getBlockPos(), Hand.MAIN_HAND, mc.player.getInventory().selectedSlot, false, 0, true, true, false);
+            BlockUtils.place(((BlockHitResult) hitResult).getBlockPos(), Hand.MAIN_HAND, mc.player.getInventory().selectedSlot, false, 0, true, true, false);
         }
     }
 
     @EventHandler
     private void onRender(Render3DEvent event) {
-        if (!(mc.crosshairTarget instanceof BlockHitResult)
-            || !mc.world.getBlockState(((BlockHitResult) mc.crosshairTarget).getBlockPos()).getMaterial().isReplaceable()
+        if (!(hitResult instanceof BlockHitResult)
+            || !mc.world.getBlockState(((BlockHitResult) hitResult).getBlockPos()).getMaterial().isReplaceable()
             || !(mc.player.getMainHandStack().getItem() instanceof BlockItem)
             || !render.get()) return;
 
-        event.renderer.box(((BlockHitResult) mc.crosshairTarget).getBlockPos(), sideColor.get(), lineColor.get(), shapeMode.get(), 0);
+        event.renderer.box(((BlockHitResult) hitResult).getBlockPos(), sideColor.get(), lineColor.get(), shapeMode.get(), 0);
     }
 }
