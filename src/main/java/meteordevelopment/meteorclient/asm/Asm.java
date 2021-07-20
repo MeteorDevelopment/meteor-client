c97d7d64b1216f65495f9653c325cb7ae122df9d
MineGame159 <petulko08@gmail.com>
2021-07-20 18:15:02 +0200

    Added meteor.asm.export property

--- src/main/java/meteordevelopment/meteorclient/asm/Asm.java
+++ src/main/java/meteordevelopment/meteorclient/asm/Asm.java
@@ -7,21 +7,29 @@ package meteordevelopment.meteorclient.asm;
 
 import meteordevelopment.meteorclient.asm.transformers.CanvasWorldRendererTransformer;
 import meteordevelopment.meteorclient.asm.transformers.GameRendererTransformer;
+import net.fabricmc.loader.api.FabricLoader;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.transformers.MixinClassWriter;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
 /** When mixins are just not good enough **/
 public class Asm {
     private final Map<String, AsmTransformer> transformers = new HashMap<>();
+    private final boolean export;
 
     public Asm() {
         add(new GameRendererTransformer());
         add(new CanvasWorldRendererTransformer());
+
+        export = System.getProperty("meteor.asm.export") != null;
     }
 
     private void add(AsmTransformer transformer) {
@@ -40,7 +48,17 @@ public class Asm {
 
             ClassWriter writer = new MixinClassWriter(reader, ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
             klass.accept(writer);
-            return writer.toByteArray();
+            bytes = writer.toByteArray();
+
+            if (export) {
+                try {
+                    Path path = Path.of(FabricLoader.getInstance().getGameDir().toString(), ".meteor.asm.out", name.replace('.', '/') + ".class");
+                    new File(path.toUri()).getParentFile().mkdirs();
+                    Files.write(path, bytes);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
         return  bytes;
