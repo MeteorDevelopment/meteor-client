1930b7df8940079c4b59f8c3be0bc237817fcbeb
Based Ricky <80427814+RickyTheRacc@users.noreply.github.com>
2021-08-26 20:43:51 -0500

    Delete src/main
    Fuck this client shit fr all my homies use labymod 😎😎💯

--- src/main/java/meteordevelopment/meteorclient/systems/accounts/ProfileResponse.java
+++
@@ -1,9 +0,0 @@
-/*
- * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
- * Copyright (c) 2021 Meteor Development.
- */
-package meteordevelopment.meteorclient.systems.accounts;
-
-public class ProfileResponse {
-    public String id;
-}
--- src/main/resources/assets/meteor-client/shaders/pos_color.frag
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-out vec4 color;
-
-in vec4 v_Color;
-
-void main() {
-    color = v_Color;
-}
