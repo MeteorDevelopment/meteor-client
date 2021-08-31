0cb3b0cdbc6ee19d1f5b8f842cce01bc5ac85250
MineGame159 <petulko08@gmail.com>
2021-08-31 18:51:25 +0200

    Title screen credits now displays addons

--- src/main/java/meteordevelopment/meteorclient/mixin/TitleScreenMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/TitleScreenMixin.java
@@ -11,8 +11,8 @@ import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.meteorclient.utils.misc.Version;
 import meteordevelopment.meteorclient.utils.network.Http;
 import meteordevelopment.meteorclient.utils.network.MeteorExecutor;
+import meteordevelopment.meteorclient.utils.player.TitleScreenCreditsRenderer;
 import meteordevelopment.meteorclient.utils.render.PromptBuilder;
-import meteordevelopment.meteorclient.utils.render.color.Color;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.util.math.MatrixStack;
@@ -25,52 +25,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin extends Screen {
-    private final int WHITE = Color.fromRGBA(255, 255, 255, 255);
-    private final int GRAY = Color.fromRGBA(175, 175, 175, 255);
-
-    private String text1;
-    private int text1Length;
-
-    private String text2;
-    private int text2Length;
-
-    private String text3;
-    private int text3Length;
-
-    private String text4;
-    private int text4Length;
-
-    private String text5;
-    private int text5Length;
-
-    private String text6;
-
-    private int fullLength;
-    private int prevWidth;
-
     public TitleScreenMixin(Text title) {
         super(title);
-    }
-
-    @Inject(method = "init", at = @At("TAIL"))
-    private void onInit(CallbackInfo info) {
-
-        text1 = "Meteor Client by ";
-        text2 = "MineGame159";
-        text3 = ", ";
-        text4 = "squidoodly";
-        text5 = " & ";
-        text6 = "seasnail";
-
-        text1Length = textRenderer.getWidth(text1);
-        text2Length = textRenderer.getWidth(text2);
-        text3Length = textRenderer.getWidth(text3);
-        text4Length = textRenderer.getWidth(text4);
-        text5Length = textRenderer.getWidth(text5);
-        int text6Length = textRenderer.getWidth(text6);
-
-        fullLength = text1Length + text2Length + text3Length + text4Length + text5Length + text6Length;
-        prevWidth = 0;
     }
 
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/TitleScreen;drawStringWithShadow(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V", ordinal = 0))
@@ -92,9 +48,7 @@ public class TitleScreenMixin extends Screen {
                         .message("Your version: %s", Config.get().version)
                         .message("Latest version: %s", latestVer)
                         .message("Do you want to update?")
-                        .onYes(() -> {
-                            Util.getOperatingSystem().open("https://meteorclient.com/");
-                        })
+                        .onYes(() -> Util.getOperatingSystem().open("https://meteorclient.com/"))
                         .promptId("new-update")
                         .show();
                 }
@@ -104,18 +58,6 @@ public class TitleScreenMixin extends Screen {
 
     @Inject(method = "render", at = @At("TAIL"))
     private void onRender(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo info) {
-        if (!Config.get().titleScreenCredits) return;
-        prevWidth = 0;
-        textRenderer.drawWithShadow(matrices, text1, width - fullLength - 3, 3, WHITE);
-        prevWidth += text1Length;
-        textRenderer.drawWithShadow(matrices, text2, width - fullLength + prevWidth - 3, 3, GRAY);
-        prevWidth += text2Length;
-        textRenderer.drawWithShadow(matrices, text3, width - fullLength + prevWidth - 3, 3, WHITE);
-        prevWidth += text3Length;
-        textRenderer.drawWithShadow(matrices, text4, width - fullLength + prevWidth - 3, 3, GRAY);
-        prevWidth += text4Length;
-        textRenderer.drawWithShadow(matrices, text5, width - fullLength + prevWidth - 3, 3, WHITE);
-        prevWidth += text5Length;
-        textRenderer.drawWithShadow(matrices, text6, width - fullLength + prevWidth - 3, 3, GRAY);
+        if (Config.get().titleScreenCredits) TitleScreenCreditsRenderer.render(matrices);
     }
 }
