934712099366db145bd90d47d5a86fddb10aec20
seasnail8169 <seasnail@meteorclient.com>
2021-09-15 19:06:03 +0100

    Tweaked prompts and added OkPrompt

--- src/main/java/meteordevelopment/meteorclient/gui/tabs/builtin/ConfigTab.java
+++ src/main/java/meteordevelopment/meteorclient/gui/tabs/builtin/ConfigTab.java
@@ -14,6 +14,7 @@ import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.config.Config;
 import meteordevelopment.meteorclient.utils.render.PromptBuilder;
 import meteordevelopment.meteorclient.utils.render.color.RainbowColors;
+import meteordevelopment.meteorclient.utils.render.prompts.YesNoPrompt;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.option.KeyBinding;
 
@@ -21,136 +22,136 @@ import static meteordevelopment.meteorclient.utils.Utils.mc;
 
 public class ConfigTab extends Tab {
     private static final Settings settings = new Settings();
+
     private static final SettingGroup sgGeneral = settings.getDefaultGroup();
     private static final SettingGroup sgChat = settings.createGroup("Chat");
     private static final SettingGroup sgScreens = settings.createGroup("Screens");
 
     public static final Setting<Boolean> customFont = sgGeneral.add(new BoolSetting.Builder()
-            .name("custom-font")
-            .description("Use a custom font.")
-            .defaultValue(true)
-            .onChanged(aBoolean -> Config.get().customFont = aBoolean)
-            .onModuleActivated(booleanSetting -> booleanSetting.set(Config.get().customFont))
-            .build()
+        .name("custom-font")
+        .description("Use a custom font.")
+        .defaultValue(true)
+        .onChanged(aBoolean -> Config.get().customFont = aBoolean)
+        .onModuleActivated(booleanSetting -> booleanSetting.set(Config.get().customFont))
+        .build()
     );
 
     public static final Setting<String> font = sgGeneral.add(new ProvidedStringSetting.Builder()
-            .name("font")
-            .description("Custom font to use (picked from .minecraft/meteor-client/fonts folder).")
-            .supplier(Fonts::getAvailableFonts)
-            .defaultValue(Fonts.DEFAULT_FONT)
-            .onChanged(s -> {
-                Config.get().font = s;
-                Fonts.load();
-            })
-            .onModuleActivated(stringSetting -> stringSetting.set(Config.get().font))
-            .visible(customFont::get)
-            .build()
+        .name("font")
+        .description("Custom font to use (picked from .minecraft/meteor-client/fonts folder).")
+        .supplier(Fonts::getAvailableFonts)
+        .defaultValue(Fonts.DEFAULT_FONT)
+        .onChanged(s -> {
+            Config.get().font = s;
+            Fonts.load();
+        })
+        .onModuleActivated(stringSetting -> stringSetting.set(Config.get().font))
+        .visible(customFont::get)
+        .build()
     );
 
     public static final Setting<Double> rainbowSpeed = sgGeneral.add(new DoubleSetting.Builder()
-            .name("rainbow-speed")
-            .description("The global rainbow speed.")
-            .min(0)
-            .sliderMax(5)
-            .max(10)
-            .defaultValue(0.5)
-            .decimalPlaces(2)
-            .onChanged(value -> RainbowColors.GLOBAL.setSpeed(value / 100))
-            .onModuleActivated(setting -> setting.set(RainbowColors.GLOBAL.getSpeed() * 100))
-            .build()
+        .name("rainbow-speed")
+        .description("The global rainbow speed.")
+        .min(0).max(10)
+        .sliderMax(5)
+        .defaultValue(0.5)
+        .decimalPlaces(2)
+        .onChanged(value -> RainbowColors.GLOBAL.setSpeed(value / 100))
+        .onModuleActivated(setting -> setting.set(RainbowColors.GLOBAL.getSpeed() * 100))
+        .build()
     );
 
     public static final Setting<Integer> rotationHoldTicks = sgGeneral.add(new IntSetting.Builder()
-            .name("rotation-hold")
-            .description("Hold long to hold server side rotation when not sending any packets.")
-            .defaultValue(4)
-            .onChanged(integer -> Config.get().rotationHoldTicks = integer)
-            .onModuleActivated(integerSetting -> integerSetting.set(Config.get().rotationHoldTicks))
-            .build()
+        .name("rotation-hold")
+        .description("Hold long to hold server side rotation when not sending any packets.")
+        .defaultValue(4)
+        .onChanged(integer -> Config.get().rotationHoldTicks = integer)
+        .onModuleActivated(integerSetting -> integerSetting.set(Config.get().rotationHoldTicks))
+        .build()
     );
 
     public static final Setting<String> prefix = sgChat.add(new StringSetting.Builder()
-            .name("prefix")
-            .description("Prefix.")
-            .defaultValue(".")
-            .onChanged(s -> Config.get().prefix = s)
-            .onModuleActivated(stringSetting -> stringSetting.set(Config.get().prefix))
-            .build()
+        .name("prefix")
+        .description("Prefix.")
+        .defaultValue(".")
+        .onChanged(s -> Config.get().prefix = s)
+        .onModuleActivated(stringSetting -> stringSetting.set(Config.get().prefix))
+        .build()
     );
 
     public static final Setting<Boolean> openChatOnPrefix = sgChat.add(new BoolSetting.Builder()
-            .name("open-chat-on-prefix")
-            .description("Open chat when command prefix is pressed. Works like pressing '/' in vanilla.")
-            .defaultValue(true)
-            .onChanged(aBoolean -> Config.get().openChatOnPrefix = aBoolean)
-            .onModuleActivated(booleanSetting -> booleanSetting.set(Config.get().openChatOnPrefix))
-            .build()
+        .name("open-chat-on-prefix")
+        .description("Open chat when command prefix is pressed. Works like pressing '/' in vanilla.")
+        .defaultValue(true)
+        .onChanged(aBoolean -> Config.get().openChatOnPrefix = aBoolean)
+        .onModuleActivated(booleanSetting -> booleanSetting.set(Config.get().openChatOnPrefix))
+        .build()
     );
 
     public static final Setting<Boolean> chatCommandsInfo = sgChat.add(new BoolSetting.Builder()
-            .name("chat-commands-info")
-            .description("Sends a chat message when you use chat commands (eg toggling module, changing a setting, etc).")
-            .defaultValue(true)
-            .onChanged(aBoolean -> Config.get().chatCommandsInfo = aBoolean)
-            .onModuleActivated(booleanSetting -> booleanSetting.set(Config.get().chatCommandsInfo))
-            .build()
+        .name("chat-commands-info")
+        .description("Sends a chat message when you use chat commands (eg toggling module, changing a setting, etc).")
+        .defaultValue(true)
+        .onChanged(aBoolean -> Config.get().chatCommandsInfo = aBoolean)
+        .onModuleActivated(booleanSetting -> booleanSetting.set(Config.get().chatCommandsInfo))
+        .build()
     );
 
     public static final Setting<Boolean> deleteChatCommandsInfo = sgChat.add(new BoolSetting.Builder()
-            .name("delete-chat-commands-info")
-            .description("Delete previous chat messages.")
-            .defaultValue(true)
-            .onChanged(aBoolean -> Config.get().deleteChatCommandsInfo = aBoolean)
-            .onModuleActivated(booleanSetting -> booleanSetting.set(Config.get().deleteChatCommandsInfo))
-            .visible(chatCommandsInfo::get)
-            .build()
+        .name("delete-chat-commands-info")
+        .description("Delete previous chat messages.")
+        .defaultValue(true)
+        .onChanged(aBoolean -> Config.get().deleteChatCommandsInfo = aBoolean)
+        .onModuleActivated(booleanSetting -> booleanSetting.set(Config.get().deleteChatCommandsInfo))
+        .visible(chatCommandsInfo::get)
+        .build()
     );
 
     public static final Setting<Boolean> titleScreenCredits = sgScreens.add(new BoolSetting.Builder()
-            .name("title-screen-credits")
-            .description("Show Meteor credits on title screen")
-            .defaultValue(true)
-            .onChanged(aBool -> Config.get().titleScreenCredits = aBool)
-            .onModuleActivated(boolSetting -> boolSetting.set(Config.get().titleScreenCredits))
-            .build()
+        .name("title-screen-credits")
+        .description("Show Meteor credits on title screen")
+        .defaultValue(true)
+        .onChanged(aBool -> Config.get().titleScreenCredits = aBool)
+        .onModuleActivated(boolSetting -> boolSetting.set(Config.get().titleScreenCredits))
+        .build()
     );
 
     public static final Setting<Boolean> titleScreenSplashes = sgScreens.add(new BoolSetting.Builder()
-            .name("title-screen-splashes")
-            .description("Show Meteor splash texts on title screen")
-            .defaultValue(true)
-            .onChanged(aBool -> Config.get().titleScreenSplashes = aBool)
-            .onModuleActivated(boolSetting -> boolSetting.set(Config.get().titleScreenSplashes))
-            .build()
+        .name("title-screen-splashes")
+        .description("Show Meteor splash texts on title screen")
+        .defaultValue(true)
+        .onChanged(aBool -> Config.get().titleScreenSplashes = aBool)
+        .onModuleActivated(boolSetting -> boolSetting.set(Config.get().titleScreenSplashes))
+        .build()
     );
 
     public static final Setting<Boolean> customWindowTitle = sgScreens.add(new BoolSetting.Builder()
-            .name("custom-window-title")
-            .description("Show custom text in the window title.")
-            .defaultValue(false)
-            .onChanged(aBool -> Config.get().customWindowTitle = aBool)
-            .onModuleActivated(boolSetting -> boolSetting.set(Config.get().customWindowTitle))
-            .build()
+        .name("custom-window-title")
+        .description("Show custom text in the window title.")
+        .defaultValue(false)
+        .onChanged(aBool -> Config.get().customWindowTitle = aBool)
+        .onModuleActivated(boolSetting -> boolSetting.set(Config.get().customWindowTitle))
+        .build()
     );
 
     public static final Setting<String> customWindowTitleText = sgScreens.add(new StringSetting.Builder()
-            .name("window-title-text")
-            .description("The text it displays in the window title.")
-            .defaultValue("Minecraft {mc_version} - Meteor Client {version}")
-            .onChanged(titleText -> Config.get().customWindowTitleText = titleText)
-            .onModuleActivated(stringSetting -> stringSetting.set(Config.get().customWindowTitleText))
-            .visible(customWindowTitle::get)
-            .build()
+        .name("window-title-text")
+        .description("The text it displays in the window title.")
+        .defaultValue("Minecraft {mc_version} - Meteor Client {version}")
+        .onChanged(titleText -> Config.get().customWindowTitleText = titleText)
+        .onModuleActivated(stringSetting -> stringSetting.set(Config.get().customWindowTitleText))
+        .visible(customWindowTitle::get)
+        .build()
     );
 
     public static final Setting<Boolean> useTeamColor = sgGeneral.add(new BoolSetting.Builder()
-            .name("use-team-color")
-            .description("Uses player's team color for rendering things like esp and tracers.")
-            .defaultValue(true)
-            .onChanged(aBoolean -> Config.get().useTeamColor = aBoolean)
-            .onModuleActivated(booleanSetting -> booleanSetting.set(Config.get().useTeamColor))
-            .build()
+        .name("use-team-color")
+        .description("Uses player's team color for rendering things like esp and tracers.")
+        .defaultValue(true)
+        .onChanged(aBoolean -> Config.get().useTeamColor = aBoolean)
+        .onModuleActivated(booleanSetting -> booleanSetting.set(Config.get().useTeamColor))
+        .build()
     );
 
     public static ConfigScreen currentScreen;
@@ -179,50 +180,42 @@ public class ConfigTab extends Tab {
                 String prefix = Config.get().prefix;
 
                 if (prefix.isBlank()) {
-                    new PromptBuilder(theme, this.parent)
+                    YesNoPrompt.create(theme, this.parent)
                         .title("Empty command prefix")
                         .message("You have set your command prefix to nothing.")
                         .message("This WILL prevent you from sending chat messages.")
                         .message("Do you want to reset your prefix back to '.'?")
-                        .onYes(() -> {
-                            Config.get().prefix = ".";
-                        })
-                        .promptId("empty-command-prefix")
+                        .onYes(() -> Config.get().prefix = ".")
+                        .id("empty-command-prefix")
                         .show();
                 }
                 else if (prefix.equals("/")) {
-                    new PromptBuilder(theme, this.parent)
+                    YesNoPrompt.create(theme, this.parent)
                         .title("Potential prefix conflict")
                         .message("You have set your command prefix to '/', which is used by minecraft.")
                         .message("This can cause conflict issues between meteor and minecraft commands.")
                         .message("Do you want to reset your prefix to '.'?")
-                        .onYes(() -> {
-                            Config.get().prefix = ".";
-                        })
-                        .promptId("minecraft-prefix-conflict")
+                        .onYes(() -> Config.get().prefix = ".")
+                        .id("minecraft-prefix-conflict")
                         .show();
                 }
                 else if (prefix.length() > 7) {
-                    new PromptBuilder(theme, this.parent)
+                    YesNoPrompt.create(theme, this.parent)
                         .title("Long command prefix")
                         .message("You have set your command prefix to a very long string.")
                         .message("This means that in order to execute any command, you will need to type %s followed by the command you want to run.", prefix)
                         .message("Do you want to reset your prefix back to '.'?")
-                        .onYes(() -> {
-                            Config.get().prefix = ".";
-                        })
-                        .promptId("long-command-prefix")
+                        .onYes(() -> Config.get().prefix = ".")
+                        .id("long-command-prefix")
                         .show();
                 }
                 else if (isUsedKey()) {
-                    new PromptBuilder(theme, this.parent)
+                    YesNoPrompt.create(theme, this.parent)
                         .title("Prefix keybind")
                         .message("You have \"Open Chat On Prefix\" setting enabled and your command prefix has a conflict with another keybind.")
                         .message("Do you want to disable \"Open Chat On Prefix\" setting?")
-                        .onYes(() -> {
-                            Config.get().openChatOnPrefix = false;
-                        })
-                        .promptId("prefix-keybind")
+                        .onYes(() -> Config.get().openChatOnPrefix = false)
+                        .id("prefix-keybind")
                         .show();
                 }
             });
