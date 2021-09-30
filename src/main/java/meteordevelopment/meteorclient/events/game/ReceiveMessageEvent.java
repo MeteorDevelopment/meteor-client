253d52b67eb946e045ab3cc882745cbc7dcbefb2
MineGame159 <petulko08@gmail.com>
2021-09-30 16:36:31 +0200

    Fixed BetterChat stopping text from being logged in console

--- src/main/java/meteordevelopment/meteorclient/events/game/ReceiveMessageEvent.java
+++ src/main/java/meteordevelopment/meteorclient/events/game/ReceiveMessageEvent.java
@@ -11,13 +11,28 @@ import net.minecraft.text.Text;
 public class ReceiveMessageEvent extends Cancellable {
     private static final ReceiveMessageEvent INSTANCE = new ReceiveMessageEvent();
 
-    public Text message;
+    private Text message;
+    private boolean modified;
     public int id;
 
     public static ReceiveMessageEvent get(Text message, int id) {
         INSTANCE.setCancelled(false);
         INSTANCE.message = message;
+        INSTANCE.modified = false;
         INSTANCE.id = id;
         return INSTANCE;
+    }
+
+    public Text getMessage() {
+        return message;
+    }
+
+    public void setMessage(Text message) {
+        this.message = message;
+        this.modified = true;
+    }
+
+    public boolean isModified() {
+        return modified;
     }
 }
