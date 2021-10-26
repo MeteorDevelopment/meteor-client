1a90f87347e3eaafe7326a92b1eab97eac169165
seasnail8169 <seasnail@meteorclient.com>
2021-10-27 00:13:47 +0100

    bruh

--- src/main/java/meteordevelopment/meteorclient/systems/config/Config.java
+++ src/main/java/meteordevelopment/meteorclient/systems/config/Config.java
@@ -10,7 +10,6 @@ import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.systems.System;
 import meteordevelopment.meteorclient.systems.Systems;
 import meteordevelopment.meteorclient.utils.misc.Version;
-import meteordevelopment.meteorclient.utils.render.color.RainbowColors;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.metadata.ModMetadata;
 import net.minecraft.nbt.NbtCompound;
@@ -22,30 +21,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Config extends System<Config> {
-    public final Version version;
-    public final String devBuild;
+    // Version
+    public Version version;
+    public String devBuild;
 
+    // Visual
     public String font = ConfigTab.font.get();
     public boolean customFont = ConfigTab.customFont.get();
-    public int rotationHoldTicks = ConfigTab.rotationHoldTicks.get();
-
-    public String prefix = ConfigTab.prefix.get();
-    public boolean openChatOnPrefix = ConfigTab.openChatOnPrefix.get();
-    public boolean chatCommandsInfo = ConfigTab.chatCommandsInfo.get();
-    public boolean deleteChatCommandsInfo = ConfigTab.deleteChatCommandsInfo.get();
-
+    public double rainbowSpeed = ConfigTab.rainbowSpeed.get();
     public boolean titleScreenCredits = ConfigTab.titleScreenCredits.get();
     public boolean titleScreenSplashes = ConfigTab.titleScreenSplashes.get();
     public boolean customWindowTitle = ConfigTab.customWindowTitle.get();
     public String customWindowTitleText = ConfigTab.customWindowTitleText.get();
 
-    public boolean useTeamColor = ConfigTab.useTeamColor.get();
+    // Chat
+    public String prefix = ConfigTab.prefix.get();
+    public boolean prefixOpensConsole = ConfigTab.prefixOpensConsole.get();
+    public boolean chatFeedback = ConfigTab.chatFeedback.get();
+    public boolean deleteChatFeedback = ConfigTab.deleteChatFeedback.get();
 
+    // Misc
+    public int rotationHoldTicks = ConfigTab.rotationHoldTicks.get();
+    public boolean useTeamColor = ConfigTab.useTeamColor.get();
     public List<String> dontShowAgainPrompts = new ArrayList<>();
 
     public Config() {
         super("config");
+    }
 
+    public static Config get() {
+        return Systems.get(Config.class);
+    }
+
+    @Override
+    public void init() {
         ModMetadata metadata = FabricLoader.getInstance().getModContainer("meteor-client").get().getMetadata();
 
         String versionString = metadata.getVersion().getFriendlyString();
@@ -55,9 +64,7 @@ public class Config extends System<Config> {
         devBuild = metadata.getCustomValue("meteor-client:devbuild").getAsString();
     }
 
-    public static Config get() {
-        return Systems.get(Config.class);
-    }
+    // Serialisation
 
     @Override
     public NbtCompound toTag() {
@@ -66,22 +73,22 @@ public class Config extends System<Config> {
 
         tag.putString("font", font);
         tag.putBoolean("customFont", customFont);
-        tag.putDouble("rainbowSpeed", RainbowColors.GLOBAL.getSpeed());
-        tag.putInt("rotationHoldTicks", rotationHoldTicks);
+        tag.putDouble("rainbowSpeed", ConfigTab.rainbowSpeed.get());
 
         tag.putString("prefix", prefix);
-        tag.putBoolean("openChatOnPrefix", openChatOnPrefix);
-        tag.putBoolean("chatCommandsInfo", chatCommandsInfo);
-        tag.putBoolean("deleteChatCommandsInfo", deleteChatCommandsInfo);
+        tag.putBoolean("prefixOpensConsole", prefixOpensConsole);
+        tag.putBoolean("chatFeedback", chatFeedback);
+        tag.putBoolean("deleteChatFeedback", deleteChatFeedback);
 
         tag.putBoolean("titleScreenCredits", titleScreenCredits);
         tag.putBoolean("titleScreenSplashes", titleScreenSplashes);
         tag.putBoolean("customWindowTitle", customWindowTitle);
         tag.putString("customWindowTitleText", customWindowTitleText);
 
+        tag.putInt("rotationHoldTicks", rotationHoldTicks);
         tag.putBoolean("useTeamColor", useTeamColor);
+        tag.put("dontShowAgainPrompts", listToTag(dontShowAgainPrompts));
 
-        tag.put("dontShowAgainPrompts", listToNbt(dontShowAgainPrompts));
         return tag;
     }
 
@@ -89,49 +96,52 @@ public class Config extends System<Config> {
     public Config fromTag(NbtCompound tag) {
         font = getString(tag, "font", ConfigTab.font);
         customFont = getBoolean(tag, "customFont", ConfigTab.customFont);
-        RainbowColors.GLOBAL.setSpeed(tag.contains("rainbowSpeed") ? tag.getDouble("rainbowSpeed") : ConfigTab.rainbowSpeed.getDefaultValue() / 100);
-        rotationHoldTicks = getInt(tag, "rotationHoldTicks", ConfigTab.rotationHoldTicks);
+        rainbowSpeed = getDouble(tag, "rainbowSpeed", ConfigTab.rainbowSpeed);
 
         prefix = getString(tag, "prefix", ConfigTab.prefix);
-        openChatOnPrefix = getBoolean(tag, "openChatOnPrefix", ConfigTab.openChatOnPrefix);
-        chatCommandsInfo = getBoolean(tag, "chatCommandsInfo", ConfigTab.chatCommandsInfo);
-        deleteChatCommandsInfo = getBoolean(tag, "deleteChatCommandsInfo", ConfigTab.deleteChatCommandsInfo);
+        prefixOpensConsole = getBoolean(tag, "prefixOpensConsole", ConfigTab.prefixOpensConsole);
+        chatFeedback = getBoolean(tag, "chatFeedback", ConfigTab.chatFeedback);
+        deleteChatFeedback = getBoolean(tag, "deleteChatFeedback", ConfigTab.deleteChatFeedback);
 
         titleScreenCredits = getBoolean(tag, "titleScreenCredits", ConfigTab.titleScreenCredits);
         titleScreenSplashes = getBoolean(tag, "titleScreenSplashes", ConfigTab.titleScreenSplashes);
         customWindowTitle = getBoolean(tag, "customWindowTitle", ConfigTab.customWindowTitle);
         customWindowTitleText = getString(tag, "customWindowTitleText", ConfigTab.customWindowTitleText);
 
+        rotationHoldTicks = getInt(tag, "rotationHoldTicks", ConfigTab.rotationHoldTicks);
         useTeamColor = getBoolean(tag, "useTeamColor", ConfigTab.useTeamColor);
-
-        dontShowAgainPrompts.clear();
-        for (NbtElement item : tag.getList("dontShowAgainPrompts", NbtElement.STRING_TYPE)) {
-            dontShowAgainPrompts.add(item.asString());
-        }
+        dontShowAgainPrompts = listFromTag(tag, "dontShowAgainPrompts");
 
         return this;
     }
 
+    // Utils
+
     private boolean getBoolean(NbtCompound tag, String key, Setting<Boolean> setting) {
-        return tag.contains(key) ? tag.getBoolean(key) : setting.get();
+        return tag.contains(key) ? tag.getBoolean(key) : setting.getDefaultValue();
     }
 
     private String getString(NbtCompound tag, String key, Setting<String> setting) {
-        return tag.contains(key) ? tag.getString(key) : setting.get();
+        return tag.contains(key) ? tag.getString(key) : setting.getDefaultValue();
     }
 
     private double getDouble(NbtCompound tag, String key, Setting<Double> setting) {
-        return tag.contains(key) ? tag.getDouble(key) : setting.get();
+        return tag.contains(key) ? tag.getDouble(key) : setting.getDefaultValue();
     }
 
     private int getInt(NbtCompound tag, String key, Setting<Integer> setting) {
-        return tag.contains(key) ? tag.getInt(key) : setting.get();
+        return tag.contains(key) ? tag.getInt(key) : setting.getDefaultValue();
     }
 
-    private NbtList listToNbt(List<String> lst) {
+    private NbtList listToTag(List<String> list) {
         NbtList nbt = new NbtList();
-        for (String item: lst)
-            nbt.add(NbtString.of(item));
+        for (String item : list) nbt.add(NbtString.of(item));
         return nbt;
+    }
+
+    private List<String> listFromTag(NbtCompound tag, String key) {
+        List<String> list = new ArrayList<>();
+        for (NbtElement item : tag.getList(key, 8)) list.add(item.asString());
+        return list;
     }
 }
