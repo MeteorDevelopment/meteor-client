cbf6cc549cd40f5e1dcef14922fa47fc460b1023
MineGame159 <petulko08@gmail.com>
2021-11-12 11:49:34 +0100

    Made account manager not use any head api

--- src/main/java/meteordevelopment/meteorclient/systems/accounts/AccountUtils.java
+++ src/main/java/meteordevelopment/meteorclient/systems/accounts/AccountUtils.java
@@ -5,11 +5,14 @@
 
 package meteordevelopment.meteorclient.systems.accounts;
 
+import com.google.gson.Gson;
 import com.mojang.authlib.yggdrasil.YggdrasilMinecraftSessionService;
+import meteordevelopment.meteorclient.utils.network.Http;
 
 import java.lang.reflect.Field;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Base64;
 
 public class AccountUtils {
     public static void setBaseUrl(YggdrasilMinecraftSessionService service, String url) {
@@ -40,5 +43,21 @@ public class AccountUtils {
         } catch (IllegalAccessException | NoSuchFieldException | MalformedURLException e) {
             e.printStackTrace();
         }
+    }
+
+    public static String getSkinUrl(String username) {
+        ProfileResponse res = Http.get("https://api.mojang.com/users/profiles/minecraft/" + username).sendJson(ProfileResponse.class);
+        if (res == null) return null;
+
+        UuidToProfileResponse res2 = Http.get("https://sessionserver.mojang.com/session/minecraft/profile/" + res.id).sendJson(UuidToProfileResponse.class);
+        if (res2 == null) return null;
+
+        String base64Textures = res2.getPropertyValue("textures");
+        if (base64Textures == null) return null;
+
+        TexturesJson textures = new Gson().fromJson(new String(Base64.getDecoder().decode(base64Textures)), TexturesJson.class);
+        if (textures.textures.SKIN == null) return null;
+
+        return textures.textures.SKIN.url;
     }
 }
--- src/main/java/meteordevelopment/meteorclient/systems/accounts/types/TheAlteningAccount.java
+++ src/main/java/meteordevelopment/meteorclient/systems/accounts/types/TheAlteningAccount.java
@@ -47,15 +47,6 @@ public class TheAlteningAccount extends Account<TheAlteningAccount> {
     }
 
     @Override
-    public boolean fetchHead() {
-        try {
-            return cache.makeHead("https://www.mc-heads.net/avatar/" + cache.uuid + "/8");
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    @Override
     public boolean login() {
         YggdrasilMinecraftSessionService service = (YggdrasilMinecraftSessionService) mc.getSessionService();
         AccountUtils.setBaseUrl(service, SESSION + "/session/minecraft/");
