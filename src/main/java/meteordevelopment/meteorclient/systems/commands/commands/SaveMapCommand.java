5f8cb5b54bdc71a19147698c0cfce07868c6b0c8
Wide_Cat <tom.radden@gmail.com>
2021-11-19 22:52:39 +0000

    option to change the scale of saved maps with the save-map cmd

--- src/main/java/meteordevelopment/meteorclient/systems/commands/commands/SaveMapCommand.java
+++ src/main/java/meteordevelopment/meteorclient/systems/commands/commands/SaveMapCommand.java
@@ -5,11 +5,12 @@
 
 package meteordevelopment.meteorclient.systems.commands.commands;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
+import meteordevelopment.meteorclient.mixin.MapRendererAccessor;
 import meteordevelopment.meteorclient.systems.commands.Command;
-import net.minecraft.block.MapColor;
-import net.minecraft.client.texture.NativeImage;
+import net.minecraft.client.render.MapRenderer;
 import net.minecraft.command.CommandSource;
 import net.minecraft.item.FilledMapItem;
 import net.minecraft.item.ItemStack;
@@ -21,6 +22,9 @@ import org.lwjgl.PointerBuffer;
 import org.lwjgl.system.MemoryUtil;
 import org.lwjgl.util.tinyfd.TinyFileDialogs;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -47,36 +51,74 @@ public class SaveMapCommand extends Command {
     @Override
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         builder.executes(context -> {
-            ItemStack map = getMap();
-            if (map == null) throw MAP_NOT_FOUND.create();
 
-            MapState state = FilledMapItem.getMapState(FilledMapItem.getMapId(map), mc.world);
+            MapState state = getMapState();
             if (state == null) throw MAP_NOT_FOUND.create();
+            ItemStack map = getMap();
 
-            String path = TinyFileDialogs.tinyfd_saveFileDialog("Save image", null, filters, null);
+            String path = getPath();
             if (path == null) throw OOPS.create();
-            if (!path.endsWith(".png")) path += ".png";
 
-            NativeImage image = new NativeImage(128, 128, true);
-
-            for (int x = 0; x < 128; x++) {
-                for (int y = 0; y < 128; y++) {
-                    int c = state.colors[x + y * 128] & 255;
-
-                    if (c / 4 == 0) image.setColor(x, y, 0);
-                    else image.setColor(x, y, MapColor.COLORS[c / 4].getRenderColor(c & 3));
-                }
-            }
-
-            try {
-                image.writeTo(new File(path));
-                image.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            downloadMap(map, state, path, 128);
 
             return SINGLE_SUCCESS;
-        });
+        }).then(argument("scale", IntegerArgumentType.integer()).executes(context -> {
+            int scale = IntegerArgumentType.getInteger(context, "scale");
+
+            MapState state = getMapState();
+            if (state == null) throw MAP_NOT_FOUND.create();
+            ItemStack map = getMap();
+
+            String path = getPath();
+            if (path == null) throw OOPS.create();
+
+            downloadMap(map, state, path, scale);
+
+            return SINGLE_SUCCESS;
+        }));
+    }
+
+    private void downloadMap(ItemStack map, MapState state, String path, int scale) {
+        //this is horrible code but it somehow works
+
+        MapRenderer mapRenderer = mc.gameRenderer.getMapRenderer();
+        MapRenderer.MapTexture texture = ((MapRendererAccessor) mapRenderer).invokeGetMapTexture(FilledMapItem.getMapId(map), state);
+
+        int[] data = texture.texture.getImage().makePixelArray();
+        BufferedImage image = new BufferedImage(128, 128, 2);
+        image.setRGB(0, 0, image.getWidth(), image.getHeight(), data, 0, 128);
+
+        BufferedImage scaledImage = new BufferedImage(scale, scale, 2);
+        if (scale != 128) {
+            Graphics2D g = scaledImage.createGraphics();
+            g.setComposite(AlphaComposite.Src);
+            g.drawImage(image, 0, 0, scale, scale, null);
+            g.dispose();
+        }
+
+        try {
+            ImageIO.write((scale == 128 ? image : scaledImage), "png", new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private MapState getMapState() {
+        ItemStack map = getMap();
+        if (map == null) return null;
+
+        MapState state = FilledMapItem.getMapState(FilledMapItem.getMapId(map), mc.world);
+        if (state == null) return null;
+
+        return state;
+    }
+
+    private String getPath() {
+        String path = TinyFileDialogs.tinyfd_saveFileDialog("Save image", null, filters, null);
+        if (path == null) return null;
+        if (!path.endsWith(".png")) path += ".png";
+
+        return path;
     }
 
     private ItemStack getMap() {
