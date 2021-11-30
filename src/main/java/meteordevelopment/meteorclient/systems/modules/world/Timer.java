79e93defa5091f665ec4e3580bddd511485c221c
seasnail8169 <seasnail@meteorclient.com>
2021-11-30 17:08:20 +0000

    Fixed builds

--- src/main/java/meteordevelopment/meteorclient/systems/modules/world/Timer.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/world/Timer.java
@@ -5,28 +5,22 @@
 
 package meteordevelopment.meteorclient.systems.modules.world;
 
-import meteordevelopment.meteorclient.gui.GuiTheme;
-import meteordevelopment.meteorclient.gui.widgets.WWidget;
-import meteordevelopment.meteorclient.gui.widgets.containers.WHorizontalList;
-import meteordevelopment.meteorclient.gui.widgets.pressable.WButton;
 import meteordevelopment.meteorclient.settings.DoubleSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Categories;
-import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.meteorclient.systems.modules.misc.TPSSync;
 
 public class Timer extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
     private final Setting<Double> multiplier = sgGeneral.add(new DoubleSetting.Builder()
-            .name("multiplier")
-            .description("The timer multiplier amount.")
-            .defaultValue(1)
-            .min(0.1)
-            .sliderMin(1)
-            .build()
+        .name("multiplier")
+        .description("The timer multiplier amount.")
+        .defaultValue(1)
+        .min(0.1)
+        .sliderMin(0.1)
+        .build()
     );
 
     public static final double OFF = 1;
@@ -42,21 +36,5 @@ public class Timer extends Module {
 
     public void setOverride(double override) {
         this.override = override;
-    }
-
-    @Override
-    public WWidget getWidget(GuiTheme theme) {
-        if (Modules.get().get(TPSSync.class).isActive()) {
-            WHorizontalList list = theme.horizontalList();
-            list.add(theme.label("Multiplier is overwritten by TPSSync."));
-            WButton disableBtn = list.add(theme.button("Disable TPSSync")).widget();
-            disableBtn.action = () -> {
-                TPSSync tpsSync = Modules.get().get(TPSSync.class);
-                if (tpsSync.isActive()) tpsSync.toggle();
-                list.visible = false;
-            };
-            return list;
-        }
-        return null;
     }
 }
