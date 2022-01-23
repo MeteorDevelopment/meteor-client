07f670f33bbc6151c97e30ee59cb8d444947b102
DiOnFire <di0nik.gd@gmail.com>
2022-01-23 18:21:14 +0300

    clean up

--- src/main/java/meteordevelopment/meteorclient/events/render/HeldItemRendererEvent.java
+++ src/main/java/meteordevelopment/meteorclient/events/render/HeldItemRendererEvent.java
@@ -5,33 +5,18 @@
 
 package meteordevelopment.meteorclient.events.render;
 
-import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 
 public class HeldItemRendererEvent {
     private static final HeldItemRendererEvent INSTANCE = new HeldItemRendererEvent();
 
-    public AbstractClientPlayerEntity player;
-    public float tickDelta, pitch, swingProgress, equipProgress;
     public Hand hand;
-    public ItemStack item;
     public MatrixStack matrix;
-    public VertexConsumerProvider vertex;
-    public int light;
 
-    public static HeldItemRendererEvent get(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
-        INSTANCE.equipProgress = equipProgress;
-        INSTANCE.tickDelta = tickDelta;
-        INSTANCE.pitch = pitch;
-        INSTANCE.player = player;
+    public static HeldItemRendererEvent get(Hand hand, MatrixStack matrices) {
         INSTANCE.hand = hand;
-        INSTANCE.swingProgress = swingProgress;
         INSTANCE.matrix = matrices;
-        INSTANCE.vertex = vertexConsumers;
-        INSTANCE.light = light;
         return INSTANCE;
     }
 }
