6eeb95b397ce1895ee7b0b5f1435931f38d9b489
Wide-Cat <tom.radden@gmail.com>
2022-02-10 16:15:52 +0000

    hand view: fix old settings and add new ones

--- src/main/java/meteordevelopment/meteorclient/systems/modules/render/HandView.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/render/HandView.java
@@ -5,210 +5,71 @@
 
 package meteordevelopment.meteorclient.systems.modules.render;
 
+import meteordevelopment.meteorclient.events.render.ArmRenderEvent;
 import meteordevelopment.meteorclient.events.render.HeldItemRendererEvent;
-import meteordevelopment.meteorclient.settings.DoubleSetting;
-import meteordevelopment.meteorclient.settings.EnumSetting;
-import meteordevelopment.meteorclient.settings.IntSetting;
-import meteordevelopment.meteorclient.settings.Setting;
-import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.player.Rotations;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Hand;
-import net.minecraft.util.math.Quaternion;
 import net.minecraft.util.math.Vec3f;
 
 public class HandView extends Module {
-    private final SettingGroup mainHand = settings.createGroup("Main Hand");
-    private final SettingGroup offHand = settings.createGroup("Off Hand");
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final SettingGroup sgMainHand = settings.createGroup("Main Hand");
+    private final SettingGroup sgOffHand = settings.createGroup("Off Hand");
+    private final SettingGroup sgArm = settings.createGroup("Arm");
     private final SettingGroup sgSwing = settings.createGroup("Swing");
 
+    //general
+    private final Setting<Boolean> followRotations = sgGeneral.add(new BoolSetting.Builder()
+        .name("follow-rotations")
+        .description("Makes your hands follow your serverside rotations.")
+        .defaultValue(false)
+        .build()
+    );
+
     // Scale
-
-    private final Setting<Double> scaleXMain = mainHand.add(new DoubleSetting.Builder()
-        .name("Scale X")
-        .description("The X scale of your main hand.")
-        .defaultValue(1)
-        .sliderMax(5)
-        .build()
-    );
-
-    private final Setting<Double> scaleYMain = mainHand.add(new DoubleSetting.Builder()
-        .name("Scale Y")
-        .description("The Y scale of your main hand.")
-        .defaultValue(1)
-        .sliderMax(5)
-        .build()
-    );
-
-    private final Setting<Double> scaleZMain = mainHand.add(new DoubleSetting.Builder()
-        .name("Scale Z")
-        .description("The Z scale of your main hand.")
-        .defaultValue(1)
-        .sliderMax(5)
-        .build()
-    );
-
-    private final Setting<Double> scaleXOff = offHand.add(new DoubleSetting.Builder()
-        .name("Scale X")
-        .description("The X scale of your offhand.")
-        .defaultValue(1)
-        .sliderMax(5)
-        .build()
-    );
-
-    private final Setting<Double> scaleYOff = offHand.add(new DoubleSetting.Builder()
-        .name("Scale Y")
-        .description("The Y scale of your offhand.")
-        .defaultValue(1)
-        .sliderMax(5)
-        .build()
-    );
-
-    private final Setting<Double> scaleZOff = offHand.add(new DoubleSetting.Builder()
-        .name("Scale Z")
-        .description("The Z scale of your offhand.")
-        .defaultValue(1)
-        .sliderMax(5)
-        .build()
-    );
+    private final Setting<Double> scaleXMain = sgMainHand.add(new DoubleSetting.Builder().name("mainhand-scale-x").description("The X scale of your main hand.").defaultValue(1).sliderMax(5).build());
+    private final Setting<Double> scaleYMain = sgMainHand.add(new DoubleSetting.Builder().name("mainhand-scale-y").description("The Y scale of your main hand.").defaultValue(1).sliderMax(5).build());
+    private final Setting<Double> scaleZMain = sgMainHand.add(new DoubleSetting.Builder().name("mainhand-scale-z").description("The Z scale of your main hand.").defaultValue(1).sliderMax(5).build());
+    private final Setting<Double> scaleXOff = sgOffHand.add(new DoubleSetting.Builder().name("offhand-scale-x").description("The X scale of your offhand.").defaultValue(1).sliderMax(5).build());
+    private final Setting<Double> scaleYOff = sgOffHand.add(new DoubleSetting.Builder().name("offhand-scale-y").description("The Y scale of your offhand.").defaultValue(1).sliderMax(5).build());
+    private final Setting<Double> scaleZOff = sgOffHand.add(new DoubleSetting.Builder().name("offhand-scale-z").description("The Z scale of your offhand.").defaultValue(1).sliderMax(5).build());
+    private final Setting<Double> scaleXArm = sgArm.add(new DoubleSetting.Builder().name("arm-scale-x").description("The X scale of your arm.").defaultValue(1).sliderMax(5).build());
+    private final Setting<Double> scaleYArm = sgArm.add(new DoubleSetting.Builder().name("arm-scale-y").description("The Y scale of your arm.").defaultValue(1).sliderMax(5).build());
+    private final Setting<Double> scaleZArm = sgArm.add(new DoubleSetting.Builder().name("arm-scale-z").description("The Z scale of your arm.").defaultValue(1).sliderMax(5).build());
 
     // Position
-
-    private final Setting<Double> posXMain = mainHand.add(new DoubleSetting.Builder()
-        .name("Position X")
-        .description("The X position offset of your main hand.")
-        .defaultValue(0)
-        .sliderRange(-3, 3)
-        .build()
-    );
-
-    private final Setting<Double> posYMain = mainHand.add(new DoubleSetting.Builder()
-        .name("Position Y")
-        .description("The Y position offset of your main hand.")
-        .defaultValue(0)
-        .sliderRange(-3, 3)
-        .build()
-    );
-
-    private final Setting<Double> posZMain = mainHand.add(new DoubleSetting.Builder()
-        .name("Position Z")
-        .description("The Z position offset of your main hand.")
-        .defaultValue(0)
-        .sliderRange(-3, 3)
-        .build()
-    );
-
-    private final Setting<Double> posXOff = offHand.add(new DoubleSetting.Builder()
-        .name("Position X")
-        .description("The X position offset of your offhand.")
-        .defaultValue(0)
-        .sliderRange(-3, 3)
-        .build()
-    );
-
-    private final Setting<Double> posYOff = offHand.add(new DoubleSetting.Builder()
-        .name("Position Y")
-        .description("The Y position offset of your offhand.")
-        .defaultValue(0)
-        .sliderRange(-3, 3)
-        .build()
-    );
-
-    private final Setting<Double> posZOff = offHand.add(new DoubleSetting.Builder()
-        .name("Position Z")
-        .description("The Z position offset of your offhand.")
-        .defaultValue(0)
-        .sliderRange(-3, 3)
-        .build()
-    );
+    private final Setting<Double> posXMain = sgMainHand.add(new DoubleSetting.Builder().name("mainhand-position-x").description("The X position offset of your main hand.").defaultValue(0).sliderRange(-3, 3).build());
+    private final Setting<Double> posYMain = sgMainHand.add(new DoubleSetting.Builder().name("mainhand-position-y").description("The Y position offset of your main hand.").defaultValue(0).sliderRange(-3, 3).build());
+    private final Setting<Double> posZMain = sgMainHand.add(new DoubleSetting.Builder().name("mainhand-position-z").description("The Z position offset of your main hand.").defaultValue(0).sliderRange(-3, 3).build());
+    private final Setting<Double> posXOff = sgOffHand.add(new DoubleSetting.Builder().name("offhand-position-x").description("The X position offset of your offhand.").defaultValue(0).sliderRange(-3, 3).build());
+    private final Setting<Double> posYOff = sgOffHand.add(new DoubleSetting.Builder().name("offhand-position-y").description("The Y position offset of your offhand.").defaultValue(0).sliderRange(-3, 3).build());
+    private final Setting<Double> posZOff = sgOffHand.add(new DoubleSetting.Builder().name("offhand-position-z").description("The Z position offset of your offhand.").defaultValue(0).sliderRange(-3, 3).build());
+    private final Setting<Double> posXArm = sgArm.add(new DoubleSetting.Builder().name("arm-position-x").description("The X position offset of your arm.").defaultValue(0).sliderRange(-3, 3).build());
+    private final Setting<Double> posYArm = sgArm.add(new DoubleSetting.Builder().name("arm-position-y").description("The Y position offset of your arm.").defaultValue(0).sliderRange(-3, 3).build());
+    private final Setting<Double> posZArm = sgArm.add(new DoubleSetting.Builder().name("arm-position-z").description("The Z position offset of your arm.").defaultValue(0).sliderRange(-3, 3).build());
 
     // Rotation
-
-    private final Setting<Double> rotationXMain = mainHand.add(new DoubleSetting.Builder()
-        .name("Rotation X")
-        .description("The X orientation of your main hand.")
-        .defaultValue(0)
-        .sliderRange(-180, 180)
-        .build()
-    );
-
-    private final Setting<Double> rotationYMain = mainHand.add(new DoubleSetting.Builder()
-        .name("Rotation Y")
-        .description("The Y orientation of your main hand.")
-        .defaultValue(0)
-        .sliderRange(-180, 180)
-        .build()
-    );
-
-    private final Setting<Double> rotationZMain = mainHand.add(new DoubleSetting.Builder()
-        .name("Rotation Z")
-        .description("The Z orientation of your main hand.")
-        .defaultValue(0)
-        .sliderRange(-180, 180)
-        .build()
-    );
-
-    private final Setting<Double> rotationXOff = offHand.add(new DoubleSetting.Builder()
-        .name("Rotation X")
-        .description("The X orientation of your offhand.")
-        .defaultValue(0)
-        .sliderRange(-180, 180)
-        .build()
-    );
-
-    private final Setting<Double> rotationYOff = offHand.add(new DoubleSetting.Builder()
-        .name("Rotation Y")
-        .description("The Y orientation of your offhand.")
-        .defaultValue(0)
-        .sliderRange(-180, 180)
-        .build()
-    );
-
-    private final Setting<Double> rotationZOff = offHand.add(new DoubleSetting.Builder()
-        .name("Rotation Z")
-        .description("The Z orientation of your offhand.")
-        .defaultValue(0)
-        .sliderRange(-180, 180)
-        .build()
-    );
+    private final Setting<Double> rotationXMain = sgMainHand.add(new DoubleSetting.Builder().name("mainhand-rotation-x").description("The X orientation of your main hand.").defaultValue(0).sliderRange(-180, 180).build());
+    private final Setting<Double> rotationYMain = sgMainHand.add(new DoubleSetting.Builder().name("mainhand-rotation-y").description("The Y orientation of your main hand.").defaultValue(0).sliderRange(-180, 180).build());
+    private final Setting<Double> rotationZMain = sgMainHand.add(new DoubleSetting.Builder().name("mainhand-rotation-z").description("The Z orientation of your main hand.").defaultValue(0).sliderRange(-180, 180).build());
+    private final Setting<Double> rotationXOff = sgOffHand.add(new DoubleSetting.Builder().name("offhand-rotation-x").description("The X orientation of your offhand.").defaultValue(0).sliderRange(-180, 180).build());
+    private final Setting<Double> rotationYOff = sgOffHand.add(new DoubleSetting.Builder().name("offhand-rotation-y").description("The Y orientation of your offhand.").defaultValue(0).sliderRange(-180, 180).build());
+    private final Setting<Double> rotationZOff = sgOffHand.add(new DoubleSetting.Builder().name("offhand-rotation-z").description("The Z orientation of your offhand.").defaultValue(0).sliderRange(-180, 180).build());
+    private final Setting<Double> rotationXArm = sgArm.add(new DoubleSetting.Builder().name("arm-rotation-x").description("The X orientation of your arm.").defaultValue(0).sliderRange(-180, 180).build());
+    private final Setting<Double> rotationYArm = sgArm.add(new DoubleSetting.Builder().name("arm-rotation-y").description("The Y orientation of your arm.").defaultValue(0).sliderRange(-180, 180).build());
+    private final Setting<Double> rotationZArm = sgArm.add(new DoubleSetting.Builder().name("arm-rotation-z").description("The Z orientation of your arm.").defaultValue(0).sliderRange(-180, 180).build());
 
     // Swing
+    public final Setting<SwingMode> swingMode = sgSwing.add(new EnumSetting.Builder<SwingMode>().name("mode").description("Modifies your client & server hand swinging.").defaultValue(SwingMode.None).build());
+    public final Setting<Double> mainSwing = sgSwing.add(new DoubleSetting.Builder().name("main-progress").description("The swing progress of your main hand.").defaultValue(0).range(0, 1).sliderMax(1).build());
+    public final Setting<Double> offSwing = sgSwing.add(new DoubleSetting.Builder().name("offhand-progress").description("The swing progress of your offhand.").defaultValue(0).range(0, 1).sliderMax(1).build());
+    public final Setting<Integer> swingSpeed = sgSwing.add(new IntSetting.Builder().name("swing-speed").description("The swing speed of your hands. (higher = slower swing)").defaultValue(6).range(0, 20).sliderMax(20).build());
 
-    public final Setting<SwingMode> swingMode = sgSwing.add(new EnumSetting.Builder<SwingMode>()
-        .name("mode")
-        .description("Modifies your client & server hand swinging.")
-        .defaultValue(SwingMode.None)
-        .build()
-    );
-
-    public final Setting<Double> mainSwing = sgSwing.add(new DoubleSetting.Builder()
-        .name("main-progress")
-        .description("The swing progress of your main hand.")
-        .defaultValue(0)
-        .range(0, 1)
-        .sliderMax(1)
-        .build()
-    );
-
-    public final Setting<Double> offSwing = sgSwing.add(new DoubleSetting.Builder()
-        .name("offhand-progress")
-        .description("The swing progress of your offhand.")
-        .defaultValue(0)
-        .range(0, 1)
-        .sliderMax(1)
-        .build()
-    );
-
-    public final Setting<Integer> swingSpeed = sgSwing.add(new IntSetting.Builder()
-        .name("swing-speed")
-        .description("The swing speed of your hands. (higher = slower swing)")
-        .defaultValue(6)
-        .range(0, 20)
-        .sliderMax(20)
-        .build()
-    );
 
     public HandView() {
         super(Categories.Render, "hand-view", "Alters the way items are rendered in your hands.");
@@ -230,6 +91,25 @@ public class HandView extends Module {
             event.matrix.scale(scaleXOff.get().floatValue(), scaleYOff.get().floatValue(), scaleZOff.get().floatValue());
             event.matrix.translate(posXOff.get().floatValue(), posYOff.get().floatValue(), posZOff.get().floatValue());
         }
+        if (Rotations.rotating && followRotations.get()) {
+            applyServerRotations(event.matrix);
+        }
+    }
+
+    @EventHandler
+    private void onRenderArm(ArmRenderEvent event) {
+        if (!isActive()) return;
+
+        event.matrix.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(rotationXArm.get().floatValue()));
+        event.matrix.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(rotationYArm.get().floatValue()));
+        event.matrix.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion(rotationZArm.get().floatValue()));
+        event.matrix.scale(scaleXArm.get().floatValue(), scaleYArm.get().floatValue(), scaleZArm.get().floatValue());
+        event.matrix.translate(posXArm.get().floatValue(), posYArm.get().floatValue(), posZArm.get().floatValue());
+    }
+
+    private void applyServerRotations(MatrixStack matrix) {
+        matrix.multiply(Vec3f.POSITIVE_X.getDegreesQuaternion(mc.player.getPitch() - Rotations.serverPitch));
+        matrix.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(mc.player.getYaw() - Rotations.serverYaw));
     }
 
     public enum SwingMode {
