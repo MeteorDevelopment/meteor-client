7ad3c90fb8f66c5a673014c88d610afad423a0ab
nothub <48992448+nothub@users.noreply.github.com>
2022-02-22 02:25:43 +0100

    visibility config setting

--- src/main/java/meteordevelopment/meteorclient/systems/hud/modules/BreakingBlockHud.java
+++ src/main/java/meteordevelopment/meteorclient/systems/hud/modules/BreakingBlockHud.java
@@ -6,9 +6,21 @@
 package meteordevelopment.meteorclient.systems.hud.modules;
 
 import meteordevelopment.meteorclient.mixin.ClientPlayerInteractionManagerAccessor;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.hud.HUD;
 
 public class BreakingBlockHud extends DoubleTextHudElement {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Boolean> hide = sgGeneral.add(new BoolSetting.Builder()
+        .name("hide")
+        .description("Hide while not breaking any block.")
+        .defaultValue(true)
+        .build()
+    );
+
     public BreakingBlockHud(HUD hud) {
         super(hud, "breaking-block", "Displays percentage of the block you are breaking.", "Breaking Block: ");
     }
@@ -21,7 +33,7 @@ public class BreakingBlockHud extends DoubleTextHudElement {
         }
 
         float breakingProgress = ((ClientPlayerInteractionManagerAccessor) mc.interactionManager).getBreakingProgress();
-        visible = breakingProgress > 0;
+        if (hide.get()) visible = breakingProgress > 0;
         return String.format("%.0f%%", breakingProgress * 100);
     }
 }
