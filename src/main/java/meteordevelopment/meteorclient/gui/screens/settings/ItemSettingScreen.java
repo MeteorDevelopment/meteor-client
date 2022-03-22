0c02e4e0f0865b694c29899b54fc61c74dcc04d4
Wide-Cat <tom.radden@gmail.com>
2022-03-22 19:31:09 +0000

    fix item setting screen

--- src/main/java/meteordevelopment/meteorclient/gui/screens/settings/ItemSettingScreen.java
+++ src/main/java/meteordevelopment/meteorclient/gui/screens/settings/ItemSettingScreen.java
@@ -40,10 +40,11 @@ public class ItemSettingScreen extends WindowScreen {
             filterText = filter.get().trim();
 
             table.clear();
-            initWidgets();
+            initTable();
         };
 
         table = add(theme.table()).expandX().widget();
+        initTable();
     }
 
     public void initTable() {
