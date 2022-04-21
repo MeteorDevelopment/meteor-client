bf672802e7ac8e43078c81784f51d23bb0d2b220
Cloudburst <18114966+C10udburst@users.noreply.github.com>
2022-04-21 17:30:56 +0200

    make .server plugins slower, but harder to prevent (#2227)

--- src/main/java/meteordevelopment/meteorclient/systems/commands/commands/ServerCommand.java
+++ src/main/java/meteordevelopment/meteorclient/systems/commands/commands/ServerCommand.java
@@ -33,12 +33,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
 
 public class ServerCommand extends Command {
-    private static final List<String> ANTICHEAT_LIST = Arrays.asList("nocheatplus", "negativity", "warden", "horizon","illegalstack","coreprotect","exploitsx");
-    private Integer ticks = 0;
+    private static final List<String> ANTICHEAT_LIST = Arrays.asList("nocheatplus", "negativity", "warden", "horizon", "illegalstack", "coreprotect", "exploitsx");
+    private static final String completionStarts = "/:abcdefghijklmnopqrstuvwxyz0123456789-";
+    private int ticks = 0;
+    private List<String> plugins = new ArrayList<>();
+
 
     public ServerCommand() {
         super("server", "Prints server information");
@@ -58,8 +62,20 @@ public class ServerCommand extends Command {
 
         builder.then(literal("plugins").executes(ctx -> {
             ticks = 0;
+            plugins.clear();
             MeteorClient.EVENT_BUS.subscribe(this);
-            mc.player.networkHandler.sendPacket(new RequestCommandCompletionsC2SPacket(0, "/"));
+            info("Please wait around 5 seconds...");
+            (new Thread(() -> {
+                Random random = new Random();
+                completionStarts.chars().forEach(i -> {
+                    mc.player.networkHandler.sendPacket(new RequestCommandCompletionsC2SPacket(random.nextInt(200), Character.toString(i)));
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
+            })).start();
             return SINGLE_SUCCESS;
         }));
 
@@ -162,10 +178,22 @@ public class ServerCommand extends Command {
     private void onTick(TickEvent.Post event) {
         ticks++;
 
-        if (ticks >= 5000) {
-            error("Plugins check timed out");
-            MeteorClient.EVENT_BUS.unsubscribe(this);
+        if (ticks >= 100) {
+            Collections.sort(plugins);
+
+            for (int i = 0; i < plugins.size(); i++) {
+                plugins.set(i, formatName(plugins.get(i)));
+            }
+
+            if (!plugins.isEmpty()) {
+                info("Plugins (%d): %s ", plugins.size(), Strings.join(plugins.toArray(new String[0]), ", "));
+            } else {
+                error("No plugins found.");
+            }
+
             ticks = 0;
+            plugins.clear();
+            MeteorClient.EVENT_BUS.unsubscribe(this);
         }
     }
 
@@ -173,44 +201,27 @@ public class ServerCommand extends Command {
     private void onReadPacket(PacketEvent.Receive event) {
         try {
             if (event.packet instanceof CommandSuggestionsS2CPacket packet) {
-                List<String> plugins = new ArrayList<>();
+                
                 Suggestions matches = packet.getSuggestions();
 
                 if (matches == null) {
                     error("Invalid Packet.");
                     return;
                 }
-
-                for (Suggestion yes : matches.getList()) {
-                    String[] command = yes.getText().split(":");
+                
+                for (Suggestion suggestion : matches.getList()) {
+                    String[] command = suggestion.getText().split(":");
                     if (command.length > 1) {
                         String pluginName = command[0].replace("/", "");
-
+        
                         if (!plugins.contains(pluginName)) {
                             plugins.add(pluginName);
                         }
                     }
                 }
-
-                Collections.sort(plugins);
-                for (int i = 0; i < plugins.size(); i++) {
-                    plugins.set(i, formatName(plugins.get(i)));
-                }
-
-                if (!plugins.isEmpty()) {
-                    info("Plugins (%d): %s ", plugins.size(), Strings.join(plugins.toArray(new String[0]), ", "));
-                } else {
-                    error("No plugins found.");
-                }
-
-                ticks = 0;
-                MeteorClient.EVENT_BUS.unsubscribe(this);
             }
-
         } catch (Exception e) {
             error("An error occurred while trying to find plugins");
-            ticks = 0;
-            MeteorClient.EVENT_BUS.unsubscribe(this);
         }
     }
 
