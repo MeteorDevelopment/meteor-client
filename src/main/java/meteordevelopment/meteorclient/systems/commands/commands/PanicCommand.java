feb19128781d03c82fde528fd3be8a38e8b376a4
Unknown-User2 <benjaminjneudorf@gmail.com>
2022-04-26 17:08:59 -0500

    Add license header and remove unsed imports

--- src/main/java/meteordevelopment/meteorclient/systems/commands/commands/PanicCommand.java
+++ src/main/java/meteordevelopment/meteorclient/systems/commands/commands/PanicCommand.java
@@ -1,15 +1,16 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client/).
+ * Copyright (c) 2021 Meteor Development.
+ */
+
 package meteordevelopment.meteorclient.systems.commands.commands;
 
-import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import meteordevelopment.meteorclient.systems.commands.Command;
 import net.minecraft.command.CommandSource;
 import meteordevelopment.meteorclient.systems.hud.HUD;
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
-
-import java.util.Collection;
-import java.util.Iterator;
 
 public class PanicCommand extends Command {
     public PanicCommand() {
