4c725df0817177327039d593067b3fbcd228fb3c
CodeDoc537 <62254737+CodeDoc537@users.noreply.github.com>
2022-05-24 14:11:31 -0700

    Update to match other parts in the client

--- src/main/java/meteordevelopment/meteorclient/systems/modules/world/EChestFarmer.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/world/EChestFarmer.java
@@ -66,7 +66,7 @@ public class EChestFarmer extends Module {
 
     private final Setting<Boolean> swingHand = sgRender.add(new BoolSetting.Builder()
         .name("swing-hand")
-        .description("Swing hand client side.")
+        .description("Swing hand client-side.")
         .defaultValue(true)
         .build()
     );
