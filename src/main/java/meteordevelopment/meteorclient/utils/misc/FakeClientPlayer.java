0b9bb3b8a8536667cdc7f5d7c837eba68c1fccc6
arlomcwalter <mcwalterarlo@gmail.com>
2022-06-12 00:54:23 +0100

    fix hud crashing

--- src/main/java/meteordevelopment/meteorclient/utils/misc/FakeClientPlayer.java
+++ src/main/java/meteordevelopment/meteorclient/utils/misc/FakeClientPlayer.java
@@ -10,12 +10,19 @@ import meteordevelopment.meteorclient.events.game.GameJoinedEvent;
 import meteordevelopment.meteorclient.utils.Init;
 import meteordevelopment.meteorclient.utils.InitStage;
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.OtherClientPlayerEntity;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.NetworkSide;
 import net.minecraft.text.Text;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.GameMode;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionTypes;
 
 import static meteordevelopment.meteorclient.MeteorClient.mc;
 
@@ -34,14 +41,17 @@ public class FakeClientPlayer {
 
     @EventHandler
     private static void onGameJoined(GameJoinedEvent event) {
-//        world = new ClientWorld(new ClientPlayNetworkHandler(mc, null, new ClientConnection(NetworkSide.CLIENTBOUND), mc.getSession().getProfile(), null), new ClientWorld.Properties(Difficulty.NORMAL, false, false), World.OVERWORLD, RegistryEntry.of(BuiltinRegistries.DIMENSION_TYPE.get(DimensionTypes.OVERWORLD)), 1, 1, mc::getProfiler, null, false, 0);
     }
 
     public static PlayerEntity getPlayer() {
         String id = mc.getSession().getUuid();
 
         if (player == null || (!id.equals(lastId))) {
-            player = new OtherClientPlayerEntity(world, mc.getSession().getProfile(), mc.player.getPublicKey());
+            if (world == null) {
+                world = new ClientWorld(new ClientPlayNetworkHandler(mc, null, new ClientConnection(NetworkSide.CLIENTBOUND), mc.getSession().getProfile(), null), new ClientWorld.Properties(Difficulty.NORMAL, false, false), World.OVERWORLD, BuiltinRegistries.DIMENSION_TYPE.entryOf(DimensionTypes.OVERWORLD), 1, 1, mc::getProfiler, null, false, 0);
+            }
+
+            player = new OtherClientPlayerEntity(world, mc.getSession().getProfile(), null);
 
             lastId = id;
             needsNewEntry = true;
@@ -52,7 +62,7 @@ public class FakeClientPlayer {
 
     public static PlayerListEntry getPlayerListEntry() {
         if (playerListEntry == null || needsNewEntry) {
-            playerListEntry = new PlayerListEntry(PlayerListEntryFactory.create(mc.getSession().getProfile(), 0, GameMode.SURVIVAL, Text.of(mc.getSession().getProfile().getName()), player.getPublicKey().data()), mc.getServicesSignatureVerifier());
+            playerListEntry = new PlayerListEntry(PlayerListEntryFactory.create(mc.getSession().getProfile(), 0, GameMode.SURVIVAL, Text.of(mc.getSession().getProfile().getName()), null), null);
             needsNewEntry = false;
         }
 
