6ff39abf4b7a8fbd631a35309bd1b0cc3af5506f
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2022-07-17 17:45:13 -0400

    remove redundant loops + autoweapon bane of arthropods/smite compat

--- src/main/java/meteordevelopment/meteorclient/utils/entity/EntityUtils.java
+++ src/main/java/meteordevelopment/meteorclient/utils/entity/EntityUtils.java
@@ -18,7 +18,9 @@ import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.Fluids;
@@ -185,5 +187,10 @@ public class EntityUtils {
         });
 
         return found.get();
+    }
+
+    public static EntityGroup getGroup(Entity entity) {
+        if (entity instanceof LivingEntity livingEntity) return livingEntity.getGroup();
+        else return EntityGroup.DEFAULT;
     }
 }
