f662aa5dcfa539acdba0474b4a70bae541dd790e
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2022-07-17 18:49:55 -0400

    Improve Better Beacons
    -  Gray out secondary power buttons when unusable instead of drawing text.
    -  Remove mixin warning

--- src/main/java/meteordevelopment/meteorclient/mixin/BeaconScreenMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/BeaconScreenMixin.java
@@ -17,7 +17,9 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.BeaconScreenHandler;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -30,7 +32,7 @@ import java.util.List;
 @Mixin(BeaconScreen.class)
 public abstract class BeaconScreenMixin extends HandledScreen<BeaconScreenHandler> {
     @Shadow
-    protected abstract <T extends ClickableWidget> void addButton(T button);
+    protected abstract <T extends ClickableWidget & BeaconScreen.BeaconButtonWidget> void addButton(T button);
 
     public BeaconScreenMixin(BeaconScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
@@ -45,7 +47,9 @@ public abstract class BeaconScreenMixin extends HandledScreen<BeaconScreenHandle
             for (int x = 0; x<3;x++) {
                 for (int y = 0; y<2; y++) {
                     addButton(beaconScreen.new EffectButtonWidget(this.x+x*25+27,this.y+y*25+32,effects.get(x*2+y),true,-1));
-                    addButton(beaconScreen.new EffectButtonWidget(this.x+x*25+133,this.y+y*25+32,effects.get(x*2+y),false,-1));
+                    BeaconScreen.EffectButtonWidget secondaryWidget = beaconScreen.new EffectButtonWidget(this.x+x*25+133,this.y+y*25+32,effects.get(x*2+y),false,3);
+                    if (getScreenHandler().getProperties() != 4) secondaryWidget.active = false;
+                    addButton(secondaryWidget);
                 }
             }
         }
@@ -57,14 +61,5 @@ public abstract class BeaconScreenMixin extends HandledScreen<BeaconScreenHandle
         if (!Modules.get().get(BetterBeacons.class).isActive()) return;
         //this will clear the background from useless pyramid graphics
         DrawableHelper.fill(matrices,this.x+10,this.y+7,this.x+220,this.y+98, 0xFF212121);
-    }
-
-    @Inject(method = "drawForeground", at = @At("HEAD"))
-    private void onDrawForeground(MatrixStack matrices, int mouseX, int mouseY, CallbackInfo ci) {
-        if (!Modules.get().get(BetterBeacons.class).isActive()) return;
-        int beaconLvl = getScreenHandler().getProperties();
-        if (beaconLvl < 4) {
-            drawCenteredText(matrices, this.textRenderer, "Secondary power inactive at beacon lvl " + beaconLvl, backgroundWidth/2, 86, 14737632);
-        }
     }
 }
