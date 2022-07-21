d0997f9b27f817f9e7ef9cc80da0fe009ae58b06
Wide-Cat <tom.radden@gmail.com>
2022-07-21 14:47:00 +0100

    fix nuker rendering

--- src/main/java/meteordevelopment/meteorclient/systems/modules/world/Nuker.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/world/Nuker.java
@@ -169,7 +169,7 @@ public class Nuker extends Module {
     );
 
     private final Setting<ShapeMode> shapeModeBox = sgRender.add(new EnumSetting.Builder<ShapeMode>()
-        .name("nuke-block-mode")
+        .name("nuke-box-mode")
         .description("How the shape for the bounding box is rendered.")
         .defaultValue(ShapeMode.Both)
         .build()
@@ -280,6 +280,7 @@ public class Nuker extends Module {
     @EventHandler
     private void onTickPre(TickEvent.Pre event) {
         renderBlocks.forEach(RenderBlock::tick);
+        renderBlocks.removeIf(renderBlock -> renderBlock.ticks <= 0);
 
         // Update timer
         if (timer > 0) {
