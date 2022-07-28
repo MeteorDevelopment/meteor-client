7452ae47bec3772553466d3570f307a87371a0ed
MineGame159 <petulko08@gmail.com>
2022-07-28 20:36:03 +0200

    Added none-mode setting to ItemHud

--- src/main/java/meteordevelopment/meteorclient/systems/hud/elements/ItemHud.java
+++ src/main/java/meteordevelopment/meteorclient/systems/hud/elements/ItemHud.java
@@ -12,6 +12,7 @@ import meteordevelopment.meteorclient.systems.hud.HudElement;
 import meteordevelopment.meteorclient.systems.hud.HudElementInfo;
 import meteordevelopment.meteorclient.systems.hud.HudRenderer;
 import meteordevelopment.meteorclient.utils.player.InvUtils;
+import meteordevelopment.meteorclient.utils.render.color.Color;
 import meteordevelopment.meteorclient.utils.render.color.SettingColor;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.Item;
@@ -32,6 +33,13 @@ public class ItemHud extends HudElement {
         .name("item")
         .description("Item to display")
         .defaultValue(Items.TOTEM_OF_UNDYING)
+        .build()
+    );
+
+    private final Setting<NoneMode> noneMode = sgGeneral.add(new EnumSetting.Builder<NoneMode>()
+        .name("none-mode")
+        .description("How to render the item when you don't have the specified item in your inventory.")
+        .defaultValue(NoneMode.HideCount)
         .build()
     );
 
@@ -87,27 +95,71 @@ public class ItemHud extends HudElement {
 
     @Override
     public void render(HudRenderer renderer) {
-        renderer.post(() -> {
-            ItemStack itemStack = new ItemStack(item.get(), InvUtils.find(item.get()).count());
-            MatrixStack matrices = RenderSystem.getModelViewStack();
+        ItemStack itemStack = new ItemStack(item.get(), InvUtils.find(item.get()).count());
 
-            matrices.push();
-            matrices.scale(scale.get().floatValue(), scale.get().floatValue(), 1);
+        if (noneMode.get() == NoneMode.HideItem && itemStack.isEmpty()) {
+            if (isInEditor()) {
+                renderer.line(x, y, x + getWidth(), y + getHeight(), Color.GRAY);
+                renderer.line(x, y + getHeight(), x + getWidth(), y, Color.GRAY);
+            }
+        }
+        else {
+            renderer.post(() -> {
+                MatrixStack matrices = RenderSystem.getModelViewStack();
 
-            double x = this.x + border.get();
-            double y = this.y + border.get();
+                matrices.push();
+                matrices.scale(scale.get().floatValue(), scale.get().floatValue(), 1);
 
-            if (itemStack.getCount() == 0) itemStack.setCount(Integer.MAX_VALUE);
-            mc.getItemRenderer().renderGuiItemIcon(itemStack, (int) (x / scale.get()), (int) (y / scale.get()));
-            if (itemStack.getCount() == Integer.MAX_VALUE) itemStack.setCount(0);
+                double x = this.x + border.get();
+                double y = this.y + border.get();
 
-            mc.getItemRenderer().renderGuiItemOverlay(mc.textRenderer, itemStack, (int) (x / scale.get()), (int) (y / scale.get()), Integer.toString(itemStack.getCount()));
+                render(itemStack, (int) (x / scale.get()), (int) (y / scale.get()));
 
-            matrices.pop();
-        });
+                matrices.pop();
+            });
+        }
 
         if (background.get()) {
             renderer.quad(this.x, this.y, getWidth(), getHeight(), backgroundColor.get());
+        }
+    }
+
+    private void render(ItemStack itemStack, int x, int y) {
+        switch (noneMode.get()) {
+            case HideItem -> {
+                mc.getItemRenderer().renderGuiItemIcon(itemStack, x, y);
+                mc.getItemRenderer().renderGuiItemOverlay(mc.textRenderer, itemStack, x, y, Integer.toString(itemStack.getCount()));
+            }
+            case HideCount -> {
+                if (itemStack.getCount() == 0) itemStack.setCount(Integer.MAX_VALUE);
+                mc.getItemRenderer().renderGuiItemIcon(itemStack, x, y);
+                if (itemStack.getCount() == Integer.MAX_VALUE) itemStack.setCount(0);
+
+                if (!itemStack.isEmpty()) {
+                    mc.getItemRenderer().renderGuiItemOverlay(mc.textRenderer, itemStack, x, y, Integer.toString(itemStack.getCount()));
+                }
+            }
+            case ShowCount -> {
+                if (itemStack.getCount() == 0) itemStack.setCount(Integer.MAX_VALUE);
+                mc.getItemRenderer().renderGuiItemIcon(itemStack, x, y);
+                mc.getItemRenderer().renderGuiItemOverlay(mc.textRenderer, itemStack, x, y, Integer.toString(itemStack.getCount() == Integer.MAX_VALUE ? 0 : itemStack.getCount()));
+                if (itemStack.getCount() == Integer.MAX_VALUE) itemStack.setCount(0);
+            }
+        }
+    }
+
+    public enum NoneMode {
+        HideItem,
+        HideCount,
+        ShowCount;
+
+        @Override
+        public String toString() {
+            return switch (this) {
+                case HideItem -> "Hide Item";
+                case HideCount -> "Hide Count";
+                case ShowCount -> "Show Count";
+            };
         }
     }
 }
