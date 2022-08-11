fa21b47d9ffc9beba36eee7c716d4ad2b37af219
ThebestkillerTBK <2593828650@qq.com>
2022-08-11 18:45:24 +0800

    NoBlind and NoDarkness for NoRender

--- src/main/java/meteordevelopment/meteorclient/mixin/BackgroundRendererMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/BackgroundRendererMixin.java
@@ -11,10 +11,12 @@ import meteordevelopment.meteorclient.systems.modules.render.NoRender;
 import meteordevelopment.meteorclient.systems.modules.render.Xray;
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
+import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BackgroundRenderer.class)
 public class BackgroundRendererMixin {
@@ -26,5 +28,10 @@ public class BackgroundRendererMixin {
                 RenderSystem.setShaderFogEnd(viewDistance * 4.25f);
             }
         }
+    }
+
+    @Inject(method = {"getFogModifier(Lnet/minecraft/entity/Entity;F)Lnet/minecraft/client/render/BackgroundRenderer$StatusEffectFogModifier;"}, at = @At("HEAD"), cancellable = true)
+    private static void onGetFogModifier(Entity entity, float tickDelta, CallbackInfoReturnable<Object> info) {
+        if (Modules.get().get(NoRender.class).noBlind()) info.setReturnValue(null);
     }
 }
