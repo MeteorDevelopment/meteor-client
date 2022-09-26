e9f3969065bd4d38786ce2d79af724a562750ea5
Wide-Cat <tom.radden@gmail.com>
2022-09-26 19:33:05 +0100

    removed the name history command

--- src/main/java/meteordevelopment/meteorclient/systems/commands/Commands.java
+++ src/main/java/meteordevelopment/meteorclient/systems/commands/Commands.java
@@ -68,7 +68,6 @@ public class Commands extends System<Commands> {
         add(new ModulesCommand());
         add(new BindsCommand());
         add(new GiveCommand());
-        add(new NameHistoryCommand());
         add(new BindCommand());
         add(new FOVCommand());
         add(new RotationCommand());
