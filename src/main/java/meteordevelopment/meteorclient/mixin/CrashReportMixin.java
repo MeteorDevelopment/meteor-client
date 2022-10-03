8f0e7d73d7bc7f81fd1cf163b038c813d5157c14
SByte <stormybytes@gmail.com>
2022-10-03 16:15:27 +0700

    Add Active Hud Elements to crash reports

--- src/main/java/meteordevelopment/meteorclient/mixin/CrashReportMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/CrashReportMixin.java
@@ -6,6 +6,9 @@
 package meteordevelopment.meteorclient.mixin;
 
 import meteordevelopment.meteorclient.MeteorClient;
+import meteordevelopment.meteorclient.systems.Systems;
+import meteordevelopment.meteorclient.systems.hud.Hud;
+import meteordevelopment.meteorclient.systems.hud.HudElement;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.Modules;
@@ -23,13 +26,14 @@ public class CrashReportMixin {
     private void onAddStackTrace(StringBuilder sb, CallbackInfo info) {
         if (Modules.get() != null) {
             sb.append("\n\n");
-            sb.append("-- Meteor Client --\n");
+            sb.append("<--- Meteor Client --->\n");
             sb.append("Version: ").append(MeteorClient.VERSION).append("\n");
 
             if (!MeteorClient.DEV_BUILD.isEmpty()) {
                 sb.append("Dev Build: ").append(MeteorClient.DEV_BUILD).append("\n");
             }
 
+            boolean modulesActive = false;
             for (Category category : Modules.loopCategories()) {
                 List<Module> modules = Modules.get().getGroup(category);
                 boolean active = false;
@@ -37,19 +41,36 @@ public class CrashReportMixin {
                 for (Module module : modules) {
                     if (module != null && module.isActive()) {
                         active = true;
+                        if (!modulesActive) {
+                            modulesActive = true;
+                            sb.append("\n-- Active Modules --\n");
+                        }
                         break;
                     }
                 }
 
                 if (active) {
-                    sb.append("\n");
-                    sb.append("[").append(category).append("]:").append("\n");
+                    sb.append("\n[").append(category).append("]:\n");
 
                     for (Module module : modules) {
                         if (module != null && module.isActive()) {
                             sb.append(module.name).append("\n");
                         }
                     }
+                }
+            }
+
+            if (Hud.get().active) {
+                boolean hudActive = false;
+                for (HudElement element : Hud.get()) {
+                    if (!element.isActive()) continue;
+
+                    if (!hudActive) {
+                        hudActive = true;
+                        sb.append("\n-- Active Hud Elements --\n");
+                    }
+
+                    sb.append(element.info.name).append("\n");
                 }
             }
         }
