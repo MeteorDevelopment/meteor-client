8d69884cf81f375ab064719ec5642e17f36b8424
SByte <stormybytes@gmail.com>
2022-10-06 04:26:35 +0700

    Minor CrashReportMixin cleanup

--- src/main/java/meteordevelopment/meteorclient/mixin/CrashReportMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/CrashReportMixin.java
@@ -6,7 +6,6 @@
 package meteordevelopment.meteorclient.mixin;
 
 import meteordevelopment.meteorclient.MeteorClient;
-import meteordevelopment.meteorclient.systems.Systems;
 import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.hud.HudElement;
 import meteordevelopment.meteorclient.systems.modules.Category;
@@ -24,54 +23,49 @@ import java.util.List;
 public class CrashReportMixin {
     @Inject(method = "addStackTrace", at = @At("TAIL"))
     private void onAddStackTrace(StringBuilder sb, CallbackInfo info) {
+        sb.append("<--- Meteor Client --->\n\n");
+        sb.append("Version: ").append(MeteorClient.VERSION).append("\n");
+        if (!MeteorClient.DEV_BUILD.isEmpty()) {
+            sb.append("Dev Build: ").append(MeteorClient.DEV_BUILD).append("\n");
+        }
+
         if (Modules.get() != null) {
-            sb.append("\n\n");
-            sb.append("<--- Meteor Client --->\n");
-            sb.append("Version: ").append(MeteorClient.VERSION).append("\n");
-
-            if (!MeteorClient.DEV_BUILD.isEmpty()) {
-                sb.append("Dev Build: ").append(MeteorClient.DEV_BUILD).append("\n");
-            }
-
             boolean modulesActive = false;
             for (Category category : Modules.loopCategories()) {
                 List<Module> modules = Modules.get().getGroup(category);
-                boolean active = false;
+                boolean categoryActive = false;
 
                 for (Module module : modules) {
-                    if (module != null && module.isActive()) {
-                        active = true;
-                        if (!modulesActive) {
-                            modulesActive = true;
-                            sb.append("\n-- Active Modules --\n");
-                        }
-                        break;
+                    if (module == null || module.isActive()) continue;
+
+                    if (!modulesActive) {
+                        modulesActive = true;
+                        sb.append("\n-- Active Modules --\n");
                     }
+
+                    if (!categoryActive) {
+                        categoryActive = true;
+                        sb.append("\n[").append(category).append("]:\n");
+                    }
+
+                    sb.append(module.name).append("\n");
                 }
 
-                if (active) {
-                    sb.append("\n[").append(category).append("]:\n");
-
-                    for (Module module : modules) {
-                        if (module != null && module.isActive()) {
-                            sb.append(module.name).append("\n");
-                        }
-                    }
-                }
             }
 
-            if (Hud.get().active) {
-                boolean hudActive = false;
-                for (HudElement element : Hud.get()) {
-                    if (!element.isActive()) continue;
+        }
 
-                    if (!hudActive) {
-                        hudActive = true;
-                        sb.append("\n-- Active Hud Elements --\n");
-                    }
+        if (Hud.get() != null && Hud.get().active) {
+            boolean hudActive = false;
+            for (HudElement element : Hud.get()) {
+                if (element == null || !element.isActive()) continue;
 
-                    sb.append(element.info.name).append("\n");
+                if (!hudActive) {
+                    hudActive = true;
+                    sb.append("\n-- Active Hud Elements --\n");
                 }
+
+                sb.append(element.info.name).append("\n");
             }
         }
     }
