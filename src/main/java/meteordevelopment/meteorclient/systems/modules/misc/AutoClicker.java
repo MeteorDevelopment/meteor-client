ef7c7a9bb903285d213728287d6cd156b00f7a2b
SByte <stormybytes@gmail.com>
2022-10-10 18:48:24 +0700

    Rewrite left and right clicks in autoclicker

--- src/main/java/meteordevelopment/meteorclient/systems/modules/misc/AutoClicker.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/misc/AutoClicker.java
@@ -16,42 +16,41 @@ import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.orbit.EventHandler;
 
 public class AutoClicker extends Module {
-    public enum Mode {
-        Hold,
-        Press
-    }
-
-    public enum Button {
-        Right,
-        Left
-    }
-
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    private final Setting<Mode> mode = sgGeneral.add(new EnumSetting.Builder<Mode>()
-            .name("mode")
-            .description("The method of clicking.")
-            .defaultValue(Mode.Press)
-            .build()
+    private final Setting<Mode> leftClickMode = sgGeneral.add(new EnumSetting.Builder<Mode>()
+        .name("mode-left")
+        .description("The method of clicking for left clicks.")
+        .defaultValue(Mode.Press)
+        .build()
     );
 
-    private final Setting<Button> button = sgGeneral.add(new EnumSetting.Builder<Button>()
-            .name("button")
-            .description("Which button to press.")
-            .defaultValue(Button.Right)
-            .build()
+    private final Setting<Integer> leftClickDelay = sgGeneral.add(new IntSetting.Builder()
+        .name("delay-left")
+        .description("The amount of delay between left clicks in ticks.")
+        .defaultValue(2)
+        .min(0)
+        .sliderMax(60)
+        .build()
     );
 
-    private final Setting<Integer> delay = sgGeneral.add(new IntSetting.Builder()
-            .name("click-delay")
-            .description("The amount of delay between clicks in ticks.")
-            .defaultValue(2)
-            .min(0)
-            .sliderMax(60)
-            .build()
+    private final Setting<Mode> rightClickMode = sgGeneral.add(new EnumSetting.Builder<Mode>()
+        .name("mode-right")
+        .description("The method of clicking for right clicks.")
+        .defaultValue(Mode.Press)
+        .build()
     );
 
-    private int timer;
+    private final Setting<Integer> rightClickDelay = sgGeneral.add(new IntSetting.Builder()
+        .name("delay-right")
+        .description("The amount of delay between right clicks in ticks.")
+        .defaultValue(2)
+        .min(0)
+        .sliderMax(60)
+        .build()
+    );
+
+    private int rightClickTimer, leftClickTimer;
 
     public AutoClicker() {
         super(Categories.Player, "auto-clicker", "Automatically clicks.");
@@ -59,7 +58,8 @@ public class AutoClicker extends Module {
 
     @Override
     public void onActivate() {
-        timer = 0;
+        rightClickTimer = 0;
+        leftClickTimer = 0;
         mc.options.attackKey.setPressed(false);
         mc.options.useKey.setPressed(false);
     }
@@ -72,23 +72,30 @@ public class AutoClicker extends Module {
 
     @EventHandler
     private void onTick(TickEvent.Post event) {
-        switch (mode.get()) {
-            case Hold:
-                switch (button.get()) {
-                    case Left -> mc.options.attackKey.setPressed(true);
-                    case Right -> mc.options.useKey.setPressed(true);
+        switch (leftClickMode.get()) {
+            case Hold -> mc.options.attackKey.setPressed(true);
+            case Press -> {
+                leftClickTimer++;
+                if (leftClickTimer > leftClickDelay.get()) {
+                    Utils.leftClick();
+                    leftClickTimer = 0;
                 }
-                break;
-            case Press:
-                timer++;
-                if (!(delay.get() > timer)) {
-                    switch (button.get()) {
-                        case Left -> Utils.leftClick();
-                        case Right -> Utils.rightClick();
-                    }
-                    timer = 0;
-                }
-                break;
+            }
         }
+        switch (rightClickMode.get()) {
+            case Hold -> mc.options.useKey.setPressed(true);
+            case Press -> {
+                rightClickTimer++;
+                if (rightClickTimer > rightClickDelay.get()) {
+                    Utils.rightClick();
+                    rightClickTimer = 0;
+                }
+            }
+        }
+    }
+
+    public enum Mode {
+        Hold,
+        Press
     }
 }
