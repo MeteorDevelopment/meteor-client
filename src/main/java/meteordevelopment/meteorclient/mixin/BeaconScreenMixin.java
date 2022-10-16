1bb6b2fd865dd0660cccea0c8041f15c9058d834
Olivia <90091374+InstallHappiness@users.noreply.github.com>
2022-10-16 17:40:17 +0200

    Revert

--- src/main/java/meteordevelopment/meteorclient/mixin/BeaconScreenMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/BeaconScreenMixin.java
@@ -29,6 +29,7 @@ import java.util.List;
 
 @Mixin(BeaconScreen.class)
 public abstract class BeaconScreenMixin extends HandledScreen<BeaconScreenHandler> {
+    @Shadow
     protected abstract <T extends ClickableWidget> void addButton(T button);
 
     public BeaconScreenMixin(BeaconScreenHandler handler, PlayerInventory inventory, Text title) {
