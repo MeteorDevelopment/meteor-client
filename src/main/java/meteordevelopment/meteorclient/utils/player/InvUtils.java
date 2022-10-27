a2a3b130bc26bae7210e7450262f5369a0130464
SByte <72693226+StormyBytes@users.noreply.github.com>
2022-10-27 21:35:14 +0700

    Have !swapBack set prevSlot to swap slot (#2907)

--- src/main/java/meteordevelopment/meteorclient/utils/player/InvUtils.java
+++ src/main/java/meteordevelopment/meteorclient/utils/player/InvUtils.java
@@ -141,6 +141,7 @@ public class InvUtils {
     public static boolean swap(int slot, boolean swapBack) {
         if (slot < 0 || slot > 8) return false;
         if (swapBack && previousSlot == -1) previousSlot = mc.player.getInventory().selectedSlot;
+        else if (!swapBack) previousSlot = -1;
 
         mc.player.getInventory().selectedSlot = slot;
         ((IClientPlayerInteractionManager) mc.interactionManager).syncSelected();
