327661302b49e58c335f373655e4e2cb44514469
ThebestkillerTBK <2593828650@qq.com>
2022-11-26 13:04:00 +0800

    re-add BoatMoveEvent

---
+++ src/main/java/meteordevelopment/meteorclient/events/entity/BoatMoveEvent.java
@@ -0,0 +1,19 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package meteordevelopment.meteorclient.events.entity;
+
+import net.minecraft.entity.vehicle.BoatEntity;
+
+public class BoatMoveEvent {
+    private static final BoatMoveEvent INSTANCE = new BoatMoveEvent();
+
+    public BoatEntity boat;
+
+    public static BoatMoveEvent get(BoatEntity entity) {
+        INSTANCE.boat = entity;
+        return INSTANCE;
+    }
+}
--- src/main/java/meteordevelopment/meteorclient/mixin/BoatEntityMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/BoatEntityMixin.java
@@ -5,21 +5,28 @@
 
 package meteordevelopment.meteorclient.mixin;
 
+import meteordevelopment.meteorclient.MeteorClient;
+import meteordevelopment.meteorclient.events.entity.BoatMoveEvent;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.movement.EntityControl;
 import net.minecraft.entity.vehicle.BoatEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BoatEntity.class)
 public class BoatEntityMixin {
-    @Shadow
-    private boolean pressingLeft;
+    @Shadow private boolean pressingLeft;
 
-    @Shadow
-    private boolean pressingRight;
+    @Shadow private boolean pressingRight;
+
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/BoatEntity;move(Lnet/minecraft/entity/MovementType;Lnet/minecraft/util/math/Vec3d;)V"))
+    private void onTickInvokeMove(CallbackInfo info) {
+        MeteorClient.EVENT_BUS.post(BoatMoveEvent.get((BoatEntity) (Object) this));
+    }
 
     @Redirect(method = "updatePaddles", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/vehicle/BoatEntity;pressingLeft:Z"))
     private boolean onUpdatePaddlesPressingLeft(BoatEntity boat) {
