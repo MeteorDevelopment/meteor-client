c7a6bb340dca55423c9268894ba5a8f4494070b8
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2022-12-04 22:24:57 -0500

    fix a 2 month old fumble on my part (#3016)

--- src/main/java/meteordevelopment/meteorclient/systems/modules/misc/BookBot.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/misc/BookBot.java
@@ -173,7 +173,7 @@ public class BookBot extends Module {
 
         // If somehow it failed, just dont do anything until it tries again
         boolean foundFinalBook = InvUtils.testInMainHand(Items.WRITABLE_BOOK);
-        if (foundFinalBook) return;
+        if (!foundFinalBook) return;
 
         // Check delay
         if (delayTimer > 0) {
