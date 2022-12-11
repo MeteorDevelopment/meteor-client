8c4c60d6bdeb7dbf6881c7e7c5b20618e88bc89c
MineGame159 <petulko08@gmail.com>
2022-12-11 11:49:37 +0100

    Fix StorageOutlineShader running even without StorageESP being enabled

--- src/main/java/meteordevelopment/meteorclient/systems/modules/render/StorageESP.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/render/StorageESP.java
@@ -256,6 +256,10 @@ public class StorageESP extends Module {
         return Integer.toString(count);
     }
 
+    public boolean isShader() {
+        return isActive() && mode.get() == Mode.Shader;
+    }
+
     public enum Mode {
         Box,
         Shader
