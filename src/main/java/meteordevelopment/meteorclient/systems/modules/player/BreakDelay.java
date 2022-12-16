84ed8a33976bb2be5fcf6ae9b681b610b5ceec55
SodaXwX <62250232+ThebestkillerTBK@users.noreply.github.com>
2022-12-16 08:44:01 +0800

    NoBreakDelay -> BreakDelay, now you can modify the break cooldown (#2865)

---
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/player/BreakDelay.java
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package meteordevelopment.meteorclient.systems.modules.player;
+
+import meteordevelopment.meteorclient.events.entity.player.BlockBreakingCooldownEvent;
+import meteordevelopment.meteorclient.settings.IntSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.systems.modules.Categories;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+
+public class BreakDelay extends Module {
+    SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Integer> cooldown = sgGeneral.add(new IntSetting.Builder()
+        .name("cooldown")
+        .description("Block break cooldown in ticks.")
+        .defaultValue(0)
+        .min(0)
+        .sliderMax(5)
+        .build()
+    );
+
+    public BreakDelay() {
+        super(Categories.Player, "break-delay", "Changes the delay between breaking blocks.");
+    }
+
+    @EventHandler()
+    private void onBlockBreakingCooldown(BlockBreakingCooldownEvent event) {
+        event.cooldown = cooldown.get();
+    }
+}
