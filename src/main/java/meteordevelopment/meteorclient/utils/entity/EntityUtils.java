8aa494153f6361f5e6e76c72a605e0b522cc1268
MineGame159 <petulko08@gmail.com>
2022-12-21 15:46:04 +0100

    Update EntityUtils.java

--- src/main/java/meteordevelopment/meteorclient/utils/entity/EntityUtils.java
+++ src/main/java/meteordevelopment/meteorclient/utils/entity/EntityUtils.java
@@ -46,7 +46,7 @@ import java.util.function.Predicate;
 import static meteordevelopment.meteorclient.MeteorClient.mc;
 
 public class EntityUtils {
-    public static BlockPos.Mutable testPos = new BlockPos.Mutable();
+    private static BlockPos.Mutable testPos = new BlockPos.Mutable();
 
     public static boolean isAttackable(EntityType<?> type) {
         return type != EntityType.AREA_EFFECT_CLOUD && type != EntityType.ARROW && type != EntityType.FALLING_BLOCK && type != EntityType.FIREWORK_ROCKET && type != EntityType.ITEM && type != EntityType.LLAMA_SPIT && type != EntityType.SPECTRAL_ARROW && type != EntityType.ENDER_PEARL && type != EntityType.EXPERIENCE_BOTTLE && type != EntityType.POTION && type != EntityType.TRIDENT && type != EntityType.LIGHTNING_BOLT && type != EntityType.FISHING_BOBBER && type != EntityType.EXPERIENCE_ORB && type != EntityType.EGG;
