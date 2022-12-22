0a4dc13ac6e1d91904af360151dcf2416a43e4de
ThebestkillerTBK <2593828650@qq.com>
2022-12-22 21:39:39 +0800

    Fix FOV crash

--- src/main/java/meteordevelopment/meteorclient/mixin/SimpleOptionMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/SimpleOptionMixin.java
@@ -9,6 +9,7 @@ import com.mojang.serialization.Codec;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.SimpleOption;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,17 +17,27 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.function.Consumer;
+
 @Mixin(SimpleOption.class)
 public class SimpleOptionMixin<T> {
     @Shadow T value;
 
+    @Shadow @Final
+    private Consumer<T> changeCallback;
+
     @Inject(method = "setValue", at = @At("HEAD"), cancellable = true)
-    private void onSetValueHead(T value, CallbackInfo info) {
+    private void onSetValue(T value, CallbackInfo info) {
         GameOptions options = MinecraftClient.getInstance().options;
         if (options == null) return;
 
         if ((Object) this == options.getGamma() || (Object) this == options.getFov()) {
             this.value = value;
+
+            if (MinecraftClient.getInstance().isRunning()) {
+                changeCallback.accept(value);
+            }
+
             info.cancel();
         }
     }
