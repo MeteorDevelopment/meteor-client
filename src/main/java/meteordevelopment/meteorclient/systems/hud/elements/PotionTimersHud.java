d085147df17940d89ddcfc7a989be3c99eaa79f6
RickyTheRacc <rickytheracc@gmail.com>
2022-12-23 15:00:49 -0600

    Improve Potions Hud
    Add a whitelist/blacklist setting, sorting by length, an option to use the normal hud colors, and a choice between normal numbers or roman numerals for effect strength

--- src/main/java/meteordevelopment/meteorclient/systems/hud/elements/PotionTimersHud.java
+++ src/main/java/meteordevelopment/meteorclient/systems/hud/elements/PotionTimersHud.java
@@ -7,28 +7,64 @@ package meteordevelopment.meteorclient.systems.hud.elements;
 
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.hud.*;
+import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.meteorclient.utils.misc.Names;
+import meteordevelopment.meteorclient.utils.misc.Pool;
 import meteordevelopment.meteorclient.utils.render.color.Color;
-import meteordevelopment.meteorclient.utils.render.color.SettingColor;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffectUtil;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 import static meteordevelopment.meteorclient.MeteorClient.mc;
 
 public class PotionTimersHud extends HudElement {
     public static final HudElementInfo<PotionTimersHud> INFO = new HudElementInfo<>(Hud.GROUP, "potion-timers", "Displays active potion effects with timers.", PotionTimersHud::new);
-
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
-    private final SettingGroup sgScale = settings.createGroup("Scale");
-    private final SettingGroup sgBackground = settings.createGroup("Background");
+
 
     // General
+    private final Setting<FilterMode> filterMode = sgGeneral.add(new EnumSetting.Builder<FilterMode>()
+        .name("filter-mode")
+        .description("Whether or not to use a whitelist or a blacklist.")
+        .defaultValue(FilterMode.Whitelist)
+        .build()
+    );
+
+    private final Setting<List<StatusEffect>> potions = sgGeneral.add(new StatusEffectListSetting.Builder()
+        .name("effects")
+        .description("Which effects to whitelist/blacklist.")
+        .build()
+    );
+
+    private final Setting<Boolean> roman = sgGeneral.add(new BoolSetting.Builder()
+        .name("roman-numerals")
+        .description("Display the effect amplifier as Roman numerals rather than numbers.")
+        .defaultValue(true)
+        .build()
+    );
+
+    private final Setting<Boolean> vanilla = sgGeneral.add(new BoolSetting.Builder()
+        .name("vanilla-colors")
+        .description("Use the vanilla potion colors instead of the normal HUD colors.")
+        .defaultValue(true)
+        .build()
+    );
 
     private final Setting<Boolean> shadow = sgGeneral.add(new BoolSetting.Builder()
         .name("shadow")
         .description("Renders shadow behind text.")
         .defaultValue(true)
+        .build()
+    );
+
+    private final Setting<SortMode> sortMode = sgGeneral.add(new EnumSetting.Builder<SortMode>()
+        .name("sort-mode")
+        .description("How to sort the displayed potions.")
+        .defaultValue(SortMode.Biggest)
         .build()
     );
 
@@ -39,118 +75,125 @@ public class PotionTimersHud extends HudElement {
         .build()
     );
 
-    private final Setting<Integer> border = sgGeneral.add(new IntSetting.Builder()
-        .name("border")
-        .description("How much space to add around the element.")
-        .defaultValue(0)
-        .build()
-    );
-
-    // Scale
-
-    private final Setting<Boolean> customScale = sgScale.add(new BoolSetting.Builder()
-        .name("custom-scale")
-        .description("Applies custom text scale rather than the global one.")
-        .defaultValue(false)
-        .build()
-    );
-
-    private final Setting<Double> scale = sgScale.add(new DoubleSetting.Builder()
-        .name("scale")
-        .description("Custom scale.")
-        .visible(customScale::get)
-        .defaultValue(1)
-        .min(0.5)
-        .sliderRange(0.5, 3)
-        .build()
-    );
-
-    // Background
-
-    private final Setting<Boolean> background = sgBackground.add(new BoolSetting.Builder()
-        .name("background")
-        .description("Displays background.")
-        .defaultValue(false)
-        .build()
-    );
-
-    private final Setting<SettingColor> backgroundColor = sgBackground.add(new ColorSetting.Builder()
-        .name("background-color")
-        .description("Color used for the background.")
-        .visible(background::get)
-        .defaultValue(new SettingColor(25, 25, 25, 50))
-        .build()
-    );
-
-    private final Color color = new Color();
 
     public PotionTimersHud() {
         super(INFO);
     }
 
-    @Override
-    public void setSize(double width, double height) {
-        super.setSize(width + border.get() * 2, height + border.get() * 2);
-    }
-
-    @Override
-    protected double alignX(double width, Alignment alignment) {
-        return box.alignX(getWidth() - border.get() * 2, width, alignment);
-    }
+    private final List<CountedEffect> effects = new ArrayList<>();
+    private final Pool<CountedEffect> effectPool = new Pool<>(CountedEffect::new);
+    private final int[] numbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private final String[] letters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
     @Override
     public void tick(HudRenderer renderer) {
-        if (mc.player == null || isInEditor()) {
-            setSize(renderer.textWidth("Potion Timers 0:00", shadow.get(), getScale()), renderer.textHeight(shadow.get(), getScale()));
-            return;
-        }
+        if (!Utils.canUpdate()) return;
+
+        updateEffects(renderer);
 
         double width = 0;
         double height = 0;
 
-        for (StatusEffectInstance statusEffectInstance : mc.player.getStatusEffects()) {
-            width = Math.max(width, renderer.textWidth(getString(statusEffectInstance), shadow.get(), getScale()));
-            height += renderer.textHeight(shadow.get(), getScale());
+        if (effects.isEmpty() && isInEditor()) {
+            width = Math.max(width, renderer.textWidth("Potion Timers (0:00)"));
+            height += renderer.textHeight(shadow.get());
+        } else {
+            double i = 0;
+            for (CountedEffect effect : effects) {
+                width = Math.max(width, effect.width);
+                height += renderer.textHeight(shadow.get());
+                if (i > 0) height += 2;
+                i++;
+            }
         }
 
-        setSize(width, height);
+        box.setSize(width, height);
     }
 
     @Override
     public void render(HudRenderer renderer) {
-        double x = this.x + border.get();
-        double y = this.y + border.get();
+        if (!Utils.canUpdate()) return;
 
-        if (background.get()) {
-            renderer.quad(this.x, this.y, getWidth(), getHeight(), backgroundColor.get());
-        }
+        updateEffects(renderer);
 
-        if (mc.player == null || isInEditor()) {
-            renderer.text("Potion Timers 0:00", x, y, color, shadow.get(), getScale());
-            return;
-        }
+        if (effects.isEmpty() && isInEditor()) {
+            renderer.text("Potion Timers (0:00)", x, this.y, TextHud.getSectionColor(0), shadow.get());
+        } else {
+            double y = this.y;
+            double i = 0;
 
-        for (StatusEffectInstance statusEffectInstance : mc.player.getStatusEffects()) {
-            StatusEffect statusEffect = statusEffectInstance.getEffectType();
+            for (CountedEffect effect : effects) {
+                double lineWidth = renderer.textWidth(effect.name + effect.time);
 
-            int c = statusEffect.getColor();
-            color.r = Color.toRGBAR(c);
-            color.g = Color.toRGBAG(c);
-            color.b = Color.toRGBAB(c);
+                double x = this.x + alignX(lineWidth, alignment.get());
+                x = renderer.text(effect.name, x, y, (vanilla.get()) ? effect.vanillaColor : TextHud.getSectionColor(0), shadow.get());
+                renderer.text(effect.time, x, y, (vanilla.get()) ? effect.vanillaColor : TextHud.getSectionColor(1), shadow.get());
 
-            String text = getString(statusEffectInstance);
-            renderer.text(text, x + alignX(renderer.textWidth(text, shadow.get(), getScale()), alignment.get()), y, color, shadow.get(), getScale());
-
-            color.r = color.g = color.b = 255;
-            y += renderer.textHeight(shadow.get(), getScale());
+                y += renderer.textHeight(shadow.get());
+                if (i > 0) y += 2;
+                i++;
+            }
         }
     }
 
-    private String getString(StatusEffectInstance statusEffectInstance) {
-        return String.format("%s %d (%s)", Names.get(statusEffectInstance.getEffectType()), statusEffectInstance.getAmplifier() + 1, StatusEffectUtil.durationToString(statusEffectInstance, 1));
+    private void updateEffects(HudRenderer renderer) {
+        for (CountedEffect effect : effects) effectPool.free(effect);
+        effects.clear();
+
+        for (StatusEffectInstance effect : mc.player.getStatusEffects()) {
+            if (potions.get().contains(effect.getEffectType()) && filterMode.get() == FilterMode.Blacklist) continue;
+            else if (!potions.get().contains(effect.getEffectType()) && filterMode.get() == FilterMode.Whitelist) continue;
+
+            effects.add(effectPool.get().set(renderer, effect));
+        }
+
+        if (sortMode.get() == SortMode.Smallest) {
+            effects.sort(Comparator.comparing(effect -> effect.width));
+        } else effects.sort(Comparator.comparing(effect -> -effect.width));
     }
 
-    private double getScale() {
-        return customScale.get() ? scale.get() : -1;
+
+    private class CountedEffect {
+        public String name;
+        public String time;
+        public double width;
+        public Color vanillaColor = new Color();
+
+        public CountedEffect set(HudRenderer renderer, StatusEffectInstance effect) {
+            name = Names.get(effect.getEffectType()) + " ";
+            if (roman.get()) {
+                int a = effect.getAmplifier() + 1;
+                StringBuilder roman = new StringBuilder();
+                for (int i = 0; i < numbers.length; i++) {
+                    while (a >= numbers[i]) {
+                        a -= numbers[i];
+                        roman.append(letters[i]);
+                    }
+                }
+                name += roman + " ";
+            } else name += effect.getAmplifier() + 1 + " ";
+
+            time = "(" + StatusEffectUtil.durationToString(effect, 1) + ")";
+            width = renderer.textWidth(name + time);
+
+            if (vanilla.get()) {
+                int c = effect.getEffectType().getColor();
+                vanillaColor.r = Color.toRGBAR(c);
+                vanillaColor.g = Color.toRGBAG(c);
+                vanillaColor.b = Color.toRGBAB(c);
+            }
+
+            return this;
+        }
+    }
+
+    public enum FilterMode {
+        Whitelist,
+        Blacklist
+    }
+
+    public enum SortMode {
+        Biggest,
+        Smallest
     }
 }
