ef083ecbbc8ae80b9380af8f3a50d2af28da90e3
RickyTheRacc <rickytheracc@gmail.com>
2022-12-22 19:23:28 -0600

    Prevent overlapping fading blocks

--- src/main/java/meteordevelopment/meteorclient/utils/render/RenderUtils.java
+++ src/main/java/meteordevelopment/meteorclient/utils/render/RenderUtils.java
@@ -98,8 +98,17 @@ public class RenderUtils {
     public static void renderTickingBlock(BlockPos blockPos, Color sideColor, Color lineColor, ShapeMode shapeMode, int excludeDir, int duration, boolean fade, boolean shrink) {
         RenderBlock block = new RenderBlock();
         block.set(blockPos, sideColor, lineColor, shapeMode, excludeDir, duration, fade, shrink);
-        renderBlocks.remove(block);
-        renderBlockPool.free(block);
+
+        // Ensure there aren't multiple fading blocks in one pos
+        Iterator<RenderBlock> iterator = renderBlocks.iterator();
+        while (iterator.hasNext()) {
+            RenderBlock next = iterator.next();
+            if (next.pos.equals(block.pos)) {
+                iterator.remove();
+                renderBlockPool.free(next);
+            }
+        }
+
         renderBlocks.add(renderBlockPool.get().set(blockPos, sideColor, lineColor, shapeMode, excludeDir, duration, fade, shrink));
     }
 
