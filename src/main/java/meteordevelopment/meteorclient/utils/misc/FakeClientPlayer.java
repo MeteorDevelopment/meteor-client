782f3ca3973512d722bd200b56705e7a0df113e9
Marko <34315725+rospino74@users.noreply.github.com>
2022-12-31 17:42:14 +0100

    Using current world instead of custom ClientWorld
    Null safety is guaranteed because getPlayer() is only called by CombatHud and PlayerModelHud, which are only rendered when in a world, so mc.world is never null.

--- src/main/java/meteordevelopment/meteorclient/utils/misc/FakeClientPlayer.java
+++ src/main/java/meteordevelopment/meteorclient/utils/misc/FakeClientPlayer.java
@@ -9,19 +9,13 @@ import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.events.game.GameJoinedEvent;
 import meteordevelopment.meteorclient.utils.PreInit;
 import meteordevelopment.orbit.EventHandler;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.OtherClientPlayerEntity;
 import net.minecraft.client.network.PlayerListEntry;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.network.ClientConnection;
-import net.minecraft.network.NetworkSide;
-import net.minecraft.world.Difficulty;
 
 import static meteordevelopment.meteorclient.MeteorClient.mc;
 
 public class FakeClientPlayer {
-    private static ClientWorld world;
     private static PlayerEntity player;
     private static PlayerListEntry playerListEntry;
 
@@ -41,12 +35,7 @@ public class FakeClientPlayer {
         String id = mc.getSession().getUuid();
 
         if (player == null || (!id.equals(lastId))) {
-            if (world == null) {
-                world = new ClientWorld(new ClientPlayNetworkHandler(mc, null, new ClientConnection(NetworkSide.CLIENTBOUND), mc.getCurrentServerEntry(), mc.getSession().getProfile(), null), new ClientWorld.Properties(Difficulty.NORMAL, false, false), world.getRegistryKey(), world.getDimensionEntry(), 1, 1, mc::getProfiler, null, false, 0);
-            }
-
-            player = new OtherClientPlayerEntity(world, mc.getSession().getProfile());
-
+            player = new OtherClientPlayerEntity(mc.world, mc.getSession().getProfile());
             lastId = id;
             needsNewEntry = true;
         }
