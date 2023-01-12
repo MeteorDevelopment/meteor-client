006bf77bea99aebe60556cda33f2bdca1e110f30
DesiCow <uupadhyay1000@gmail.com>
2023-01-12 16:48:27 +0530

    Added noBeaconBeams setting to NoRender module

---
+++ src/main/java/meteordevelopment/meteorclient/mixin/BeaconBlockEntityRendererMixin.java
@@ -0,0 +1,25 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package meteordevelopment.meteorclient.mixin;
+
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.systems.modules.render.NoRender;
+import net.minecraft.block.entity.BeaconBlockEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BeaconBlockEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BeaconBlockEntityRenderer.class)
+public class BeaconBlockEntityRendererMixin {
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    private void onRender(BeaconBlockEntity beaconBlockEntity, float f, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, int j, CallbackInfo info) {
+        if (Modules.get().get(NoRender.class).noBeaconBeams()) info.cancel();
+    }
+}
