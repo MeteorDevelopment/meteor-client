7dc00ee87314781ec0f3b6ea910c24435d51fb77
Lovecrafts-Cat <petrocchi.yuri2002@gmail.com>
2023-01-15 14:46:04 +0100

    Added keybindings settings
    Cleaned up handlers, added optional logging, cleanup.

--- src/main/java/meteordevelopment/meteorclient/systems/modules/world/Excavator.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/world/Excavator.java
@@ -7,17 +7,19 @@ package meteordevelopment.meteorclient.systems.modules.world;
 
 import baritone.api.BaritoneAPI;
 import baritone.api.IBaritone;
-import baritone.api.selection.ISelection;
-import baritone.api.utils.BetterBlockPos;
+import meteordevelopment.meteorclient.events.meteor.KeyEvent;
 import meteordevelopment.meteorclient.events.meteor.MouseButtonEvent;
 import meteordevelopment.meteorclient.events.render.Render3DEvent;
 import meteordevelopment.meteorclient.renderer.ShapeMode;
+import meteordevelopment.meteorclient.settings.BoolSetting;
 import meteordevelopment.meteorclient.settings.ColorSetting;
 import meteordevelopment.meteorclient.settings.EnumSetting;
+import meteordevelopment.meteorclient.settings.KeybindSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.misc.Keybind;
 import meteordevelopment.meteorclient.utils.misc.input.KeyAction;
 import meteordevelopment.meteorclient.utils.render.color.SettingColor;
 import meteordevelopment.orbit.EventHandler;
@@ -29,19 +31,38 @@ import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_RIGHT;
 public class Excavator extends Module {
     private final IBaritone baritone = BaritoneAPI.getProvider().getPrimaryBaritone();
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
-    private final Setting<ShapeMode> shapeMode = sgGeneral.add(new EnumSetting.Builder<ShapeMode>()
+    private final SettingGroup sgRendering = settings.createGroup("Rendering");
+
+    // Keybindings
+    private final Setting<Keybind> selectionKey = sgGeneral.add(new KeybindSetting.Builder()
+        .name("selection-key")
+        .description("Key to draw the selection.")
+        .defaultValue(Keybind.fromButton(GLFW_MOUSE_BUTTON_RIGHT))
+        .build()
+    );
+
+    // Logging
+    private final Setting<Boolean> logSelection = sgGeneral.add(new BoolSetting.Builder()
+        .name("log-selection")
+        .description("Logs the selection coordinates to the chat.")
+        .defaultValue(true)
+        .build()
+    );
+
+    // Rendering
+    private final Setting<ShapeMode> shapeMode = sgRendering.add(new EnumSetting.Builder<ShapeMode>()
         .name("shape-mode")
         .description("How the shapes are rendered.")
         .defaultValue(ShapeMode.Both)
         .build()
     );
-    private final Setting<SettingColor> sideColor = sgGeneral.add(new ColorSetting.Builder()
+    private final Setting<SettingColor> sideColor = sgRendering.add(new ColorSetting.Builder()
         .name("side-color")
         .description("The side color.")
         .defaultValue(new SettingColor(255, 255, 255, 50))
         .build()
     );
-    private final Setting<SettingColor> lineColor = sgGeneral.add(new ColorSetting.Builder()
+    private final Setting<SettingColor> lineColor = sgRendering.add(new ColorSetting.Builder()
         .name("line-color")
         .description("The line color.")
         .defaultValue(new SettingColor(255, 255, 255, 255))
@@ -50,7 +71,6 @@ public class Excavator extends Module {
     private Status status;
     private BlockPos start;
     private BlockPos end;
-    private boolean excavating;
 
     public Excavator() {
         super(Categories.World, "excavator", "Excavate a selection area.");
@@ -61,33 +81,53 @@ public class Excavator extends Module {
         status = Status.SEL_START;
         start = null;
         end = null;
-        excavating = false;
     }
 
     @Override
     public void onDeactivate() {
-        ISelection selection = baritone.getSelectionManager().getLastSelection();
-        baritone.getSelectionManager().removeSelection(selection);
+        if (status == Status.SEL_END) baritone.getCommandManager().execute("sel clear");
+        else baritone.getSelectionManager().removeSelection(baritone.getSelectionManager().getLastSelection());
 
         if (baritone.getBuilderProcess().isActive()) baritone.getCommandManager().execute("stop");
     }
 
     @EventHandler
     private void onMouseButton(MouseButtonEvent event) {
-        if (event.action != KeyAction.Press || event.button != GLFW_MOUSE_BUTTON_RIGHT || mc.currentScreen != null) {
+        if (event.action != KeyAction.Press || event.button != selectionKey.get().getValue() || mc.currentScreen != null) {
             return;
         }
+        selectCorners();
+    }
 
+    @EventHandler
+    private void onKey(KeyEvent event) {
+        if (event.action != KeyAction.Press || event.key != selectionKey.get().getValue() || mc.currentScreen != null) {
+            return;
+        }
+        selectCorners();
+    }
+
+    /**
+     * Selects the corners of the selection; Relays the selection to Baritone.
+     */
+    private void selectCorners() {
         if (!(mc.crosshairTarget instanceof BlockHitResult result)) return;
 
         if (status == Status.SEL_START) {
             start = result.getBlockPos();
             status = Status.SEL_END;
-            info("Start corner set: (%d, %d, %d)".formatted(start.getX(), start.getY(), start.getZ()));
+            baritone.getCommandManager().execute("sel 1 %d %d %d".formatted(start.getX(), start.getY(), start.getZ()));
+            if (logSelection.get()) {
+                info("Start corner set: (%d, %d, %d)".formatted(start.getX(), start.getY(), start.getZ()));
+            }
         } else if (status == Status.SEL_END) {
             end = result.getBlockPos();
             status = Status.WORKING;
-            info("End corner set: (%d, %d, %d)".formatted(end.getX(), end.getY(), end.getZ()));
+            baritone.getCommandManager().execute("sel 2 %d %d %d".formatted(end.getX(), end.getY(), end.getZ()));
+            if (logSelection.get()) {
+                info("End corner set: (%d, %d, %d)".formatted(end.getX(), end.getY(), end.getZ()));
+            }
+            baritone.getCommandManager().execute("sel cleararea");
         }
     }
 
@@ -96,16 +136,7 @@ public class Excavator extends Module {
         if (status == Status.SEL_START || status == Status.SEL_END) {
             if (!(mc.crosshairTarget instanceof BlockHitResult result)) return;
             event.renderer.box(result.getBlockPos(), sideColor.get(), lineColor.get(), shapeMode.get(), 0);
-        } else if (status == Status.WORKING) {
-            if (!excavating) excavate();
-            if (!baritone.getBuilderProcess().isActive()) toggle();
-        }
-    }
-
-    private void excavate() {
-        baritone.getSelectionManager().addSelection(new BetterBlockPos(start), new BetterBlockPos(end));
-        baritone.getCommandManager().execute("sel cleararea");
-        excavating = true;
+        } else if (status == Status.WORKING && !baritone.getBuilderProcess().isActive()) toggle();
     }
 
     private enum Status {
