db8a182f986c8bec0ac2f94f76459050b74accdf
tympanicblock61 <99092380+tympanicblock61@users.noreply.github.com>
2023-02-02 00:50:13 -0500

    Update ChatUtils.java

--- src/main/java/meteordevelopment/meteorclient/utils/player/ChatUtils.java
+++ src/main/java/meteordevelopment/meteorclient/utils/player/ChatUtils.java
@@ -64,7 +64,7 @@ public class ChatUtils {
                 return pair.getRight();
             }
         }
-        return Text::empty;
+        return null;
     }
 
     public static void forceNextPrefixClass(Class<?> klass) {
