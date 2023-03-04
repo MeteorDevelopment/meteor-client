e4821e6d5e086a6f55f41eb2308ac77ef0daf64d
DAM <43420467+DAMcraft@users.noreply.github.com>
2023-03-04 15:15:43 +0100

    Added damage indicators to WItem
    Before:
    https://i.imgur.com/lrD1w4L.png
    After:
    https://i.imgur.com/rJ3EOVj.png

--- src/main/java/meteordevelopment/meteorclient/gui/widgets/WItem.java
+++ src/main/java/meteordevelopment/meteorclient/gui/widgets/WItem.java
@@ -39,6 +39,7 @@ public class WItem extends WWidget {
                 matrices.scale((float) s, (float) s, 1);
                 matrices.translate(x / s, y / s, 0);
 
+                mc.getItemRenderer().renderGuiItemOverlay(mc.textRenderer, itemStack, 0, 0);
                 mc.getItemRenderer().renderGuiItemIcon(itemStack, 0, 0);
 
                 matrices.pop();
