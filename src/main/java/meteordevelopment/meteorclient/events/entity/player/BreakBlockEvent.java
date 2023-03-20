01249159eced8eb1d689ee890915261c3bc4e24b
arlo <mcwalterarlo@gmail.com>
2023-03-20 01:20:21 +0000

    cleanup events code

--- src/main/java/meteordevelopment/meteorclient/events/entity/player/BreakBlockEvent.java
+++ src/main/java/meteordevelopment/meteorclient/events/entity/player/BreakBlockEvent.java
@@ -6,22 +6,16 @@
 package meteordevelopment.meteorclient.events.entity.player;
 
 import meteordevelopment.meteorclient.events.Cancellable;
-import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 public class BreakBlockEvent extends Cancellable {
     private static final BreakBlockEvent INSTANCE = new BreakBlockEvent();
 
     public BlockPos blockPos;
 
-    public BlockState getBlockState(World world) {
-        return world.getBlockState(blockPos);
-    }
-
     public static BreakBlockEvent get(BlockPos blockPos) {
-        INSTANCE.blockPos = blockPos;
         INSTANCE.setCancelled(false);
+        INSTANCE.blockPos = blockPos;
         return INSTANCE;
     }
 }
