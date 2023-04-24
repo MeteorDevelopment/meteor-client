7c783af31017646c684c0e3d6e5e7e8f59d4a738
Lusii <65312404+Beta-51@users.noreply.github.com>
2023-04-24 10:56:12 -0400

    Update AutoWeapon.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/combat/AutoWeapon.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/combat/AutoWeapon.java
@@ -14,6 +14,7 @@ import meteordevelopment.meteorclient.utils.player.InvUtils;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityGroup;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
@@ -42,13 +43,83 @@ public class AutoWeapon extends Module {
         .build()
     );
 
+    private final Setting<Boolean> switchSecondary = sgGeneral.add(new BoolSetting.Builder()
+        .name("switch to secondary weapon on one-hit")
+        .description("Switches to secondary weapon when the victim's health goes below the desired number")
+        .defaultValue(false)
+        .build()
+    );
+    private final Setting<Weapon2> weapon2 = sgGeneral.add(new EnumSetting.Builder<Weapon2>()
+        .name("secondary weapon")
+        .description("what weapon to switch to on the desired value")
+        .defaultValue(Weapon2.Sword)
+        .visible(switchSecondary::get)
+        .build()
+    );
+    private final Setting<Double> healthThreshold = sgGeneral.add(new DoubleSetting.Builder()
+        .name("health threshold")
+        .description("When to trigger the secondary weapon based on victim's health")
+        .defaultValue(5)
+        .sliderRange(1, 36)
+        .visible(switchSecondary::get)
+        .build()
+    );
+
+    public int bsfr(LivingEntity entity) {
+        return (int) entity.getHealth();
+    }
+
     public AutoWeapon() {
         super(Categories.Combat, "auto-weapon", "Finds the best weapon to use in your hotbar.");
     }
 
     @EventHandler
     private void onAttack(AttackEntityEvent event) {
+        if (switchSecondary.get() && bsfr((LivingEntity) event.entity) < healthThreshold.get()) {
+            InvUtils.swap(getSecondaryWeapon(EntityUtils.getGroup(event.entity), (int) (bsfr((LivingEntity) event.entity))), false);
+        } else
         InvUtils.swap(getBestWeapon(EntityUtils.getGroup(event.entity)), false);
+    }
+    private int getSecondaryWeapon(EntityGroup group, int entityHealth) {
+        int slotS = mc.player.getInventory().selectedSlot;
+        int slotA = mc.player.getInventory().selectedSlot;
+        double speedS = 0;
+        double speedA = 0;
+        double currentSpeedS= 0;
+        double currentSpeedA = 0;
+        for (int i = 0; i < 9; i++) {
+            ItemStack stack = mc.player.getInventory().getStack(i);
+            if (stack.getItem() instanceof SwordItem swordItem
+                && (!antiBreak.get() || (stack.getMaxDamage() - stack.getDamage()) > 10)) {
+                currentSpeedS = swordItem.getMaterial().getAttackDamage() + EnchantmentHelper.getAttackDamage(stack, group) + 2;
+                if (currentSpeedS > speedS) {
+                    speedS = currentSpeedS;
+                    slotS = i;
+                }
+            } else if (stack.getItem() instanceof AxeItem axeItem
+                && (!antiBreak.get() || (stack.getMaxDamage() - stack.getDamage()) > 10)) {
+                currentSpeedA = axeItem.getMaterial().getAttackDamage() + EnchantmentHelper.getAttackDamage(stack, group) + 2;
+                if (currentSpeedA > speedA) {
+                    speedA = currentSpeedA;
+                    slotA = i;
+                }
+            }
+        }
+        if (weapon2.get() == Weapon2.Sword && threshold.get() > speedA - speedS && healthThreshold.get() > entityHealth) {
+            return slotS;
+        }
+        else if (weapon2.get() == Weapon2.Axe && threshold.get() > speedS - speedA && healthThreshold.get() > entityHealth) {
+            return slotA;
+        }
+        else if (weapon2.get() == Weapon2.Sword && threshold.get() < speedA - speedS && healthThreshold.get() > entityHealth) {
+            return slotA;
+        }
+        else if (weapon2.get() == Weapon2.Axe && threshold.get() < speedS - speedA && healthThreshold.get() > entityHealth) {
+            return slotS;
+        }
+        else {
+            return mc.player.getInventory().selectedSlot;
+        }
     }
 
     private int getBestWeapon(EntityGroup group) {
@@ -84,6 +155,11 @@ public class AutoWeapon extends Module {
     }
 
     public enum Weapon {
+        Sword,
+        Axe
+    }
+    public enum Weapon2 {
+
         Sword,
         Axe
     }
