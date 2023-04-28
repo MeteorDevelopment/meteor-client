73184965ee9205cbe8f73330d3e23d2726559ca3
IlmarsXd <ilmars500@gmail.com>
2023-04-28 18:37:40 +0300

    fix AutoReconnect

--- src/main/java/meteordevelopment/meteorclient/events/world/ConnectToServerEvent.java
+++ src/main/java/meteordevelopment/meteorclient/events/world/ConnectToServerEvent.java
@@ -5,10 +5,14 @@
 
 package meteordevelopment.meteorclient.events.world;
 
+import java.net.InetSocketAddress;
+
 public class ConnectToServerEvent {
     private static final ConnectToServerEvent INSTANCE = new ConnectToServerEvent();
+    public InetSocketAddress address;
 
-    public static ConnectToServerEvent get() {
+    public static ConnectToServerEvent get(InetSocketAddress address) {
+        INSTANCE.address = address;
         return INSTANCE;
     }
 }
