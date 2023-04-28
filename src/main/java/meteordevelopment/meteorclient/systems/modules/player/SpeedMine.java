d58075f7daa3f3d77527e93a89200d027cbb56d8
IlmarsXd <ilmars500@gmail.com>
2023-04-28 18:31:11 +0300

    Speedmine improvements

--- src/main/java/meteordevelopment/meteorclient/systems/modules/player/SpeedMine.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/player/SpeedMine.java
@@ -6,14 +6,10 @@
 package meteordevelopment.meteorclient.systems.modules.player;
 
 import meteordevelopment.meteorclient.events.world.TickEvent;
-import meteordevelopment.meteorclient.mixin.StatusEffectInstanceAccessor;
-import meteordevelopment.meteorclient.settings.BlockListSetting;
-import meteordevelopment.meteorclient.settings.DoubleSetting;
-import meteordevelopment.meteorclient.settings.EnumSetting;
-import meteordevelopment.meteorclient.settings.Setting;
-import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.block.Block;
 import net.minecraft.entity.effect.StatusEffectInstance;
@@ -25,10 +21,18 @@ import static net.minecraft.entity.effect.StatusEffects.HASTE;
 public class SpeedMine extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
+    public final Setting<Mode> mode = sgGeneral.add(new EnumSetting.Builder<Mode>()
+        .name("mode")
+        .defaultValue(Mode.Normal)
+        .onChanged(mode -> removeHaste())
+        .build()
+    );
+
     private final Setting<List<Block>> blocks = sgGeneral.add(new BlockListSetting.Builder()
         .name("blocks")
         .description("Selected blocks.")
         .filter(block -> block.getHardness() > 0)
+        .visible(() -> mode.get() == Mode.Normal)
         .build()
     );
 
@@ -36,18 +40,15 @@ public class SpeedMine extends Module {
         .name("blocks-filter")
         .description("How to use the blocks setting.")
         .defaultValue(ListMode.Blacklist)
+        .visible(() -> mode.get() == Mode.Normal)
         .build()
     );
 
-    public final Setting<Mode> mode = sgGeneral.add(new EnumSetting.Builder<Mode>()
-        .name("mode")
-        .defaultValue(Mode.Normal)
-        .build()
-    );
     public final Setting<Double> modifier = sgGeneral.add(new DoubleSetting.Builder()
         .name("modifier")
         .description("Mining speed modifier. An additional value of 0.2 is equivalent to one haste level (1.2 = haste 1).")
         .defaultValue(1.4)
+        .visible(() -> mode.get() == Mode.Normal)
         .min(0)
         .build()
     );
@@ -56,26 +57,35 @@ public class SpeedMine extends Module {
         super(Categories.Player, "speed-mine", "Allows you to quickly mine blocks.");
     }
 
+    @Override
+    public void onDeactivate() {
+        removeHaste();
+    }
+
     @EventHandler
-    private void onTick(TickEvent.Post event) {
+    private void onTick(TickEvent.Pre event) {
+        if (!Utils.canUpdate()) return;
         if (mode.get() == Mode.Normal) return;
 
         int amplifier = mode.get() == Mode.Haste2 ? 1 : 0;
 
-        if (!mc.player.hasStatusEffect(HASTE)) {
-            mc.player.addStatusEffect(new StatusEffectInstance(HASTE, 255, amplifier, false, false, false));
-        }
+        StatusEffectInstance haste = mc.player.getStatusEffect(HASTE);
 
-        StatusEffectInstance effect = mc.player.getStatusEffect(HASTE);
-        ((StatusEffectInstanceAccessor) effect).setAmplifier(amplifier);
-        if (effect.getDuration() < 20) ((StatusEffectInstanceAccessor) effect).setDuration(20);
+        if (haste == null || haste.getAmplifier() <= amplifier) {
+            mc.player.setStatusEffect(new StatusEffectInstance(HASTE, -1, amplifier, false, false, false), null);
+        }
+    }
+
+    private void removeHaste() {
+        if (!Utils.canUpdate()) return;
+
+        StatusEffectInstance haste = mc.player.getStatusEffect(HASTE);
+        if (haste != null && !haste.shouldShowIcon()) mc.player.removeStatusEffect(HASTE);
     }
 
     public boolean filter(Block block) {
         if (blocksFilter.get() == ListMode.Blacklist && !blocks.get().contains(block)) return true;
-        else if (blocksFilter.get() == ListMode.Whitelist && blocks.get().contains(block)) return true;
-
-        return false;
+        return blocksFilter.get() == ListMode.Whitelist && blocks.get().contains(block);
     }
 
     public enum Mode {
