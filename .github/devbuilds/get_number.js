cf5102342d81095179db2b290dc033fb0c590e30
8Mi_Yile <admin@8mi.tech>
2023-05-08 22:29:26 +0800

    Fix: the warning reported about get-number.js hint (#3602)

--- .github/devbuilds/get_number.js
+++ .github/devbuilds/get_number.js
@@ -1,5 +1,5 @@
 const axios = require("axios").default;
 
 axios.get("https://meteorclient.com/api/stats").then(res => {
-    console.log("::set-output name=number::" + (parseInt(res.data.devBuild) + 1));
+    console.log("number=" + (parseInt(res.data.devBuild) + 1));
 });
