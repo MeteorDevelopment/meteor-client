a3e1271ae1c5fc9ac1f3f60a729ebc5a0acad4da
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2023-05-15 23:22:29 -0400

    Update MeteorExecutor.java

--- src/main/java/meteordevelopment/meteorclient/utils/network/MeteorExecutor.java
+++ src/main/java/meteordevelopment/meteorclient/utils/network/MeteorExecutor.java
@@ -9,13 +9,21 @@ import meteordevelopment.meteorclient.utils.PreInit;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MeteorExecutor {
     public static ExecutorService executor;
 
     @PreInit
     public static void init() {
-        executor = Executors.newSingleThreadExecutor();
+        AtomicInteger threadNumber = new AtomicInteger(1);
+
+        executor = Executors.newCachedThreadPool((task) -> {
+            Thread thread = new Thread(task);
+            thread.setDaemon(true);
+            thread.setName("Meteor-Executor-" + threadNumber.getAndIncrement());
+            return thread;
+        });
     }
 
     public static void execute(Runnable task) {
