29a9c10f0aec2be25a3876c191bef499ed867564
thebest9178 <thebest9178@john.doe>
2023-07-13 03:46:24 -0700

    Added PacketDelay

--- src/main/java/meteordevelopment/meteorclient/systems/modules/Modules.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/Modules.java
@@ -560,6 +560,7 @@ public class Modules extends System<Modules> {
         add(new Notebot());
         add(new Notifier());
         add(new PacketCanceller());
+        add(new PacketDelay());
         add(new SoundBlocker());
         add(new Spam());
         add(new ServerSpoof());
