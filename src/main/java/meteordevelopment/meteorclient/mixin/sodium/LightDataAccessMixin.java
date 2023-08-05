70659b3fe524f36b079479efa66fdb63de0d1052
Fiz-Victor <soda114514@proton.me>
2023-08-05 13:27:30 +0800

    Updated to sodium 0.5.0 Fixed luminance rip xray alpha again

--- src/main/java/meteordevelopment/meteorclient/mixin/sodium/LightDataAccessMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/sodium/LightDataAccessMixin.java
@@ -5,42 +5,55 @@
 
 package meteordevelopment.meteorclient.mixin.sodium;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import me.jellysquid.mods.sodium.client.model.light.data.LightDataAccess;
 import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.systems.modules.render.Fullbright;
 import meteordevelopment.meteorclient.systems.modules.render.Xray;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockRenderView;
+import net.minecraft.world.LightType;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = LightDataAccess.class, remap = false)
-public class LightDataAccessMixin {
-    private static final int FULL_LIGHT = 15 << 20 | 15 << 4;
+public abstract class LightDataAccessMixin {
+    @Unique private static final int FULL_LIGHT = 15 | 15 << 4 | 15 << 8;
 
     @Shadow protected BlockRenderView world;
     @Shadow @Final private BlockPos.Mutable pos;
 
-    @Unique private Xray xray;
+    @Shadow public static int packBL(int blockLight) { return 0; }
+    @Shadow public static int packSL(int skyLight) { return 0; }
+
+    @Unique
+    private Xray xray;
 
     @Inject(method = "<init>", at = @At("TAIL"))
     private void onInit(CallbackInfo info) {
         xray = Modules.get().get(Xray.class);
     }
 
-    @ModifyVariable(method = "compute", at = @At(value = "STORE"), name = "lm")
-    private int compute_modifyAO(int light) {
+    @Redirect(method = "compute", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/model/light/data/LightDataAccess;packBL(I)I"))
+    private int onPackBL(int blockLight) {
         if (xray.isActive()) {
             BlockState state = world.getBlockState(pos);
             if (!xray.isBlocked(state.getBlock(), pos)) return FULL_LIGHT;
         }
+        return packBL(blockLight);
+    }
 
-        return light;
+    @ModifyArg(method = "compute", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/model/light/data/LightDataAccess;packSL(I)I"))
+    private int onPackSL(int blockLight) {
+        return Math.max(Modules.get().get(Fullbright.class).getLuminance(), blockLight);
     }
 }
--- src/main/java/meteordevelopment/meteorclient/mixin/sodium/SodiumFluidRendererMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/sodium/SodiumFluidRendererMixin.java
@@ -5,21 +5,21 @@
 
 package meteordevelopment.meteorclient.mixin.sodium;
 
+import me.jellysquid.mods.sodium.client.model.color.ColorProvider;
 import me.jellysquid.mods.sodium.client.model.light.LightPipeline;
 import me.jellysquid.mods.sodium.client.model.quad.ModelQuadView;
-import me.jellysquid.mods.sodium.client.model.quad.blender.ColorSampler;
-import me.jellysquid.mods.sodium.client.render.chunk.compile.buffers.ChunkModelBuilder;
+import me.jellysquid.mods.sodium.client.render.chunk.compile.ChunkBuildBuffers;
 import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.FluidRenderer;
-import me.jellysquid.mods.sodium.client.util.color.ColorABGR;
-import me.jellysquid.mods.sodium.client.util.color.ColorARGB;
+import me.jellysquid.mods.sodium.client.world.WorldSlice;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.render.Xray;
 import meteordevelopment.meteorclient.systems.modules.world.Ambience;
+import net.caffeinemc.mods.sodium.api.util.ColorABGR;
+import net.caffeinemc.mods.sodium.api.util.ColorARGB;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.world.BlockRenderView;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -27,7 +27,6 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Arrays;
 
@@ -38,15 +37,15 @@ public class SodiumFluidRendererMixin {
     @Unique private final ThreadLocal<Integer> alphas = new ThreadLocal<>();
 
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
-    private void onRender(BlockRenderView world, FluidState fluidState, BlockPos pos, BlockPos offset, ChunkModelBuilder buffers, CallbackInfoReturnable<Boolean> info) {
-        int alpha = Xray.getAlpha(fluidState.getBlockState(), pos);
+    private void onRender(WorldSlice world, FluidState fluidState, BlockPos blockPos, BlockPos offset, ChunkBuildBuffers buffers, CallbackInfo info) {
+        int alpha = Xray.getAlpha(fluidState.getBlockState(), blockPos);
 
-        if (alpha == 0) info.setReturnValue(false);
+        if (alpha == 0) info.cancel();
         else alphas.set(alpha);
     }
 
     @Inject(method = "updateQuad", at = @At("TAIL"))
-    private void onUpdateQuad(ModelQuadView quad, BlockRenderView world, BlockPos pos, LightPipeline lighter, Direction dir, float brightness, ColorSampler<FluidState> colorSampler, FluidState fluidState, CallbackInfo info) {
+    private void onUpdateQuad(ModelQuadView quad, WorldSlice world, BlockPos pos, LightPipeline lighter, Direction dir, float brightness, ColorProvider<FluidState> colorProvider, FluidState fluidState, CallbackInfo info) {
         // Ambience
         Ambience ambience = Modules.get().get(Ambience.class);
 
