5dca50a57ec3b233dcba4854d90beaab7d2e06cc
Fiz-Victor <soda114514@proton.me>
2023-08-05 13:38:03 +0800

    Update

--- src/main/java/meteordevelopment/meteorclient/mixin/sodium/SodiumBlockRendererMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/sodium/SodiumBlockRendererMixin.java
@@ -10,29 +10,14 @@ import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRende
 import me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderer;
 import meteordevelopment.meteorclient.systems.modules.render.Xray;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = BlockRenderer.class, remap = false)
 public class SodiumBlockRendererMixin {
-    @Unique private final ThreadLocal<Integer> alphas = new ThreadLocal<>();
-
     @Inject(method = "renderModel", at = @At("HEAD"), cancellable = true)
-    private void onRenderModel(BlockRenderContext ctx, ChunkBuildBuffers buffers, CallbackInfo info) {
-        if (Xray.getAlpha(ctx.state(), ctx.pos()) >= 0) info.cancel();
-        //int alpha = Xray.getAlpha(ctx.state(), ctx.pos());
-        //if (alpha == 0) info.cancel();
-        //else alphas.set(alpha);
+    private void onRenderModel(BlockRenderContext ctx, ChunkBuildBuffers buffers, CallbackInfo ci) {
+        if (Xray.getAlpha(ctx.state(), ctx.pos()) != -1) ci.cancel();
     }
-
-
-    //@ModifyArg(method = "writeGeometry", at = @At(value = "INVOKE", target = "Lnet/caffeinemc/mods/sodium/api/util/ColorABGR;withAlpha(IF)I"), index = 1)
-    //private float setColor(float alpha) {
-    //    int alpha0 = alphas.get();
-    //    if (alpha0 == 0 || alpha0 == -1) return alpha;
-    //    else return alpha0 / 255f;
-    //}
 }
