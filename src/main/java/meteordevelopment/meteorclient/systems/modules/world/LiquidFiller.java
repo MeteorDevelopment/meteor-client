0382484950a196be0a17379e3a429e3e9bf68ed1
thebest9178 <76120883+thebest9178@users.noreply.github.com>
2023-08-17 22:20:15 -0700

    Added Shape and SortMode Refactoring

--- src/main/java/meteordevelopment/meteorclient/systems/modules/world/LiquidFiller.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/world/LiquidFiller.java
@@ -9,6 +9,7 @@ import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.meteorclient.utils.player.FindItemResult;
 import meteordevelopment.meteorclient.utils.player.InvUtils;
 import meteordevelopment.meteorclient.utils.world.BlockIterator;
@@ -17,10 +18,13 @@ import meteordevelopment.orbit.EventHandler;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.BlockItem;
 import net.minecraft.util.math.BlockPos;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class LiquidFiller extends Module {
@@ -29,25 +33,22 @@ public class LiquidFiller extends Module {
     private final Setting<PlaceIn> placeInLiquids = sgGeneral.add(new EnumSetting.Builder<PlaceIn>()
         .name("place-in")
         .description("What type of liquids to place in.")
-        .defaultValue(PlaceIn.Lava)
+        .defaultValue(PlaceIn.Both)
         .build()
     );
 
-    private final Setting<Integer> horizontalRadius = sgGeneral.add(new IntSetting.Builder()
-        .name("horizontal-radius")
-        .description("Horizontal radius in which to search for liquids.")
-        .defaultValue(4)
-        .min(0)
-        .sliderMax(6)
+    private final Setting<Shape> shape = sgGeneral.add(new EnumSetting.Builder<Shape>()
+        .name("shape")
+        .description("The shape of placing algorithm.")
+        .defaultValue(Shape.Sphere)
         .build()
     );
 
-    private final Setting<Integer> verticalRadius = sgGeneral.add(new IntSetting.Builder()
-        .name("vertical-radius")
-        .description("Vertical radius in which to search for liquids.")
+    private final Setting<Double> range = sgGeneral.add(new DoubleSetting.Builder()
+        .name("range")
+        .description("The place range.")
         .defaultValue(4)
         .min(0)
-        .sliderMax(6)
         .build()
     );
 
@@ -65,6 +66,13 @@ public class LiquidFiller extends Module {
         .defaultValue(1)
         .min(1)
         .sliderRange(1, 10)
+        .build()
+    );
+
+    private final Setting<SortMode> sortMode = sgGeneral.add(new EnumSetting.Builder<SortMode>()
+        .name("sort-mode")
+        .description("The blocks you want to place first.")
+        .defaultValue(SortMode.Closest)
         .build()
     );
 
@@ -113,27 +121,55 @@ public class LiquidFiller extends Module {
             timer = 0;
         }
 
+        // Calculate some stuff
+        double pX = mc.player.getX();
+        double pY = mc.player.getY();
+        double pZ = mc.player.getZ();
+
+        double rangeSq = Math.pow(range.get(), 2);
+
+        if (shape.get() == Shape.UniformCube) range.set((double) Math.round(range.get()));
+
         // Find slot with a block
         FindItemResult item = InvUtils.findInHotbar(itemStack -> itemStack.getItem() instanceof BlockItem && whitelist.get().contains(Block.getBlockFromItem(itemStack.getItem())));
         if (!item.found()) return;
 
         // Loop blocks around the player
-        BlockIterator.register(horizontalRadius.get(), verticalRadius.get(), (blockPos, blockState) -> {
-            // Check if the block is a source block
-            if (blockState.getFluidState().getLevel() == 8 && blockState.getFluidState().isStill()) {
-                Block liquid = blockState.getBlock();
+        BlockIterator.register((int) Math.ceil(range.get()+1), (int) Math.ceil(range.get()), (blockPos, blockState) -> {
+            boolean toofarSphere = Utils.squaredDistance(pX, pY, pZ, blockPos.getX() + 0.5, blockPos.getY() + 0.5, blockPos.getZ() + 0.5) > rangeSq;
+            boolean toofarUniformCube = maxDist(Math.floor(pX), Math.floor(pY), Math.floor(pZ), blockPos.getX(), blockPos.getY(), blockPos.getZ()) >= range.get();
 
-                PlaceIn placeIn = placeInLiquids.get();
+            // Check distance
+            if ((toofarSphere && shape.get() == Shape.Sphere) || (toofarUniformCube && shape.get() == Shape.UniformCube)) return;
 
-                if ((placeIn == PlaceIn.Both || (placeIn == PlaceIn.Lava && liquid == Blocks.LAVA) || (placeIn == PlaceIn.Water && liquid == Blocks.WATER)) && mc.world.canPlace(Blocks.OBSIDIAN.getDefaultState(), blockPos, ShapeContext.absent())) {
-                    blocks.add(blockPos.mutableCopy());
-                    if (blocks.size() >= maxBlocksPerTick.get()) BlockIterator.disableCurrent();
-                }
-            }
+            // Check if the block is a source block and set to be filled
+            Fluid fluid = blockState.getFluidState().getFluid();
+            if ((placeInLiquids.get() == PlaceIn.Both && (fluid != Fluids.WATER && fluid != Fluids.LAVA))
+                || (placeInLiquids.get() == PlaceIn.Lava && fluid != Fluids.LAVA)
+                || (placeInLiquids.get() == PlaceIn.Water && fluid != Fluids.WATER))
+                return;
+
+            // Check if the player can place at pos
+            if (!mc.world.canPlace(Blocks.OBSIDIAN.getDefaultState(), blockPos, ShapeContext.absent())) return;
+
+            // Add block
+            blocks.add(blockPos.mutableCopy());
         });
 
         BlockIterator.after(() -> {
-            for (BlockPos pos : blocks) BlockUtils.place(pos, item, rotate.get(), 0, true);
+            // Sort blocks
+            if (sortMode.get() == SortMode.TopDown || sortMode.get() == SortMode.BottomUp)
+                blocks.sort(Comparator.comparingDouble(value -> value.getY() * (sortMode.get() == SortMode.BottomUp ? 1 : -1)));
+            else if (sortMode.get() != SortMode.None)
+                blocks.sort(Comparator.comparingDouble(value -> Utils.squaredDistance(pX, pY, pZ, value.getX() + 0.5, value.getY() + 0.5, value.getZ() + 0.5) * (sortMode.get() == SortMode.Closest ? 1 : -1)));
+
+            // Place and clear place positions
+            int count = 0;
+            for (BlockPos pos : blocks) {
+                if (count >= maxBlocksPerTick.get()) break;
+                BlockUtils.place(pos, item, rotate.get(), 0, true);
+                count++;
+            }
             blocks.clear();
         });
     }
@@ -142,5 +178,26 @@ public class LiquidFiller extends Module {
         Lava,
         Water,
         Both
+    }
+
+    public enum SortMode {
+        None,
+        Closest,
+        Furthest,
+        TopDown,
+        BottomUp
+    }
+
+    public enum Shape {
+        UniformCube,
+        Sphere
+    }
+
+    private static double maxDist(double x1, double y1, double z1, double x2, double y2, double z2) {
+        // Gets the largest X, Y or Z difference, manhattan style
+        double dX = Math.ceil(Math.abs(x2 - x1));
+        double dY = Math.ceil(Math.abs(y2 - y1));
+        double dZ = Math.ceil(Math.abs(z2 - z1));
+        return Math.max(Math.max(dX, dY), dZ);
     }
 }
