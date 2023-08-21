863f91297ab800e44608760ba9d531366489643a
Cryo <110951096+CrytoPal@users.noreply.github.com>
2023-08-21 17:41:26 -0400

    Removed Omni and made Hunger check mandatory
    Omni didn't make you any faster while going sideways or backwards
    (Only reason I kept rage is because it was a setting in the old sprint)

--- src/main/java/meteordevelopment/meteorclient/systems/modules/movement/Sprint.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/movement/Sprint.java
@@ -6,13 +6,11 @@
 package meteordevelopment.meteorclient.systems.modules.movement;
 
 import meteordevelopment.meteorclient.events.world.TickEvent;
-import meteordevelopment.meteorclient.settings.BoolSetting;
 import meteordevelopment.meteorclient.settings.EnumSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.meteorclient.utils.player.PlayerUtils;
 import meteordevelopment.orbit.EventHandler;
 
 public class Sprint extends Module {
@@ -20,8 +18,7 @@ public class Sprint extends Module {
 
     public enum Mode {
         Strict,
-        Rage,
-        Omni
+        Rage
     }
 
     private final Setting<Mode> mode = sgGeneral.add(new EnumSetting.Builder<Mode>()
@@ -32,12 +29,7 @@ public class Sprint extends Module {
     );
 
     // Removed whenStationary as it was just Rage sprint
-    private final Setting<Boolean> hunger = sgGeneral.add(new BoolSetting.Builder()
-        .name("check-for-hunger")
-        .description("Checks if you have enough hunger to sprint")
-        .defaultValue(false)
-        .build()
-    );
+
 
     public Sprint() {
         super(Categories.Movement, "sprint", "Automatically sprints.");
@@ -49,7 +41,7 @@ public class Sprint extends Module {
     }
 
     private void sprint() {
-        if (mc.player.getHungerManager().getFoodLevel() <= 6 && hunger.get()) return;
+        if (mc.player.getHungerManager().getFoodLevel() <= 6) return;
         mc.player.setSprinting(true);
     }
 
@@ -61,12 +53,7 @@ public class Sprint extends Module {
                     sprint();
                 }
             }
-            case Omni -> {
-                if (PlayerUtils.isMoving()) {
-                    sprint();
-                }
-            }
             case Rage -> sprint();
+            }
         }
-    }
 }
