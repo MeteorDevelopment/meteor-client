c1a28cfa435fc9edaaae6dc1b2bb11229dd6373e
Headzz <103905611+headzz06@users.noreply.github.com>
2023-09-02 22:38:25 +0100

    compatibility with origins

--- src/main/java/meteordevelopment/meteorclient/mixin/GameRendererMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/GameRendererMixin.java
@@ -169,12 +169,12 @@ public abstract class GameRendererMixin {
             info.cancel();
     }
 
-    @ModifyConstant(method = "updateTargetedEntity", constant = @Constant(doubleValue = 3))
+    @ModifyConstant(method = "updateTargetedEntity", constant = @Constant(doubleValue = 3), require = 0)
     private double updateTargetedEntityModifySurvivalReach(double d) {
         return Modules.get().get(Reach.class).entityReach();
     }
 
-    @ModifyConstant(method = "updateTargetedEntity", constant = @Constant(doubleValue = 9))
+    @ModifyConstant(method = "updateTargetedEntity", constant = @Constant(doubleValue = 9), require = 0)
     private double updateTargetedEntityModifySquaredMaxReach(double d) {
         Reach reach = Modules.get().get(Reach.class);
         return reach.entityReach() * reach.entityReach();
