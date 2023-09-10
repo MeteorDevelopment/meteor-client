5aa840f22db566e093fc39873f65aae8e3f3ef3a
CD450 <bertolottotavoleinc@gmail.com>
2023-09-10 18:22:12 +0200

    Network failsafe for multiplayer

--- src/main/java/meteordevelopment/meteorclient/commands/commands/ShutDownCommand.java
+++ src/main/java/meteordevelopment/meteorclient/commands/commands/ShutDownCommand.java
@@ -7,7 +7,9 @@ package meteordevelopment.meteorclient.commands.commands;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import meteordevelopment.meteorclient.commands.Command;
+import net.minecraft.client.gui.screen.MessageScreen;
 import net.minecraft.command.CommandSource;
+import net.minecraft.text.Text;
 
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
 import static meteordevelopment.meteorclient.MeteorClient.mc;
@@ -21,9 +23,27 @@ public class ShutDownCommand extends Command {
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         builder.executes(context -> {
             info("Shutting down...");
+            this.disconnect();
+
             mc.stop();
 
             return SINGLE_SUCCESS;
         });
+    }
+
+    /**
+     * Repurposed from GameMenuScreen
+     */
+    private void disconnect() {
+        boolean isSp = mc.isInSingleplayer();
+
+        if (mc.world != null) mc.world.disconnect();
+
+        // Handle network
+        if (isSp) {
+            mc.disconnect(new MessageScreen(Text.translatable("menu.savingLevel")));
+        } else {
+            mc.disconnect();
+        }
     }
 }
