992ebdc8af0d631aa2413a94c739e178cad5857e
CD450 <bertolottotavoleinc@gmail.com>
2023-09-10 17:51:24 +0200

    Clean imports

--- src/main/java/meteordevelopment/meteorclient/commands/commands/ShutDownCommand.java
+++ src/main/java/meteordevelopment/meteorclient/commands/commands/ShutDownCommand.java
@@ -5,24 +5,16 @@
 
 package meteordevelopment.meteorclient.commands.commands;
 
-import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import meteordevelopment.meteorclient.commands.Command;
-import meteordevelopment.meteorclient.systems.modules.Modules;
-import meteordevelopment.meteorclient.systems.modules.movement.NoFall;
-import meteordevelopment.meteorclient.systems.modules.player.AntiHunger;
 import net.minecraft.command.CommandSource;
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
-import net.minecraft.text.Text;
-import net.minecraft.util.math.Vec3d;
 
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
 import static meteordevelopment.meteorclient.MeteorClient.mc;
 
 public class ShutDownCommand extends Command {
     public ShutDownCommand() {
-        super("shutdown", "Closes the client", "shut");
+        super("shutdown", "Closes the client.", "shut");
     }
 
     @Override
