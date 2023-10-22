e5713e4e03831c0558f6f4c5440e03441a8d2f0c
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2023-10-22 05:07:10 -0400

    Echest memory patch (#4153)

---
+++ src/main/java/meteordevelopment/meteorclient/utils/tooltip/TextTooltipComponent.java
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package meteordevelopment.meteorclient.utils.tooltip;
+
+import net.minecraft.client.gui.tooltip.OrderedTextTooltipComponent;
+import net.minecraft.client.gui.tooltip.TooltipComponent;
+import net.minecraft.text.OrderedText;
+import net.minecraft.text.Text;
+
+public class TextTooltipComponent extends OrderedTextTooltipComponent implements MeteorTooltipData {
+    public TextTooltipComponent(OrderedText text) {
+        super(text);
+    }
+
+    public TextTooltipComponent(Text text) {
+        this(text.asOrderedText());
+    }
+
+    @Override
+    public TooltipComponent getComponent() {
+        return this;
+    }
+}
