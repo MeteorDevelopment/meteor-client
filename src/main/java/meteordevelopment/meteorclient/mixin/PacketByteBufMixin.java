7ccc4c0f1d33aadf2e23c126bce9f24c14595095
Wide-Cat <widecat@duck.com>
2023-10-28 17:04:40 +0100

    fix anti packet kick

--- src/main/java/meteordevelopment/meteorclient/mixin/PacketByteBufMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/PacketByteBufMixin.java
@@ -10,17 +10,14 @@ import meteordevelopment.meteorclient.systems.modules.misc.AntiPacketKick;
 import net.minecraft.nbt.NbtTagSizeTracker;
 import net.minecraft.network.PacketByteBuf;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(PacketByteBuf.class)
 public class PacketByteBufMixin {
-    @Unique
-    private static final NbtTagSizeTracker EMPTY = new NbtTagSizeTracker(0L, 0);
 
     @ModifyArg(method = "readNbt()Lnet/minecraft/nbt/NbtCompound;", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/PacketByteBuf;readNbt(Lnet/minecraft/nbt/NbtTagSizeTracker;)Lnet/minecraft/nbt/NbtElement;"))
     private NbtTagSizeTracker xlPackets(NbtTagSizeTracker sizeTracker) {
-        return Modules.get().isActive(AntiPacketKick.class) ? EMPTY : sizeTracker;
+        return Modules.get().isActive(AntiPacketKick.class) ? NbtTagSizeTracker.ofUnlimitedBytes() : sizeTracker;
     }
 }
