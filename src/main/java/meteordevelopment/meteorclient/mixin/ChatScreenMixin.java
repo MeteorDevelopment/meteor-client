21860f107d3bf361951c774247827d24d98bf04e
bruh1273 <69420hahafuni@gmail.com>
2023-11-10 00:55:53 -0700

    Improve BetterChat

--- src/main/java/meteordevelopment/meteorclient/mixin/ChatScreenMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/ChatScreenMixin.java
@@ -8,19 +8,23 @@ package meteordevelopment.meteorclient.mixin;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.misc.BetterChat;
 import net.minecraft.client.gui.screen.ChatScreen;
-import net.minecraft.client.gui.widget.TextFieldWidget;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = ChatScreen.class, priority = 1001)
+@Mixin(value = ChatScreen.class, priority = 1337)
 public abstract class ChatScreenMixin {
-    @Shadow protected TextFieldWidget chatField;
 
-    @Inject(method = "init", at = @At(value = "RETURN"))
-    private void onInit(CallbackInfo info) {
-        if (Modules.get().get(BetterChat.class).isInfiniteChatBox()) chatField.setMaxLength(Integer.MAX_VALUE);
+    @ModifyArg(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/TextFieldWidget;setMaxLength(I)V"))
+    private int removeChatLength(int maxLength) { /* This is better to use as we aren't injecting again, just modifying an argument. */
+        return Modules.get().get(BetterChat.class).isInfiniteChatBox() ? Integer.MAX_VALUE : 256;
+    }
+
+    @Inject(method = "normalize", at = @At("RETURN"), cancellable = true)
+    private void removeChatBox(String chatText, CallbackInfoReturnable<String> cir) {
+        /* This will bypass the check to truncate the chat string to 256 chars. */
+        cir.setReturnValue(Modules.get().get(BetterChat.class).isInfiniteChatBox() ? chatText : cir.getReturnValue());
     }
 }
