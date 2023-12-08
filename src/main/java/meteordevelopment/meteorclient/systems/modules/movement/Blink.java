14481871ee69fd9f036886452f2eef0661a0e61d
Moritz Tim W <90388353+moritz-t-w@users.noreply.github.com>
2023-12-08 01:15:16 +0100

    "keybinds are used, but in a hacky way"
    Co-Authored-By: Big Iron <52252627+Big-Iron-Cheems@users.noreply.github.com>

--- src/main/java/meteordevelopment/meteorclient/systems/modules/movement/Blink.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/movement/Blink.java
@@ -32,7 +32,8 @@ public class Blink extends Module {
         .defaultValue(true)
         .build()
     );
-
+    
+    @SuppressWarnings("unused")
     private final Setting<Keybind> cancelBlink = sgGeneral.add(new KeybindSetting.Builder()
         .name("cancel-blink")
         .description("Cancels sending packets and sends you back to your original position.")
