47f94678e76dfd0a570422b3820766b7504eb348
crazycat256 <63475640+crazycat256@users.noreply.github.com>
2023-12-31 17:39:30 -0500

    Fix Jesus preventing players from using a boat (#4277)

--- src/main/java/meteordevelopment/meteorclient/systems/modules/movement/Jesus.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/movement/Jesus.java
@@ -22,6 +22,7 @@ import meteordevelopment.orbit.EventHandler;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.enchantment.ProtectionEnchantment;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.network.packet.Packet;
@@ -270,7 +271,14 @@ public class Jesus extends Module {
     private boolean waterShouldBeSolid() {
         if (EntityUtils.getGameMode(mc.player) == GameMode.SPECTATOR || mc.player.getAbilities().flying) return false;
 
+
+        if (mc.player.getVehicle() != null) {
+            EntityType<?> vehicle = mc.player.getVehicle().getType();
+            if (vehicle == EntityType.BOAT || vehicle == EntityType.CHEST_BOAT) return false;
+        }
+
         if (Modules.get().get(Flight.class).isActive()) return false;
+
 
         if (dipIfBurning.get() && mc.player.isOnFire()) return false;
 
