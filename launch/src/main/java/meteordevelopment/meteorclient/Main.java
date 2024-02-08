7aa93ab712dad21fe3d43911146d03cd603a71df
Wide-Cat <74723656+Wide-Cat@users.noreply.github.com>
2024-02-08 19:54:48 +0000

    Update installation link

--- launch/src/main/java/meteordevelopment/meteorclient/Main.java
+++ launch/src/main/java/meteordevelopment/meteorclient/Main.java
@@ -28,7 +28,7 @@ public class Main {
         );
 
         switch (option) {
-            case 0: getOS().open("https://meteorclient.com/installation"); break;
+            case 0: getOS().open("https://meteorclient.com/faq/installation"); break;
             case 1: {
                 String path;
 
