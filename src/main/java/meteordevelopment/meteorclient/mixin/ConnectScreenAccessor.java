4da9de74bf7cdf2cc80ecd6ffb80da4ec850836d
Wide-Cat <widecat@duck.com>
2024-02-10 17:30:05 +0000

    oops

---
+++ src/main/java/meteordevelopment/meteorclient/mixin/ConnectScreenAccessor.java
@@ -0,0 +1,17 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package meteordevelopment.meteorclient.mixin;
+
+import net.minecraft.client.gui.screen.multiplayer.ConnectScreen;
+import net.minecraft.network.ClientConnection;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(ConnectScreen.class)
+public interface ConnectScreenAccessor {
+    @Accessor("connection")
+    ClientConnection getConnection();
+}
