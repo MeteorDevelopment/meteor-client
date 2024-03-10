1a2b5d69a739c6b9be98b234a6e7e8a564e44fe0
mist64 <91566571+misten1@users.noreply.github.com>
2024-03-10 17:29:43 +0000

    Save EasyMC/Altening tokens (#4428)

---
+++ src/main/java/meteordevelopment/meteorclient/gui/screens/accounts/AccountInfoScreen.java
@@ -0,0 +1,39 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package meteordevelopment.meteorclient.gui.screens.accounts;
+
+import meteordevelopment.meteorclient.gui.GuiTheme;
+import meteordevelopment.meteorclient.gui.WindowScreen;
+import meteordevelopment.meteorclient.gui.widgets.containers.WHorizontalList;
+import meteordevelopment.meteorclient.gui.widgets.pressable.WButton;
+import meteordevelopment.meteorclient.systems.accounts.Account;
+import meteordevelopment.meteorclient.systems.accounts.AccountType;
+import meteordevelopment.meteorclient.systems.accounts.TokenAccount;
+import meteordevelopment.meteorclient.utils.render.color.Color;
+
+import static meteordevelopment.meteorclient.MeteorClient.mc;
+
+public class AccountInfoScreen extends WindowScreen {
+    private Account<?> account;
+
+    public AccountInfoScreen(GuiTheme theme, Account<?> account) {
+        super(theme, account.getUsername() + " details");
+        this.account = account;
+    }
+
+    @Override
+    public void initWidgets() {
+        TokenAccount e = (TokenAccount) account;
+        WHorizontalList l = add(theme.horizontalList()).expandX().widget();
+        
+        WButton copy = theme.button("Copy");
+        copy.action = () -> mc.keyboard.setClipboard(e.getToken());
+        
+        l.add(theme.label((account.getType() == AccountType.EasyMC ? "EasyMC" : "TheAltening") + " token"));
+        l.add(theme.label(e.getToken()).color(Color.GRAY)).pad(5);
+        l.add(copy);
+    }
+}
