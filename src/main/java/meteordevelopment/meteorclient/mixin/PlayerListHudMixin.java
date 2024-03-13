8460010f3a0671b9edc730fd55448565ba481d55
0xTas <admin@0xtas.dev>
2024-03-12 20:15:40 -0700

    Fix compatibility with Future Client
    I've reimplemented modifyHeight() in PlayerListHudMixin.java to restore mixin compatibility with Future Client, while maintaining full functionality of the BetterTab module in both clients.
    
    I tested these changes on Minecraft 1.20.4 with the newest versions of both Future Client and Rusherhack installed. No additional conflicts occurred.

--- src/main/java/meteordevelopment/meteorclient/mixin/PlayerListHudMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/PlayerListHudMixin.java
@@ -5,6 +5,8 @@
 
 package meteordevelopment.meteorclient.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.misc.BetterTab;
 import net.minecraft.client.MinecraftClient;
@@ -42,10 +44,20 @@ public class PlayerListHudMixin {
         return module.isActive() && module.accurateLatency.get() ? width + 30 : width;
     }
 
-    @ModifyConstant(constant = @Constant(intValue = 20), method = "render")
-    private int modifyHeight(int height) {
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Ljava/lang/Math;min(II)I", shift = At.Shift.BEFORE))
+    private void modifyHeight(CallbackInfo ci, @Local(ordinal = 5)LocalIntRef o, @Local(ordinal = 6)LocalIntRef p) {
         BetterTab module = Modules.get().get(BetterTab.class);
-        return module.isActive() ? module.tabHeight.get() : height;
+        if (!module.isActive()) return;
+
+        int newO;
+        int newP = 1;
+        int totalPlayers = newO = module.tabSize.get();
+        while (newO > module.tabHeight.get()) {
+            newO = (totalPlayers + ++newP - 1) / newP;
+        }
+
+        o.set(newO);
+        p.set(newP);
     }
 
     @Inject(method = "renderLatencyIcon", at = @At("HEAD"), cancellable = true)
