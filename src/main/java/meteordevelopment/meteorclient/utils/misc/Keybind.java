38504f8e52ce02d48fd400aef21fc13879421938
Roman Danilov <35975332+Nekiplay@users.noreply.github.com>
2024-03-23 01:01:12 +0500

    Update Keybind.java

--- src/main/java/meteordevelopment/meteorclient/utils/misc/Keybind.java
+++ src/main/java/meteordevelopment/meteorclient/utils/misc/Keybind.java
@@ -42,8 +42,8 @@ public class Keybind implements ISerializable<Keybind>, ICopyable<Keybind> {
     }
 
     public boolean canBindTo(boolean isKey, int value) {
-        if (isKey) return value != GLFW_KEY_ESCAPE;
-        return true;
+        if (isKey) { return value != GLFW_KEY_ESCAPE; }
+        else { return false; }
     }
 
     public void set(boolean isKey, int value) {
