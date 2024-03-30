55b4da96a0e10f0beef429c18f3adc28cedcb547
lauron <itzlauron@gmail.com>
2024-03-30 14:12:13 +0800

    fixed string reader

---
+++ src/main/java/meteordevelopment/meteorclient/mixin/StringReaderMixin.java
@@ -0,0 +1,25 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package meteordevelopment.meteorclient.mixin;
+
+import com.mojang.brigadier.StringReader;
+import meteordevelopment.meteorclient.MeteorClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(StringReader.class)
+public class StringReaderMixin {
+    @Inject(method = "isAllowedInUnquotedString", at = @At("TAIL"), remap = false, cancellable = true)
+    private static void onIsAllowedInUnquotedString(char c, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(
+            Character.isLetterOrDigit(c)
+                || c == '_' || c == '-'
+                || c == '.' || c == '+'
+        );
+    }
+}
