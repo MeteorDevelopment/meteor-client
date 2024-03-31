e386a9705d982e021a9476e647d71401715402db
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2024-03-31 18:05:11 -0400

    improve damage utils accuracy, performance, and api

--- src/main/java/meteordevelopment/meteorclient/utils/player/PlayerUtils.java
+++ src/main/java/meteordevelopment/meteorclient/utils/player/PlayerUtils.java
@@ -227,6 +227,10 @@ public class PlayerUtils {
         return damageTaken;
     }
 
+    public static double distance(double x1, double y1, double z1, double x2, double y2, double z2) {
+        return Math.sqrt(squaredDistance(x1, y1, z1, x2, y2, z2));
+    }
+
     public static double distanceTo(Entity entity) {
         return distanceTo(entity.getX(), entity.getY(), entity.getZ());
     }
@@ -256,9 +260,9 @@ public class PlayerUtils {
     }
 
     public static double squaredDistance(double x1, double y1, double z1, double x2, double y2, double z2) {
-        float f = (float) (x1 - x2);
-        float g = (float) (y1 - y2);
-        float h = (float) (z1 - z2);
+        double f = x1 - x2;
+        double g = y1 - y2;
+        double h = z1 - z2;
         return org.joml.Math.fma(f, f, org.joml.Math.fma(g, g, h * h));
     }
 
