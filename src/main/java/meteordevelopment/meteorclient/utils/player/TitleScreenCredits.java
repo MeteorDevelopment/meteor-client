487fa223c40a0eb4f7d2843af3074db6e210279c
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2024-04-03 09:09:30 -0400

    Fix and improve title screen credits, improve the HTTP API (#4501)

--- src/main/java/meteordevelopment/meteorclient/utils/player/TitleScreenCredits.java
+++ src/main/java/meteordevelopment/meteorclient/utils/player/TitleScreenCredits.java
@@ -11,13 +11,17 @@ import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.gui.GuiThemes;
 import meteordevelopment.meteorclient.gui.screens.CommitsScreen;
+import meteordevelopment.meteorclient.mixininterface.IText;
 import meteordevelopment.meteorclient.utils.network.Http;
 import meteordevelopment.meteorclient.utils.network.MeteorExecutor;
+import meteordevelopment.meteorclient.utils.render.MeteorToast;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.item.Items;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -41,10 +45,35 @@ public abstract class TitleScreenCredits {
                 if (credit.addon.getRepo() == null || credit.addon.getCommit() == null) continue;
 
                 GithubRepo repo = credit.addon.getRepo();
-                Response res = Http.get("https://api.github.com/repos/%s/branches/%s".formatted(repo.getOwnerName(), repo.branch())).sendJson(Response.class);
+                Http.Request request = Http.get("https://api.github.com/repos/%s/branches/%s".formatted(repo.getOwnerName(), repo.branch()));
+                repo.authenticate(request);
+                HttpResponse<Response> res = request.sendJsonReponse(Response.class);
 
-                if (res != null && !credit.addon.getCommit().equals(res.commit.sha)) {
-                    credit.text.append(Text.literal("*").formatted(Formatting.RED));
+                switch (res.statusCode()) {
+                    case Http.UNAUTHORIZED -> {
+                        String message = "Invalid authentication token for repository '%s'".formatted(repo.getOwnerName());
+                        mc.getToastManager().add(new MeteorToast(Items.BARRIER, "GitHub: Unauthorized", message));
+                        MeteorClient.LOG.warn(message);
+                        if (System.getenv("meteor.github.authorization") == null) {
+                            MeteorClient.LOG.info("Consider setting an authorization " +
+                                "token with the 'meteor.github.authorization' environment variable.");
+                            MeteorClient.LOG.info("See: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens");
+                        }
+                    }
+                    case Http.FORBIDDEN -> {
+                        MeteorClient.LOG.warn("Could not fetch updates for addon '%s': Rate-limited by GitHub.".formatted(credit.addon.name));
+                    }
+                    case Http.NOT_FOUND -> {
+                        MeteorClient.LOG.warn("Could not fetch updates for addon '%s': GitHub repository '%s' not found.".formatted(credit.addon.name, repo.getOwnerName()));
+                    }
+                    case Http.SUCCESS -> {
+                        if (!credit.addon.getCommit().equals(res.body().commit.sha)) {
+                            synchronized (credit.text) {
+                                credit.text.append(Text.literal("*").formatted(Formatting.RED));
+                                ((IText) credit.text).meteor$invalidateCache();
+                            }
+                        }
+                    }
                 }
             }
         });
@@ -72,9 +101,11 @@ public abstract class TitleScreenCredits {
 
         int y = 3;
         for (Credit credit : credits) {
-            int x = mc.currentScreen.width - 3 - mc.textRenderer.getWidth(credit.text);
+            synchronized (credit.text) {
+                int x = mc.currentScreen.width - 3 - mc.textRenderer.getWidth(credit.text);
 
-            context.drawTextWithShadow(mc.textRenderer, credit.text, x, y, -1);
+                context.drawTextWithShadow(mc.textRenderer, credit.text, x, y, -1);
+            }
 
             y += mc.textRenderer.fontHeight + 2;
         }
@@ -83,9 +114,14 @@ public abstract class TitleScreenCredits {
     public static boolean onClicked(double mouseX, double mouseY) {
         int y = 3;
         for (Credit credit : credits) {
-            int x = mc.currentScreen.width - 3 - mc.textRenderer.getWidth(credit.text);
+            int width;
+            synchronized (credit.text) {
+                width = mc.textRenderer.getWidth(credit.text);
+            }
 
-            if (mouseX >= x && mouseX <= x + mc.textRenderer.getWidth(credit.text) && mouseY >= y && mouseY <= y + mc.textRenderer.fontHeight + 2) {
+            int x = mc.currentScreen.width - 3 - width;
+
+            if (mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + mc.textRenderer.fontHeight + 2) {
                 if (credit.addon.getRepo() != null && credit.addon.getCommit() != null) {
                     mc.setScreen(new CommitsScreen(GuiThemes.get(), credit.addon));
                     return true;
