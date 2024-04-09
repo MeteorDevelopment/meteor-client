b69c1b5f4bf97b27f3b3faf44fc6af6160eacbb4
Wide-Cat <widecat@duck.com>
2024-04-10 00:58:57 +0100

    Add block placement to the no ghost blocks module

--- src/main/java/meteordevelopment/meteorclient/mixin/BlockItemMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/BlockItemMixin.java
@@ -7,12 +7,17 @@ package meteordevelopment.meteorclient.mixin;
 
 import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.events.entity.player.PlaceBlockEvent;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.systems.modules.world.NoGhostBlocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockItem.class)
@@ -20,5 +25,11 @@ public class BlockItemMixin {
     @Inject(method = "place(Lnet/minecraft/item/ItemPlacementContext;Lnet/minecraft/block/BlockState;)Z", at = @At("HEAD"))
     private void onPlace(ItemPlacementContext context, BlockState state, CallbackInfoReturnable<Boolean> info) {
         if (context.getWorld().isClient) MeteorClient.EVENT_BUS.post(PlaceBlockEvent.get(context.getBlockPos(), state.getBlock()));
+    }
+
+    @Redirect(method = "place(Lnet/minecraft/item/ItemPlacementContext;Lnet/minecraft/block/BlockState;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"))
+    private boolean onPlace(World instance, BlockPos pos, BlockState state, int flags) {
+        if (Modules.get().get(NoGhostBlocks.class).placing()) return true;
+        return instance.setBlockState(pos, state, flags);
     }
 }
--- src/main/java/meteordevelopment/meteorclient/systems/modules/world/NoGhostBlocks.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/world/NoGhostBlocks.java
@@ -6,23 +6,46 @@
 package meteordevelopment.meteorclient.systems.modules.world;
 
 import meteordevelopment.meteorclient.events.entity.player.BreakBlockEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.block.BlockState;
 
 public class NoGhostBlocks extends Module {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    private final Setting<Boolean> breaking = sgGeneral.add(new BoolSetting.Builder()
+        .name("breaking")
+        .description("Whether to apply for block breaking actions.")
+        .defaultValue(true)
+        .build()
+    );
+
+    private final Setting<Boolean> placing = sgGeneral.add(new BoolSetting.Builder()
+        .name("placing")
+        .description("Whether to apply for block placement actions.")
+        .defaultValue(true)
+        .build()
+    );
+
     public NoGhostBlocks() {
-        super(Categories.World, "no-ghost-blocks", "Attempts to prevent ghost blocks arising from breaking blocks quickly. Especially useful with multiconnect.");
+        super(Categories.World, "no-ghost-blocks", "Attempts to prevent ghost blocks arising .");
     }
 
     @EventHandler
     public void onBreakBlock(BreakBlockEvent event) {
-        if (mc.isInSingleplayer()) return;
+        if (mc.isInSingleplayer() || !breaking.get()) return;
 
         event.cancel();
 
         BlockState blockState = mc.world.getBlockState(event.blockPos);
         blockState.getBlock().onBreak(mc.world, event.blockPos, blockState, mc.player);
+    }
+
+    public boolean placing() {
+        return isActive() && placing.get();
     }
 }
