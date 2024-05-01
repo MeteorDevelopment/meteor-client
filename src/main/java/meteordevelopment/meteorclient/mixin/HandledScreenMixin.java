31aafaab7a3314bb7f43bd326c80fd5708e14a0e
Big-Iron-Cheems <52252627+Big-Iron-Cheems@users.noreply.github.com>
2024-05-01 17:25:16 +0200

    Move Steal/Dump buttons to more comfortable position

--- src/main/java/meteordevelopment/meteorclient/mixin/HandledScreenMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/HandledScreenMixin.java
@@ -21,6 +21,7 @@ import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -35,18 +36,26 @@ import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_LEFT;
 
 @Mixin(HandledScreen.class)
 public abstract class HandledScreenMixin<T extends ScreenHandler> extends Screen implements ScreenHandlerProvider<T> {
-    @Shadow protected Slot focusedSlot;
+    @Shadow
+    protected Slot focusedSlot;
 
-    @Shadow protected int x;
-    @Shadow protected int y;
+    @Shadow
+    protected int x;
+    @Shadow
+    protected int y;
 
-    @Shadow @org.jetbrains.annotations.Nullable protected abstract Slot getSlotAt(double xPosition, double yPosition);
+    @Shadow
+    @Nullable
+    protected abstract Slot getSlotAt(double xPosition, double yPosition);
 
-    @Shadow public abstract T getScreenHandler();
+    @Shadow
+    public abstract T getScreenHandler();
 
-    @Shadow private boolean doubleClicking;
+    @Shadow
+    private boolean doubleClicking;
 
-    @Shadow protected abstract void onMouseClick(Slot slot, int invSlot, int clickData, SlotActionType actionType);
+    @Shadow
+    protected abstract void onMouseClick(Slot slot, int invSlot, int clickData, SlotActionType actionType);
 
     @Unique
     private static final ItemStack[] ITEMS = new ItemStack[27];
@@ -62,14 +71,14 @@ public abstract class HandledScreenMixin<T extends ScreenHandler> extends Screen
         if (invTweaks.isActive() && invTweaks.showButtons() && invTweaks.canSteal(getScreenHandler())) {
             addDrawableChild(
                 new ButtonWidget.Builder(Text.literal("Steal"), button -> invTweaks.steal(getScreenHandler()))
-                    .position(width / 2 - 41, 3)
+                    .position(x, y - 22)
                     .size(40, 20)
                     .build()
             );
 
             addDrawableChild(
                 new ButtonWidget.Builder(Text.literal("Dump"), button -> invTweaks.dump(getScreenHandler()))
-                    .position(width / 2 + 2, 3)
+                    .position(x + 42, y - 22)
                     .size(40, 20)
                     .build()
             );
