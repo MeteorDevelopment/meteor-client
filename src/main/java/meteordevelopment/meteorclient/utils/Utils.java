340200c2ba0a0b8c07eea438003d0582051ec58e
misterx <weinboy11@gmail.com>
2024-06-13 18:16:23 +0200

    hostnames can have minus hyphens

--- src/main/java/meteordevelopment/meteorclient/utils/Utils.java
+++ src/main/java/meteordevelopment/meteorclient/utils/Utils.java
@@ -595,6 +595,6 @@ public class Utils {
 
     public static boolean ipFilter(String text, char character) {
         if (text.contains(":") && character == ':') return false;
-        return (character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z') || (character >= '0' && character <= '9') || character == '.';
+        return (character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z') || (character >= '0' && character <= '9') || character == '.' || character == '-';
     }
 }
