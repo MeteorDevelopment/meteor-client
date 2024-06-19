e5dd04a2a502a73e5d1de139dffa06e57768cddc
Wide-Cat <widecat@duck.com>
2024-06-19 15:53:40 +0100

    Fix anti-clear in better chat.
    smh crosby. Closes #4660

--- src/main/java/meteordevelopment/meteorclient/systems/modules/misc/BetterChat.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/misc/BetterChat.java
@@ -225,7 +225,7 @@ public class BetterChat extends Module {
     );
 
     private static final Pattern antiSpamRegex = Pattern.compile(" \\(([0-9]+)\\)$");
-    private static final Pattern antiClearRegex = Pattern.compile("\\\\n(\\\\n|\\s)+\\\\n");
+    private static final Pattern antiClearRegex = Pattern.compile("\\n(\\n|\\s)+\\n");
     private static final Pattern timestampRegex = Pattern.compile("^(<[0-9]{2}:[0-9]{2}>\\s)");
     private static final Pattern usernameRegex = Pattern.compile("^(?:<[0-9]{2}:[0-9]{2}>\\s)?<(.*?)>.*");
 
@@ -258,7 +258,7 @@ public class BetterChat extends Module {
 
         if (antiClear.get()) {
             String messageString = message.getString();
-            if (antiClearRegex.matcher(messageString).matches()) {
+            if (antiClearRegex.matcher(messageString).find()) {
                 MutableText newMessage = Text.empty();
                 TextVisitor.visit(message, (text, style, string) -> {
                     Matcher antiClearMatcher = antiClearRegex.matcher(string);
