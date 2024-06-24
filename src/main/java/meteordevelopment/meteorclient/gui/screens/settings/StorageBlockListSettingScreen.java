f44592c3f226f4902f746445bcfe3e52695cf846
tympanicblock61 <99092380+tympanicblock61@users.noreply.github.com>
2024-06-24 13:31:08 -0400

    Update StorageBlockListSettingScreen.java to use reflection + fix

--- src/main/java/meteordevelopment/meteorclient/gui/screens/settings/StorageBlockListSettingScreen.java
+++ src/main/java/meteordevelopment/meteorclient/gui/screens/settings/StorageBlockListSettingScreen.java
@@ -5,6 +5,9 @@
 
 package meteordevelopment.meteorclient.gui.screens.settings;
 
+import it.unimi.dsi.fastutil.Pair;
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.objects.ObjectObjectImmutablePair;
 import meteordevelopment.meteorclient.gui.GuiTheme;
 import meteordevelopment.meteorclient.gui.widgets.WWidget;
 import meteordevelopment.meteorclient.settings.Setting;
@@ -13,17 +16,38 @@ import net.fabricmc.loader.impl.util.StringUtil;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
-import net.minecraft.util.Pair;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Field;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class StorageBlockListSettingScreen extends LeftRightListSettingScreen<BlockEntityType<?>> {
-    private static final Map<BlockEntityType<?>, Pair<String, Item>> STORAGE_BLOCK_ENTITY_MAP = new HashMap<>();
+public class StorageBlockListSettingScreen extends RegistryListSettingScreen<BlockEntityType<?>> {
+    private static final Pair<String,Item> UNKNOWN = new ObjectObjectImmutablePair<>("Unknown", Items.BARRIER);
+    private static final Map<BlockEntityType<?>, Pair<String,Item>> STORAGE_BLOCK_ENTITY_MAP = new Object2ObjectOpenHashMap<>();
+
+    public StorageBlockListSettingScreen(GuiTheme theme, Setting<List<BlockEntityType<?>>> setting) {
+        super(theme, "Select Storage Blocks", setting, setting.get(), StorageBlockListSetting.REGISTRY);
+    }
+
+    @Override
+    protected WWidget getValueWidget(BlockEntityType<?> value) {
+        Item item = STORAGE_BLOCK_ENTITY_MAP.getOrDefault(value, UNKNOWN).right();
+        return theme.itemWithLabel(item.getDefaultStack(), getValueName(value));
+    }
+
+    @Override
+    protected String getValueName(BlockEntityType<?> value) {
+        return STORAGE_BLOCK_ENTITY_MAP.getOrDefault(value, UNKNOWN).left();
+    }
+
+    private static Field findFieldObject(Class<?> clazz, Predicate<Field> condition) {
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) if (condition.test(field)) return field;
+        return null;
+    }
 
     static {
         for (BlockEntityType<?> block : StorageBlockListSetting.STORAGE_BLOCKS) {
@@ -41,35 +65,8 @@ public class StorageBlockListSettingScreen extends LeftRightListSettingScreen<Bl
                 });
                 if (itemField == null) continue;
                 String displayName = Arrays.stream(nameField.getName().toLowerCase().split("_")).map(StringUtil::capitalize).collect(Collectors.joining(" "));
-                STORAGE_BLOCK_ENTITY_MAP.put(block, new Pair<>(displayName, (Item) itemField.get(null)));
+                STORAGE_BLOCK_ENTITY_MAP.put(block, new ObjectObjectImmutablePair<>(displayName, (Item) itemField.get(null)));
             } catch (IllegalAccessException ignored) {}
         }
-    }
-
-    private static Field findFieldObject(Class<?> clazz, Predicate<Field> condition) {
-        Field[] fields = clazz.getDeclaredFields();
-        for (Field field : fields) if (condition.test(field)) return field;
-        return null;
-    }
-
-    public StorageBlockListSettingScreen(GuiTheme theme, Setting<List<BlockEntityType<?>>> setting) {
-        super(theme, "Select Storage Blocks", setting, setting.get(), StorageBlockListSetting.REGISTRY);
-    }
-
-    @Override
-    protected WWidget getValueWidget(BlockEntityType<?> value) {
-        Item item = Items.BARRIER;
-        if (STORAGE_BLOCK_ENTITY_MAP.containsKey(value)) {
-            item = STORAGE_BLOCK_ENTITY_MAP.get(value).getRight();
-        }
-        return theme.itemWithLabel(item.getDefaultStack(), getValueName(value));
-    }
-
-    @Override
-    protected String getValueName(BlockEntityType<?> value) {
-        if (STORAGE_BLOCK_ENTITY_MAP.containsKey(value)) {
-            return STORAGE_BLOCK_ENTITY_MAP.get(value).getLeft();
-        }
-        return "Unknown";
     }
 }
