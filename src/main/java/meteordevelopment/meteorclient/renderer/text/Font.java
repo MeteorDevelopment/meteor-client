6a377e05820d539ca7f3508a083c974a943bbbb1
Nippaku-Zanmu <Nippaku-Zanmu@aExample.com>
2024-07-02 15:15:52 +0800

    Update Font.java

--- src/main/java/meteordevelopment/meteorclient/renderer/text/Font.java
+++ src/main/java/meteordevelopment/meteorclient/renderer/text/Font.java
@@ -1,8 +1,3 @@
-/*
- * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
- * Copyright (c) Meteor Development.
- */
-
 package meteordevelopment.meteorclient.renderer.text;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
@@ -16,50 +11,37 @@ import org.lwjgl.system.MemoryStack;
 
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Font {
-    public AbstractTexture texture;
+    public ByteTexture texture;
     private final int height;
     private final float scale;
     private final float ascent;
     private final Int2ObjectOpenHashMap<CharData> charMap = new Int2ObjectOpenHashMap<>();
     private final static int size = 2048;
 
+    private final ByteBuffer buffer;
+    private final STBTTFontinfo fontInfo;
+    private final ByteBuffer bitmap;
+    private final STBTTPackContext packContext;
+    private final Map<Integer, STBTTPackedchar> packedChars = new HashMap<>();
+
     public Font(ByteBuffer buffer, int height) {
+        this.buffer = buffer;
         this.height = height;
 
         // Initialize font
-        STBTTFontinfo fontInfo = STBTTFontinfo.create();
+        fontInfo = STBTTFontinfo.create();
         STBTruetype.stbtt_InitFont(fontInfo, buffer);
 
         // Allocate buffers
-        ByteBuffer bitmap = BufferUtils.createByteBuffer(size * size);
-        STBTTPackedchar.Buffer[] cdata = {
-            STBTTPackedchar.create(95), // Basic Latin
-            STBTTPackedchar.create(96), // Latin 1 Supplement
-            STBTTPackedchar.create(128), // Latin Extended-A
-            STBTTPackedchar.create(144), // Greek and Coptic
-            STBTTPackedchar.create(256), // Cyrillic
-            STBTTPackedchar.create(1) // infinity symbol
-        };
+        bitmap = BufferUtils.createByteBuffer(size * size);
 
         // create and initialise packing context
-        STBTTPackContext packContext = STBTTPackContext.create();
-        STBTruetype.stbtt_PackBegin(packContext, bitmap, size, size, 0 ,1);
-
-        // create the pack range, populate with the specific packing ranges
-        STBTTPackRange.Buffer packRange = STBTTPackRange.create(cdata.length);
-        packRange.put(STBTTPackRange.create().set(height, 32, null, 95, cdata[0], (byte) 2, (byte) 2));
-        packRange.put(STBTTPackRange.create().set(height, 160, null, 96, cdata[1], (byte) 2, (byte) 2));
-        packRange.put(STBTTPackRange.create().set(height, 256, null, 128, cdata[2], (byte) 2, (byte) 2));
-        packRange.put(STBTTPackRange.create().set(height, 880, null, 144, cdata[3], (byte) 2, (byte) 2));
-        packRange.put(STBTTPackRange.create().set(height, 1024, null, 256, cdata[4], (byte) 2, (byte) 2));
-        packRange.put(STBTTPackRange.create().set(height, 8734, null, 1, cdata[5], (byte) 2, (byte) 2)); // lol
-        packRange.flip();
-
-        // write and finish
-        STBTruetype.stbtt_PackFontRanges(packContext, buffer, 0, packRange);
-        STBTruetype.stbtt_PackEnd(packContext);
+        packContext = STBTTPackContext.create();
+        STBTruetype.stbtt_PackBegin(packContext, bitmap, size, size, 0, 1);
 
         // Create texture object and get font scale
         texture = new ByteTexture(size, size, bitmap, ByteTexture.Format.A, ByteTexture.Filter.Linear, ByteTexture.Filter.Linear);
@@ -72,29 +54,83 @@ public class Font {
             this.ascent = ascent.get(0);
         }
 
-        for (int i = 0; i < cdata.length; i++) {
-            STBTTPackedchar.Buffer cbuf = cdata[i];
-            int offset = packRange.get(i).first_unicode_codepoint_in_range();
+        // Preload basic ASCII characters
+        preloadAsciiCharacters();
+    }
 
-            for (int j = 0; j < cbuf.capacity(); j++) {
-                STBTTPackedchar packedChar = cbuf.get(j);
+    private void preloadAsciiCharacters() {
+        STBTTPackedchar.Buffer cdata = STBTTPackedchar.create(128); // Basic Latin
 
-                float ipw = 1f / size; // pixel width and height
-                float iph = 1f / size;
+        // create the pack range
+        STBTTPackRange.Buffer packRange = STBTTPackRange.create(1);
+        packRange.put(STBTTPackRange.create().set(height, 32, null, 128, cdata, (byte) 2, (byte) 2));
+        packRange.flip();
 
-                charMap.put(j + offset, new CharData(
-                    packedChar.xoff(),
-                    packedChar.yoff(),
-                    packedChar.xoff2(),
-                    packedChar.yoff2(),
-                    packedChar.x0() * ipw,
-                    packedChar.y0() * iph,
-                    packedChar.x1() * ipw,
-                    packedChar.y1() * iph,
-                    packedChar.xadvance()
-                ));
-            }
+        // pack font ranges
+        STBTruetype.stbtt_PackFontRanges(packContext, buffer, 0, packRange);
+
+        // Load character data into charMap
+        for (int i = 0; i < cdata.capacity(); i++) {
+            STBTTPackedchar packedChar = cdata.get(i);
+
+            float ipw = 1f / size; // pixel width and height
+            float iph = 1f / size;
+
+            charMap.put(i + 32, new CharData(
+                packedChar.xoff(),
+                packedChar.yoff(),
+                packedChar.xoff2(),
+                packedChar.yoff2(),
+                packedChar.x0() * ipw,
+                packedChar.y0() * iph,
+                packedChar.x1() * ipw,
+                packedChar.y1() * iph,
+                packedChar.xadvance()
+            ));
         }
+
+        // Create texture
+        createTexture();
+    }
+
+    private void loadCharacter(int codePoint) {
+        if (packedChars.containsKey(codePoint)) return;
+
+        STBTTPackedchar.Buffer cdata = STBTTPackedchar.create(1);
+
+        // create the pack range
+        STBTTPackRange.Buffer packRange = STBTTPackRange.create(1);
+        packRange.put(STBTTPackRange.create().set(height, codePoint, null, 1, cdata, (byte) 2, (byte) 2));
+        packRange.flip();
+
+        // pack font ranges
+        STBTruetype.stbtt_PackFontRanges(packContext, buffer, 0, packRange);
+
+        STBTTPackedchar packedChar = cdata.get(0);
+
+        float ipw = 1f / size; // pixel width and height
+        float iph = 1f / size;
+
+        charMap.put(codePoint, new CharData(
+            packedChar.xoff(),
+            packedChar.yoff(),
+            packedChar.xoff2(),
+            packedChar.yoff2(),
+            packedChar.x0() * ipw,
+            packedChar.y0() * iph,
+            packedChar.x1() * ipw,
+            packedChar.y1() * iph,
+            packedChar.xadvance()
+        ));
+
+        packedChars.put(codePoint, packedChar);
+
+        // Re-create texture
+        createTexture();
+    }
+
+    private void createTexture() {
+        texture = new ByteTexture(size, size, bitmap, ByteTexture.Format.A, ByteTexture.Filter.Linear, ByteTexture.Filter.Linear);
     }
 
     public double getWidth(String string, int length) {
@@ -103,7 +139,10 @@ public class Font {
         for (int i = 0; i < length; i++) {
             int cp = string.charAt(i);
             CharData c = charMap.get(cp);
-            if (c == null) c = charMap.get(32);
+            if (c == null) {
+                loadCharacter(cp);
+                c = charMap.get(cp);
+            }
 
             width += c.xAdvance;
         }
@@ -121,7 +160,10 @@ public class Font {
         for (int i = 0; i < string.length(); i++) {
             int cp = string.charAt(i);
             CharData c = charMap.get(cp);
-            if (c == null) c = charMap.get(32);
+            if (c == null) {
+                loadCharacter(cp);
+                c = charMap.get(cp);
+            }
 
             mesh.quad(
                 mesh.vec2(x + c.x0 * scale, y + c.y0 * scale).vec2(c.u0, c.v0).color(color).next(),
