8d223b3797ab2b2c54b81546cbadfea504b88519
Anand Surendra Bhasme <96731982+AnandBhasme@users.noreply.github.com>
2024-07-21 18:56:53 +0530

    NoFall consistently picks up Powder Snow after fall (#4761)

--- src/main/java/meteordevelopment/meteorclient/systems/modules/movement/NoFall.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/movement/NoFall.java
@@ -178,6 +178,8 @@ public class NoFall extends Module {
                 timer++;
                 if (mc.player.getBlockStateAtPos().getBlock() == placedItem1.block) {
                     useItem(InvUtils.findInHotbar(Items.BUCKET), false, targetPos, true);
+                } else if (mc.world.getBlockState(mc.player.getBlockPos().down()).getBlock() == Blocks.POWDER_SNOW && mc.player.fallDistance==0 && placedItem1.block==Blocks.POWDER_SNOW){ //check if the powder snow block is still there and the player is on the ground
+                    useItem(InvUtils.findInHotbar(Items.BUCKET), false, targetPos.down(), true);
                 }
             }
         }
