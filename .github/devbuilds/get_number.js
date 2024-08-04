df0d3c57e5bc5f1fb86123a1292815a939d4b6b9
MrWalper <MrWalper@proton.me>
2024-08-04 21:31:07 +0200

    Fix text box and int/double/blockpos setting issues closes #4807
    ## Type of change
    
    - [ ] Bug fix
    - [ ] New feature
    - [  x ] Car
    
    ## Description
    
    Removed trash code, added eeper.
    
    ## Related issues
    
    No issues
    
    # How Has This Been Tested?
    
    Walper aprooved
    
    # Checklist:
    
    - [ ] My code follows the style guidelines of this project.
    - [ ] I have added comments to my code in more complex areas.
    - [ ] I have tested the code in both development and production environments.
    - [ x ] eeping

--- .github/devbuilds/get_number.js
+++
@@ -1,3 +0,0 @@
-fetch("https://meteorclient.com/api/stats")
-    .then(res => res.json())
-    .then(res => console.log("number=" + (parseInt(res.devBuild) + 1)))
--- src/main/java/meteordevelopment/meteorclient/systems/hud/HudGroup.java
+++
@@ -1,9 +0,0 @@
-/*
- * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
- * Copyright (c) Meteor Development.
- */
-
-package meteordevelopment.meteorclient.systems.hud;
-
-public record HudGroup(String title) {
-}
