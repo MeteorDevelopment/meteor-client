2b6e9b8eba1a768aaef059d26e6a17d3b3059d0e
RickyTheRacc <rickytheracc@gmail.com>
2024-08-15 22:43:33 -0500

    Subproject Kotlin Gradle

---
+++ launch/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("java")
+}
+
+tasks {
+    java {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+base {
+    group = "meteordevelopment"
+    version = "0.1.0"
+}
