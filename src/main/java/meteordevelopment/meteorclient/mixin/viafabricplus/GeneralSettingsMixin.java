f6c2c9b941452a1912b7374c591f9baea22bd052
Big-Iron-Cheems <52252627+Big-Iron-Cheems@users.noreply.github.com>
2024-08-17 14:14:31 +0200

    Mark other mixins abstract

--- src/main/java/meteordevelopment/meteorclient/mixin/viafabricplus/GeneralSettingsMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/viafabricplus/GeneralSettingsMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(GeneralSettings.class)
-public class GeneralSettingsMixin {
+public abstract class GeneralSettingsMixin {
     // specifies the '2' value on this line:
     // public final ModeSetting multiplayerScreenButtonOrientation = new ModeSetting(this, Text.translatable("general_settings.viafabricplus.multiplayer_screen_button_orientation"), 2, ORIENTATION_OPTIONS);
     @ModifyExpressionValue(method = "<init>", at = @At(value = "CONSTANT", args = "intValue=2", ordinal = 1), remap = false)
