082b871098875dfd89c58031937005a5b3c548d4
Funtimes909 <funtimes90969@gmail.com>
2024-10-01 15:25:58 +1300

    Add a blacklist for no mining trace

--- src/main/java/meteordevelopment/meteorclient/systems/modules/player/NoMiningTrace.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/player/NoMiningTrace.java
@@ -5,16 +5,28 @@
 
 package meteordevelopment.meteorclient.systems.modules.player;
 
+import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.EntityTypeListSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.PickaxeItem;
 
+import java.util.Set;
 public class NoMiningTrace extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
+    private final Setting<Set<EntityType<?>>> entities = sgGeneral.add(new EntityTypeListSetting.Builder()
+        .name("entities")
+        .description("Entity blacklist")
+        .defaultValue()
+        .build()
+    );
     private final Setting<Boolean> onlyWhenHoldingPickaxe = sgGeneral.add(new BoolSetting.Builder()
         .name("only-when-holding-a-pickaxe")
         .description("Whether or not to work only when holding a pickaxe.")
@@ -33,6 +45,9 @@ public class NoMiningTrace extends Module {
             return mc.player.getMainHandStack().getItem() instanceof PickaxeItem || mc.player.getOffHandStack().getItem() instanceof PickaxeItem;
         }
 
+        Entity target = mc.targetedEntity;
+        if (target == null) return false;
+        if (entities.get().contains(target.getType())) return false;
         return true;
     }
 }
