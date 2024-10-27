37f9414ffb7497e6db9e1fdbb4e2dddd4661e6d6
Tas <103238549+0xTas@users.noreply.github.com>
2024-10-27 10:08:00 -0700

    Fix PotionSpoof flickering with night vision and add potion mode to Fullbright (#4985)

--- src/main/java/meteordevelopment/meteorclient/systems/modules/render/Fullbright.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/render/Fullbright.java
@@ -5,12 +5,18 @@
 
 package meteordevelopment.meteorclient.systems.modules.render;
 
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.mixin.StatusEffectInstanceAccessor;
 import meteordevelopment.meteorclient.settings.EnumSetting;
 import meteordevelopment.meteorclient.settings.IntSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.registry.Registries;
 import net.minecraft.world.LightType;
 
 public class Fullbright extends Module {
@@ -21,7 +27,10 @@ public class Fullbright extends Module {
         .description("The mode to use for Fullbright.")
         .defaultValue(Mode.Gamma)
         .onChanged(mode -> {
-            if (mc.worldRenderer != null && isActive()) mc.worldRenderer.reload();
+            if (isActive()) {
+                if (mode != Mode.Potion) disableNightVision();
+                if (mc.worldRenderer != null) mc.worldRenderer.reload();
+            }
         })
         .build()
     );
@@ -62,6 +71,7 @@ public class Fullbright extends Module {
     @Override
     public void onDeactivate() {
         if (mode.get() == Mode.Luminance) mc.worldRenderer.reload();
+        else if (mode.get() == Mode.Potion) disableNightVision();
     }
 
     public int getLuminance(LightType type) {
@@ -73,8 +83,27 @@ public class Fullbright extends Module {
         return isActive() && mode.get() == Mode.Gamma;
     }
 
+    @EventHandler
+    private void onTick(TickEvent.Post event) {
+        if (mc.player == null || !mode.get().equals(Mode.Potion)) return;
+        if (mc.player.hasStatusEffect(Registries.STATUS_EFFECT.getEntry(StatusEffects.NIGHT_VISION.value()))) {
+            StatusEffectInstance instance = mc.player.getStatusEffect(Registries.STATUS_EFFECT.getEntry(StatusEffects.NIGHT_VISION.value()));
+            if (instance != null && instance.getDuration() < 420) ((StatusEffectInstanceAccessor) instance).setDuration(420);
+        } else {
+            mc.player.addStatusEffect(new StatusEffectInstance(Registries.STATUS_EFFECT.getEntry(StatusEffects.NIGHT_VISION.value()), 420, 0));
+        }
+    }
+
+    private void disableNightVision() {
+        if (mc.player == null) return;
+        if (mc.player.hasStatusEffect(Registries.STATUS_EFFECT.getEntry(StatusEffects.NIGHT_VISION.value()))) {
+            mc.player.removeStatusEffect(Registries.STATUS_EFFECT.getEntry(StatusEffects.NIGHT_VISION.value()));
+        }
+    }
+
     public enum Mode {
         Gamma,
+        Potion,
         Luminance
     }
 }
