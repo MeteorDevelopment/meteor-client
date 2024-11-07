302df1f4e64cd89d954a61179eb091764c3c6097
Wide_Cat <widecat@duck.com>
2024-11-07 18:49:24 +0000

    please actually test the code next time :bruh:

--- src/main/java/meteordevelopment/meteorclient/systems/modules/player/NoMiningTrace.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/player/NoMiningTrace.java
@@ -5,28 +5,28 @@
 
 package meteordevelopment.meteorclient.systems.modules.player;
 
-import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.settings.BoolSetting;
 import meteordevelopment.meteorclient.settings.EntityTypeListSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.orbit.EventHandler;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.PickaxeItem;
 
 import java.util.Set;
+
 public class NoMiningTrace extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
     private final Setting<Set<EntityType<?>>> entities = sgGeneral.add(new EntityTypeListSetting.Builder()
-        .name("entities")
-        .description("Entity blacklist")
+        .name("blacklisted-entities")
+        .description("Entities you will interact with as normal.")
         .defaultValue()
         .build()
     );
+
     private final Setting<Boolean> onlyWhenHoldingPickaxe = sgGeneral.add(new BoolSetting.Builder()
         .name("only-when-holding-a-pickaxe")
         .description("Whether or not to work only when holding a pickaxe.")
@@ -38,16 +38,10 @@ public class NoMiningTrace extends Module {
         super(Categories.Player, "no-mining-trace", "Allows you to mine blocks through entities.");
     }
 
-    public boolean canWork() {
+    public boolean canWork(Entity entity) {
         if (!isActive()) return false;
 
-        if (onlyWhenHoldingPickaxe.get()) {
-            return mc.player.getMainHandStack().getItem() instanceof PickaxeItem || mc.player.getOffHandStack().getItem() instanceof PickaxeItem;
-        }
-
-        Entity target = mc.targetedEntity;
-        if (target == null) return false;
-        if (entities.get().contains(target.getType())) return false;
-        return true;
+        return (!onlyWhenHoldingPickaxe.get() || mc.player.getMainHandStack().getItem() instanceof PickaxeItem || mc.player.getOffHandStack().getItem() instanceof PickaxeItem) &&
+            (entity == null || !entities.get().contains(entity.getType()));
     }
 }
