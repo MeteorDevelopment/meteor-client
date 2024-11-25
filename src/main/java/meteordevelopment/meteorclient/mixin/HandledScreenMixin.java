9ac4187fda982861865c33cdd75da429fc19961d
Roman Danilov <35975332+Nekiplay@users.noreply.github.com>
2024-11-25 06:17:11 +0500

    Update HandledScreenMixin.java

--- src/main/java/meteordevelopment/meteorclient/mixin/HandledScreenMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/HandledScreenMixin.java
@@ -87,7 +87,7 @@ public abstract class HandledScreenMixin<T extends ScreenHandler> extends Screen
             );
 
             addDrawableChild(
-                new ButtonWidget.Builder(Text.literal(I18n.translate(dump)), button -> invTweaks.dump(getScreenHandler()))
+                new ButtonWidget.Builder(Text.literal(dump), button -> invTweaks.dump(getScreenHandler()))
                     .position(x + steal.length() * 10 + 2, y - 22)
                     .size(dump.length() * 10, 20)
                     .build()
