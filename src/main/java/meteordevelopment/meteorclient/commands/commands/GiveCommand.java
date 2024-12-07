51e172b3bc9f8156a129bb3d0c467abf4884cc9f
RacoonDog <32882447+racoondog@users.noreply.github.com>
2024-12-06 19:34:33 -0500

    rewrite .give to use new utils

--- src/main/java/meteordevelopment/meteorclient/commands/commands/GiveCommand.java
+++ src/main/java/meteordevelopment/meteorclient/commands/commands/GiveCommand.java
@@ -7,19 +7,21 @@ package meteordevelopment.meteorclient.commands.commands;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import meteordevelopment.meteorclient.commands.Command;
+import meteordevelopment.meteorclient.commands.arguments.ItemSlotArgumentType;
+import meteordevelopment.meteorclient.utils.commands.CreativeCommandHelper;
 import meteordevelopment.meteorclient.utils.player.FindItemResult;
 import meteordevelopment.meteorclient.utils.player.InvUtils;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.argument.ItemStackArgumentType;
 import net.minecraft.item.ItemStack;
-import net.minecraft.network.packet.c2s.play.CreativeInventoryActionC2SPacket;
 import net.minecraft.text.Text;
 
 public class GiveCommand extends Command {
-    private final static SimpleCommandExceptionType NOT_IN_CREATIVE = new SimpleCommandExceptionType(Text.literal("You must be in creative mode to use this."));
-    private final static SimpleCommandExceptionType NO_SPACE = new SimpleCommandExceptionType(Text.literal("No space in hotbar."));
+    private final static SimpleCommandExceptionType NO_FREE_SPACE = new SimpleCommandExceptionType(Text.literal("No free space in hotbar."));
 
     public GiveCommand() {
         super("give", "Gives you any item.");
@@ -27,26 +29,30 @@ public class GiveCommand extends Command {
 
     @Override
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
-        builder.then(argument("item", ItemStackArgumentType.itemStack(REGISTRY_ACCESS)).executes(context -> {
-            if (!mc.player.getAbilities().creativeMode) throw NOT_IN_CREATIVE.create();
+        builder.then(argument("item", ItemStackArgumentType.itemStack(REGISTRY_ACCESS))
+            .executes(context -> executeGive(context, 1, findEmptySlot()))
+            .then(literal("slot").then(argument("slot", ItemSlotArgumentType.modifiableSlot()).executes(context ->
+                executeGive(context, 1, ItemSlotArgumentType.getItemSlot(context))
+            )))
+            .then(argument("count", IntegerArgumentType.integer(1, 99))
+                .executes(context -> executeGive(context, IntegerArgumentType.getInteger(context, "count"), findEmptySlot()))
+                .then(literal("slot").then(argument("slot", ItemSlotArgumentType.modifiableSlot()).executes(context ->
+                    executeGive(context, IntegerArgumentType.getInteger(context, "count"), ItemSlotArgumentType.getItemSlot(context))
+                )))
+            )
+        );
+    }
 
-            ItemStack item = ItemStackArgumentType.getItemStackArgument(context, "item").createStack(1, false);
-            FindItemResult fir = InvUtils.find(ItemStack::isEmpty, 0, 8);
-            if (!fir.found()) throw NO_SPACE.create();
+    private int findEmptySlot() throws CommandSyntaxException {
+        FindItemResult fir = InvUtils.findInHotbar(ItemStack::isEmpty);
+        if (!fir.found()) throw NO_FREE_SPACE.create();
+        return fir.slot();
+    }
 
-            mc.getNetworkHandler().sendPacket(new CreativeInventoryActionC2SPacket(36 + fir.slot(), item));
+    private <S> int executeGive(CommandContext<S> context, int count, int slot) throws CommandSyntaxException {
+        ItemStack stack = ItemStackArgumentType.getItemStackArgument(context, "item").createStack(count, false);
+        CreativeCommandHelper.setStack(stack, slot);
 
-            return SINGLE_SUCCESS;
-        }).then(argument("number", IntegerArgumentType.integer()).executes(context -> {
-            if (!mc.player.getAbilities().creativeMode) throw NOT_IN_CREATIVE.create();
-
-            ItemStack item = ItemStackArgumentType.getItemStackArgument(context, "item").createStack(IntegerArgumentType.getInteger(context, "number"), false);
-            FindItemResult fir = InvUtils.find(ItemStack::isEmpty, 0, 8);
-            if (!fir.found()) throw NO_SPACE.create();
-
-            mc.getNetworkHandler().sendPacket(new CreativeInventoryActionC2SPacket(36 + fir.slot(), item));
-
-            return SINGLE_SUCCESS;
-        })));
+        return SINGLE_SUCCESS;
     }
 }
