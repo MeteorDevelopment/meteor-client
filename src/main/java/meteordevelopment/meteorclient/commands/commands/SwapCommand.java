c3d77c0f618997ecd6398adaf4a9b4f414a2f088
DrParanoya <158059657+DrParanoya@users.noreply.github.com>
2025-02-06 20:53:53 +0100

    Create SwapCommand.java
    New swap command, swaps to a slot in the hotbar

---
+++ src/main/java/meteordevelopment/meteorclient/commands/commands/SwapCommand.java
@@ -0,0 +1,30 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package meteordevelopment.meteorclient.commands.commands;
+
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import meteordevelopment.meteorclient.commands.Command;
+import meteordevelopment.meteorclient.utils.player.InvUtils;
+import net.minecraft.command.CommandSource;
+
+import java.util.ArrayList;
+
+public class SwapCommand extends Command {
+    public SwapCommand() {
+        super("swap", "Swaps to a hotbar slot");
+    }
+
+    @Override
+    public void build(LiteralArgumentBuilder<CommandSource> builder) {
+        builder.then(argument("slot", IntegerArgumentType.integer(1, 9)).executes(context -> {
+            int slot = IntegerArgumentType.getInteger(context, "slot") - 1;
+            InvUtils.swap(slot, false);
+            return SINGLE_SUCCESS;
+        }));
+
+    }
+}
