32a91461ee2729d52c885cb3fad5b5c71913753b
Wide_Cat <widecat@duck.com>
2025-02-20 15:43:18 +0000

    Remove the startup update notifier
    Since the versioning scheme change it flashed a popup after every new build, which was annoying. This is a stopgap measure for now; it should probably be added back in some capacity in the future.

--- src/main/java/meteordevelopment/meteorclient/mixin/TitleScreenMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/TitleScreenMixin.java
@@ -5,21 +5,12 @@
 
 package meteordevelopment.meteorclient.mixin;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.systems.config.Config;
-import meteordevelopment.meteorclient.utils.Utils;
-import meteordevelopment.meteorclient.utils.network.Http;
-import meteordevelopment.meteorclient.utils.network.MeteorExecutor;
 import meteordevelopment.meteorclient.utils.player.TitleScreenCredits;
-import meteordevelopment.meteorclient.utils.render.prompts.OkPrompt;
-import meteordevelopment.meteorclient.utils.render.prompts.YesNoPrompt;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.text.Text;
-import net.minecraft.util.Util;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -31,50 +22,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class TitleScreenMixin extends Screen {
     public TitleScreenMixin(Text title) {
         super(title);
-    }
-
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)I", ordinal = 0))
-    private void onRenderIdkDude(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        if (Utils.firstTimeTitleScreen) {
-            Utils.firstTimeTitleScreen = false;
-
-            if (!MeteorClient.VERSION.isZero() && !MeteorClient.BUILD_NUMBER.isEmpty()) {
-                MeteorClient.LOG.info("Checking latest version of Meteor Client");
-
-                MeteorExecutor.execute(() -> {
-                    String res = Http.get("https://meteorclient.com/api/stats")
-                        .exceptionHandler(e -> MeteorClient.LOG.error("Could not fetch version information."))
-                        .sendString();
-                    if (res == null) return;
-
-                    JsonElement latestBuild = JsonParser.parseString(res).getAsJsonObject()
-                        .getAsJsonObject("builds")
-                        .get(MeteorClient.VERSION.toString());
-
-                    if (latestBuild == null)
-                        return;
-
-                    if (latestBuild.getAsInt() > Integer.parseInt(MeteorClient.BUILD_NUMBER)) {
-                        YesNoPrompt.create()
-                            .title("New Update")
-                            .message("A new version of Meteor has been released.")
-                            .message("Your version: %s", MeteorClient.VERSION + "-" + MeteorClient.BUILD_NUMBER)
-                            .message("Latest version: %s", MeteorClient.VERSION + "-" + latestBuild.getAsInt())
-                            .message("Do you want to update?")
-                            .onYes(() -> Util.getOperatingSystem().open("https://meteorclient.com/"))
-                            .onNo(() -> OkPrompt.create()
-                                .title("Are you sure?")
-                                .message("Using old versions of Meteor is not recommended")
-                                .message("and could result in issues.")
-                                .id("new-update-no")
-                                .onOk(this::close)
-                                .show())
-                            .id("new-update")
-                            .show();
-                    }
-                });
-            }
-        }
     }
 
     @Inject(method = "render", at = @At("TAIL"))
