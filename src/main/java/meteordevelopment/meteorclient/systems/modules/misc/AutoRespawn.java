9c5e184f07d54d98bf2ef47de49f7dd008817699
LitSkillz <124302831+LitSkillz@users.noreply.github.com>
2025-03-30 10:42:51 -0700

    Add respawn message settings and functionality.

--- src/main/java/meteordevelopment/meteorclient/systems/modules/misc/AutoRespawn.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/misc/AutoRespawn.java
@@ -6,6 +6,11 @@
 package meteordevelopment.meteorclient.systems.modules.misc;
 
 import meteordevelopment.meteorclient.events.game.OpenScreenEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.DoubleSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.StringSetting;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.Modules;
@@ -13,10 +18,51 @@ import meteordevelopment.meteorclient.systems.modules.render.WaypointsModule;
 import meteordevelopment.orbit.EventHandler;
 import meteordevelopment.orbit.EventPriority;
 import net.minecraft.client.gui.screen.DeathScreen;
+import meteordevelopment.meteorclient.utils.network.MeteorExecutor;
+import meteordevelopment.meteorclient.utils.player.ChatUtils;
 
 public class AutoRespawn extends Module {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+
+    public final Setting<Boolean> sendRespawnMessage = sgGeneral.add(new BoolSetting.Builder()
+        .name("respawn-message")
+        .description("Sends a message or command upon respawning.")
+        .defaultValue(false)
+        .build()
+    );
+
+    private final Setting<String> respawnMessage = sgGeneral.add(new StringSetting.Builder()
+        .name("message")
+        .description("The message/command to send.")
+        .defaultValue("Meteor on Crack!")
+        .visible(sendRespawnMessage::get)
+        .build()
+    );
+
+    private final Setting<Double> respawnMessageDelay = sgGeneral.add(new DoubleSetting.Builder()
+        .name("initial-delay")
+        .description("The delay before sending respawn message in seconds.")
+        .defaultValue(0.5)
+        .sliderMax(10)
+        .visible(sendRespawnMessage::get)
+        .build()
+    );
+
     public AutoRespawn() {
         super(Categories.Player, "auto-respawn", "Automatically respawns after death.");
+    }
+
+    private void sendDeathMessage() {
+        long delay = (long) Math.floor(respawnMessageDelay.get() * 1000);
+        if (delay > 0) {
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        ChatUtils.sendPlayerMsg(respawnMessage.get());
     }
 
     @EventHandler(priority = EventPriority.HIGH)
@@ -26,5 +72,9 @@ public class AutoRespawn extends Module {
         Modules.get().get(WaypointsModule.class).addDeath(mc.player.getPos());
         mc.player.requestRespawn();
         event.cancel();
+
+        if (!sendRespawnMessage.get()) return;
+        // execute on a different thread to not yield literally everything.
+        MeteorExecutor.execute(() -> sendDeathMessage());
     }
 }
