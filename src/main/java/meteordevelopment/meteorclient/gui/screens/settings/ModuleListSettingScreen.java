1a6fbbc4bdc4bca1c2654a5a4466a0ee2d366008
RacoonDog <32882447+racoondog@users.noreply.github.com>
2025-03-30 18:55:42 -0400

    `RegistryListSettingScreen` -> `CollectionListSettingScreen` relax parameter from `Registry<T>` to `Iterable<T>`

--- src/main/java/meteordevelopment/meteorclient/gui/screens/settings/ModuleListSettingScreen.java
+++ src/main/java/meteordevelopment/meteorclient/gui/screens/settings/ModuleListSettingScreen.java
@@ -13,7 +13,7 @@ import meteordevelopment.meteorclient.systems.modules.Modules;
 
 import java.util.List;
 
-public class ModuleListSettingScreen extends RegistryListSettingScreen<Module> {
+public class ModuleListSettingScreen extends CollectionListSettingScreen<Module> {
     public ModuleListSettingScreen(GuiTheme theme, Setting<List<Module>> setting) {
         super(theme, "Select Modules", setting, setting.get(), Modules.REGISTRY);
     }
