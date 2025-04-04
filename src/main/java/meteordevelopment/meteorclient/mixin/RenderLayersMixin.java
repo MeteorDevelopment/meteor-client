ecad5ce56b0caa262adafcb916fb7e1d960ca048
Wide_Cat <widecat@duck.com>
2025-04-04 14:41:49 +0100

    Fix being unable to modify lava opacity in Xray/Wallhack/Ambience

--- src/main/java/meteordevelopment/meteorclient/mixin/RenderLayersMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/RenderLayersMixin.java
@@ -7,9 +7,11 @@ package meteordevelopment.meteorclient.mixin;
 
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.render.Xray;
+import meteordevelopment.meteorclient.systems.modules.world.Ambience;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.RenderLayers;
+import net.minecraft.fluid.FluidState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,10 +20,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(RenderLayers.class)
 public abstract class RenderLayersMixin {
     @Inject(method = "getBlockLayer", at = @At("HEAD"), cancellable = true)
-    private static void onGetBlockLayer(BlockState state, CallbackInfoReturnable<RenderLayer> info) {
+    private static void onGetBlockLayer(BlockState state, CallbackInfoReturnable<RenderLayer> cir) {
         if (Modules.get() == null) return;
 
         int alpha = Xray.getAlpha(state, null);
-        if (alpha > 0 && alpha < 255) info.setReturnValue(RenderLayer.getTranslucent());
+        if (alpha > 0 && alpha < 255) cir.setReturnValue(RenderLayer.getTranslucent());
+    }
+
+    @Inject(method = "getFluidLayer", at = @At("HEAD"), cancellable = true)
+    private static void onGetFluidLayer(FluidState state, CallbackInfoReturnable<RenderLayer> cir) {
+        if (Modules.get() == null) return;
+
+        int alpha = Xray.getAlpha(state.getBlockState(), null);
+        if (alpha > 0 && alpha < 255) {
+            cir.setReturnValue(RenderLayer.getTranslucent());
+        }
+
+        else {
+            Ambience ambience = Modules.get().get(Ambience.class);
+            int a = ambience.lavaColor.get().a;
+            if (ambience.isActive() && ambience.customLavaColor.get() && a > 0 && a < 255) {
+                cir.setReturnValue(RenderLayer.getTranslucent());
+            }
+        }
     }
 }
