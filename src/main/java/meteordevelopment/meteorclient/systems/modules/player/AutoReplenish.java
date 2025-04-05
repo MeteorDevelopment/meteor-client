162b3d93598c301e0de4199033b3008bdc970c1d
MachieCodes <rickytheracc@gmail.com>
2025-04-05 13:20:44 -0500

    Add "In Inventories" Setting and Fix Unstackables

--- src/main/java/meteordevelopment/meteorclient/systems/modules/player/AutoReplenish.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/player/AutoReplenish.java
@@ -5,7 +5,6 @@
 
 package meteordevelopment.meteorclient.systems.modules.player;
 
-import it.unimi.dsi.fastutil.ints.IntArraySet;
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Categories;
@@ -16,20 +15,20 @@ import meteordevelopment.meteorclient.utils.player.InvUtils;
 import meteordevelopment.meteorclient.utils.player.SlotUtils;
 import meteordevelopment.meteorclient.utils.world.TickRate;
 import meteordevelopment.orbit.EventHandler;
-import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 public class AutoReplenish extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    private final Setting<Integer> threshold = sgGeneral.add(new IntSetting.Builder()
-        .name("threshold")
+    private final Setting<Integer> minCount = sgGeneral.add(new IntSetting.Builder()
+        .name("min-count")
         .description("How many items should be left before refilling.")
         .defaultValue(8)
         .min(1)
@@ -73,6 +72,13 @@ public class AutoReplenish extends Module {
         .build()
     );
 
+    private final Setting<Boolean> inInventory = sgGeneral.add(new BoolSetting.Builder()
+        .name("in-inventory")
+        .description("Replenish items even when in inventory screens")
+        .defaultValue(true)
+        .build()
+    );
+
     private final Setting<List<Item>> excludedItems = sgGeneral.add(new ItemListSetting.Builder()
         .name("excluded-items")
         .description("Items that will not be replenished.")
@@ -83,7 +89,7 @@ public class AutoReplenish extends Module {
     private double tickDelayLeft;
 
     public AutoReplenish() {
-        super(Categories.Player, "auto-replenish", "Automatically refills items in your hotbar, main hand, or offhand.");
+        super(Categories.Player, "auto-replenish", "Automatically refills items in your hotbar or offhand.");
 
         Arrays.fill(items, 0, 9, Items.AIR.getDefaultStack());
     }
@@ -96,13 +102,10 @@ public class AutoReplenish extends Module {
 
     @EventHandler
     private void onTick(TickEvent.Pre event) {
-        fillItems();
-
-        boolean hasOpenScreen = mc.currentScreen instanceof GenericContainerScreen;
-        if (mc.player.currentScreenHandler.getStacks().size() != 46 || hasOpenScreen) return;
-
         tickDelayLeft -= TickRate.INSTANCE.getTickRate() / 20.0;
         if (tickDelayLeft > 0) return;
+
+        if (!inInventory.get() && mc.currentScreen instanceof HandledScreen<?>) return;
 
         // Hotbar
         for (int i = 0; i < 9; i++) {
@@ -119,36 +122,40 @@ public class AutoReplenish extends Module {
             ItemStack stack = mc.player.getOffHandStack();
             replenishSlot(SlotUtils.OFFHAND, stack);
         }
+
+        fillItems();
     }
 
     private void replenishSlot(int slot, ItemStack currentStack) {
         int itemsIndex = slot == SlotUtils.OFFHAND ? 9 : slot;
         ItemStack prevStack = items[itemsIndex];
 
+        if (excludedItems.get().contains(currentStack.getItem())) return;
+        if (excludedItems.get().contains(prevStack.getItem())) return;
+
         int itemSlot = -1;
 
         // If you still have some left in the stack and just now crossed the threshold
-        if (!currentStack.isEmpty() && currentStack.isStackable() && currentStack.getCount() <= threshold.get()) {
-            if (excludedItems.get().contains(currentStack.getItem())) return;
+        if (!currentStack.isEmpty() && currentStack.isStackable() && currentStack.getCount() <= minCount.get()) {
             itemSlot = findItem(currentStack, slot);
         }
 
         // If you just went from above the threshold to zero in a single tick, which is possible
-        // with a low enough threshold and using modules that place multiple blocks per tick,
-        // e.g. surround or holefiller using obsidian
+        // with a low enough min item count and using modules that place multiple blocks per tick,
+        // e.g. surround or holefiller placing four blocks at once
         if (currentStack.isEmpty() && !prevStack.isEmpty() && prevStack.isStackable()) {
-            if (excludedItems.get().contains(prevStack.getItem())) return;
             itemSlot = findItem(prevStack, slot);
         }
 
         // Unstackable items
         if (currentStack.isEmpty() && !prevStack.isEmpty() && !prevStack.isStackable() && unstackable.get()) {
-            if (excludedItems.get().contains(prevStack.getItem())) return;
             itemSlot = findItem(prevStack, slot);
         }
 
         InvUtils.move().from(itemSlot).to(slot);
         items[itemsIndex] = currentStack.copy();
+
+        tickDelayLeft = tickDelay.get();
     }
 
     private int findItem(ItemStack itemStack, int excludedSlot) {
