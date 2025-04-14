0052e8f35bde79a3a96e1c3d13a39b7368475810
Big-Iron-Cheems <52252627+Big-Iron-Cheems@users.noreply.github.com>
2025-04-14 19:09:11 +0200

    Refactor mixin `@Redirect`s

--- src/main/java/meteordevelopment/meteorclient/mixin/BossBarHudMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/BossBarHudMixin.java
@@ -5,6 +5,8 @@
 
 package meteordevelopment.meteorclient.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.events.render.RenderBossBarEvent;
 import meteordevelopment.meteorclient.systems.modules.Modules;
@@ -13,10 +15,12 @@ import net.minecraft.client.gui.hud.BossBarHud;
 import net.minecraft.client.gui.hud.ClientBossBar;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Collection;
 import java.util.Iterator;
 
 @Mixin(BossBarHud.class)
@@ -26,15 +30,15 @@ public abstract class BossBarHudMixin {
         if (Modules.get().get(NoRender.class).noBossBar()) info.cancel();
     }
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Ljava/util/Collection;iterator()Ljava/util/Iterator;"))
-    public Iterator<ClientBossBar> onRender(Collection<ClientBossBar> collection) {
-        RenderBossBarEvent.BossIterator event = MeteorClient.EVENT_BUS.post(RenderBossBarEvent.BossIterator.get(collection.iterator()));
+    @ModifyExpressionValue(method = "render", at = @At(value = "INVOKE", target = "Ljava/util/Collection;iterator()Ljava/util/Iterator;"))
+    public Iterator<ClientBossBar> modifyBossBarIterator(Iterator<ClientBossBar> original) {
+        RenderBossBarEvent.BossIterator event = MeteorClient.EVENT_BUS.post(RenderBossBarEvent.BossIterator.get(original));
         return event.iterator;
     }
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/ClientBossBar;getName()Lnet/minecraft/text/Text;"))
-    public Text onAsFormattedString(ClientBossBar clientBossBar) {
-        RenderBossBarEvent.BossText event = MeteorClient.EVENT_BUS.post(RenderBossBarEvent.BossText.get(clientBossBar, clientBossBar.getName()));
+    @ModifyExpressionValue(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/ClientBossBar;getName()Lnet/minecraft/text/Text;"))
+    public Text modifyBossBarName(Text original, @Local ClientBossBar clientBossBar) {
+        RenderBossBarEvent.BossText event = MeteorClient.EVENT_BUS.post(RenderBossBarEvent.BossText.get(clientBossBar, original));
         return event.name;
     }
 
