00f42e09b6868a2dcc770d186bdd4c84f41b2910
Wide_Cat <widecat@duck.com>
2025-04-25 17:08:43 +0100

    Add a 'Copy link' button to AddMicrosoftAccountScreen

--- src/main/java/meteordevelopment/meteorclient/gui/screens/accounts/AddMicrosoftAccountScreen.java
+++ src/main/java/meteordevelopment/meteorclient/gui/screens/accounts/AddMicrosoftAccountScreen.java
@@ -6,9 +6,12 @@
 package meteordevelopment.meteorclient.gui.screens.accounts;
 
 import meteordevelopment.meteorclient.gui.GuiTheme;
+import meteordevelopment.meteorclient.gui.widgets.containers.WHorizontalList;
 import meteordevelopment.meteorclient.gui.widgets.pressable.WButton;
 import meteordevelopment.meteorclient.systems.accounts.MicrosoftLogin;
 import meteordevelopment.meteorclient.systems.accounts.types.MicrosoftAccount;
+
+import static meteordevelopment.meteorclient.MeteorClient.mc;
 
 public class AddMicrosoftAccountScreen extends AddAccountScreen {
     public AddMicrosoftAccountScreen(GuiTheme theme, AccountsScreen parent) {
@@ -17,7 +20,7 @@ public class AddMicrosoftAccountScreen extends AddAccountScreen {
 
     @Override
     public void initWidgets() {
-        MicrosoftLogin.getRefreshToken(refreshToken -> {
+        String url = MicrosoftLogin.getRefreshToken(refreshToken -> {
 
             if (refreshToken != null) {
                 MicrosoftAccount account = new MicrosoftAccount(refreshToken);
@@ -28,8 +31,14 @@ public class AddMicrosoftAccountScreen extends AddAccountScreen {
         });
 
         add(theme.label("Please select the account to log into in your browser."));
+        add(theme.label("If the link does not automatically open in a few seconds, copy it into your browser."));
 
-        WButton cancel = add(theme.button("Cancel")).expandX().widget();
+        WHorizontalList l = add(theme.horizontalList()).expandX().widget();
+
+        WButton copy = l.add(theme.button("Copy link")).expandX().widget();
+        copy.action = () -> mc.keyboard.setClipboard(url);
+
+        WButton cancel = l.add(theme.button("Cancel")).expandX().widget();
         cancel.action = () -> {
             MicrosoftLogin.stopServer();
             close();
