5599a045ed83b8658362be5740f2980c8a3689e2
Wide_Cat <widecat@duck.com>
2025-05-01 17:31:56 +0100

    Fix mixin issue

--- src/main/java/meteordevelopment/meteorclient/mixin/BackgroundRendererMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/BackgroundRendererMixin.java
@@ -9,29 +9,31 @@ import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.render.NoRender;
 import meteordevelopment.meteorclient.systems.modules.world.Ambience;
-import meteordevelopment.meteorclient.utils.render.color.Color;
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.entity.Entity;
+import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(BackgroundRenderer.class)
 public abstract class BackgroundRendererMixin {
-    @ModifyArgs(method = "applyFog(Lnet/minecraft/client/render/Camera;Lorg/joml/Vector4f;FZF)V", at = @At("HEAD"))
-    private void modifyFogDistance(Args args) {
+    @ModifyVariable(method = "applyFog(Lnet/minecraft/client/render/Camera;Lorg/joml/Vector4f;FZF)V", at = @At("HEAD"), argsOnly = true)
+    private Vector4f modifyFogDistance(Vector4f original) {
         Ambience ambience = Modules.get().get(Ambience.class);
         if (ambience.isActive() && ambience.customFogColor.get()) {
-            Color fogColor = ambience.fogColor.get();
-            args.set(1, fogColor.getVec4f());
+            return ambience.fogColor.get().getVec4f();
         }
+
+        return original;
     }
 
     @ModifyExpressionValue(method = "getFogBuffer", at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/BackgroundRenderer;fogEnabled:Z"))
     private boolean modifyFogEnabled(boolean original) {
+        if (Modules.get() == null) return original;
+
         return original && !Modules.get().get(NoRender.class).noFog();
     }
 
