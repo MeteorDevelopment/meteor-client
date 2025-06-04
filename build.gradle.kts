61ab74b4d82cb177adf34e75072ff925788770ff
RacoonDog <32882447+racoondog@users.noreply.github.com>
2025-06-04 00:14:59 -0400

    fix gradle configuration cache task inputs

--- build.gradle.kts
+++ build.gradle.kts
@@ -165,9 +165,10 @@ tasks {
     shadowJar {
         configurations = listOf(project.configurations.shadow.get())
 
-        val licenseSuffix = project.base.archivesName.get()
+        inputs.property("archivesName", project.base.archivesName.get())
+
         from("LICENSE") {
-            rename { "${it}_${licenseSuffix}" }
+            rename { "${it}_${inputs.properties["archivesName"]}" }
         }
 
         dependencies {
