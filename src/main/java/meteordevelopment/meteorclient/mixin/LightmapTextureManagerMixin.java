19809bc90cec629fec28ebb635e481fddf84ca0a
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2025-06-05 11:18:19 -0400

    skip regular lightmap computations with Fullbright & Xray (#5464)

--- src/main/java/meteordevelopment/meteorclient/mixin/LightmapTextureManagerMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/LightmapTextureManagerMixin.java
@@ -5,6 +5,7 @@
 
 package meteordevelopment.meteorclient.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.textures.GpuTexture;
 import meteordevelopment.meteorclient.systems.modules.Modules;
@@ -14,6 +15,7 @@ import meteordevelopment.meteorclient.systems.modules.render.Xray;
 import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.ColorHelper;
+import net.minecraft.util.profiler.Profiler;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -22,18 +24,18 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.OptionalInt;
-
 @Mixin(LightmapTextureManager.class)
 public abstract class LightmapTextureManagerMixin {
     @Shadow
     @Final
     private GpuTexture glTexture;
 
-    @Inject(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;pop()V", shift = At.Shift.BEFORE))
-    private void update$clear(float tickProgress, CallbackInfo info) {
+    @Inject(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;push(Ljava/lang/String;)V", shift = At.Shift.AFTER), cancellable = true)
+    private void update$skip(float tickProgress, CallbackInfo ci, @Local Profiler profiler) {
         if (Modules.get().get(Fullbright.class).getGamma() || Modules.get().isActive(Xray.class)) {
-            RenderSystem.getDevice().createCommandEncoder().createRenderPass(glTexture, OptionalInt.of(ColorHelper.getArgb(255, 255, 255, 255))).close();
+            RenderSystem.getDevice().createCommandEncoder().clearColorTexture(glTexture, ColorHelper.getArgb(255, 255, 255, 255));
+            profiler.pop();
+            ci.cancel();
         }
     }
 
