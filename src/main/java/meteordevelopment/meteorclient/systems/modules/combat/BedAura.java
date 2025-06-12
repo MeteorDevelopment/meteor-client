56e747f4588b670a883a6a4534f71dd1a59457f8
Gingerbeard <fathergingerbeard@gmail.com>
2025-06-12 12:18:26 -0400

    Fixed wrong maximum for damage settings

--- src/main/java/meteordevelopment/meteorclient/systems/modules/combat/BedAura.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/combat/BedAura.java
@@ -147,7 +147,8 @@ public class BedAura extends Module {
         .name("min-damage")
         .description("The minimum damage to inflict on your target.")
         .defaultValue(7)
-        .range(0, 36)
+        .min(0)
+        .sliderMax(36)
         .build()
     );
 
@@ -155,7 +156,8 @@ public class BedAura extends Module {
         .name("max-self-damage")
         .description("The maximum damage to inflict on yourself.")
         .defaultValue(7)
-        .range(0, 36)
+        .min(0)
+        .sliderMax(36)
         .build()
     );
 
