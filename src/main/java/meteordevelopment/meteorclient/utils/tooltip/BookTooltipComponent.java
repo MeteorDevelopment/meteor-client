faa04801ef02461d9b1eb3d88c38821b20ab67c5
Wide_Cat <widecat@duck.com>
2025-06-20 14:29:48 +0100

    Fix BookTooltipComponent

--- src/main/java/meteordevelopment/meteorclient/utils/tooltip/BookTooltipComponent.java
+++ src/main/java/meteordevelopment/meteorclient/utils/tooltip/BookTooltipComponent.java
@@ -41,7 +41,7 @@ public class BookTooltipComponent implements TooltipComponent, MeteorTooltipData
     @Override
     public void drawItems(TextRenderer textRenderer, int x, int y, int width, int height, DrawContext context) {
         // Background
-        context.drawTexture(RenderPipelines.GUI_TEXTURED, TEXTURE_BOOK_BACKGROUND, x, y, 0, 12, 0, 112, 134, 179, 179);
+        context.drawTexture(RenderPipelines.GUI_TEXTURED, TEXTURE_BOOK_BACKGROUND, x - 10, y, 0, 0, 128, 128, 179, 179);
 
         // Content
         Matrix3x2fStack matrices = context.getMatrices();
@@ -50,7 +50,7 @@ public class BookTooltipComponent implements TooltipComponent, MeteorTooltipData
         matrices.scale(0.7f, 0.7f);
         int offset = 0;
         for (OrderedText line : textRenderer.wrapLines(page, 112)) {
-            context.drawText(textRenderer, line, 0, offset, 0x000000, false);
+            context.drawText(textRenderer, line, 0, offset, 0xFF000000, false);
             offset += 8;
         }
         matrices.popMatrix();
