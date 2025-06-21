2ff7bb6946d734decccbf24095bc4d01db26b903
Big-Iron-Cheems <52252627+Big-Iron-Cheems@users.noreply.github.com>
2025-06-21 13:26:40 +0200

    Fix EntityControl
    TODO: fix maxJump setting

---
+++ src/main/java/meteordevelopment/meteorclient/mixin/MobEntityMixin.java
@@ -0,0 +1,21 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package meteordevelopment.meteorclient.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.systems.modules.movement.EntityControl;
+import net.minecraft.entity.mob.MobEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(MobEntity.class)
+public abstract class MobEntityMixin {
+    @ModifyReturnValue(method = "hasSaddleEquipped", at = @At("RETURN"))
+    private boolean hasSaddleEquipped(boolean original) {
+        return Modules.get().get(EntityControl.class).isActive() || original;
+    }
+}
