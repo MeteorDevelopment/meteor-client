4c41a15c737bb35c6df1e7b2b571e6d4ffbdc9dd
NDev007 <ndev.developer102@gmail.com>
2025-06-27 22:39:16 +0300

    InventoryTweaks Inventory Drop button

---
+++ src/main/java/meteordevelopment/meteorclient/mixininterface/IDropItems.java
@@ -0,0 +1,14 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package meteordevelopment.meteorclient.mixininterface;
+
+/**
+ * @author NDev007
+ * @since 27.06.2025
+ */
+public interface IDropItems {
+    void dropItems();
+}
