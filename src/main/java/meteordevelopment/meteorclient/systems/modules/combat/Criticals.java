69ec157ceba8c4c0cde3dfe6341778571c9247d2
Isaac <isaaclevirandle@gmail.com>
2025-06-30 22:17:58 +0100

    Removed mace feature from criticals

--- src/main/java/meteordevelopment/meteorclient/systems/modules/combat/Criticals.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/combat/Criticals.java
@@ -18,7 +18,6 @@ import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.MaceItem;
 import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
@@ -26,7 +25,6 @@ import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 public class Criticals extends Module {
 
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
-    private final SettingGroup sgMace = settings.createGroup("Mace");
 
     private final Setting<Mode> mode = sgGeneral.add(new EnumSetting.Builder<Mode>()
         .name("mode")
@@ -42,24 +40,6 @@ public class Criticals extends Module {
         .visible(() -> mode.get() != Mode.None)
         .build()
     );
-
-    private final Setting<Boolean> mace = sgMace.add(new BoolSetting.Builder()
-        .name("smash-attack")
-        .description("Will always perform smash attacks when using a mace.")
-        .defaultValue(true)
-        .build()
-    );
-
-    private final Setting<Double> extraHeight = sgMace.add(new DoubleSetting.Builder()
-    	.name("additional-height")
-    	.description("The amount of additional height to spoof. More height means more damage.")
-    	.defaultValue(0.0)
-        .min(0)
-        .sliderRange(0, 100)
-        .visible(mace::get)
-    	.build()
-    );
-
     private PlayerInteractEntityC2SPacket attackPacket;
     private HandSwingC2SPacket swingPacket;
     private boolean sendPackets;
@@ -84,45 +64,37 @@ public class Criticals extends Module {
     @EventHandler
     private void onSendPacket(PacketEvent.Send event) {
         if (event.packet instanceof IPlayerInteractEntityC2SPacket packet && packet.meteor$getType() == PlayerInteractEntityC2SPacket.InteractType.ATTACK) {
-            if (mace.get() && mc.player.getMainHandStack().getItem() instanceof MaceItem) {
-                if (mc.player.isGliding()) return;
+            if (skipCrit()) return;
 
-                sendPacket(0);
-                sendPacket(1.501 + extraHeight.get());
-                sendPacket(0);
-            } else {
-                if (skipCrit()) return;
+            Entity entity = packet.meteor$getEntity();
 
-                Entity entity = packet.meteor$getEntity();
+            if (!(entity instanceof LivingEntity) || (entity != Modules.get().get(KillAura.class).getTarget() && ka.get()))
+                return;
 
-                if (!(entity instanceof LivingEntity) || (entity != Modules.get().get(KillAura.class).getTarget() && ka.get()))
-                    return;
+            switch (mode.get()) {
+                case Packet -> {
+                    sendPacket(0.0625);
+                    sendPacket(0);
+                }
+                case Bypass -> {
+                    sendPacket(0.11);
+                    sendPacket(0.1100013579);
+                    sendPacket(0.0000013579);
+                }
+                case Jump, MiniJump -> {
+                    if (!sendPackets) {
+                        sendPackets = true;
+                        attackPacket = (PlayerInteractEntityC2SPacket) event.packet;
 
-                switch (mode.get()) {
-                    case Packet -> {
-                        sendPacket(0.0625);
-                        sendPacket(0);
-                    }
-                    case Bypass -> {
-                        sendPacket(0.11);
-                        sendPacket(0.1100013579);
-                        sendPacket(0.0000013579);
-                    }
-                    case Jump, MiniJump -> {
-                        if (!sendPackets) {
-                            sendPackets = true;
-                            attackPacket = (PlayerInteractEntityC2SPacket) event.packet;
-
-                            if (mode.get() == Mode.Jump) {
-                                mc.player.jump();
-                                waitingForPeak = true;
-                                lastY = mc.player.getY();
-                            } else {
-                                ((IVec3d) mc.player.getVelocity()).meteor$setY(0.25);
-                                sendTimer = 4;
-                            }
-                            event.cancel();
+                        if (mode.get() == Mode.Jump) {
+                            mc.player.jump();
+                            waitingForPeak = true;
+                            lastY = mc.player.getY();
+                        } else {
+                            ((IVec3d) mc.player.getVelocity()).meteor$setY(0.25);
+                            sendTimer = 4;
                         }
+                        event.cancel();
                     }
                 }
             }
