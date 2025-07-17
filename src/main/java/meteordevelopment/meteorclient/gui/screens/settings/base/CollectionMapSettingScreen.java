646b2a32c9a8db3d562b02bdb69c463809a5f9b2
RacoonDog <32882447+racoondog@users.noreply.github.com>
2025-07-17 19:03:40 -0400

    add `BlockDataSettingScreen` abstraction

---
+++ src/main/java/meteordevelopment/meteorclient/gui/screens/settings/base/CollectionMapSettingScreen.java
@@ -0,0 +1,99 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package meteordevelopment.meteorclient.gui.screens.settings.base;
+
+import meteordevelopment.meteorclient.gui.GuiTheme;
+import meteordevelopment.meteorclient.gui.WindowScreen;
+import meteordevelopment.meteorclient.gui.renderer.GuiRenderer;
+import meteordevelopment.meteorclient.gui.widgets.WWidget;
+import meteordevelopment.meteorclient.gui.widgets.containers.WTable;
+import meteordevelopment.meteorclient.gui.widgets.input.WTextBox;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.utils.Utils;
+import meteordevelopment.meteorclient.utils.misc.IChangeable;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+
+public abstract class CollectionMapSettingScreen<K, V> extends WindowScreen {
+    private final Setting<?> setting;
+    protected final Map<K, V> map;
+    private final Iterable<K> registry;
+
+    private WTable table;
+    private String filterText = "";
+
+    public CollectionMapSettingScreen(GuiTheme theme, String title, Setting<?> setting, Map<K, V> map, Iterable<K> registry) {
+        super(theme, title);
+
+        this.setting = setting;
+        this.map = map;
+        this.registry = registry;
+    }
+
+    @Override
+    public void initWidgets() {
+        // Filter
+        WTextBox filter = add(theme.textBox("")).minWidth(400).expandX().widget();
+        filter.setFocused(true);
+        filter.action = () -> {
+            filterText = filter.get().trim();
+
+            table.clear();
+            initTable();
+        };
+
+        table = add(theme.table()).expandX().widget();
+
+        initTable();
+    }
+
+    private void initTable() {
+        registry.forEach(t -> { // todo sorting
+            if (skipValue(t) || !includeValue(t)) return; // todo redundancy
+
+            String name = getValueName(t);
+            int words = Utils.searchInWords(name, filterText);
+            int diff = Utils.searchLevenshteinDefault(name, filterText, false);
+            if (words <= 0 && diff > name.length() / 2) return;
+
+            @Nullable V data = map.get(t);
+            boolean isChanged = data instanceof IChangeable changeable && changeable.isChanged();
+
+            table.add(getValueWidget(t)).expandCellX();
+            table.add(theme.label(isChanged ? "*" : " "));
+            table.add(getDataWidget(t, data));
+
+            table.add(theme.button(GuiRenderer.RESET)).widget().action = () -> {
+                removeValue(t);
+            };
+
+            table.row();
+        });
+    }
+
+    protected void removeValue(K value) {
+        if (map.remove(value) != null) {
+            setting.onChanged();
+            table.clear();
+            initWidgets();
+        }
+    }
+
+    protected boolean includeValue(K value) {
+        return true;
+    }
+
+    protected abstract WWidget getValueWidget(K value);
+
+    protected abstract WWidget getDataWidget(K value, @Nullable V data);
+
+    protected abstract String getValueName(K value);
+
+    protected boolean skipValue(K value) {
+        return false;
+    }
+}
