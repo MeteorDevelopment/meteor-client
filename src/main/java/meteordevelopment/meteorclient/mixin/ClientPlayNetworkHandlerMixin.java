55c41e5d5b4192ed9a5bd82c43992af72061d32e
Crosby <32882447+crosby-moe@users.noreply.github.com>
2025-11-03 18:03:50 -0500

    fix command handling race condition (#5820)

--- src/main/java/meteordevelopment/meteorclient/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/ClientPlayNetworkHandlerMixin.java
@@ -5,6 +5,8 @@
 
 package meteordevelopment.meteorclient.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.commands.Commands;
@@ -46,12 +48,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class ClientPlayNetworkHandlerMixin extends ClientCommonNetworkHandler {
     @Shadow
     private ClientWorld world;
-
-    @Shadow
-    public abstract void sendChatMessage(String content);
-
-    @Unique
-    private boolean ignoreChatMessage;
 
     @Unique
     private boolean worldNotNull;
@@ -139,18 +135,16 @@ public abstract class ClientPlayNetworkHandlerMixin extends ClientCommonNetworkH
     }
 
     @Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
-    private void onSendChatMessage(String message, CallbackInfo ci) {
-        if (ignoreChatMessage) return;
-
+    private void onSendChatMessage(String message, CallbackInfo ci, @Local(argsOnly = true) LocalRef<String> messageRef) {
         if (!message.startsWith(Config.get().prefix.get()) && !(BaritoneUtils.IS_AVAILABLE && message.startsWith(BaritoneUtils.getPrefix()))) {
             SendMessageEvent event = MeteorClient.EVENT_BUS.post(SendMessageEvent.get(message));
 
             if (!event.isCancelled()) {
-                ignoreChatMessage = true;
-                sendChatMessage(event.message);
-                ignoreChatMessage = false;
+                messageRef.set(event.message);
+            } else {
+                ci.cancel();
             }
-            ci.cancel();
+
             return;
         }
 
