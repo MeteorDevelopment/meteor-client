d50805712f2103ef549d350fc056492bca3ecf9e
Wide-Cat <widecat@duck.com>
2025-11-10 17:55:10 +0000

    Add crossbow support to bow spam.
    If the modules finds loaded crossbows in your inventory, using them takes priority over using bows.
    
    closes #3370

--- src/main/java/meteordevelopment/meteorclient/systems/modules/combat/BowSpam.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/combat/BowSpam.java
@@ -12,13 +12,18 @@ import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.player.FindItemResult;
 import meteordevelopment.meteorclient.utils.player.InvUtils;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.item.ArrowItem;
+import net.minecraft.item.CrossbowItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.Hand;
 
 public class BowSpam extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
+    private final SettingGroup sgCrossbows = settings.createGroup("Crossbows");
 
     private final Setting<Integer> charge = sgGeneral.add(new IntSetting.Builder()
         .name("charge")
@@ -36,11 +41,35 @@ public class BowSpam extends Module {
         .build()
     );
 
+    private final Setting<Boolean> spamCrossbows = sgCrossbows.add(new BoolSetting.Builder()
+        .name("spam-crossbows")
+        .description("Whether to spam loaded crossbows; takes priority over charging bows.")
+        .defaultValue(true)
+        .build()
+    );
+
+    private final Setting<Integer> crossbowDelay = sgCrossbows.add(new IntSetting.Builder()
+        .name("crossbow-delay")
+        .description("Delay between shooting crossbows in ticks.")
+        .defaultValue(10)
+        .sliderRange(0, 20)
+        .min(0)
+        .build()
+    );
+
+    private final Setting<Boolean> searchInventory = sgCrossbows.add(new BoolSetting.Builder()
+        .name("search-inventory")
+        .description("Whether to search your inventory to find loaded crossbows.")
+        .defaultValue(true)
+        .build()
+    );
+
     private boolean wasBow = false;
     private boolean wasHoldingRightClick = false;
+    private int ticks = 0;
 
     public BowSpam() {
-        super(Categories.Combat, "bow-spam", "Spams arrows.");
+        super(Categories.Combat, "bow-spam", "Spams bows and crossbows.", "auto-bow", "crossbow-spam", "auto-crossbow");
     }
 
     @Override
@@ -56,6 +85,31 @@ public class BowSpam extends Module {
 
     @EventHandler
     private void onTick(TickEvent.Pre event) {
+        FindItemResult crossbow = searchInventory.get() ? InvUtils.find(this::crossbow) : InvUtils.find(this::crossbow, 0, 8);
+        if (spamCrossbows.get() && crossbow.found()) {
+            if (ticks >= crossbowDelay.get()) {
+                int slot = crossbow.slot();
+                if (!crossbow.isHotbar()) {
+                    FindItemResult valid = InvUtils.find(stack -> stack.isEmpty() || stack.isOf(Items.CROSSBOW) || stack.isOf(Items.ARROW), 0, 8);
+                    if (!valid.found()) return;
+
+                    InvUtils.quickSwap().fromId(valid.slot()).to(crossbow.slot());
+                    slot = valid.slot();
+                }
+
+                InvUtils.swap(slot, true);
+                mc.interactionManager.interactItem(mc.player, Hand.MAIN_HAND);
+                InvUtils.swapBack();
+
+                ticks = 0;
+            }
+            else {
+                ticks++;
+            }
+
+            return;
+        }
+
         if (!mc.player.getAbilities().creativeMode && !InvUtils.find(itemStack -> itemStack.getItem() instanceof ArrowItem).found())
             return;
 
@@ -83,5 +137,9 @@ public class BowSpam extends Module {
 
     private void setPressed(boolean pressed) {
         mc.options.useKey.setPressed(pressed);
+    }
+
+    private boolean crossbow(ItemStack stack) {
+        return stack.getItem() instanceof CrossbowItem && CrossbowItem.isCharged(stack);
     }
 }
