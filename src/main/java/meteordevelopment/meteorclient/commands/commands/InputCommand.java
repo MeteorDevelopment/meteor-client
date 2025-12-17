19db59b194058b4be6a173efbe8b78a0ac562358
Crosby <32882447+crosby-moe@users.noreply.github.com>
2025-12-17 04:05:34 -0500

    make `ticks` argument to `.input` optional

--- src/main/java/meteordevelopment/meteorclient/commands/commands/InputCommand.java
+++ src/main/java/meteordevelopment/meteorclient/commands/commands/InputCommand.java
@@ -48,6 +48,10 @@ public class InputCommand extends Command {
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         for (Pair<KeyBinding, String> keyBinding : holdKeys) {
             builder.then(literal(keyBinding.getSecond())
+                .executes(context -> {
+                    activeHandlers.add(new KeypressHandler(keyBinding.getFirst(), 1));
+                    return SINGLE_SUCCESS;
+                })
                 .then(argument("ticks", IntegerArgumentType.integer(1))
                     .executes(context -> {
                         activeHandlers.add(new KeypressHandler(keyBinding.getFirst(), context.getArgument("ticks", Integer.class)));
