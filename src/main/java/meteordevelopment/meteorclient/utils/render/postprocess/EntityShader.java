9dbbd27ac919b9d5ab543f4fc7edfc5c28947e45
Crosby <32882447+crosby-moe@users.noreply.github.com>
2025-12-17 04:16:24 -0500

    faster texture clearing for esp entity shader

--- src/main/java/meteordevelopment/meteorclient/utils/render/postprocess/EntityShader.java
+++ src/main/java/meteordevelopment/meteorclient/utils/render/postprocess/EntityShader.java
@@ -11,7 +11,7 @@ public abstract class EntityShader extends PostProcessShader {
     @Override
     public boolean beginRender() {
         if (super.beginRender()) {
-            RenderSystem.getDevice().createCommandEncoder().createRenderPass(() -> "Meteor EntityShader", framebuffer.getColorAttachmentView(), OptionalInt.of(0)).close();
+            RenderSystem.getDevice().createCommandEncoder().clearColorTexture(framebuffer.getColorAttachment(), 0);
             return true;
         }
 
