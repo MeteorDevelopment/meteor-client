05d61666e666fd27c97a4bc50fd98be0e75562e4
noramibuu <50046813+noramibu@users.noreply.github.com>
2025-12-18 12:25:06 +0300

    fix sodium compatibility for no render's fog option

--- src/main/java/meteordevelopment/meteorclient/mixin/sodium/SodiumWorldRendererMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/sodium/SodiumWorldRendererMixin.java
@@ -10,16 +10,20 @@ import meteordevelopment.meteorclient.systems.modules.render.NoRender;
 import net.caffeinemc.mods.sodium.client.render.SodiumWorldRenderer;
 import net.caffeinemc.mods.sodium.client.util.FogParameters;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(SodiumWorldRenderer.class)
 public class SodiumWorldRendererMixin {
+    @Unique
+    private static final FogParameters DISABLED_FOG = new FogParameters(0, 0, 0, 0, Float.MAX_VALUE, Float.MAX_VALUE, Float.MAX_VALUE, Float.MAX_VALUE);
+
     @ModifyVariable(method = "setupTerrain", at = @At("HEAD"), argsOnly = true)
     private FogParameters modifyFogParameters(FogParameters fogParameters) {
         if (Modules.get() == null) return fogParameters;
 
-        if (Modules.get().get(NoRender.class).noFog()) return FogParameters.NONE;
+        if (Modules.get().get(NoRender.class).noFog()) return DISABLED_FOG;
 
         return fogParameters;
     }
