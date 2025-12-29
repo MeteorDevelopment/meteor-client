2f651bfabf7401cd932a8de440af74811f3f8c35
BerryRock0 <mdenials128@gmail.com>
2025-12-29 15:42:51 +0500

    Update Hitboxes.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/combat/Hitboxes.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/combat/Hitboxes.java
@@ -14,9 +14,10 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.MaceItem;
-import net.minecraft.item.TridentItem;
 import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.util.math.Box;
 
+import org.joml.Vector3d;
 import java.util.Set;
 
 public class Hitboxes extends Module {
@@ -30,88 +31,26 @@ public class Hitboxes extends Module {
         .build()
     );
 
-    private final Setting<Double> value = sgGeneral.add(new DoubleSetting.Builder()
+   public final Setting<Mode> mode = sgGeneral.add(new EnumSetting.Builder<Mode>()
+        .name("mode")
+        .description("Select hitbox expland mode.")
+        .defaultValue(Mode.Margin)
+        .build()
+    );
+    
+    public final Setting<Double> value = sgGeneral.add(new DoubleSetting.Builder()
         .name("expand")
         .description("How much to expand the hitbox of the entity.")
         .defaultValue(0.5)
+        .visible(() -> mode.get() == Mode.Margin)
         .build()
     );
-
-    private final Setting<Boolean> ignoreFriends = sgGeneral.add(new BoolSetting.Builder()
-        .name("ignore-friends")
-        .description("Doesn't expand the hitboxes of friends.")
-        .defaultValue(true)
-        .build()
-    );
-
-    private final Setting<Boolean> onlyOnWeapon = sgWeapon.add(new BoolSetting.Builder()
-        .name("only-on-weapon")
-        .description("Only modifies hitbox when holding a weapon in hand.")
-        .defaultValue(false)
-        .build()
-    );
-
-    private final Setting<Boolean> sword = sgWeapon.add(new BoolSetting.Builder()
-        .name("sword")
-        .description("Enable when holding a sword.")
-        .defaultValue(true)
-        .visible(onlyOnWeapon::get)
-        .build()
-    );
-
-    private final Setting<Boolean> axe = sgWeapon.add(new BoolSetting.Builder()
-        .name("axe")
-        .description("Enable when holding an axe.")
-        .defaultValue(true)
-        .visible(onlyOnWeapon::get)
-        .build()
-    );
-
-    private final Setting<Boolean> pickaxe = sgWeapon.add(new BoolSetting.Builder()
-        .name("pickaxe")
-        .description("Enable when holding a pickaxe.")
-        .defaultValue(true)
-        .visible(onlyOnWeapon::get)
-        .build()
-    );
-
-    private final Setting<Boolean> shovel = sgWeapon.add(new BoolSetting.Builder()
-        .name("shovel")
-        .description("Enable when holding a shovel.")
-        .defaultValue(true)
-        .visible(onlyOnWeapon::get)
-        .build()
-    );
-
-    private final Setting<Boolean> hoe = sgWeapon.add(new BoolSetting.Builder()
-        .name("hoe")
-        .description("Enable when holding a hoe.")
-        .defaultValue(true)
-        .visible(onlyOnWeapon::get)
-        .build()
-    );
-
-    private final Setting<Boolean> mace = sgWeapon.add(new BoolSetting.Builder()
-        .name("mace")
-        .description("Enable when holding a mace.")
-        .defaultValue(true)
-        .visible(onlyOnWeapon::get)
-        .build()
-    );
-
-    private final Setting<Boolean> spear = sgWeapon.add(new BoolSetting.Builder()
-        .name("spear")
-        .description("Enable when holding a spear.")
-        .defaultValue(true)
-        .visible(onlyOnWeapon::get)
-        .build()
-    );
-
-    private final Setting<Boolean> trident = sgWeapon.add(new BoolSetting.Builder()
-        .name("trident")
-        .description("Enable when holding a trident.")
-        .defaultValue(true)
-        .visible(onlyOnWeapon::get)
+    
+    public final Setting<Vector3d> box = sgGeneral.add(new Vector3dSetting.Builder()
+        .name("box")
+        .description("Entites box.")
+        .defaultValue(0, 0, 0)
+        .visible(() -> mode.get() == Mode.Box)
         .build()
     );
 
@@ -119,24 +58,25 @@ public class Hitboxes extends Module {
         super(Categories.Combat, "hitboxes", "Expands an entity's hitboxes.");
     }
 
-    public double getEntityValue(Entity entity) {
-        if (!(isActive() && testWeapon()) || (ignoreFriends.get() && entity instanceof PlayerEntity playerEntity && Friends.get().isFriend(playerEntity))) return 0;
-        if (entities.get().contains(entity.getType())) return value.get();
+    public double getEntityValue(Entity entity)
+    {
+        if (isActive() && entities.get().contains(entity.getType()))
+			return value.get();
+
         return 0;
     }
-
-    private boolean testWeapon() {
-        if (!onlyOnWeapon.get()) return true;
-        return InvUtils.testInMainHand(itemStack -> {
-            if (sword.get() && itemStack.isIn(ItemTags.SWORDS)) return true;
-            if (axe.get() && itemStack.isIn(ItemTags.AXES)) return true;
-            if (pickaxe.get() && itemStack.isIn(ItemTags.PICKAXES)) return true;
-            if (shovel.get() && itemStack.isIn(ItemTags.SHOVELS)) return true;
-            if (hoe.get() && itemStack.isIn(ItemTags.HOES)) return true;
-            if (mace.get() && itemStack.getItem() instanceof MaceItem) return true;
-            if (spear.get() && itemStack.isIn(ItemTags.SPEARS)) return true;
-            if (trident.get() && itemStack.getItem() instanceof TridentItem) return true;
-            return false;
-        });
+    
+    public Box getEntityBox(Entity entity)
+    {
+		if (isActive())
+			return new Box(entity.boundingBox.minX - box.get().x, entity.boundingBox.minY - box.get().y, entity.boundingBox.minZ - box.get().z, entity.boundingBox.maxX + box.get().x, entity.boundingBox.maxY + box.get().y, entity.boundingBox.maxZ + box.get().z);
+			
+		return entity.boundingBox;		
+	}
+    
+    public enum Mode
+    {
+        Margin,
+        Box
     }
 }
