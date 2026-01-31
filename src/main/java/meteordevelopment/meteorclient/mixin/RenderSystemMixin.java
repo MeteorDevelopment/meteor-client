02973974afdfb715b193813959a837695049f7c1
Wide_Cat <widecat@duck.com>
2026-01-31 19:05:53 +0000

    Add a setting to InventoryTweaks to make input handling happen every frame instead of every tick.
    Closes #6065

--- src/main/java/meteordevelopment/meteorclient/mixin/RenderSystemMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/RenderSystemMixin.java
@@ -7,6 +7,8 @@ package meteordevelopment.meteorclient.mixin;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import meteordevelopment.meteorclient.renderer.MeshUniforms;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.systems.modules.misc.InventoryTweaks;
 import meteordevelopment.meteorclient.utils.render.postprocess.ChamsShader;
 import meteordevelopment.meteorclient.utils.render.postprocess.OutlineUniforms;
 import meteordevelopment.meteorclient.utils.render.postprocess.PostProcessShader;
@@ -14,6 +16,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static meteordevelopment.meteorclient.MeteorClient.mc;
 
 @Mixin(RenderSystem.class)
 public abstract class RenderSystemMixin {
@@ -23,5 +27,8 @@ public abstract class RenderSystemMixin {
         PostProcessShader.flipFrame();
         ChamsShader.flipFrame();
         OutlineUniforms.flipFrame();
+
+        if (Modules.get() == null || mc.player == null) return;
+        if (Modules.get().get(InventoryTweaks.class).frameInput()) ((MinecraftClientAccessor) mc).meteor$handleInputEvents();
     }
 }
