5d0bdb2e07df41fd09863147c355c2e4245c79f2
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2026-02-01 20:01:23 +0800

    Refactor FontUtils Font loading

--- src/main/java/meteordevelopment/meteorclient/renderer/text/BuiltinFontFace.java
+++ src/main/java/meteordevelopment/meteorclient/renderer/text/BuiltinFontFace.java
@@ -1,10 +1,14 @@
 package meteordevelopment.meteorclient.renderer.text;
 
 import meteordevelopment.meteorclient.utils.render.FontUtils;
+import org.jspecify.annotations.NullMarked;
 
 import java.io.InputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
-public class BuiltinFontFace extends FontFace {
+@NullMarked
+public non-sealed class BuiltinFontFace extends FontFace {
     private final String name;
 
     public BuiltinFontFace(FontInfo info, String name) {
@@ -14,10 +18,12 @@ public class BuiltinFontFace extends FontFace {
     }
 
     @Override
-    public InputStream toStream() {
-        InputStream in = FontUtils.stream(name);
-        if (in == null) throw new RuntimeException("Failed to load builtin font " + name + ".");
-        return in;
+    public ReadableByteChannel byteChannelForRead() {
+        InputStream inputStream = FontUtils.builtinFontStream(this.name);
+        if (inputStream == null) {
+            throw new IllegalArgumentException("Builtin font '" + this.name + "' not found");
+        }
+        return Channels.newChannel(inputStream);
     }
 
     @Override
