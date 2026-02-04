791820dc3dadc9579c85a34269f336403e2c8608
XC0 <102973604+X-C-0@users.noreply.github.com>
2026-02-04 23:13:12 +0100

    Fix scrolling for view-in-view scenario

--- src/main/java/meteordevelopment/meteorclient/gui/widgets/containers/WView.java
+++ src/main/java/meteordevelopment/meteorclient/gui/widgets/containers/WView.java
@@ -26,7 +26,6 @@ public abstract class WView extends WVerticalList {
     private boolean moveAfterPositionWidgets;
 
     protected boolean handleMouseOver;
-    protected boolean handlePressed;
 
     @Override
     public void init() {
@@ -77,7 +76,7 @@ public abstract class WView extends WVerticalList {
     @Override
     public boolean onMouseClicked(Click click, boolean doubled) {
         if (handleMouseOver && click.button() == GLFW_MOUSE_BUTTON_LEFT && !doubled) {
-            handlePressed = true;
+            setFocused(true);
             return true;
         }
 
@@ -86,7 +85,7 @@ public abstract class WView extends WVerticalList {
 
     @Override
     public boolean onMouseReleased(Click click) {
-        if (handlePressed) handlePressed = false;
+        if (focused) setFocused(false);
 
         return false;
     }
@@ -104,7 +103,7 @@ public abstract class WView extends WVerticalList {
             }
         }
 
-        if (handlePressed) {
+        if (focused) {
             double preScroll = scroll;
             double mouseDelta = mouseY - lastMouseY;
 
@@ -123,9 +122,13 @@ public abstract class WView extends WVerticalList {
     @Override
     public boolean onMouseScrolled(double amount) {
         if (!scrollOnlyWhenMouseOver || mouseOver) {
+            double max = actualHeight - height;
+
             targetScroll -= Math.round(theme.scale(amount * 40));
-            targetScroll = MathHelper.clamp(targetScroll, 0, actualHeight - height);
-            return true;
+            targetScroll = MathHelper.clamp(targetScroll, 0, max);
+
+            // Only consume the event if the view actually scrolled, otherwise propagate to parent.
+            return targetScroll > 0 && targetScroll < max;
         }
 
         return false;
@@ -164,7 +167,13 @@ public abstract class WView extends WVerticalList {
 
     @Override
     protected boolean propagateEvents(WWidget widget) {
-        return (mouseOver && isWidgetInView(widget)) || widget.isFocused();
+        if (widget.isFocused()) return true;
+
+        // Propagate to any visible view, to allow inputs even when not hovered
+        if (widget instanceof WView) return isWidgetInView(widget);
+
+        // Propagate to any visible widget while the view is hovered
+        return mouseOver && isWidgetInView(widget);
     }
 
     protected double handleWidth() {
