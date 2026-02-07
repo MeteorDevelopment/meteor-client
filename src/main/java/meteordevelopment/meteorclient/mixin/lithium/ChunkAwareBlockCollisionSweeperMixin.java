39217db00062e6997fc394d3e59a0cdf13cb99eb
Wide_Cat <widecat@duck.com>
2026-02-07 17:19:13 +0000

    Fix a crash when joining worlds with the newest version of lithium
    Meteor is no longer compatible with any version of lithium below 0.21.3
    
    closes #6118
    closes #6122

--- src/main/java/meteordevelopment/meteorclient/mixin/lithium/ChunkAwareBlockCollisionSweeperMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/lithium/ChunkAwareBlockCollisionSweeperMixin.java
@@ -10,29 +10,26 @@ import com.llamalad7.mixinextras.sugar.Local;
 import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.events.world.CollisionShapeEvent;
 import net.caffeinemc.mods.lithium.common.entity.movement.ChunkAwareBlockCollisionSweeper;
+import net.caffeinemc.mods.lithium.common.entity.movement.ChunkAwareBlockCollisionSweeperVoxelShape;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Final;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(value = ChunkAwareBlockCollisionSweeper.class)
-public abstract class ChunkAwareBlockCollisionSweeperMixin {
-    @Shadow
-    @Final
-    private World world;
-
-    @Final
-    @Shadow
-    private BlockPos.Mutable pos;
+@Mixin(value = ChunkAwareBlockCollisionSweeperVoxelShape.class)
+public abstract class ChunkAwareBlockCollisionSweeperMixin extends ChunkAwareBlockCollisionSweeper<VoxelShape> {
+    public ChunkAwareBlockCollisionSweeperMixin(World world, @Nullable Entity entity, Box box, boolean hideLastCollision) {
+        super(world, entity, box, hideLastCollision);
+    }
 
     @ModifyExpressionValue(method = "computeNext()Lnet/minecraft/util/shape/VoxelShape;", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/ShapeContext;getCollisionShape(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/CollisionView;Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/util/shape/VoxelShape;"))
-    private VoxelShape modifyCollisionShape(VoxelShape original, @Local BlockState state) {
+    private VoxelShape modifyCollisionShape(VoxelShape original, @Local(name = "state") BlockState state) {
         if (world != MinecraftClient.getInstance().world) return original;
 
         CollisionShapeEvent event = MeteorClient.EVENT_BUS.post(CollisionShapeEvent.get(state, pos, original));
