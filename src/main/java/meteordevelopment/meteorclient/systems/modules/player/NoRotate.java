be022f20a145d106fa5eaf02a02d274421e466f1
Marc3D <154929866+Marccccccccccccccc@users.noreply.github.com>
2026-02-26 22:51:33 +0100

    Fix No Rotate (#6155)

--- src/main/java/meteordevelopment/meteorclient/systems/modules/player/NoRotate.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/player/NoRotate.java
@@ -5,33 +5,18 @@
 
 package meteordevelopment.meteorclient.systems.modules.player;
 
-import meteordevelopment.meteorclient.events.packets.PacketEvent;
+import com.llamalad7.mixinextras.sugar.ref.LocalFloatRef;
+import meteordevelopment.meteorclient.mixin.ClientPlayNetworkHandlerMixin;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.orbit.EventHandler;
-import net.minecraft.entity.EntityPosition;
 import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+/**
+ * @see ClientPlayNetworkHandlerMixin#onPlayerPositionLookHead(PlayerPositionLookS2CPacket, CallbackInfo, LocalFloatRef, LocalFloatRef)
+ */
 public class NoRotate extends Module {
     public NoRotate() {
         super(Categories.Player, "no-rotate", "Attempts to block rotations sent from server to client.");
-    }
-
-    @EventHandler
-    private void onReceivePacket(PacketEvent.Receive event) {
-        if (event.packet instanceof PlayerPositionLookS2CPacket packet) {
-            EntityPosition oldPosition = packet.change();
-            EntityPosition newPosition = new EntityPosition(
-                oldPosition.position(),
-                oldPosition.deltaMovement(),
-                mc.player.getYaw(),
-                mc.player.getPitch()
-            );
-            event.packet = PlayerPositionLookS2CPacket.of(
-                packet.teleportId(),
-                newPosition,
-                packet.relatives()
-            );
-        }
     }
 }
