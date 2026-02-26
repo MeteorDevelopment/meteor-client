e9d3daf5527b142fcffa197bbe14851c905ca60d
Wide_Cat <widecat@duck.com>
2026-02-26 21:51:02 +0000

    update comment

--- src/main/java/meteordevelopment/meteorclient/systems/modules/player/NoRotate.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/player/NoRotate.java
@@ -5,12 +5,18 @@
 
 package meteordevelopment.meteorclient.systems.modules.player;
 
+import com.llamalad7.mixinextras.sugar.ref.LocalFloatRef;
+import meteordevelopment.meteorclient.mixin.ClientPlayNetworkHandlerMixin;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+/**
+ * @see ClientPlayNetworkHandlerMixin#onPlayerPositionLookHead(PlayerPositionLookS2CPacket, CallbackInfo, LocalFloatRef, LocalFloatRef)
+ */
 public class NoRotate extends Module {
     public NoRotate() {
         super(Categories.Player, "no-rotate", "Attempts to block rotations sent from server to client.");
     }
-    // Logic is in ClientPlayNetworkHandlerMixin.onPlayerPositionLook
 }
