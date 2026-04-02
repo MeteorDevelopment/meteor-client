cca1a52af8be476b11b3b1867dc60d5c40e36dfa
MineGame159 <petulko08@gmail.com>
2026-04-02 16:39:16 +0200

    Fix remaining TODO(Ravel) comments

--- src/main/java/meteordevelopment/meteorclient/mixin/WebBlockMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/WebBlockMixin.java
@@ -5,20 +5,25 @@
 
 package meteordevelopment.meteorclient.mixin;
 
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.systems.modules.movement.NoSlow;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.InsideBlockEffectApplier;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.WebBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static meteordevelopment.meteorclient.MeteorClient.mc;
 
 @Mixin(WebBlock.class)
 public abstract class WebBlockMixin {
-    // TODO(Ravel): target method method_9548 with the signature not found
-// TODO(Ravel): target method onEntityCollision with the signature not found
-    /*@Dynamic("Explicit 1.21.9 Support")
-    @Inject(method = {
-        "onEntityCollision", // 1.21.10
-        "onEntityCollision(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/EntityCollisionHandler;)V", // 1.21.9 yarn
-        "method_9548(Lnet/minecraft/class_2680;Lnet/minecraft/class_1937;Lnet/minecraft/class_2338;Lnet/minecraft/class_1297;Lnet/minecraft/class_10774;)V" // 1.21.9 intermediary
-    }, at = @At("HEAD"), cancellable = true)
-    private void onEntityCollision(CallbackInfo ci, @Local(argsOnly = true) Entity entity) {
-        if (entity == mc.player && Modules.get().get(NoSlow.class).cobweb()) ci.cancel();
-    }*/
+    @Inject(method = "entityInside", at = @At("HEAD"), cancellable = true)
+    private void onEntityCollision(BlockState blockState, Level level, BlockPos blockPos, Entity entity, InsideBlockEffectApplier insideBlockEffectApplier, boolean bl, CallbackInfo info) {
+        if (entity == mc.player && Modules.get().get(NoSlow.class).cobweb()) info.cancel();
+    }
 }
