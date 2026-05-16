12576f8a35db8f8c123cc3e32a16632f0b531e2a
cqb13 <cqb13.main@gmail.com>
2026-05-15 23:02:39 -0400

    added back header in item list setting screen oops

--- src/main/java/meteordevelopment/meteorclient/gui/screens/settings/ItemListSettingScreen.java
+++ src/main/java/meteordevelopment/meteorclient/gui/screens/settings/ItemListSettingScreen.java
@@ -1,3 +1,8 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
 package meteordevelopment.meteorclient.gui.screens.settings;
 
 import meteordevelopment.meteorclient.gui.GuiTheme;
