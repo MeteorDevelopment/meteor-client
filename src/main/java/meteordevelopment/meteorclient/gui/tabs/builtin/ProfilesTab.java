ab6dea15f079fdc93f3da226431f831ebd79bad3
木葉 Scarlet <93977077+MukjepScarlet@users.noreply.github.com>
2026-05-25 11:26:00 +0800

    Remove unused import

--- src/main/java/meteordevelopment/meteorclient/gui/tabs/builtin/ProfilesTab.java
+++ src/main/java/meteordevelopment/meteorclient/gui/tabs/builtin/ProfilesTab.java
@@ -38,7 +38,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
