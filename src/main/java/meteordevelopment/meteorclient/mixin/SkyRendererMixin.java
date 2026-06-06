363d241d186d27d16946bb492638d6f788cb422a
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2026-06-06 22:05:01 +0800

    Fix SkyRendererMixin

--- src/main/java/meteordevelopment/meteorclient/mixin/SkyRendererMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/SkyRendererMixin.java
@@ -5,18 +5,21 @@
 
 package meteordevelopment.meteorclient.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.mojang.blaze3d.buffers.GpuBufferSlice;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.systems.modules.world.Ambience;
 import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.DynamicUniforms;
 import net.minecraft.client.renderer.SkyRenderer;
 import net.minecraft.client.renderer.state.level.SkyRenderState;
 import net.minecraft.world.level.dimension.DimensionType;
-import org.joml.Vector4fc;
+import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SkyRenderer.class)
@@ -30,12 +33,15 @@ public abstract class SkyRendererMixin {
         if (ambience.customSkyColor.get()) state.skyColor = ambience.skyColor().getPacked();
     }
 
-    @ModifyArg(method = "renderEndSky", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/DynamicUniforms;writeTransform(Lorg/joml/Matrix4fc;Lorg/joml/Vector4fc;Lorg/joml/Vector3fc;Lorg/joml/Matrix4fc;)Lcom/mojang/blaze3d/buffers/GpuBufferSlice;"))
-    private Vector4fc modifyEndSkyColor(Vector4fc original) {
+    @WrapOperation(method = "renderEndSky", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/DynamicUniforms;writeTransform(Lorg/joml/Matrix4f;)Lcom/mojang/blaze3d/buffers/GpuBufferSlice;"))
+    private GpuBufferSlice modifyEndSkyColor(DynamicUniforms instance, Matrix4f modelView, Operation<GpuBufferSlice> original) {
         Ambience ambience = Modules.get().get(Ambience.class);
 
-        if (ambience.isActive() && ambience.endSky.get() && ambience.customSkyColor.get())
-            return ambience.skyColor().getVec4f();
-        else return original;
+        if (ambience.isActive() && ambience.endSky.get() && ambience.customSkyColor.get()) {
+            var colorModulator = ambience.skyColor().getVec4f();
+            return instance.writeTransform(modelView, colorModulator);
+        } else {
+            return original.call(instance, modelView);
+        }
     }
 }
