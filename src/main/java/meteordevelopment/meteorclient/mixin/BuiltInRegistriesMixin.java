2ca030922580569837133721f38ddc7a6ea3183c
Big-Iron-Cheems <52252627+Big-Iron-Cheems@users.noreply.github.com>
2026-06-11 19:06:21 +0200

    refactor(mixin): migrate Redirect usage to MixinExtras (WrapOperation/WrapWithCondition)

--- src/main/java/meteordevelopment/meteorclient/mixin/BuiltInRegistriesMixin.java
+++ src/main/java/meteordevelopment/meteorclient/mixin/BuiltInRegistriesMixin.java
@@ -5,17 +5,17 @@
 
 package meteordevelopment.meteorclient.mixin;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import net.minecraft.core.registries.BuiltInRegistries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.function.Supplier;
 
 @Mixin(BuiltInRegistries.class)
 public abstract class BuiltInRegistriesMixin {
-    @Redirect(method = "internalRegister(Lnet/minecraft/resources/ResourceKey;Lnet/minecraft/core/WritableRegistry;Lnet/minecraft/core/registries/BuiltInRegistries$RegistryBootstrap;)Lnet/minecraft/core/WritableRegistry;", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/Bootstrap;checkBootstrapCalled(Ljava/util/function/Supplier;)V"))
-    private static void ignoreBootstrap(Supplier<String> location) {
-        // nothing
+    @WrapWithCondition(method = "internalRegister", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/Bootstrap;checkBootstrapCalled(Ljava/util/function/Supplier;)V"))
+    private static boolean skipBootstrapCheck(Supplier<String> location) {
+        return false;
     }
 }
