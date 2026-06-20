9915d5965c28c4f336528c4178a3ec6e42fef2a5
gta66666668 <130151078+gta66666668@users.noreply.github.com>
2026-06-20 20:09:55 +0800

    Add Tuple compatibility shim for Baritone

---
+++ src/main/java/net/minecraft/util/Tuple.java
@@ -0,0 +1,24 @@
+/*
+ * This file is part of the Meteor Client distribution (https://github.com/MeteorDevelopment/meteor-client).
+ * Copyright (c) Meteor Development.
+ */
+
+package net.minecraft.util;
+
+public class Tuple<A, B> {
+    private final A a;
+    private final B b;
+
+    public Tuple(A a, B b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public A getA() {
+        return a;
+    }
+
+    public B getB() {
+        return b;
+    }
+}
