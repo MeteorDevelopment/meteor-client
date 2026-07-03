7f4c0f080b7bd1dc0d691aad7662b2296fdd9334
Crosby <32882447+crosby-moe@users.noreply.github.com>
2026-07-03 02:37:44 -0400

    disallow periods in profile names

--- src/main/java/meteordevelopment/meteorclient/systems/profiles/Profile.java
+++ src/main/java/meteordevelopment/meteorclient/systems/profiles/Profile.java
@@ -31,7 +31,7 @@ public class Profile implements ISerializable<Profile> {
     public Setting<String> name = sgGeneral.add(new StringSetting.Builder()
         .name("name")
         .description("The name of the profile.")
-        .filter(Utils::nameFilter)
+        .filter(Utils::fileNameFilter)
         .build()
     );
 
